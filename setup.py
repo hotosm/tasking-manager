e8d2bcd96e4bfc778b2c0a709ad82ac213dc195f
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2013-02-02 10:10:41 +0100

    Changing version number as well as path to README file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, 'README.md')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
@@ -17,7 +17,7 @@ requires = [
     ]
 
 setup(name='osmtm',
-      version='0.0',
+      version='2.0',
       description='osmtm',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
