dfd6cb1ab396fc4e95708ed08141b53251ca39c1
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-03-17 12:36:07 +0100

    Travis configuration

--- osmtm/scripts/create_db.sh
+++ osmtm/scripts/create_db.sh
@@ -1,6 +1,6 @@
 #5. Create your app database
 dropdb osmtm
-createdb -T postgis_template -O www-data osmtm;
+createdb -T template_postgis -O www-data osmtm;
 
 dropdb osmtm_tests
-createdb -T postgis_template -O www-data osmtm_tests;
+createdb -T template_postgis -O www-data osmtm_tests;
