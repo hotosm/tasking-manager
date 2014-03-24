897ddfc965b75d04f1d7643fd87511804ee9c872
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-03-24 15:42:21 +0100

    Factorize setup, teardown and test creation

--- osmtm/tests/test_task.py
+++ osmtm/tests/test_task.py
@@ -1,35 +1,11 @@
-import unittest
-from . import db_url
+from . import BaseTestCase
 
-class TestTaskFunctional(unittest.TestCase):
-
-    def __create_project(self):
-        import geoalchemy2
-        import shapely
-        from osmtm.models import Area, Project
-        shape = shapely.geometry.Polygon(
-            [(7.23, 41.25), (7.23, 41.12), (7.41, 41.20)])
-        geometry = geoalchemy2.shape.from_shape(shape, 4326)
-        area = Area(geometry)
-        project = Project(u'test project')
-        project.area = area
-        project.auto_fill(12)
-        return project
-
-    def setUp(self):
-        from osmtm import main
-        from webtest import TestApp
-        settings = {
-            'available_languages': 'en',
-            'sqlalchemy.url': db_url,
-        }
-        app = main({}, **settings)
-        self.testapp = TestApp(app)
+class TestTaskFunctional(BaseTestCase):
 
     def test_task(self):
         import transaction
         from osmtm.models import DBSession
-        project = self.__create_project()
+        project = self.create_project()
         DBSession.add(project)
         DBSession.flush()
         project_id = project.id
