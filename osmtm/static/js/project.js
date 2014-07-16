9bd99140be58613372966177412c447575f4a4f8
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2014-07-16 17:37:17 +0200

    Export task as .osm. Automatically loaded in JOSM.

--- osmtm/static/js/project.js
+++ osmtm/static/js/project.js
@@ -344,6 +344,13 @@ osmtm.project = (function() {
         complete: function(t) {
           if (t.status != 200) {
             alert("JOSM remote control did not respond. Do you have JOSM running and configured to be controlled remotely?");
+          } else {
+            $.ajax({
+              url: 'http://127.0.0.1:8111/import',
+              data: {
+                url: task_osm_url
+              }
+            });
           }
         }
       });
---
+++ osmtm/templates/task.osm.mako
@@ -0,0 +1,17 @@
+<osm version="0.6" generator="HOT Tasking Manager" upload="false">
+<%
+    id = -2
+%>
+% for polygon in multipolygon:
+% for point in polygon.exterior.coords:
+<node id="${id}" lon="${point[0]}" lat="${point[1]}"><tag k="josm/ignore" v="true" /></node>
+<% id = id -1 %>
+% endfor
+<way id="-1">
+% for i in range(1, len(polygon.exterior.coords) + 1):
+<nd ref="-${i + 1}"/>
+% endfor
+<tag k="josm/ignore" v="true" />
+</way>
+% endfor
+</osm>
