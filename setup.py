75ceb4353c072df1e43a1dafbfcec54057ed2795
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-02-04 22:03:27 +0100

    Bumping version 2.12.0

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ requires = [
 ]
 
 setup(name='osmtm',
-      version='2.12-dev',
+      version='2.12.0',
       description='osmtm',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
