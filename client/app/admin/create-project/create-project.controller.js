fc632ff300c4b6425b10c997ea19a7a61d8afdc1
LindaAlblas <linda.alblas@thinkwhere.com>
2017-02-17 16:50:49 +0000

    create project skeleton

---
+++ client/app/admin/create-project/create-project.controller.js
@@ -0,0 +1,77 @@
+(function () {
+
+    'use strict';
+
+    /**
+     * Create project controller which manages creating a new project
+     */
+    angular
+        .module('taskingManager')
+        .controller('createProjectController', [createProjectController]);
+
+    function createProjectController() {
+        var vm = this;
+        vm.currentStep = '';
+
+        activate();
+
+        function activate() {
+            vm.currentStep = 'area';
+            
+            // TODO: move to map service
+            var map = new ol.Map({
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                ],
+                target: 'map',
+                view: new ol.View({
+                    center: [0, 0],
+                    zoom: 2
+                })
+            });
+        }
+
+        /**
+         * Set the current step
+         * @param step
+         */
+        vm.setStep = function(step){
+            vm.currentStep = step;
+        };
+
+        /**
+         * Decides if a step should be shown as completed in the progress bar
+         * @param step
+         * @returns {boolean}
+         */
+        vm.showStep = function(step){
+            var showStep = false;
+            if (step === 'area'){
+                if (vm.currentStep === 'area' || vm.currentStep === 'tasks' || vm.currentStep === 'templates' || vm.currentStep === 'review'){
+                    showStep = true;
+                }
+            }
+            else if (step === 'tasks'){
+                if ( vm.currentStep === 'tasks' || vm.currentStep === 'templates' || vm.currentStep === 'review'){
+                    showStep = true;
+                }
+            }
+            else if (step === 'templates'){
+                if (vm.currentStep === 'templates' || vm.currentStep === 'review'){
+                    showStep = true;
+                }
+            }
+            else if (step === 'review'){
+                if (vm.currentStep === 'review'){
+                    showStep = true;
+                }
+            }
+            else {
+                showStep = false;
+            }
+            return showStep;
+        }
+    }
+})();
--- client/app/taskingmanager.app.js
+++ client/app/taskingmanager.app.js
@@ -3,16 +3,23 @@
 (function () {
 
     angular.module('taskingManager', ['ngRoute'])
-        
-        .config(['$routeProvider','$locationProvider', function($routeProvider, $locationProvider) {
-            
+
+        .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+
             $routeProvider
-                
+
                 .when('/', {
                     templateUrl: 'app/home/home.html'
+                })
+
+                .when('/admin/create-project', {
+                    templateUrl: 'app/admin/create-project/create-project.html',
+                    controller: 'createProjectController',
+                    controllerAs: 'createProjectCtrl'
                 });
 
             // Enable HTML5Mode which means URLS don't have ugly hashbangs in them
             $locationProvider.html5Mode(true);
+            
         }]);
 })();
