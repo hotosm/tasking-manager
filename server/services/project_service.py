dc16fcfc3d74689b49a573e4d48a2630862023d3
Iain Hunter <iain.hunter@thinkwhere.com>
2017-02-24 16:03:58 +0000

    Test logger

--- server/services/project_service.py
+++ server/services/project_service.py
@@ -1,9 +1,12 @@
+from flask import current_app
 from server.models.project import AreaOfInterest, Project
 
 
 class ProjectService:
 
     def create_draft_project(self, data):
+
+        current_app.logger.debug('Create draft project')
 
         area_of_interest = AreaOfInterest(data['area_of_interest'])
         project = Project(data, area_of_interest=area_of_interest)
