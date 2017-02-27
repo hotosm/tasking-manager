5cea1b8d43e71d22b2c657d315419d33c579f757
Iain Hunter <iain.hunter@thinkwhere.com>
2017-02-27 12:34:53 +0000

    Ensure we expose global app object for web server

--- manage.py
+++ manage.py
@@ -3,7 +3,8 @@ from flask_script import Manager
 from server import create_app
 
 # Initialise the flask app object
-manager = Manager(create_app)
+application = create_app()
+manager = Manager(application)
 
 # Enable db migrations to be run via the command line
 manager.add_command('db', MigrateCommand)
