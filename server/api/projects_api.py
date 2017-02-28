b5de35effec2ca8ee278eb214cbee91085189fdf
Iain Hunter <iain.hunter@thinkwhere.com>
2017-02-28 17:25:00 +0000

    Wire up GeoAlchemy with AOI Model (#22)
    * Initial work
    
    * Work on API docs
    
    * Work on validating geoJson
    
    * Working persistanice of geoJson
    
    * Working geoalchemy

--- server/api/projects_api.py
+++ server/api/projects_api.py
@@ -1,5 +1,6 @@
+import json
 from flask_restful import Resource, request
-from server.services.project_service import ProjectService
+from server.services.project_service import ProjectService, InvalidGeoJson
 
 
 class ProjectsAPI(Resource):
@@ -15,27 +16,52 @@ class ProjectsAPI(Resource):
             - projects
         produces:
             - application/json
+        definitions:
+            - schema:
+                id: Multipolygon
+                properties:
+                    type:
+                        type: string
+                        default: MultiPolygon
+                    coordinates:
+                        type: array
+                        items:
+                            type: number
+                            default: [[[-4.0237,56.0904],[-3.9111,56.1715],[-3.8122,56.0980],[-4.0237,56.0904]]]
         parameters:
             - in: body
               name: body
               required: true
               description: JSON object for creating draft project
               schema:
-                  type: object
                   properties:
                       name:
                           type: string
                           default: HOT Project
-                      area_of_interest:
-                          type: object
-                          properties:
-                              name:
-                                  type: string
-                                  default: Project Area
+                      areaOfInterest:
+                          schema:
+                              properties:
+                                  geometryGeoJSON:
+                                      schema:
+                                          $ref: "#/definitions/Multipolygon"
+
+
         responses:
-          201:
-            description: Draft project created
+            201:
+                description: Draft project created
+            400:
+                description: Client Error - Invalid Request
         """
-        project_service = ProjectService()
-        project_service.create_draft_project(request.get_json())
-        return {"status": "healthy"}, 200
+        try:
+            # TODO this a little clunky but avoids DTO object, however DTOs may be cleaner - will decide later
+            data = request.get_json()
+            aoi_geometry_geojson = json.dumps(data['areaOfInterest']['geometryGeoJSON'])
+        except KeyError as e:
+            return {"error": f'Key {str(e)} not found in JSON, note parser is case sensitive'}, 400
+
+        try:
+            project_service = ProjectService()
+            project_service.create_draft_project(data, aoi_geometry_geojson)
+            return {"status": "success"}, 201
+        except InvalidGeoJson as e:
+            return {"error": f'{str(e)}'}, 400
--- server/services/project_service.py
+++ server/services/project_service.py
@@ -1,13 +1,23 @@
 from flask import current_app
-from server.models.project import AreaOfInterest, Project
+from server.models.project import AreaOfInterest, Project, InvalidGeoJson
 
 
 class ProjectService:
 
-    def create_draft_project(self, data):
-
+    def create_draft_project(self, data, aoi_geometry_geojson):
+        """
+        Validates and then persists draft projects in the DB
+        :param data:
+        :param aoi_geometry_geojson: AOI Geometry as a geoJSON string
+        :return:
+        """
+        # TODO - prob unpack the data object in the API
         current_app.logger.debug('Create draft project')
 
-        area_of_interest = AreaOfInterest(data['area_of_interest'])
-        project = Project(data, area_of_interest=area_of_interest)
-        project.save()
+        try:
+            area_of_interest = AreaOfInterest(aoi_geometry_geojson)
+        except InvalidGeoJson as e:
+            raise e
+
+        draft_project = Project(data, area_of_interest=area_of_interest)
+        draft_project.create()
