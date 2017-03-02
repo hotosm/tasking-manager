99d6dc91e980a4ef8ac100f114fcda64b2a829b8
LindaAlblas <linda.alblas@thinkwhere.com>
2017-03-02 16:29:10 +0000

    file upload start

--- client/app/admin/create-project/create-project.controller.js
+++ client/app/admin/create-project/create-project.controller.js
@@ -160,6 +160,21 @@
             vm.userZoomLevelOffset += zoomLevelOffset;
             vm.createTaskGrid();
         };
+        
+        // upload on file select or drop
+    vm.upload = function (file) {
+        Upload.upload({
+            url: 'upload/url',
+            data: {file: file, 'username': $scope.username}
+        }).then(function (resp) {
+            console.log('Success ' + resp.config.data.file.name + 'uploaded. Response: ' + resp.data);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
+        });
+    };
 
         /**
          * Adds a geocoder control to the map
