cc929df5c13a6488ae9809f6f4e6320ddf016bd6
Iain Hunter <iain.hunter@thinkwhere.com>
2017-03-03 11:49:30 +0000

    Add Tasks to Project API and persist in DB (#26)
    * Multiple definitions
    
    * Working Swagger for goejson
    
    * Move definitions to docs endpoint
    
    * Initial task model
    
    * Working task persist
    
    * Add validation
    
    * Add tests
    
    * Docs

--- server/api/projects_api.py
+++ server/api/projects_api.py
@@ -1,5 +1,5 @@
 import json
-from flask_restful import Resource, request
+from flask_restful import Resource, request, current_app
 from server.services.project_service import ProjectService, InvalidGeoJson
 
 
@@ -16,18 +16,6 @@ class ProjectsAPI(Resource):
             - projects
         produces:
             - application/json
-        definitions:
-            - schema:
-                id: Multipolygon
-                properties:
-                    type:
-                        type: string
-                        default: MultiPolygon
-                    coordinates:
-                        type: array
-                        items:
-                            type: number
-                            default: [[[-4.0237,56.0904],[-3.9111,56.1715],[-3.8122,56.0980],[-4.0237,56.0904]]]
         parameters:
             - in: body
               name: body
@@ -35,33 +23,55 @@ class ProjectsAPI(Resource):
               description: JSON object for creating draft project
               schema:
                   properties:
-                      name:
+                      projectName:
                           type: string
                           default: HOT Project
                       areaOfInterest:
                           schema:
+                              $ref: "#/definitions/GeoJsonMultiPolygon"
+                      tasks:
+                          schema:
                               properties:
-                                  geometryGeoJSON:
-                                      schema:
-                                          $ref: "#/definitions/Multipolygon"
-
-
+                                  type:
+                                      type: string
+                                      default: FeatureCollection
+                                  features:
+                                      type: array
+                                      items:
+                                          schema:
+                                              $ref: "#/definitions/GeoJsonMultiPolygonWithProperties"
         responses:
             201:
-                description: Draft project created
+                description: Draft project created successfully
             400:
                 description: Client Error - Invalid Request
+            500:
+                description: Internal Server Error
         """
         try:
             # TODO this a little clunky but avoids DTO object, however DTOs may be cleaner - will decide later
             data = request.get_json()
-            aoi_geometry_geojson = json.dumps(data['areaOfInterest']['geometryGeoJSON'])
+            project_name = data['projectName']
+            aoi_geometry_geojson = json.dumps(data['areaOfInterest'])
+            tasks_geojson = json.dumps(data['tasks'])
         except KeyError as e:
-            return {"error": f'Key {str(e)} not found in JSON, note parser is case sensitive'}, 400
+            error_msg = f'Key {str(e)} not found in JSON, note parser is case sensitive'
+            current_app.logger.error(error_msg)
+            return {"error": error_msg}, 400
+
+        # Check that none of the required fields are empty
+        if '' in [project_name, aoi_geometry_geojson, tasks_geojson]:
+            error_msg = 'Empty required field detected'
+            current_app.logger.error(error_msg)
+            return {"error": error_msg}, 400
 
         try:
             project_service = ProjectService()
-            project_service.create_draft_project(data, aoi_geometry_geojson)
+            project_service.create_draft_project(project_name, aoi_geometry_geojson, tasks_geojson)
             return {"status": "success"}, 201
         except InvalidGeoJson as e:
             return {"error": f'{str(e)}'}, 400
+        except Exception as e:
+            error_msg = f'Project Creation - unhandled error: {str(e)}'
+            current_app.logger.critical(error_msg)
+            return {"error": error_msg}, 500
--- server/services/project_service.py
+++ server/services/project_service.py
@@ -1,23 +1,49 @@
-from flask import current_app
-from server.models.project import AreaOfInterest, Project, InvalidGeoJson
+import geojson
+from server.models.project import AreaOfInterest, Project, InvalidGeoJson, Task, InvalidData
 
 
 class ProjectService:
 
-    def create_draft_project(self, data, aoi_geometry_geojson):
+    def create_draft_project(self, project_name, aoi_geojson, tasks_geojson):
         """
         Validates and then persists draft projects in the DB
-        :param data:
-        :param aoi_geometry_geojson: AOI Geometry as a geoJSON string
-        :return:
+        :param project_name: Name the Project Manager has given the project
+        :param aoi_geojson: Area Of Interest Geometry as a geoJSON multipolygon
+        :param tasks_geojson: All tasks associated with the project as a geoJSON feature collection
+        :raises InvalidGeoJson
         """
-        # TODO - prob unpack the data object in the API
-        current_app.logger.debug('Create draft project')
-
         try:
-            area_of_interest = AreaOfInterest(aoi_geometry_geojson)
+            area_of_interest = AreaOfInterest(aoi_geojson)
         except InvalidGeoJson as e:
             raise e
 
-        draft_project = Project(data, area_of_interest=area_of_interest)
+        try:
+            draft_project = Project(project_name, area_of_interest)
+        except InvalidData as e:
+            raise e
+
+        self._attach_tasks_to_project(draft_project, tasks_geojson)
+
         draft_project.create()
+
+    def _attach_tasks_to_project(self, draft_project, tasks_geojson):
+        """
+        Validates then iterates over the array of tasks and attach them to the draft project
+        :param draft_project: Draft project in scope
+        :param tasks_geojson: GeoJSON feature collection of mapping tasks
+        :raises InvalidGeoJson
+        """
+        tasks = geojson.loads(tasks_geojson)
+
+        if type(tasks) is not geojson.FeatureCollection:
+            raise InvalidGeoJson('Tasks: Invalid GeoJson must be FeatureCollection')
+
+        is_valid_geojson = geojson.is_valid(tasks)
+        if is_valid_geojson['valid'] == 'no':
+            raise InvalidGeoJson(f"Tasks: Invalid FeatureCollection - {is_valid_geojson['message']}")
+
+        task_id = 1
+        for task in tasks['features']:
+            new_task = Task(task_id, task)
+            draft_project.tasks.append(new_task)
+            task_id += 1
