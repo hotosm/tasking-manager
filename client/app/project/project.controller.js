0116b213ff8aea98a26a4039183df563ed5e29be
Ian Feeney <ian.feeney@thinkwhere.com>
2017-03-08 14:15:15 +0000

    add aoi

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -13,11 +13,13 @@
         var vm = this;
         vm.project = null;
         vm.map = null;
+        vm.currentTab = '';
 
         activate();
 
         function activate() {
             //TODO: Set up sidebar tabs
+            vm.currentTab = 'description';
 
             mapService.createOSMMap('map');
             vm.map = mapService.getOSMMap();
@@ -44,6 +46,7 @@
             resultsPromise.then(function (data) {
                 //project returned successfully
                 vm.project = data;
+                addAoiToMap(vm.project.areaOfInterest);
                 addProjectTasksToMap(vm.project.tasks);
             }, function(){
                 // project not returned successfully
@@ -54,6 +57,7 @@
 
         /**
          * Adds project tasks to map as features from geojson
+         * @param tasks
          */
         function addProjectTasksToMap(tasks){
             //TODO: may want to refactor this into a service at some point so that it can be resused
@@ -71,6 +75,27 @@
             });
             source.addFeatures(taskFeatures);
             vm.map.getView().fit(source.getExtent());
+        }
+
+        /**
+         * Adds the aoi feature to the map
+         * @param aoi
+         */
+        function addAoiToMap(aoi){
+            //TODO: may want to refactor this into a service at some point so that it can be resused
+            var source = new ol.source.Vector();
+            var vector = new ol.layer.Vector({
+                source: source
+            });
+            vm.map.addLayer(vector);
+
+            // read tasks JSON into features
+            var format = new ol.format.GeoJSON();
+            var aoiFeatures = format.readFeature(aoi, {
+                dataProjection: 'EPSG:4326',
+                featureProjection: 'EPSG:3857'
+            });
+            source.addFeature(aoiFeatures);
         }
     }
 })();
