952fca872738d7766bddf3010a3c009f1832cbf9
Iain Hunter <iain.hunter@thinkwhere.com>
2017-03-21 16:46:17 +0000

    Add validation tests

--- server/services/validator_service.py
+++ server/services/validator_service.py
@@ -1,10 +1,17 @@
 from flask import current_app
 from server.models.dtos.validator_dto import LockForValidationDTO
-from server.models.postgis.task import Task
+from server.models.postgis.task import Task, TaskStatus
 
 
 class TaskNotFound(Exception):
-    """ Custom exception to notify callers that a requested mapping task was not found"""
+    """ Custom exception to notify callers that a requested mapping task was not found """
+    def __init__(self, message):
+        if current_app:
+            current_app.logger.error(message)
+
+
+class ValidatatorServiceError(Exception):
+    """ Custom exception to notify callers that error has occurred """
     def __init__(self, message):
         if current_app:
             current_app.logger.error(message)
@@ -15,8 +22,16 @@ class ValidatorService:
     def lock_tasks_for_validation(self, validation_dto: LockForValidationDTO):
         """ Lock provides tasks for validation """
 
+        tasks_to_lock = []
         for task_id in validation_dto.task_ids:
             task = Task.get(task_id, validation_dto.project_id)
+
             if task is None:
                 raise TaskNotFound(f'Task {task_id} not found')
+
+            if TaskStatus(task.task_status) != TaskStatus.DONE:
+                raise ValidatatorServiceError(f'Task {task_id} in not DONE')
+
+            tasks_to_lock.append(task)
+
 
--- tests/server/unit/services/test_validator_service.py
+++ tests/server/unit/services/test_validator_service.py
@@ -1,11 +1,11 @@
 import unittest
-from server.services.validator_service import ValidatorService, Task, TaskNotFound, LockForValidationDTO
+from server.services.validator_service import ValidatorService, Task, TaskNotFound, LockForValidationDTO, TaskStatus, \
+    ValidatatorServiceError
 from unittest.mock import patch
 from server import create_app
 
 
 class TestValidatorService(unittest.TestCase):
-
     def setUp(self):
         self.app = create_app()
         self.ctx = self.app.app_context()
@@ -15,7 +15,7 @@ class TestValidatorService(unittest.TestCase):
         self.ctx.pop()
 
     @patch.object(Task, 'get')
-    def test_get_project_dto_for_mapping_returns_none_if_project_not_found(self, mock_task):
+    def test_lock_tasks_for_validation_raises_error_if_task_not_found(self, mock_task):
         # Arrange
         mock_task.return_value = None
 
@@ -25,4 +25,19 @@ class TestValidatorService(unittest.TestCase):
 
         # Act / Assert
         with self.assertRaises(TaskNotFound):
+            ValidatorService().lock_tasks_for_validation(lock_dto)
+
+    @patch.object(Task, 'get')
+    def test_lock_tasks_for_validation_raises_error_if_task_not_done(self, mock_task):
+        # Arrange
+        task_stub = Task()
+        task_stub.task_status = TaskStatus.READY.value
+        mock_task.return_value = task_stub
+
+        lock_dto = LockForValidationDTO()
+        lock_dto.project_id = 1
+        lock_dto.task_ids = [1, 2]
+
+        # Act / Assert
+        with self.assertRaises(ValidatatorServiceError):
             ValidatorService().lock_tasks_for_validation(lock_dto)
