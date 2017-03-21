2e2f32c927336182ea51b727a4d643e6b0828a77
LindaAlblas <linda.alblas@thinkwhere.com>
2017-03-21 12:49:44 +0000

    fix default locale

--- client/app/admin/edit-project/edit-project.controller.js
+++ client/app/admin/edit-project/edit-project.controller.js
@@ -80,7 +80,6 @@
                     info.instructions = $showdown.makeHtml(info.instructions);
                     populatedLocale = true;
                 }
-                vm.project.defaultLocale = vm.defaultLocale;
                 
                 // if no fields for this locale are populated, remove from array
                 if (!populatedLocale){
@@ -89,6 +88,8 @@
                     i--;
                 }
             }
+            
+            vm.project.defaultLocale = vm.defaultLocale;
 
             var resultsPromise = projectService.updateProject(vm.project.projectId, vm.project);
             resultsPromise.then(function (data) {
