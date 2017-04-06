d8d6be30e77ebcbc1be25a39aa16a41486d3dcaa
LindaAlblas <linda.alblas@thinkwhere.com>
2017-04-06 12:36:32 +0100

    beginnings of wiring up search

---
+++ client/app/services/search.service.js
@@ -0,0 +1,49 @@
+(function () {
+    'use strict';
+    /**
+     * @fileoverview This file provides a search service.
+     */
+
+    angular
+        .module('taskingManager')
+        .service('searchService', ['$http', '$q','configService', searchService]);
+
+    function searchService($http, $q, configService) {
+        
+        var service = {
+            searchProjects: searchProjects
+        };
+
+        return service;
+
+        /**
+         * Search projects
+         * @param mapperLevel
+         * @returns {*|!jQuery.deferred|!jQuery.Promise|!jQuery.jqXHR}
+         */
+        function searchProjects(mapperLevel){
+            
+            var searchParams = {};
+            // default to beginner
+            searchParams.mapperLevel = mapperLevel || 'BEGINNER';
+            
+            // Returns a promise
+            return $http({
+                method: 'GET',
+                url: configService.tmAPI + '/project/search',
+                params: searchParams,
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                return response.data;
+            }, function errorCallback() {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                return $q.reject("error");
+            })
+        }
+    }
+})();
