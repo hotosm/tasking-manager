fa40794b510fc3e9f44b3a82e51d31a5ecee6772
Iain Hunter <iain.hunter@thinkwhere.com>
2017-04-14 17:20:45 +0100

    Modified to invalidate all, except ready

--- server/services/validator_service.py
+++ server/services/validator_service.py
@@ -95,11 +95,11 @@ class ValidatorService:
         return mapped_tasks
 
     @staticmethod
-    def invalidate_all_tasks(project_id: int, user_id: int, comment: str):
+    def invalidate_all_tasks(project_id: int, user_id: int):
         """ Invalidates all mapped tasks on a project"""
-        Task.validate_invalidate_all(project_id, user_id, comment, TaskStatus.INVALIDATED)
+        Task.validate_invalidate_all(project_id, user_id, TaskStatus.INVALIDATED)
 
     @staticmethod
-    def validate_all_tasks(project_id: int, user_id: int, comment: str):
+    def validate_all_tasks(project_id: int, user_id: int):
         """ Validates all mapped tasks on a project"""
-        Task.validate_invalidate_all(project_id, user_id, comment, TaskStatus.VALIDATED)
+        Task.validate_invalidate_all(project_id, user_id, TaskStatus.VALIDATED)
