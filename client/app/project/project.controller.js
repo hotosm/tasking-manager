aeb1ed05a93ee458bd32bf7a6068820cb235f4b0
Ian Feeney <ian.feeney@thinkwhere.com>
2017-04-19 10:40:59 +0100

    reset timer

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -111,7 +111,7 @@
                 refreshProject(id);
                 updateMappedTaskPerUser(id);
                 //TODO do a selected task refesh too
-            }, 10000);
+            }, 10000pu);
         }
 
         // listen for navigation away from the page event and stop the autrefresh timer
