6e5d2290596b601c085e07cef6bd2a832bf7974d
Ian Feeney <ian.feeney@thinkwhere.com>
2017-04-19 11:03:15 +0100

    Update project.controller.js

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -111,7 +111,7 @@
                 refreshProject(id);
                 updateMappedTaskPerUser(id);
                 //TODO do a selected task refesh too
-            }, 10000pu);
+            }, 10000);
         }
 
         // listen for navigation away from the page event and stop the autrefresh timer
