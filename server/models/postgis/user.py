9072a4ab12a44936c30f08fd707b7f67ec2c1cab
Iain Hunter <iain.hunter@thinkwhere.com>
2017-04-24 17:29:09 +0100

    Add aoi and centroid to mapped projects

--- server/models/postgis/user.py
+++ server/models/postgis/user.py
@@ -1,3 +1,4 @@
+import geojson
 from enum import Enum
 from server import db
 from server.models.dtos.user_dto import UserDTO, UserMappedProjectsDTO, MappedProject
@@ -58,14 +59,17 @@ class User(db.Model):
     @staticmethod
     def get_mapped_projects(user_id: int, preferred_locale: str) -> UserMappedProjectsDTO:
         """ Get all projects a user has mapped on """
-        sql = '''select p.id, p.status, p.default_locale, count(t.mapped_by), count(t.validated_by)
+        sql = '''select p.id, p.status, p.default_locale, count(t.mapped_by), count(t.validated_by), st_asgeojson(a.centroid),
+                        st_asgeojson(a.geometry)
                    from projects p,
-                        tasks t
+                        tasks t,
+                        areas_of_interest a
                   where p.id in (select unnest(projects_mapped) from users where id = {0})
                     and p.id = t.project_id
+                    and p.id = a.id
                     and (t.mapped_by = {0} or t.mapped_by is null)
                     and (t.validated_by = {0} or t.validated_by is null)
-               GROUP BY p.id, p.status'''.format(user_id)
+               GROUP BY p.id, p.status, a.centroid, a.geometry'''.format(user_id)
 
         results = db.engine.execute(sql)
 
@@ -79,6 +83,8 @@ class User(db.Model):
             mapped_project.status = ProjectStatus(row[1]).name
             mapped_project.tasks_mapped = row[3]
             mapped_project.tasks_validated = row[4]
+            mapped_project.centroid = geojson.loads(row[5])
+            mapped_project.aoi = geojson.loads(row[6])
 
             project_info = ProjectInfo.get_dto_for_locale(row[0], preferred_locale, row[2])
             mapped_project.name = project_info.name
