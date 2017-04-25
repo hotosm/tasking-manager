1da94bfbf2c82f91e2422f572bbf476abd9db1c8
Ian Feeney <ian.feeney@thinkwhere.com>
2017-04-25 16:01:36 +0100

    fix tests

--- server/services/grid_service.py
+++ server/services/grid_service.py
@@ -20,14 +20,11 @@ class GridService:
         aoi = grid_dto.area_of_interest
         clip_to_aoi = grid_dto.clip_to_aoi
 
-        #create a shapely shape from the aoi trasnformed to 3857
-        #need to do the transform from 4326 to 3857 to ensure the intersect operations below work
-        #aoi_polygon = GridService._transform(shapely.geometry.shape(aoi), 4326, 3857)
+        # create a shapely shape from the aoi
         aoi_polygon = shapely.geometry.shape(aoi)
         intersecting_features = []
         for feature in grid['features']:
-            # created a shapely shape for the tile in 3857
-            #tile = GridService._transform(shapely.geometry.shape(feature['geometry']), 4326, 3857)
+            # created a shapely shape for the tile
             tile = shapely.geometry.shape(feature['geometry'])
             if aoi_polygon.contains(tile):
                 # tile is completely within aoi, use as is
@@ -41,14 +38,8 @@ class GridService:
                         if intersection.geom_type == 'Polygon':
                             # shapely may return a POLYGON rather than a MULTIPOLYGON if there is just one intersection area
                             intersection = MultiPolygon([intersection])
-                        #intersection = GridService._transform(intersection, 3857, 4326)
                         feature['geometry'] = mapping(intersection)
                         feature['properties']['splittable'] = False
                     intersecting_features.append(feature)
         return geojson.FeatureCollection(intersecting_features)
 
-    @staticmethod
-    def _transform(source_shape: shapely.geometry.shape, source_crs: int, target_crs: int) -> shapely.geometry.shape:
-        shape_source = shape.from_shape(source_shape, source_crs)
-        shape_target = db.session.execute(ST_Transform(shape_source, target_crs)).scalar()
-        return shape.to_shape(shape_target)
