42e453f963d9350b459c93ad896c20669557da55
LindaAlblas <linda.alblas@thinkwhere.com>
2017-04-27 09:17:26 +0200

    fix test

--- client/app/contribute/contribute.controller.js
+++ client/app/contribute/contribute.controller.js
@@ -48,8 +48,10 @@
             vm.map = mapService.getOSMMap();
             // Get legend element and add it to the map as a control
             var legendContainer = document.getElementById('legend-container');
-            var legendControl = new ol.control.Control({element: legendContainer});
-            vm.map.addControl(legendControl);
+            if (legendContainer){
+                var legendControl = new ol.control.Control({element: legendContainer});
+                vm.map.addControl(legendControl);
+            }
             projectMapService.initialise(vm.map);
             projectMapService.showInfoOnHoverOrClick();
             setOrganisationTags();
