8a93ec8046b01a3d96db0cd71d8632678b205de8
Iain Hunter <iain.hunter@thinkwhere.com>
2017-04-27 12:15:16 +0100

    Add test for CRUD methods

---
+++ tests/server/integration/services/test_license_service.py
@@ -0,0 +1,72 @@
+import os
+import unittest
+from server import create_app
+from server.services.license_service import LicenseService, LicenseDTO, NotFound
+
+
+class TestAuthenticationService(unittest.TestCase):
+    skip_tests = False
+    test_user = None
+    test_project = None
+
+    @classmethod
+    def setUpClass(cls):
+        env = os.getenv('SHIPPABLE', 'false')
+
+        # Firewall rules mean we can't hit Postgres from Shippable so we have to skip them in the CI build
+        if env == 'true':
+            cls.skip_tests = True
+
+    def setUp(self):
+        if self.skip_tests:
+            return
+
+        self.app = create_app()
+        self.ctx = self.app.app_context()
+        self.ctx.push()
+
+    def tearDown(self):
+        if self.skip_tests:
+            return
+
+        self.ctx.pop()
+
+    def test_license_crud_works_as_expected(self):
+        if self.skip_tests:
+            return
+
+        # Arrange
+        license_dto = LicenseDTO()
+        license_dto.name = "thinkWhere"
+        license_dto.description = 'thinkWhere test'
+        license_dto.plain_text = 'thinkWhere test'
+
+        # Act
+        license_id = LicenseService.create_licence(license_dto)
+
+        try:
+            actual_license = LicenseService.get_license_as_dto(license_id)
+
+            # Assert PUT and GET
+            self.assertEqual(actual_license.name, license_dto.name)
+
+            license_dto.license_id = license_id
+            license_dto.name = 'Updated name'
+            LicenseService.update_licence(license_dto)
+            updated_license = LicenseService.get_license_as_dto(license_id)
+
+            # Assert POST
+            self.assertEqual(updated_license.name, license_dto.name)
+
+            all_licenses = LicenseService.get_all_licenses()
+
+            # Assert Get all
+            self.assertGreaterEqual(1, len(all_licenses))
+
+            # Assert Delete
+            LicenseService.delete_license(license_id)
+            with self.assertRaises(NotFound):
+                LicenseService.get_license_as_dto(license_id)
+        except Exception:
+            # If any problem occurs try and tidy up
+            LicenseService.delete_license(license_id)
