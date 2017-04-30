9a06698791bb75352a89646939b7a1efd4776229
Ian Feeney <ian.feeney@thinkwhere.com>
2017-04-30 11:30:15 +0100

    fix tests

--- tests/server/unit/services/test_grid_service.py
+++ tests/server/unit/services/test_grid_service.py
@@ -66,11 +66,13 @@ class TestGridService(unittest.TestCase):
 
     def test_tasks_from_aoi_features(self):
         # arrange
-        aoi_json = get_canned_json('very_large_aoi.geojson')
+        grid_json = get_canned_json('test_grid.json')
+        grid_dto = GridDTO(grid_json)
         expected = geojson.loads(json.dumps(get_canned_json('tasks_from_aoi_features.json')))
         # act
 
-        result = GridService.tasks_from_aoi_features(aoi_json)
+        result = GridService.tasks_from_aoi_features(grid_dto.area_of_interest)
+        print(result)
         # assert
         self.assertEquals(str(expected), str(result))
 
