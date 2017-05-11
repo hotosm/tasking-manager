b00979d38a2e83b7267645fc8b56c1ef17d0a089
Ian Feeney <ian.feeney@thinkwhere.com>
2017-05-11 15:59:17 +0100

    fix tests

--- tests/server/unit/services/test_validator_service.py
+++ tests/server/unit/services/test_validator_service.py
@@ -4,6 +4,8 @@ from server.services.validator_service import ValidatorService, Task, NotFound, 
 from server.models.dtos.validator_dto import ValidatedTask
 from unittest.mock import patch
 from server import create_app
+from server.services.user_service import UserService
+from server.models.dtos.mapping_dto import TaskDTOs
 
 
 class TestValidatorService(unittest.TestCase):
@@ -49,32 +51,36 @@ class TestValidatorService(unittest.TestCase):
         with self.assertRaises(ValidatatorServiceError):
             ValidatorService.lock_tasks_for_validation(lock_dto)
 
+    @patch.object(UserService, 'is_user_a_project_manager')
     @patch.object(Task, 'get')
     @patch.object(ProjectService, 'is_user_permitted_to_validate')
-    def test_lock_tasks_raises_error_if_project_validator_only_and_user_not_validator(self, mock_project, mock_task):
+    def test_lock_tasks_raises_error_if_project_validator_only_and_user_not_validator(self, mock_project, mock_task, mock_user):
         # Arrange
         task_stub = Task()
         task_stub.task_status = TaskStatus.MAPPED.value
         mock_task.return_value = task_stub
-
         mock_project.return_value = False, ValidatingNotAllowed.USER_NOT_VALIDATOR
+        mock_user.return_value = True
 
         lock_dto = LockForValidationDTO()
         lock_dto.project_id = 1
         lock_dto.task_ids = [1, 2]
+        lock_dto.user_id = 1234
 
         with self.assertRaises(ValidatatorServiceError):
             ValidatorService.lock_tasks_for_validation(lock_dto)
 
+    @patch.object(UserService, 'is_user_a_project_manager')
     @patch.object(Task, 'get')
     @patch.object(ProjectService, 'is_user_permitted_to_validate')
-    def test_lock_tasks_raises_error_if_user_has_not_accepted_license(self, mock_project, mock_task):
+    def test_lock_tasks_raises_error_if_user_has_not_accepted_license(self, mock_project, mock_task, mock_user):
         # Arrange
         task_stub = Task()
         task_stub.task_status = TaskStatus.MAPPED.value
         mock_task.return_value = task_stub
 
         mock_project.return_value = False, ValidatingNotAllowed.USER_NOT_ACCEPTED_LICENSE
+        mock_user.return_value = True
 
         lock_dto = LockForValidationDTO()
         lock_dto.project_id = 1
@@ -152,3 +158,48 @@ class TestValidatorService(unittest.TestCase):
         # Act / Assert
         with self.assertRaises(ValidatatorServiceError):
             ValidatorService.unlock_tasks_after_validation(unlock_dto)
+
+    @patch.object(UserService, 'is_user_a_project_manager')
+    @patch.object(Task, 'get')
+    @patch.object(ProjectService, 'is_user_permitted_to_validate')
+    def test_lock_tasks_raises_error_if_user_not_pm_and_mapped_the_task(self, mock_project, mock_task, mock_user):
+        # Arrange
+        task_stub = Task()
+        task_stub.task_status = TaskStatus.MAPPED.value
+        task_stub.mapped_by = 1234
+        mock_task.return_value = task_stub
+
+        mock_project.return_value = True, 'User allowed to validate'
+        mock_user.return_value = False
+
+        lock_dto = LockForValidationDTO()
+        lock_dto.project_id = 1
+        lock_dto.task_ids = [1, 2]
+        lock_dto.user_id = 1234
+
+        with self.assertRaises(ValidatatorServiceError):
+            ValidatorService.lock_tasks_for_validation(lock_dto)
+
+    @patch.object(UserService, 'is_user_a_project_manager')
+    @patch.object(Task, 'get')
+    @patch.object(ProjectService, 'is_user_permitted_to_validate')
+    def test_lock_tasks_locks_task_if_user_is_pm_and_mapped_the_task(self, mock_project, mock_task, mock_user):
+        # Arrange
+        task_stub = Task()
+        task_stub.task_status = TaskStatus.MAPPED.value
+        task_stub.mapped_by = 1234
+        mock_task.return_value = task_stub
+
+        mock_project.return_value = True, 'User allowed to validate'
+        mock_user.return_value = True
+
+        lock_dto = LockForValidationDTO()
+        lock_dto.project_id = 1
+        lock_dto.task_ids = [1, 2]
+        lock_dto.user_id = 1234
+
+        # act
+        task_dto = ValidatorService.lock_tasks_for_validation(lock_dto)
+
+        # assert
+        self.assertIsInstance(task_dto, TaskDTOs, "lock_tasks_for_validation did not return TaskDTOs object")
