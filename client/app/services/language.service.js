c0284dbf62b84c13505206c39058d93b0c0ec6b4
LindaAlblas <linda.alblas@thinkwhere.com>
2017-05-19 15:33:46 +0100

    supported languages

--- client/app/services/language.service.js
+++ client/app/services/language.service.js
@@ -6,15 +6,16 @@
 
     angular
         .module('taskingManager')
-        .service('languageService', [languageService]);
+        .service('languageService', ['$http', '$q', 'configService', languageService]);
 
-    function languageService() {
+    function languageService($http, $q, configService) {
         
         var languageCode = 'en'; // default to English
         
         var service = {
             setLanguageCode: setLanguageCode,
-            getLanguageCode: getLanguageCode
+            getLanguageCode: getLanguageCode,
+            getAvailableLanguages: getAvailableLanguages
         };
 
         return service;
@@ -33,6 +34,29 @@
          */
         function getLanguageCode(){
             return languageCode;
+        }
+
+        /**
+         * Get available languages
+         * @returns {*|!jQuery.deferred|!jQuery.Promise|!jQuery.jqXHR}
+         */
+        function getAvailableLanguages(){
+            // Returns a promise
+            return $http({
+                method: 'GET',
+                url: configService.tmAPI + '/settings/languages',
+                headers: {
+                    'Content-Type': 'application/json; charset=UTF-8'
+                }
+            }).then(function successCallback(response) {
+                // this callback will be called asynchronously
+                // when the response is available
+                return response.data;
+            }, function errorCallback() {
+                // called asynchronously if an error occurs
+                // or server returns response with an error status.
+                return $q.reject("error");
+            })
         }
     }
 })();
