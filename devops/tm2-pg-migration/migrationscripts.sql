148179f705ba6b9430432936f24bd1881e378614
Iain Hunter <iain.hunter@thinkwhere.com>
2017-05-19 11:15:25 +0100

    Text search 2 (#334)
    * Enhance text_search to do OR search
    
    * rename file

--- devops/tm2-pg-migration/migrationscripts.sql
+++ devops/tm2-pg-migration/migrationscripts.sql
@@ -83,6 +83,8 @@ tsvector_update_trigger(text_searchable, 'pg_catalog.english', project_id_str, s
 -- set project-id which will update text search index
 update project_info set project_id_str = project_id::text
 
+CREATE INDEX textsearch_idx ON project_info USING GIN (text_searchable);
+
 -- TASKS
 -- Get all tasks that don't have a state of -1 (removed) and where they relate to a project that has been migrated above
 -- default any null x, y values to -1
