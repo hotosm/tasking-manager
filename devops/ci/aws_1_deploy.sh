8de99be73309719e59102d0f574b9a6f1690026a
David Neudorfer <david@davidneudorfer.com>
2017-05-25 03:14:00 -0700

    fail on error and print env

---
+++ devops/ci/aws_1_deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -ev # halt script on error
+
+env
+
+echo Running HOT Tasking Manager Deploy, current branch is $BRANCH
+
+# We don't want to deploy Pull Requests only builds on develop and master
+if [ $IS_PULL_REQUEST == true ]
+    then
+        echo Not Deploying Build $BUILD_NUMBER - Branch is $BRANCH, Is Pull Request is $IS_PULL_REQUEST
+        return
+fi
+
+# Set Version Number
+VERSION=v.0.0.$BUILD_NUMBER-$BRANCH
+
+# Only deploy to Staging if we're on develop
+if [ $BRANCH == "develop" ]
+    then
+        # Install AWS requirements
+        pip install -r requirements.aws.txt
+        printf 'n\n' | eb init hot-tasking-manager --region eu-west-1
+        eb use tasking-manager-staging
+
+        # Deploy develop builds to Staging environment
+        echo Deploying $VERSION to tasking-manager-staging
+        eb deploy -l $VERSION
+fi
