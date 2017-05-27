07d902ec17f0f490cef4f7ef0ca148765689beda
david-HOTOSM <david.neudorfer@hotosm.org>
2017-05-26 22:42:25 -0700

    move all deploy logic to script

--- devops/ci/circleci_deploy.sh
+++ devops/ci/circleci_deploy.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -ev # halt script on error
 
+DEMO_BRANCH="develop-branch-travis"
+STAGE_BRANCH="develop-branch-travis"
+PROD_BRANCH="develop-branch-travis"
+
+DEMO_ENV="taskingmanager-demo"
+STAGE_ENV="taskingmanager-stage"
+PROD_ENV="taskingmanager-prod"
+
 echo Running HOT Tasking Manager Deploy, current branch is $CIRCLE_BRANCH
 
 # We don't want to deploy Pull Requests only builds on develop and master
@@ -13,15 +21,41 @@ fi
 # Set Version Number
 VERSION=v.0.0.$CIRCLE_BUILD_NUM-$CIRCLE_BRANCH
 
-# Only deploy to Staging if we're on develop
-if [ $CIRCLE_BRANCH == "develop-branch-travis" ]
+# Deploy to Demo Env
+if [ $CIRCLE_BRANCH == $DEMO_BRANCH ]
     then
         # Install AWS requirements
         pip install -r requirements.aws.txt
         printf 'n\n' | eb init taskingmanager --region us-east-1
-        eb use taskingmanager-demo
+        eb use $DEMO_ENV
 
         # Deploy develop builds to Staging environment
-        echo Deploying $VERSION to taskingmanager-demo
+        echo Deploying $VERSION to $DEMO_ENV
+        eb deploy -l $VERSION
+fi
+
+# Deploy to Stage Env
+if [ $CIRCLE_BRANCH == $STAGE_BRANCH ]
+    then
+        # Install AWS requirements
+        pip install -r requirements.aws.txt
+        printf 'n\n' | eb init taskingmanager --region us-east-1
+        eb use $STAGE_ENV
+
+        # Deploy develop builds to Staging environment
+        echo Deploying $VERSION to $STAGE_ENV
+        eb deploy -l $VERSION
+fi
+
+# Deploy to Prod Env
+if [ $CIRCLE_BRANCH == $PROD_BRANCH ]
+    then
+        # Install AWS requirements
+        pip install -r requirements.aws.txt
+        printf 'n\n' | eb init taskingmanager --region us-east-1
+        eb use $PROD_ENV
+
+        # Deploy develop builds to Staging environment
+        echo Deploying $VERSION to $PROD_ENV
         eb deploy -l $VERSION
 fi
