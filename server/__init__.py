74fdf46f38a54aa066a36d8747ba2aa46feb8ffb
Iain Hunter <iain.hunter@thinkwhere.com>
2017-06-01 16:16:07 +0100

    Add namespace

--- server/__init__.py
+++ server/__init__.py
@@ -1,12 +1,13 @@
 import logging
 import os
+from logging.handlers import RotatingFileHandler
+
 from flask import Flask
 from flask_cors import CORS
 from flask_migrate import Migrate
 from flask_oauthlib.client import OAuth
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
-from logging.handlers import RotatingFileHandler
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -96,8 +97,8 @@ def init_flask_restful_routes(app):
     from server.api.tags_apis import CampaignsTagsAPI, OrganisationTagsAPI
     from server.api.user_apis import UserAPI, UserOSMAPI, UserMappedProjects, UserSetRole, UserSetLevel, UserAcceptLicense, UserSearchFilterAPI, UserSearchAllAPI, UserUpdateAPI
     from server.api.validator_apis import LockTasksForValidationAPI, UnlockTasksAfterValidationAPI, StopValidatingAPI, MappedTasksByUser
-    from server.api.grid_apis import IntersectingTilesAPI
-    from server.api.split_task_apis import SplitTaskAPI
+    from server.api.grid.grid_apis import IntersectingTilesAPI
+    from server.api.grid.split_task_apis import SplitTaskAPI
     from server.api.settings_apis import LanguagesAPI
 
     api.add_resource(SwaggerDocsAPI,                '/api/docs')
