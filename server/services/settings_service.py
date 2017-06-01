c7255462ba4fa70650a75dd2872ed0b2dd123fb6
Iain Hunter <iain.hunter@thinkwhere.com>
2017-06-01 10:55:40 +0100

    Remove cruft

--- server/services/settings_service.py
+++ server/services/settings_service.py
@@ -35,19 +35,3 @@ class SettingsService:
             count += 1
 
         return supported_languages
-
-    # @staticmethod
-    # def get_supported_languages():
-    #     """ Gets all supported languages from the config """
-    #     app_languages = current_app.config['SUPPORTED_LANGUAGES']
-    #
-    #     app_codes = [x.strip() for x in app_languages['codes'].split(',')]
-    #     app_languages = [x.strip() for x in app_languages['languages'].split(',')]
-    #
-    #     supported_languages = []
-    #     count = 0
-    #     for code in app_codes:
-    #         supported_languages.append(dict(code=code, language=app_languages[count]))
-    #         count += 1
-    #
-    #     return dict(supportedLanguages=supported_languages)
