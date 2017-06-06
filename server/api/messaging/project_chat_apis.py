fce7a56233729b2a1c717ec37bbb8cb3a872715c
Iain Hunter <iain.hunter@thinkwhere.com>
2017-06-06 16:59:09 +0100

    Work on project chat

--- server/api/messaging/project_chat_apis.py
+++ server/api/messaging/project_chat_apis.py
@@ -1,5 +1,8 @@
-from flask_restful import Resource
+from flask_restful import Resource, request, current_app
+from schematics.exceptions import DataError
 
+from server.models.dtos.message_dto import ChatMessageDTO
+from server.services.messaging.chat_service import ChatService
 from server.services.users.authentication_service import token_auth, tm
 
 
@@ -7,9 +10,9 @@ class ProjectChatAPI(Resource):
 
     @tm.pm_only(False)
     @token_auth.login_required
-    def post(self, project_id):
+    def put(self, project_id):
         """
-        Post a message to project chat
+        Add a message to project chat
         ---
         tags:
           - messages
@@ -22,6 +25,12 @@ class ProjectChatAPI(Resource):
               required: true
               type: string
               default: Token sessionTokenHere==
+            - name: project_id
+              in: path
+              description: The ID of the project to attach the chat message to
+              required: true
+              type: integer
+              default: 1
             - in: body
               name: body
               required: true
@@ -32,9 +41,26 @@ class ProjectChatAPI(Resource):
                           type: string
                           default: This is an awesome project
         responses:
-            200:
-                description: Message info
+            201:
+                description: Message posted successfully
+            400:
+                description: Invalid Request
             500:
                 description: Internal Server Error
         """
-        pass
+        try:
+            chat_dto = ChatMessageDTO(request.get_json())
+            chat_dto.user_id = tm.authenticated_user_id
+            chat_dto.project_id = project_id
+            chat_dto.validate()
+        except DataError as e:
+            current_app.logger.error(f'Error validating request: {str(e)}')
+            return str(e), 400
+
+        try:
+            ChatService.post_message(chat_dto)
+            return {"Status": "Message posted successfully"}, 201
+        except Exception as e:
+            error_msg = f'Chat PUT - unhandled error: {str(e)}'
+            current_app.logger.critical(error_msg)
+            return {"error": error_msg}, 500
