5be71d9d319e9744e37f6e8c20cb73f5fcc3847e
Iain Hunter <iain.hunter@thinkwhere.com>
2017-06-07 11:41:41 +0100

    Add alerts if you are @d in message

--- server/services/messaging/chat_service.py
+++ server/services/messaging/chat_service.py
@@ -1,13 +1,16 @@
 from server.models.dtos.message_dto import ChatMessageDTO, ProjectChatDTO
 from server.models.postgis.project_chat import ProjectChat
+from server.services.messaging.message_service import MessageService
 
 
 class ChatService:
 
     @staticmethod
-    def post_message(chat_dto: ChatMessageDTO):
-        """ Save message to DB"""
+    def post_message(chat_dto: ChatMessageDTO) -> ProjectChatDTO:
+        """ Save message to DB and return latest chat"""
+        MessageService.send_message_after_chat(chat_dto.user_id, chat_dto.message, chat_dto.project_id)
         ProjectChat.create_from_dto(chat_dto)
+        return ChatService.get_messages(chat_dto.project_id, 1)
 
     @staticmethod
     def get_messages(project_id: int, page: int) -> ProjectChatDTO:
