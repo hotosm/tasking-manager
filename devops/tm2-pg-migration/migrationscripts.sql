5a524893e959f8a10df5da2d9ea7a63cac13cb1a
Ian Feeney <ian.feeney@thinkwhere.com>
2017-06-16 12:57:06 +0100

    fix splittable flag

--- devops/tm2-pg-migration/migrationscripts.sql
+++ devops/tm2-pg-migration/migrationscripts.sql
@@ -302,6 +302,10 @@ INSERT INTO hotnew.project_allowed_users(
     (select distinct project_id, user_id
     from hotold.project_allowed_users);
 
+-- TASK SPLITTABLE FLAG
+-- Ensure the splittable flag is consistent with the x,y,zoom values
+UPDATE hotnew.tasks SET splittable = (x IS NOT NULL AND y IS NOT NULL AND zoom IS NOT NULL);
+
 
 --------------------------------------------------	
 --  Migration Results 28/04/2017
