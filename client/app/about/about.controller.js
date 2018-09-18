7c799a4f289715a2c6b3bd998505d029ebc2f4b0
Zack LaVergne <zlavergne94@gmail.com>
2018-09-18 14:49:34 -0600

    updated about content and email addresses added new favicon

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -14,44 +14,9 @@
 
         vm.sponsors = [
             {
-                name: 'The Austrialian Government',
-                logo: './assets/img/aus-gov-logo-stacked-black.jpg',
-                url: 'http://dfat.gov.au'
-            },
-            {
-                name: 'USAID GeoCenter and Office of Transition Initiatives',
-                logo: './assets/img/usaid-logo.png',
-                url: 'https://www.usaid.gov'
-            },
-            {
-                name: 'World Bank - GFDRR',
-                logo: './assets/img/gfdrr-logo.png',
-                url: 'https://www.gfdrr.org'
-            },
-            {
-                name: 'American Red Cross',
-                logo: './assets/img/americanredcross-logo.png',
-                url: 'http://www.redcross.org'
-            },
-            {
-                name: 'The George Washington University',
-                logo: './assets/img/gwu-logo.png',
-                url: 'https://www.gwu.edu'
-            },
-            {
-                name: 'thinkWhere',
-                logo: './assets/img/thinkwhere_logo.png',
-                url: 'http://www.thinkwhere.com/'
-            },
-            {
-                name: 'Development Seed',
-                logo: './assets/img/ds-logo-pos.svg',
-                url: 'https://developmentseed.org/'
-            },
-            {
-                name: 'Amazon Web Services',
-                logo: './assets/img/Powered-by-Amazon-Web-Services.png',
-                url: 'https://aws.amazon.com'
+                name: 'Kaart',
+                logo: './assets/img/kaart17.jpg',
+                url: 'https://kaartgroup.com'
             }
         ]
     }
