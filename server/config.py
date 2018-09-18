bf8cc7ccb26c9526aa351bd7f9d4bcb1f42d647f
zlavergne <zlavergne94@gmail.com>
2018-09-18 17:44:15 +0000

    updated footer and home page to more appropriate info

--- server/config.py
+++ server/config.py
@@ -71,7 +71,8 @@ class StagingConfig(EnvironmentConfig):
 
 
 class DevConfig(EnvironmentConfig):
-    APP_BASE_URL = 'http://127.0.0.1:5000'
+   #APP_BASE_URL = 'http://127.0.0.1:5000'
+    APP_BASE_URL = 'https://osmcolorado.com'
     API_DOCS_URL = f'{APP_BASE_URL}/api-docs/swagger-ui/index.html?' + \
                    f'url={APP_BASE_URL}/api/docs'
     LOG_DIR = 'logs'
