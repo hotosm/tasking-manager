760bed24563a745b695873ceec66a80b2427b8cf
Neil Rotstan <neil@rotstan.org>
2018-11-14 16:16:28 -0800

    Let users see their invalidated tasks on profile
    > Note: this commit adds a new migration (server) and a new NPM package
    (client)
    
    * Let users view both tasks they mapped that were marked invalid, as
    well as tasks they invalidated themselves, from their profile page
    
    * Invalidated tasks are considered "open" if they have not subsequently
    been marked valid and may still require attention, and "closed" if
    they been subsequently validated
    
    * If user clicks through to an invalidated task from the new tables,
    highlight corresponding invalidation entry in task history on the task
    page for easy scanning
    
    * Add task_invalidation_history table for tracking invalidated task
    history going forward
    
    * Add user/:username/invalidated-tasks API endpoint to server
    
    * Maintain invalidation history on server to track tasks marked invalid
    and whether they are later marked valid

--- server/models/dtos/mapping_dto.py
+++ server/models/dtos/mapping_dto.py
@@ -48,7 +48,7 @@ class StopMappingTaskDTO(Model):
 class TaskHistoryDTO(Model):
     """ Describes an individual action that was performed on a mapping task"""
     history_id = IntType(serialized_name='historyId')
-    task_id = StringType(serialized_name='taskId')
+    task_id = IntType(serialized_name='taskId')
     action = StringType()
     action_text = StringType(serialized_name='actionText')
     action_date = DateTimeType(serialized_name='actionDate')
@@ -64,7 +64,6 @@ class TaskDTO(Model):
     task_history = ListType(ModelType(TaskHistoryDTO), serialized_name='taskHistory')
     per_task_instructions = StringType(serialized_name='perTaskInstructions', serialize_when_none=False)
     is_undoable = BooleanType(serialized_name='isUndoable', default=False)
-    auto_unlock_seconds = IntType(serialized_name='autoUnlockSeconds')
 
 
 class TaskDTOs(Model):
