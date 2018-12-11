e403765398b54ca09c9a0bac92085023d9b564f5
Jacob Lesser <jacob@prusikmapping.com>
2018-12-11 11:39:26 -0800

    Move tm data JOSM remote control commands to their own command

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -1346,7 +1346,10 @@
                     changeset_comment: encodeURIComponent(changesetComment),
                     changeset_source: encodeURIComponent(changesetSource),
                     new_layer: false,
-                    // Data required for TM JOSM plugin to download task data
+                };
+
+                // Data required for TM JOSM plugin to download task data
+                var taskParams = {
                     task_id: vm.getSelectTaskIds(),
                     project_id: vm.id,
                 };
@@ -1354,6 +1357,7 @@
                 if (taskCount == 1) {
                     //load OSM data and zoom to the bbox
                     editorService.sendJOSMCmd('http://127.0.0.1:8111/load_and_zoom', loadAndZoomParams);
+                    editorService.sendJOSMCmd('http://127.0.0.1:8111/load_tm_data', taskParams);
                 } else {
                     //probably too much OSM data to download, just zoom to the bbox
                     editorService.sendJOSMCmd('http://127.0.0.1:8111/zoom', loadAndZoomParams);
