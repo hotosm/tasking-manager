8afb93516cbc648b378f3fddc9159021132ffe41
Ethan Nelson <git@ethan-nelson.com>
2019-01-13 10:16:48 -0800

    Start angular controller for application keys

---
+++ client/app/profile/application-key.controller.js
@@ -0,0 +1,15 @@
+(function () {
+
+    'use strict';
+
+    /**
+     * Dashboard controller which manages the application page
+     */
+    angular
+        .module('taskingManager')
+        .controller('applicationKeyController', []);
+
+    function applicationKeyController() {
+
+    }
+})();
