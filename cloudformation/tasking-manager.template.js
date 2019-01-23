e14b557bb7a6fd1827d7c027345a573926828223
Aruna Sankaranarayanan <aruna.evam@gmail.com>
2019-01-23 16:03:21 +0530

    CloudFormation Template
    * Bug fixes in the Cloudformation
    
    * fix invalid datatypes in the template
    
    * parameterise security groups
    
    * RDS security group type is an ec2 security group
    
    * make user-data executable; sub as appropriate
    
    * fix ec2 instance policy
    
    * experiment with ec2 instance types
    
    mv from elb v2 -> elb v1
    
    also attach elb to asg
    
    shift to launch-config
    
    Add scale up, nginx in user-data and fix cfn
    
    modify user-data script with dakota's help
    
    bug fixes in cloudformation
    
    * increase workers for the c3.2x instance
    * add descriptions to parameters
    
    * Clean up template
    * Use set -x in install
    * Use a diff instance type, use threads and gevent in gunicorn
    * Reintroduce GitSha paramete
    * Experiment with an m3.medium instance type
    * Make the bash script an executable
    * Increase MaxSize on ASG
    
    Start work on moving from ELB -> ALB
    
    * Add ALB to cfn template without https
    * Change key back to mbtiles
    
    CloudFormation update after alb fix
    
    Allow only HTTPS access to the tasking manager (#1463)
    
    * remove https listener
    
    * redirect action
    
    Cycle ASG instances in a sustainable way (#1473)
    
    * update policy
    
    * add cfn-signal installer + permissions
    
    * change pause time to one hour
    
    * include cfn-signal as part of the code
    
    * formatting
    
    c5d.large
    
    mimic production capacity
    
    new env variables
    
    fixes for fresh stack
    
    create DB from scratch if required

---
+++ cloudformation/tasking-manager.template.js
@@ -0,0 +1,344 @@
+const cf = require('@mapbox/cloudfriend');
+
+const Parameters = {
+  GitSha: {
+    Type: 'String'
+  },
+  Environment: {
+    Type :'String',
+    AllowedValues: ['staging', 'production']
+  },
+  DBSnapshot: {
+    Type: 'String',
+    Description: 'Specify an RDS snapshot ID, if you want to create the DB from a snapshot.',
+    Default: ''
+  },
+  EmptyDatabase: {
+    Type: 'String',
+    Description: 'Are you creating an empty database, without using a snapshot or referencing to a remote RDS database?',
+    Default: 'false',
+    AllowedValues: ['true', 'false']
+  },
+  NewRelicLicense: {
+    Type: 'String',
+    Description: 'NEW_RELIC_LICENSE'
+  },
+  PostgresDB: {
+    Type: 'String',
+    Description: 'POSTGRES_DB'
+  },
+  PostgresEndpoint: {
+    Type: 'String',
+    Description: 'POSTGRES_ENDPOINT'
+  },
+  PostgresPassword: {
+    Type: 'String',
+    Description: 'POSTGRES_PASSWORD'
+  },
+  PostgresUser: {
+    Type: 'String',
+    Description: 'POSTGRES_USER'
+  },
+  TaskingManagerAppBaseUrl: {
+    Type: 'String',
+    Description: 'TM_APP_BASE_URL'
+  },
+  TaskingManagerConsumerKey: {
+    Description: 'TM_CONSUMER_KEY',
+    Type: 'String'
+  },
+  TaskingManagerConsumerSecret: {
+      Description: 'TM_CONSUMER_SECRET',
+      Type: 'String'
+  },
+  TaskingManagerSecret: {
+    Description: 'TM_SECRET',
+    Type: 'String'
+  },
+  TaskingManagerEmailFromAddress: {
+    Description: 'TM_EMAIL_FROM_ADDRESS',
+    Type: 'String'
+  },
+  TaskingManagerSMTPHost: {
+    Description: 'TM_SMTP_HOST environment variable',
+    Type: 'String'
+  },
+  TaskingManagerSMTPPassword: {
+    Description: 'TM_SMTP_PASSWORD environment variable',
+    Type: 'String'
+  },
+  TaskingManagerSMTPUser: {
+    Description: 'TM_SMTP_USER environment variable',
+    Type: 'String'
+  },
+  TaskingManagerSMTPPort: {
+    Description: 'TM_SMTP_PORT environment variable',
+    Type: 'String'
+  },
+  TaskingManagerLogDirectory: {
+    Description: 'TM_LOG_DIR environment variable',
+    Type: 'String'
+  },
+  DatabaseSize: {
+    Description: 'Database size in GB',
+    Type: 'String',
+    Default: '100'
+  },
+  ELBSubnets: {
+    Description: 'ELB subnets',
+    Type: 'String'
+  },
+  SSLCertificateIdentifier: {
+    Type: 'String',
+    Description: 'SSL certificate for HTTPS protocol'
+  }
+};
+
+const Conditions = {
+  UseASnapshot: cf.notEquals(cf.ref('DBSnapshot'), ''),
+  UsePostgresEndpoint: cf.notEquals(cf.ref('PostgresEndpoint'), ''),
+  IsDatabaseEmpty: cf.equals(cf.ref('EmptyDatabase'), 'true')
+};
+
+const Resources = {
+  TaskingManagerASG: {
+    DependsOn: 'TaskingManagerLaunchConfiguration',
+    Type: 'AWS::AutoScaling::AutoScalingGroup',
+    Properties: {
+      AutoScalingGroupName: cf.stackName,
+      Cooldown: 300,
+      MinSize: 3,
+      DesiredCapacity: 3,
+      MaxSize: 12,
+      HealthCheckGracePeriod: 300,
+      LaunchConfigurationName: cf.ref('TaskingManagerLaunchConfiguration'),
+      TargetGroupARNs: [ cf.ref('TaskingManagerTargetGroup') ],
+      HealthCheckType: 'EC2',
+      AvailabilityZones: cf.getAzs(cf.region)
+    },
+    UpdatePolicy: {
+      AutoScalingRollingUpdate: {
+        PauseTime: 'PT60M',
+        WaitOnResourceSignals: true
+      }
+    }
+  },
+  TaskingManagerScaleUp: {
+      Type: "AWS::AutoScaling::ScalingPolicy",
+      Properties: {
+        AutoScalingGroupName: cf.ref('TaskingManagerASG'),
+        PolicyType: 'TargetTrackingScaling',
+        TargetTrackingConfiguration: {
+          TargetValue: 85,
+          PredefinedMetricSpecification: {
+            PredefinedMetricType: 'ASGAverageCPUUtilization'
+          }
+        },
+        Cooldown: 300
+      }
+  },
+  TaskingManagerLaunchConfiguration: {
+    Type: 'AWS::AutoScaling::LaunchConfiguration',
+    Properties: {
+      IamInstanceProfile: cf.ref('TaskingManagerEC2InstanceProfile'),
+      ImageId: 'ami-0565af6e282977273',
+      InstanceType: 'c5d.large',
+      SecurityGroups: [cf.importValue(cf.join('-', ['hotosm-network-production', cf.ref('Environment'), 'ec2s-security-group', cf.region]))],
+      UserData: cf.userData([
+        '#!/bin/bash',
+        'set -x',
+        'export DEBIAN_FRONTEND=noninteractive',
+        'export LC_ALL="en_US.UTF-8"',
+        'export LC_CTYPE="en_US.UTF-8"',
+        'dpkg-reconfigure --frontend=noninteractive locales',
+        'sudo apt-get -y update',
+        'sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade',
+        'sudo add-apt-repository ppa:jonathonf/python-3.6 -y',
+        'sudo apt-get update',
+        'sudo apt-get -y install python3.6',
+        'sudo apt-get -y install python3.6-dev',
+        'sudo apt-get -y install python3.6-venv',
+        'sudo apt-get -y install curl',
+        'curl -sL https://deb.nodesource.com/setup_6.x > install-node6.sh',
+        'sudo chmod +x install-node6.sh',
+        'sudo ./install-node6.sh',
+        'sudo apt-get -y install nodejs',
+        'sudo npm install gulp -g',
+        'npm i browser-sync --save',
+        'sudo apt-get -y install postgresql-9.5',
+        'sudo apt-get -y install libpq-dev',
+        'sudo apt-get -y install postgresql-server-dev-9.5',
+        'sudo apt-get -y install libxml2',
+        'sudo apt-get -y install wget libxml2-dev',
+        'sudo apt-get -y install libgeos-3.5.0',
+        'sudo apt-get -y install libgeos-dev',
+        'sudo apt-get -y install libproj9',
+        'sudo apt-get -y install libproj-dev',
+        'sudo apt-get -y install python-pip libgdal1-dev',
+        'sudo apt-get -y install libjson-c-dev',
+        'sudo apt-get -y install git',
+        'git clone --recursive https://github.com/hotosm/tasking-manager.git',
+        'cd tasking-manager/',
+        cf.sub('git reset --hard ${GitSha}'),
+        'python3.6 -m venv ./venv',
+        '. ./venv/bin/activate',
+        'pip install --upgrade pip',
+        'pip install -r requirements.txt',
+        'echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf',
+        'export LC_ALL=C',
+        'wget https://s3.amazonaws.com/cloudformation-examples/aws-cfn-bootstrap-latest.tar.gz',
+        'pip2 install aws-cfn-bootstrap-latest.tar.gz',
+        'echo "Exporting environment variables:"',
+        cf.sub('export NEW_RELIC_LICENSE=${NewRelicLicense}'),
+        cf.join('', ['export POSTGRES_ENDPOINT=', cf.if('UsePostgresEndpoint', cf.ref('PostgresEndpoint'), cf.getAtt('TaskingManagerRDS', 'Endpoint.Address'))]),
+        cf.sub('export POSTGRES_DB=${PostgresDB}'),
+        cf.sub('export POSTGRES_PASSWORD="${PostgresPassword}"'),
+        cf.sub('export POSTGRES_USER="${PostgresUser}"'),
+        cf.sub('export TM_APP_BASE_URL="${TaskingManagerAppBaseUrl}"'),
+        cf.sub('export TM_CONSUMER_KEY="${TaskingManagerConsumerKey}"'),
+        cf.sub('export TM_CONSUMER_SECRET="${TaskingManagerConsumerSecret}"'),
+        cf.sub('export TM_EMAIL_FROM_ADDRESS="${TaskingManagerEmailFromAddress}"'),
+        cf.sub('export TM_LOG_DIR="${TaskingManagerLogDirectory}"'),
+        cf.sub('export TM_SECRET="${TaskingManagerSecret}"'),
+        cf.sub('export TM_SMTP_HOST="${TaskingManagerSMTPHost}"'),
+        cf.sub('export TM_SMTP_PASSWORD="${TaskingManagerSMTPPassword}"'),
+        cf.sub('export TM_SMTP_PORT="${TaskingManagerSMTPPort}"'),
+        cf.sub('export TM_SMTP_USER="${TaskingManagerSMTPUser}"'),
+        cf.if('IsDatabaseEmpty', 'psql "postgresql://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_ENDPOINT/$POSTGRES_DB" -f ./tests/database/tasking-manager.sql', ''),
+        './venv/bin/python3.6 manage.py db upgrade',
+        'cd client/',
+        'npm install',
+        'gulp build',
+        'cd ../',
+        'echo "------------------------------------------------------------"',
+        'gunicorn -b 0.0.0.0:8000 --worker-class gevent --workers 3 --threads 3 --timeout 179 manage:application &',
+        cf.sub('cfn-signal --exit-code $? --region ${AWS::Region} --resource TaskingManagerASG --stack ${AWS::StackName}')
+      ]),
+      KeyName: 'mbtiles'
+    }
+  },
+  TaskingManagerEC2Role: {
+    Type: 'AWS::IAM::Role',
+    Properties: {
+      AssumeRolePolicyDocument: {
+        Version: "2012-10-17",
+        Statement: [{
+          Effect: "Allow",
+          Principal: {
+             Service: [ "ec2.amazonaws.com" ]
+          },
+          Action: [ "sts:AssumeRole" ]
+        }]
+      },
+      Policies: [{
+        PolicyName: "RDSPolicy",
+        PolicyDocument: {
+          Version: "2012-10-17",
+          Statement:[{
+            Action: ['rds:DescribeDBInstances'],
+            Effect: 'Allow',
+            Resource: ['arn:aws:rds:*']
+          }]
+        }
+      }, {
+        PolicyName: "CloudFormationPermissions",
+        PolicyDocument: {
+          Version: "2012-10-17",
+          Statement:[{
+            Action: [
+              'cloudformation:SignalResource',
+              'cloudformation:DescribeStackResource'
+            ],
+            Effect: 'Allow',
+            Resource: ['arn:aws:cloudformation:*']
+          }]
+        }
+      }],
+      RoleName: cf.join('-', [cf.stackName, 'ec2', 'role'])
+    }
+  },
+  TaskingManagerEC2InstanceProfile: {
+     Type: "AWS::IAM::InstanceProfile",
+     Properties: {
+        Roles: [cf.ref('TaskingManagerEC2Role')],
+        InstanceProfileName: cf.join('-', [cf.stackName, 'ec2', 'instance', 'profile'])
+     }
+  },
+  TaskingManagerLoadBalancer: {
+    Type: 'AWS::ElasticLoadBalancingV2::LoadBalancer',
+    Properties: {
+      Name: cf.stackName,
+      SecurityGroups: [cf.importValue(cf.join('-', ['hotosm-network-production', cf.ref('Environment'), 'elbs-security-group', cf.region]))],
+      Subnets: cf.split(',', cf.ref('ELBSubnets')),
+      Type: 'application'
+    }
+  },
+  TaskingManagerTargetGroup: {
+    Type: 'AWS::ElasticLoadBalancingV2::TargetGroup',
+    Properties: {
+      HealthCheckIntervalSeconds: 60,
+      HealthCheckPort: 8000,
+      HealthCheckProtocol: 'HTTP',
+      HealthCheckTimeoutSeconds: 10,
+      HealthyThresholdCount: 3,
+      UnhealthyThresholdCount: 3,
+      Port: 8000,
+      Protocol: 'HTTP',
+      VpcId: cf.importValue(cf.join('-', ['hotosm-network-production', 'default-vpc', cf.region])),
+      Matcher: {
+        HttpCode: '200,202,302,304'
+      }
+    }
+  },
+  TaskingManagerLoadBalancerHTTPSListener: {
+    Type: 'AWS::ElasticLoadBalancingV2::Listener',
+    Properties: {
+      Certificates: [ {
+        CertificateArn: cf.arn('acm', cf.ref('SSLCertificateIdentifier'))
+      }],
+      DefaultActions: [{
+        Type: 'forward',
+        TargetGroupArn: cf.ref('TaskingManagerTargetGroup')
+      }],
+      LoadBalancerArn: cf.ref('TaskingManagerLoadBalancer'),
+      Port: 443,
+      Protocol: 'HTTPS'
+    }
+  },
+  TaskingManagerLoadBalancerHTTPListener: {
+    Type: 'AWS::ElasticLoadBalancingV2::Listener',
+    Properties: {
+      DefaultActions: [{
+        Type: 'redirect',
+        RedirectConfig: {
+          Protocol: 'HTTPS',
+          Port: '443',
+          Host: '#{host}',
+          Path: '/#{path}',
+          Query: '#{query}',
+          StatusCode: 'HTTP_301'
+        }
+      }],
+      LoadBalancerArn: cf.ref('TaskingManagerLoadBalancer'),
+      Port: 80,
+      Protocol: 'HTTP'
+    }
+  },
+  TaskingManagerRDS: {
+    Type: 'AWS::RDS::DBInstance',
+    Properties: {
+        Engine: 'postgres',
+        DBName: cf.if('UseASnapshot', cf.noValue, cf.ref('PostgresDB')),
+        EngineVersion: '9.5.15',
+        MasterUsername: cf.if('UseASnapshot', cf.noValue, cf.ref('PostgresUser')),
+        MasterUserPassword: cf.if('UseASnapshot', cf.noValue, cf.ref('PostgresPassword')),
+        AllocatedStorage: cf.ref('DatabaseSize'),
+        StorageType: 'gp2',
+        DBInstanceClass: 'db.m3.large', //rethink here
+        DBSnapshotIdentifier: cf.if('UseASnapshot', cf.ref('DBSnapshot'), cf.noValue),
+        VPCSecurityGroups: [cf.importValue(cf.join('-', ['hotosm-network-production', cf.ref('Environment'), 'ec2s-security-group', cf.region]))],
+    }
+  }
+};
+
+module.exports = { Parameters, Resources, Conditions }
