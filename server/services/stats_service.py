94d8a8a2955f4351537ca77132a6bae374ffd6f1
Ramya Ragupathy <r.ragupathy85@gmail.com>
2019-02-21 09:32:29 +0530

    #1147 Update task_status for validated

--- server/services/stats_service.py
+++ server/services/stats_service.py
@@ -209,19 +209,22 @@ class StatsService:
 
         tasks_mapped_area = 0
         tasks_mapped_sql = """select sum(ST_Area(geometry)) from public.tasks where task_status = 2"""
-        tasks_mapped_result = db.engine.execute(tasks_mapped_result)
+        tasks_mapped_result = db.engine.execute(tasks_mapped_sql)
+        current_app.logger.debug(tasks_mapped_result)
         for rowproxy in tasks_mapped_result:
             for tup in rowproxy:
-                tasks_mapped_area += tup[1]
+                current_app.logger.debug(tup)
+                tasks_mapped_area += tup
                 current_app.logger.debug(tasks_mapped_area)
         dto.total_mapped_area = tasks_mapped_area
 
         tasks_validated_area = 0
-        tasks_validated_sql = """select sum(ST_Area(geometry)) from public.tasks where task_status = 3"""
-        tasks_validated_result = db.engine.execute(tasks_validated_result)
+        tasks_validated_sql = """select sum(ST_Area(geometry)) from public.tasks where task_status = 4"""
+        tasks_validated_result = db.engine.execute(tasks_validated_sql)
+        current_app.logger.debug(tasks_validated_result)
         for rowproxy in tasks_validated_result:
             for tup in rowproxy:
-                tasks_validated_area += tup[1]
+                tasks_validated_area += tup
                 current_app.logger.debug(tasks_validated_area)
         dto.total_validated_area = tasks_validated_area
     
