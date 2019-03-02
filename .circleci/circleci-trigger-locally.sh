90687b2d4467eb92a41f98568ee59fe2bd90f213
John Willis <john.willis@livingmap.com>
2019-03-02 17:54:09 +0000

    CIRCLECI: Add helper script to test local changes to build config.
    Add circleci-trigger-locally.sh that can send your local version of
    the .circleci/config.yml to the remote build server.
    
    Use this to test tweaks to the config file without needing to push
    upstream until you have a stable setup.
    
    Ideal for funky new Circle CI/CD hacking about.
    
    Note: You will need a personal API key from CircleCI exported in
    your local environment for this to work and it should be run within
    your local GIT repo with origin/master pointing to the correct upstream.
    
    See: https://circleci.com/docs/2.0/managing-api-tokens/#creating-a-personal-api-token

---
+++ .circleci/circleci-trigger-locally.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+CIRCLE_UPSTREAM_COMMIT="$(git rev-parse origin/master)"
+GIT_UPSTREAM_ADDRESS="$(git config --get remote.origin.url)"
+
+echo "This script will run your LOCAL version of .circleci/config.yml on the remote build server."
+echo "It will run the config against the latest commit on the upstream master branch used for CircleCI."
+echo ""
+echo "Run from within the .circleci folder in your local repo as ./circleci-trigger-locally.sh"
+echo ""
+echo "To use you must export your CircleCI personal API key token and it has to have permission"
+echo "to trigger the build."
+echo ""
+echo "https://circleci.com/docs/2.0/managing-api-tokens/#creating-a-personal-api-token"
+echo ""
+echo "Example: export CIRCLE_TOKEN=<token-from-link-above>"
+echo ""
+echo "Running against ${CIRCLE_UPSTREAM_COMMIT} on ${GIT_UPSTREAM_ADDRESS}"
+echo ""
+
+case ${CIRCLE_TOKEN-} in '') echo "$0: FATAL ERROR: CIRCLE_TOKEN must be set correctly in your environment." >&2; exit 1;; esac
+
+curl --user ${CIRCLE_TOKEN}: \
+  --request POST \
+  --form revision=${CIRCLE_UPSTREAM_COMMIT} \
+  --form config=@config.yml \
+  --form notify=false \
+    https://circleci.com/api/v1.1/project/github/livingmap/gis-osm-tasking-manager/tree/master
