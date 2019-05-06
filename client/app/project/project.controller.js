ddb9861e5ea91efda876240b300f41e094a37d94
Zack LaVergne <zlavergne@fb.com>
2019-05-06 15:43:09 -0600

    Added RapiD  editor to the list of editors

--- client/app/project/project.controller.js
+++ client/app/project/project.controller.js
@@ -1604,6 +1604,15 @@
             else if (editor === 'fieldpapers') {
                 editorService.launchFieldPapersEditor(center);
             }
+            else if (editor === 'rapid') {
+                editorService.launchRapidEditor(
+                    center,
+                    changesetComment,
+                    imageryUrl,
+                    vm.projectData.projectId,
+                    vm.getSelectTaskIds()
+                );
+            }
             else if (editor === 'josm') {
 
                 if (taskCount > 1) {
@@ -2106,6 +2115,12 @@
                 result.push({
                     "name": "Field Papers",
                     "value": "fieldpapers"
+                });
+            }
+            if (editors.includes("RAPID")) {
+                result.push({
+                    "name": "RapiD",
+                    "value": "rapid"
                 });
             }
             return result;
