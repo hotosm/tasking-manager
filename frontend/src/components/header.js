4f5812fdf176b1029d476f445060ca54e7b144c7
Wille Marcel <wille@wille.blog.br>
2019-06-14 23:04:40 -0300

    improve SCSS and header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -9,36 +9,40 @@ import logo from '../assets/img/main-logo.svg';
 export class Header extends React.Component {
   render() {
     return (
-      <header className="grid-12">
-          <div className="header-slogan mb-neg6 border-b border-b--2 border-b--lightgray">
+      <header>
+        <div className="grid-2 top-header mb-neg6 border-b border-b--2 border-b--lightgray">
+          <div className="header-slogan">
             <span className="txt--red txt-m special-font ml24">Mapping our world together</span>
           </div>
-          <div className="header-org-link mb-neg6 border-b border-b--2 border-b--lightgray">
-              <a className="txt--red txt-m mr24" href={`http://${ORG_URL}`}>
-                {ORG_URL} <FontAwesomeIcon icon={faExternalLinkAlt}/>
-            </a>
-          </div>
-          <div className="header-brand align-middle mt18">
-            <img src={logo} alt={`${ORG_NAME} logo`} className="ml24 align-middle"
-              style={{width: '54px'}}
+          <div className="header-org-link">
+            <a className="txt--red txt-m mr24" href={`http://${ORG_URL}`}>
+              {ORG_URL} <FontAwesomeIcon icon={faExternalLinkAlt}/>
+          </a>
+        </div>
+      </div>
+      <div className="grid-12 mt18 nav">
+        <div className="header-brand align-middle">
+          <img src={logo} alt={`${ORG_NAME} logo`} className="ml24 align-middle"
+            style={{width: '54px'}}
             />
-            <span className="tm-title ml12 txt--darkest-grey align-middle special-font">
-              Tasking Manager
-            </span>
-          </div>
-          <nav className="header-menu mt18 special-font txt--darkest-grey">
-            <ul>
-              <li>Explore projects</li>
-              <li>How it works</li>
-              <li>About</li>
-              <li>Help</li>
-            </ul>
-          </nav>
-          <div className="header-user mt18">
-            <button>Log in</button>
-            <button>Sign in</button>
-          </div>
-      </header>
+          <span className="tm-title ml12 txt--secondary align-middle special-font">
+            Tasking Manager
+          </span>
+        </div>
+        <nav className="header-menu special-font txt--secondary align-middle">
+          <ul>
+            <li>Explore projects</li>
+            <li>How it works</li>
+            <li>About</li>
+            <li>Help</li>
+          </ul>
+        </nav>
+        <div className="header-user btn-group align-r">
+          <button className="btn btn-tertiary">Log in</button>
+          <button className="btn btn-secondary">Sign in</button>
+        </div>
+      </div>
+    </header>
     );
   }
 }
