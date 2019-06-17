4a6dd339636d53aabb334ff339ff3108aabed992
Wille Marcel <wille@wille.blog.br>
2019-06-17 06:59:05 -0300

    Add views, dropdown and button components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import './assets/styles/index.scss';
+import { Route } from "react-router-dom";
 
+import './assets/styles/index.scss';
 import { Header } from './components/header';
+import { Home } from './views/home';
+import { AboutPage } from './views/about';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div>
+        <Route exact path="/" component={ Home } />
+        <Route exact path="/about" component={ AboutPage } />
+      </div>
     </div>
   );
 }
