085fb1955c6e6b39eb06ac470c783e04f8114450
Wille Marcel <wille@wille.blog.br>
2019-06-17 06:59:05 -0300

    Add views, dropdown and button components

---
+++ frontend/src/views/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export class AboutPage extends React.Component {
+  render() {
+    return(
+      <div className="pt180 pull-center">This is the About page</div>
+    );
+  }
+}
