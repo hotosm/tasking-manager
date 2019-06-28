18e4025ef39ef0d4593e8b0943f8512592e6db30
Felix Delattre <felix@delattre.de>
2019-06-28 13:32:40 +0000

    Review documentation on TM2 migration

--- devops/database/migration-from-tm2-postgres.sql
+++ devops/database/migration-from-tm2-postgres.sql
@@ -1,52 +1,52 @@
 -- SQL queries for migrating data from TM2 table structure to TM3 table structure
 -- Assumes:
---   TM2 tables are in schema "hotold" 
---   TM3 tables are in schema "hotnew"
+--   TM2 tables are in schema "tm2" 
+--   New Tasking Manager tables are in schema "taskingmanager"
 --  scripts must be run by a db role which has access to update tables and sequences in both schemas.
 
 -- USERS Initial Load
 -- make sure new tables emptied of any test data first
-truncate hotnew.users cascade;
--- truncate hotnew.areas_of_interest cascade;
+truncate taskingmanager.users cascade;
+-- truncate taskingmanager.areas_of_interest cascade;
 
 -- Populate users with ids and default stats - sets users to beginner mapper level
-insert into hotnew.users (id,username,role,mapping_level, tasks_mapped, tasks_validated, tasks_invalidated,
+insert into taskingmanager.users (id,username,role,mapping_level, tasks_mapped, tasks_validated, tasks_invalidated,
                           is_email_verified, date_registered, last_validation_date)
 (select id,username,
 	case when role is null then 0 else role end,
 	1,0,0,0, FALSE, current_timestamp, current_timestamp
-	 from hotold.users);
+	 from tm2.users);
 	 
 -- update sequence  (commented out as not needed. ID comes from OSM not from the sequence.)
--- select setval('hotnew.users_id_seq',(select max(id) from hotnew.users));
+-- select setval('taskingmanager.users_id_seq',(select max(id) from taskingmanager.users));
 
 -- LICENCES
-INSERT INTO hotnew.licenses(
+INSERT INTO taskingmanager.licenses(
             id, name, description, plain_text)
     (SELECT id, name, description, plain_text
-	from hotold.licenses);
+	from tm2.licenses);
 	
 -- update sequence
-select setval('hotnew.licenses_id_seq',(select max(id) from hotnew.licenses));
+select setval('taskingmanager.licenses_id_seq',(select max(id) from taskingmanager.licenses));
 
 -- USERS_LICENSES
-INSERT INTO hotnew.users_licenses ("user", license)
+INSERT INTO taskingmanager.users_licenses ("user", license)
      (select "user", license
-     from hotold.users_licenses);
+     from tm2.users_licenses);
 
 
 -- AREAS OF INTEREST
 
 --populate areas of interest with details from old
---insert into hotnew.areas_of_interest (id, geometry, centroid)
---  (select id, geometry, centroid from hotold.areas);
+--insert into taskingmanager.areas_of_interest (id, geometry, centroid)
+--  (select id, geometry, centroid from tm2.areas);
  
---select setval('hotnew.areas_of_interest_id_seq',(select max(id) from hotnew.areas_of_interest));
+--select setval('taskingmanager.areas_of_interest_id_seq',(select max(id) from taskingmanager.areas_of_interest));
 
 -- PROJECTS
 -- Transfer project data, all projects set to mapper level beginner
 -- Skipped projects with null author_id
-INSERT INTO hotnew.projects(
+INSERT INTO taskingmanager.projects(
             id, status, created, priority, default_locale, author_id,
             mapper_level, enforce_mapper_level, enforce_validator_role, private,
             entities_to_map, changeset_comment, due_date, imagery, josm_preset,
@@ -57,52 +57,52 @@ INSERT INTO hotnew.projects(
             p.entities_to_map, p.changeset_comment, p.due_date, p.imagery, p.josm_preset,
             p.last_update, null, '', '',
             1, 0, 0, 0, a.centroid, a.geometry
-            from hotold.project p,
-                 hotold.areas a
+            from tm2.project p,
+                 tm2.areas a
             where p.area_id = a.id
             and p.author_id is not null
             );
 
-select setval('hotnew.projects_id_seq',(select max(id) from hotnew.projects));
+select setval('taskingmanager.projects_id_seq',(select max(id) from taskingmanager.projects));
 
 -- Set the task_creation_mode to 'arbitrary' when project's zoom was None in
 -- TM2 or 'grid' when it was not None
-Update hotnew.projects
+Update taskingmanager.projects
    set task_creation_mode = 1
-  from hotold.projects as p
- where p.id = hotnew.projects.id and p.zoom is NULL;
+  from tm2.projects as p
+ where p.id = taskingmanager.projects.id and p.zoom is NULL;
 
-Update hotnew.projects
+Update taskingmanager.projects
    set task_creation_mode = 0
-  from hotold.projects as p
- where p.id = hotnew.projects.id and p.zoom is not NULL;
+  from tm2.projects as p
+ where p.id = taskingmanager.projects.id and p.zoom is not NULL;
 
 -- Project info & translations
 -- Skip any records relating to projects that have not been imported
-INSERT INTO hotnew.project_info(
+INSERT INTO taskingmanager.project_info(
             project_id, locale, name, short_description, description, instructions)
     (select id, locale, name, short_description, description, instructions
-    from hotold.project_translation pt
-    where exists(select p.id from hotnew.projects p where p.id = pt.id));
+    from tm2.project_translation pt
+    where exists(select p.id from taskingmanager.projects p where p.id = pt.id));
 
 -- Delete empty languages
-delete from hotnew.project_info where name = '' and short_description = '' and description = '' and instructions = '';
+delete from taskingmanager.project_info where name = '' and short_description = '' and description = '' and instructions = '';
 
 -- Create trigger for text search
 CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE
-ON hotnew.project_info FOR EACH ROW EXECUTE PROCEDURE
+ON taskingmanager.project_info FOR EACH ROW EXECUTE PROCEDURE
 tsvector_update_trigger(text_searchable, 'pg_catalog.english', project_id_str, short_description, description);
 
 -- set project-id which will update text search index
-update hotnew.project_info set project_id_str = project_id::text;
+update taskingmanager.project_info set project_id_str = project_id::text;
 
-CREATE INDEX textsearch_idx ON hotnew.project_info USING GIN (text_searchable);
+CREATE INDEX textsearch_idx ON taskingmanager.project_info USING GIN (text_searchable);
 
 -- TASKS
 -- Get all tasks that don't have a state of -1 (removed) and where they relate to a project that has been migrated above
 -- default any null x, y values to -1
 -- default any null zoom levels to 13
-INSERT INTO hotnew.tasks(
+INSERT INTO taskingmanager.tasks(
             id, project_id, x, y, zoom, geometry, task_status)
     (SELECT t.id, t.project_id, 
 	t.x, 
@@ -110,69 +110,69 @@ INSERT INTO hotnew.tasks(
     t.zoom, 
     t.geometry,
 	0
-    from hotold.task t
-    where not exists(select id from hotold.task_state ts where ts.task_id = t.id and ts.project_id = t.project_id and ts.state = -1)
-    and exists(select id from hotnew.projects p where p.id = t.project_id) );
+    from tm2.task t
+    where not exists(select id from tm2.task_state ts where ts.task_id = t.id and ts.project_id = t.project_id and ts.state = -1)
+    and exists(select id from taskingmanager.projects p where p.id = t.project_id) );
 
 -- Copy across per-task-instructions
-update hotnew.project_info p
+update taskingmanager.project_info p
    set per_task_instructions = old.per_task_instructions
-  from (select id, locale, per_task_instructions from hotold.project_translation
+  from (select id, locale, per_task_instructions from tm2.project_translation
          where  length(per_task_instructions) > 5) old
   where project_id = old.id
     and p.locale = old.locale;
 
 -- Update tasks with "Done" task_status and mapped_by info
-update hotnew.tasks nt
+update taskingmanager.tasks nt
 	set task_status = 2,
 	mapped_by = val.user_id
     from 
-		(select * from hotold.task_state where state = 2) as val
+		(select * from tm2.task_state where state = 2) as val
 	where val.task_id = nt.id
 	and val.project_id = nt.project_id;
 
 -- Update tasks with validated task_status and validated_by info from old task_state tables
 --  Note, old task_status validated = 3;  new task status validated = 4
-update hotnew.tasks nt
+update taskingmanager.tasks nt
 	set task_status = 4,
 	validated_by = val.user_id
     from 
-		(select * from hotold.task_state where state = 3) as val
+		(select * from tm2.task_state where state = 3) as val
 	where val.task_id = nt.id
 	and val.project_id = nt.project_id;
 
 -- Update PROJECT with task stats.  Don't have info on bad-imagery
-update hotnew.projects p
-  set total_tasks = (select count(id) from hotnew.tasks t where t.project_id = p.id);
---  tasks_mapped = (select count(id) from hotnew.tasks t where t.project_id = p.id and task_status in (2,4)),
---  tasks_validated = (select count(id) from hotnew.tasks t where t.project_id = p.id and task_status = 4);
+update taskingmanager.projects p
+  set total_tasks = (select count(id) from taskingmanager.tasks t where t.project_id = p.id);
+--  tasks_mapped = (select count(id) from taskingmanager.tasks t where t.project_id = p.id and task_status in (2,4)),
+--  tasks_validated = (select count(id) from taskingmanager.tasks t where t.project_id = p.id and task_status = 4);
 
 -- update tasks mapped count
-UPDATE hotnew.projects
+UPDATE taskingmanager.projects
 SET tasks_mapped=subquery.count
 FROM (
   select project_id, count(project_id)
-  from hotnew.tasks
-  where hotnew.tasks.task_status in (2, 4)
+  from taskingmanager.tasks
+  where taskingmanager.tasks.task_status in (2, 4)
   group by tasks.project_id) AS subquery
-WHERE hotnew.projects.id=subquery.project_id
+WHERE taskingmanager.projects.id=subquery.project_id
 ;
 
-UPDATE hotnew.projects
+UPDATE taskingmanager.projects
 SET tasks_validated=subquery.count
 FROM (
   select project_id, count(project_id)
-  from hotnew.tasks
+  from taskingmanager.tasks
   where tasks.task_status = 4
   group by tasks.project_id) AS subquery
-WHERE hotnew.projects.id=subquery.project_id
+WHERE taskingmanager.projects.id=subquery.project_id
 ;
 
 
 -- TASK HISTORY
 --  State Changes
 --   only insert state changes where user_id exists, and only for tasks that have been migrated	
-INSERT INTO hotnew.task_history(
+INSERT INTO taskingmanager.task_history(
             project_id, task_id, action, action_text, action_date, user_id)
     (SELECT project_id, task_id, 'STATE_CHANGE', 
 	CASE state 
@@ -183,44 +183,44 @@ INSERT INTO hotnew.task_history(
 	end,
 	date, 
 	user_id
-	from hotold.task_state ts
+	from tm2.task_state ts
 	where user_id is not null
-	and exists(select id from hotnew.tasks t where t.project_id = ts.project_id and t.id = ts.task_id ));
+	and exists(select id from taskingmanager.tasks t where t.project_id = ts.project_id and t.id = ts.task_id ));
 
 --  Locking
 --   assuming all the lock events in the old system are locked_for_mapping events not for validation
 --   not attempting to calculate the length of time task locked
 --   only insert lock events where user_id exists, and only for tasks that have been migrated	
-INSERT INTO hotnew.task_history(
+INSERT INTO taskingmanager.task_history(
             project_id, task_id, action, action_text, action_date, user_id)
     (SELECT project_id, task_id, 'LOCKED_FOR_MAPPING', 
 	'',
 	date, 
 	user_id
-	from hotold.task_lock ts
+	from tm2.task_lock ts
 	where user_id is not null
 	and lock = true
-	and exists(select id from hotnew.tasks t where t.project_id = ts.project_id and t.id = ts.task_id ));
+	and exists(select id from taskingmanager.tasks t where t.project_id = ts.project_id and t.id = ts.task_id ));
 
 --  Comments
 --   only insert comments where author_id exists, and only for tasks that have been migrated	
-INSERT INTO hotnew.task_history(
+INSERT INTO taskingmanager.task_history(
             project_id, task_id, action, action_text, action_date, user_id)
     (SELECT project_id, task_id, 'COMMENT', 
 	comment,
 	date, 
 	author_id
-	from hotold.task_comment tc
+	from tm2.task_comment tc
 	where author_id is not null
-	and exists(select id from hotnew.tasks t where t.project_id = tc.project_id and t.id = tc.task_id ));
+	and exists(select id from taskingmanager.tasks t where t.project_id = tc.project_id and t.id = tc.task_id ));
 
 
 -- Update date registered based on first contribution in task_history, should cover 90% of users
-update hotnew.users
+update taskingmanager.users
    set date_registered = action_date
    from (select t.user_id, min(action_date) action_date
-           from hotnew.users u,
-                hotnew.task_history t
+           from taskingmanager.users u,
+                taskingmanager.task_history t
           where u.id = t.user_id
           group by user_id) old
  where id = old.user_id;
@@ -231,27 +231,27 @@ update hotnew.users
 with 
    m as
      (select user_id, count(id) as mapped
-	from hotnew.task_history
+	from taskingmanager.task_history
 	where action = 'STATE_CHANGE' 
 	and action_text = 'MAPPED'
 	group by user_id), 
    v as 
      (select user_id, count(id) as validated
-	from hotnew.task_history
+	from taskingmanager.task_history
 	where action = 'STATE_CHANGE'
 	and action_text = 'VALIDATED'
 	group by user_id),
    i as
      (select user_id, count(id) as invalidated
-	from hotnew.task_history
+	from taskingmanager.task_history
 	where action = 'STATE_CHANGE'
 	and action_text = 'INVALIDATED'
 	group by user_id)
-update hotnew.users us
+update taskingmanager.users us
    set tasks_mapped = coalesce(m.mapped,0),
    tasks_validated = coalesce(v.validated,0), 
    tasks_invalidated = coalesce(i.invalidated,0)
-from hotnew.users u
+from taskingmanager.users u
   left join m on m.user_id = u.id
   left join v on v.user_id = u.id
   left join i on i.user_id = u.id
@@ -260,10 +260,10 @@ from hotnew.users u
 -- User Project List 
 with p as
   (select user_id, array_agg(distinct project_id) as projects
-	from hotnew.task_history
+	from taskingmanager.task_history
 	where action = 'STATE_CHANGE'
 	group by user_id)
-update hotnew.users u
+update taskingmanager.users u
   set projects_mapped = p.projects
 from p
 where u.id = p.user_id;	
@@ -271,41 +271,41 @@ where u.id = p.user_id;
 
 -- MESSAGES
 -- only migrating messages that have not yet been read
-INSERT INTO hotnew.messages(
+INSERT INTO taskingmanager.messages(
             message, subject, from_user_id, to_user_id, date, read)
     (select message, subject,  from_user_id, to_user_id, date, read
-     from hotold.message
+     from tm2.message
      where read = false);
 
 	 
 -- PRIORITY_AREAS
 --  migrate all areas
-INSERT INTO hotnew.priority_areas(
+INSERT INTO taskingmanager.priority_areas(
             id, geometry)
     (SELECT id, geometry
-    from hotold.priority_area);
+    from tm2.priority_area);
 
 -- Update sequence
-select setval('hotnew.priority_areas_id_seq',(select max(id) from hotnew.priority_areas));
+select setval('taskingmanager.priority_areas_id_seq',(select max(id) from taskingmanager.priority_areas));
 
 -- Migrate project_priority areas link but only where a matching project exists.  
 -- Remove duplicate records
-INSERT INTO hotnew.project_priority_areas(
+INSERT INTO taskingmanager.project_priority_areas(
             project_id, priority_area_id)
     (SELECT distinct pa.project_id, pa.priority_area_id
-    from hotold.project_priority_areas pa
-    where exists(select null from hotnew.projects p where p.id = pa.project_id) );
+    from tm2.project_priority_areas pa
+    where exists(select null from taskingmanager.projects p where p.id = pa.project_id) );
 
 -- PROJECT ALLOWED USERS
 -- Remove duplicate records
-INSERT INTO hotnew.project_allowed_users(
+INSERT INTO taskingmanager.project_allowed_users(
             project_id, user_id)
     (select distinct project_id, user_id
-    from hotold.project_allowed_users);
+    from tm2.project_allowed_users);
 
 -- TASK ISSQUARE FLAG
 -- Ensure the is_sqaure flag is consistent with the x,y,zoom values
-UPDATE hotnew.tasks SET is_square = (x IS NOT NULL AND y IS NOT NULL AND zoom IS NOT NULL);
+UPDATE taskingmanager.tasks SET is_square = (x IS NOT NULL AND y IS NOT NULL AND zoom IS NOT NULL);
 
 
 --------------------------------------------------	
