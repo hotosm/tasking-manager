4c17cdb0b6e6603f2674b918f6fd4450fe0b0665
Wille Marcel <wille.yyz@gmail.com>
2019-07-03 10:22:49 -0300

    replace FontAwesome icons by local icons

--- frontend/src/components/tests/button.test.js
+++ frontend/src/components/tests/button.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 
 import { Button } from '../button';
 
@@ -21,18 +20,4 @@ it('children and onClick props of Button', () => {
     {'className': 'black bg-white br1 f5 bn pointer'}
   ).props.onClick();
   expect(testVar).toEqual(true);
-});
-
-it('icon prop of Button', () => {
-  const testButton = TestRenderer.create(
-    <Button className="black bg-white" icon={faExternalLinkAlt}>
-      Test it
-    </Button>
-  );
-  const buttonInstance = testButton.root;
-  expect(
-    buttonInstance.findByProps(
-      {'className': 'black bg-white br1 f5 bn pointer'}
-    ).children[1].props.icon
-  ).toEqual(faExternalLinkAlt);
 });
