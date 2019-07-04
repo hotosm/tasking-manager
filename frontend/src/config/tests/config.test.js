2715a6c8c86f71304c24682acbd55d7e638d18f8
Wille Marcel <wille.yyz@gmail.com>
2019-07-04 17:04:34 -0300

    add footer

--- frontend/src/config/tests/config.test.js
+++ frontend/src/config/tests/config.test.js
@@ -13,6 +13,21 @@ it('exports ORG_NAME', () => {
 it('exports ORG_CODE', () => {
   expect(typeof(config.ORG_CODE)).toBe('string');
 });
+it('exports ORG_TWITTER', () => {
+  expect(typeof(config.ORG_TWITTER)).toBe('string');
+});
+it('exports ORG_FB', () => {
+  expect(typeof(config.ORG_FB)).toBe('string');
+});
+it('exports ORG_INSTAGRAM', () => {
+  expect(typeof(config.ORG_INSTAGRAM)).toBe('string');
+});
+it('exports ORG_YOUTUBE', () => {
+  expect(typeof(config.ORG_YOUTUBE)).toBe('string');
+});
+it('exports ORG_GITHUB', () => {
+  expect(typeof(config.ORG_GITHUB)).toBe('string');
+});
 it('exports DEFAULT locale to be english', () => {
   expect(config.DEFAULT_LOCALE).toBe('en');
 });
