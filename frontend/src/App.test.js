2adebbfd02ce570be4bb52abd0024a48f8fe6ee4
Wille Marcel <wille.yyz@gmail.com>
2019-07-05 16:17:22 -0300

    add  StatsSection

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom';
 import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createBrowserHistory } from "history";
-import { store } from './store';
+import {IntlProvider} from 'react-intl';
 
+import { store } from './store';
 import App from './App';
 
 const history = createBrowserHistory();
@@ -15,7 +16,9 @@ it('renders without crashing', () => {
   ReactDOM.render(
     <Provider store={store}>
       <Router history={history}>
-        <App />()
+        <IntlProvider locale='en'>
+          <App />
+        </IntlProvider>
       </Router>
     </Provider>,
     div
