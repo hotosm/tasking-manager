9ebdc10ee169fdd0558c8a779b6a623960d31b12
Wille Marcel <wille@wille.blog.br>
2019-07-16 10:38:45 -0300

    add testimonials section
    Update frontend/src/assets/styles/_extra.scss
    
    Co-Authored-By: Thad Kerosky <contact@thadk.net>
    
    Update frontend/src/assets/styles/_extra.scss
    
    Co-Authored-By: Thad Kerosky <contact@thadk.net>

--- frontend/src/components/homepage/whoIsMapping.js
+++ frontend/src/components/homepage/whoIsMapping.js
@@ -9,7 +9,7 @@ import { Button } from '../button';
 function contactCard() {
   return(
     <div className="w-25-l w-100 fl pv2 ph3 bg-white blue-dark shadow-4">
-      <h3 className="ma1 f3 pb3 fw8 w-70-l w-60-m ttu barlow-condensed">
+      <h3 className="ma1 pb3 w-70-l w-60-m f3 fw8 ttu barlow-condensed">
         <FormattedMessage {...messages.organizationContactTitle} />
       </h3>
       <div className="tc cf mb2">
@@ -36,22 +36,22 @@ export function WhoIsMapping() {
     {url: 'https://www.unicef.org/', name: 'unicef'},
     {url: 'http://aws.amazon.com/', name: 'aws'},
     {url: 'https://www.msf.org/', name: 'msf'}
-  ]
+  ];
   return(
-    <div className="cf v-mid bg-blue-dark bg-split-blue-white">
-      <div className="ph6-l ph4 pt3 pb5 pb white cf">
+    <div className="cf v-mid bg-split-blue-white">
+      <div className="ph6-l ph4 pt3 pb5 white cf">
         <div className="bg-red mw5">
-          <h3 className="pa1 lh-copy f2 tc fw8 white ttu barlow-condensed">
+          <h3 className="pa1 f2 fw8 tc lh-copy white ttu barlow-condensed">
             <FormattedMessage {...messages.whoIsMappingTitle} />
           </h3>
         </div>
         <div className="w-75-l w-100 fl">
-          <p className="pr2 f5 f4-ns mw6 lh-title mb4">
+          <p className="pr2 mb4 f5 f4-ns lh-title mw6">
             <FormattedMessage {...messages.whoIsMappingHeadline} />
           </p>
           <div className="cf pb4 pb2-l">
             {organizations.map((org, n) =>
-              <div key={n} className="w-third-l fl pr5 pv4 w-50">
+              <div key={n} className="w-50 w-third-l fl pr5 pv4">
                 <a href={org.url}>
                   <div className={`contain org-${org.name} w-auto h3`}></div>
                 </a>
@@ -61,7 +61,6 @@ export function WhoIsMapping() {
         </div>
         {contactCard()}
       </div>
-
     </div>
   );
 }
