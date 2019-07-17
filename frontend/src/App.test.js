3b3c968ecc1d7fd3423ef1b98a3ac4cae2139b35
Wille Marcel <wille.yyz@gmail.com>
2019-07-17 20:14:56 -0300

    replace react-router by reach-router and change "sign in" button to "sign up"

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router } from "react-router-dom";
 import { Provider } from "react-redux";
-import { createBrowserHistory } from "history";
 import {IntlProvider} from 'react-intl';
 
 import { store } from './store';
 import App from './App';
-
-const history = createBrowserHistory();
 
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
     <Provider store={store}>
-      <Router history={history}>
-        <IntlProvider locale='en'>
-          <App />
-        </IntlProvider>
-      </Router>
+      <IntlProvider locale='en'>
+        <App />
+      </IntlProvider>
     </Provider>,
     div
   );
