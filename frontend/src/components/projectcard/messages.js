24b2fe28498b80fc001e7b7653861be5563c8d9b
Thad Kerosky <contact@thadk.net>
2019-07-18 01:59:43 -0400

    show static featured ProjectCards, ready for large displays

---
+++ frontend/src/components/projectcard/messages.js
@@ -0,0 +1,11 @@
+import { defineMessages } from 'react-intl';
+
+/**
+ * Internationalized messages for use on project cards.
+ */
+export default defineMessages({
+  projectTitle: {
+    id: 'project.mainSection.title',
+    defaultMessage: 'Generic project'
+  },
+});
