9404b8edd1f734f195c53c27c9ea3802c9243272
Wille Marcel <wille.yyz@gmail.com>
2019-07-26 09:15:44 -0300

    Add authentication views

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,17 @@
-import { createStore, compose } from "redux";
+import { applyMiddleware,  createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import { Map } from 'immutable';
 
 import * as safeStorage from '../utils/safe_storage';
 import reducers from "./reducers";
 
 
 const persistedState = {
-  auth: {userDetails: {
-     id: safeStorage.getItem('username'),
-     username: safeStorage.getItem('username'),
-     token: safeStorage.getItem('token'),
-  }},
+  auth: Map({userDetails: Map({
+    username: safeStorage.getItem('username'),
+  }),
+  token: safeStorage.getItem('token'),
+}),
   preferences: {
     language: safeStorage.getItem('language'),
   }
@@ -25,7 +27,7 @@ if (process.env.NODE_ENV === 'development') {
   }
 }
 
-const composedEnhancers = compose(...enhancers);
+const composedEnhancers = compose(applyMiddleware(thunk), ...enhancers);
 
 const store = createStore(
   reducers,
