95fcde024f2a8e68b32295ef66dfbed04518605c
Felix Delattre <felix@delattre.de>
2019-07-30 19:14:11 +0000

    Add code checks for backend migrations

--- migrations/versions/451f6bd05a19_.py
+++ migrations/versions/451f6bd05a19_.py
@@ -6,18 +6,23 @@ Create Date: 2019-07-02 17:03:55.567940
 
 """
 from alembic import op
-import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '451f6bd05a19'
-down_revision = 'a43b9748ceee'
+revision = "451f6bd05a19"
+down_revision = "a43b9748ceee"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.execute('DROP TRIGGER IF EXISTS tsvectorupdate ON project_info; CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE ON project_info FOR EACH ROW EXECUTE PROCEDURE tsvector_update_trigger(text_searchable, "pg_catalog.english", project_id_str, short_description, description)')
+    op.execute(
+        """
+        DROP TRIGGER IF EXISTS tsvectorupdate ON project_info;
+        CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE ON project_info FOR EACH ROW EXECUTE PROCEDURE
+        tsvector_update_trigger(text_searchable, "pg_catalog.english", project_id_str, short_description, description)
+        """
+    )
 
 
 def downgrade():
