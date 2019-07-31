604271cd8b58789028d5bd65aa7ccc806dcb5e32
Wille Marcel <wille.yyz@gmail.com>
2019-07-31 19:23:07 -0300

    improve total contributors line + code linting

--- frontend/src/components/homepage/featuredProjects.js
+++ frontend/src/components/homepage/featuredProjects.js
@@ -1,17 +1,15 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
 import { FormattedMessage} from 'react-intl';
-import { RightIcon, LeftIcon } from '../svgIcons';
-import { ProjectCard } from '../../components/projectcard/projectCard';
-import { useEffect, useReducer } from 'react';
-import { API_URL } from '../../config';
 import axios from 'axios';
-
 import ReactPlaceholder from 'react-placeholder';
 import "react-placeholder/lib/reactPlaceholder.css";
 
-import cards from '../projectcard/demoProjectCardsData';
+import { RightIcon, LeftIcon } from '../svgIcons';
+import { ProjectCard } from '../../components/projectcard/projectCard';
+import { API_URL } from '../../config';
 
 import messages from './messages';
+
 
 const dataFetchReducer = (state, action) => {
   switch (action.type) {
@@ -65,7 +63,7 @@ function FeaturedProjectPaginateArrows({pages, activeProjectCardPage, mobile, di
     let enableRight = false;
     if (activeProjectCardPage !== 0) {
         enableLeft = true;
-    } 
+    }
     if (pages.length - 1 > activeProjectCardPage && pages.length !== 1) {
         enableRight = true;
     }
@@ -85,80 +83,91 @@ function FeaturedProjectPaginateArrows({pages, activeProjectCardPage, mobile, di
 }
 
 const chunkArray = chunkSize => array => {
-    return array.reduce((acc, each, index, src) => {
-        if (!(index % chunkSize)) { 
-            return [...acc, src.slice(index, index + chunkSize)];
-        } 
-        return acc;
-        },
-    []);
+  return array.reduce((acc, each, index, src) => {
+    if (!(index % chunkSize)) {
+      return [...acc, src.slice(index, index + chunkSize)];
+    }
+    return acc;
+    },
+  []);
 }
 const projectPaginate = chunkArray(4);
 const projectPaginateMobile = chunkArray(2);
 
 
 export function FeaturedProjects() {
-  const initialData = {mapResults:{features: [],type:"FeatureCollection"}, results:[], pagination: {hasNext: false, hasPrev: false, page: 1}};
-  const [state, dispatch] = useFeaturedProjectAPI(initialData)
+  const initialData = {
+    mapResults: {
+      features: [],
+      type:"FeatureCollection"
+    },
+    results:[],
+    pagination: {hasNext: false, hasPrev: false, page: 1}
+  };
+  const [state, dispatch] = useFeaturedProjectAPI(initialData);
 
-  const apiResults = state.projects && state.projects.results
-  const pagedProjs = projectPaginate(apiResults)
-  const pagedProjsMobile = projectPaginateMobile(apiResults)
+  const apiResults = state.projects && state.projects.results;
+  const pagedProjs = projectPaginate(apiResults);
+  const pagedProjsMobile = projectPaginateMobile(apiResults);
 
   return(
     <section className="pt4-l pb5 pl5-l pr1-l pl3 bg-white black">
       <div className="cf">
         <div className="w-75-l w-60 fl">
-        <h3 className="f2 ttu barlow-condensed fw8">
+          <h3 className="f2 ttu barlow-condensed fw8">
             <FormattedMessage {...messages.featuredProjects} />
-            </h3>
+          </h3>
         </div>
         <div className="fl w-25-l pa3 mb4 mw6 dn db-l">
           {!state.isLoading &&
-            <FeaturedProjectPaginateArrows 
-                pages={pagedProjs}
-                activeProjectCardPage={state.activeProjectCardPage}
-                mobile={false}
-                dispatch={dispatch} />
-              }
+            <FeaturedProjectPaginateArrows
+              pages={pagedProjs}
+              activeProjectCardPage={state.activeProjectCardPage}
+              mobile={false}
+              dispatch={dispatch}
+            />
+          }
         </div>
         <div className="fl w-40 pa3 mb4 mw6 db dn-l">
-            { !state.isLoading && <FeaturedProjectPaginateArrows 
-                pages={pagedProjsMobile}
-                mobile={true}
-                activeProjectCardPage={state.activeProjectCardPageMobile}
-                dispatch={dispatch} /> }
+          { !state.isLoading &&
+            <FeaturedProjectPaginateArrows
+              pages={pagedProjsMobile}
+              mobile={true}
+              activeProjectCardPage={state.activeProjectCardPageMobile}
+              dispatch={dispatch}
+            />
+          }
         </div>
       </div>
-        {state.isError ? (
-          <div class="bg-tan">Error loading the featured projects.</div>
-        ) : null}
-        <div className="cf dn db-l">
-          <ReactPlaceholder type='media' rows={10} ready={!state.isLoading}>
-          <FeaturedProjectCards 
+      {state.isError ? (
+        <div class="bg-tan">Error loading the featured projects.</div>
+      ) : null}
+      <div className="cf dn db-l">
+        <ReactPlaceholder type='media' rows={10} ready={!state.isLoading}>
+          <FeaturedProjectCards
             pageOfCards={pagedProjs}
             pageNum={state.activeProjectCardPage}
-            ready={!state.isLoading} />
-          </ReactPlaceholder> 
-        </div>
-        <div className="cf db dn-l">
-          <ReactPlaceholder type='media' rows={10} ready={!state.isLoading}>
-          <FeaturedProjectCards 
+            ready={!state.isLoading}
+          />
+        </ReactPlaceholder>
+      </div>
+      <div className="cf db dn-l">
+        <ReactPlaceholder type='media' rows={10} ready={!state.isLoading}>
+          <FeaturedProjectCards
             pageOfCards={pagedProjsMobile}
             pageNum={state.activeProjectCardPageMobile}
-             />
-          </ReactPlaceholder>
-        </div>
+          />
+        </ReactPlaceholder>
+      </div>
     </section>
   );
 }
 
 function FeaturedProjectCards({pageOfCards, pageNum}: Object) {
   if (pageOfCards && pageOfCards.length === 0) {
-    return null
-  } 
-  return pageOfCards[pageNum].map((card, n) => <ProjectCard { ...card } key={n} />)
-
+    return null;
+  }
+  return pageOfCards[pageNum].map((card, n) => <ProjectCard { ...card } key={n} />);
 }
 
 
@@ -167,7 +176,7 @@ const useFeaturedProjectAPI = (initialData) => {
     isLoading: true,
     isError: false,
     activeProjectCardPageMobile: 0,
-    activeProjectCardPage: 0, 
+    activeProjectCardPage: 0,
     projects: initialData,
   });
 
@@ -176,28 +185,25 @@ const useFeaturedProjectAPI = (initialData) => {
 
     const fetchData = async () => {
       dispatch({ type: 'FETCH_INIT' });
-
       try {
-        /* TODO: finalize API for featured projects, 
+        /* TODO: finalize API for featured projects,
         for now, this just gets first page of all projects */
-      const result = await axios(
-        `${API_URL}project/search?mapperLevel=ALL`,
-      );
+        const result = await axios(
+          `${API_URL}project/search?mapperLevel=ALL`,
+        );
 
         if (!didCancel) {
           dispatch({ type: 'FETCH_SUCCESS', payload: result.data});
         }
       } catch (error) {
         dispatch({ type: 'FETCH_FAILURE' });
-      } 
+      }
     };
 
     fetchData();
-
     return () => {
       didCancel = true;
     };
-    
   },[]);
 
   return [state, dispatch]
--- frontend/src/components/projectcard/projectCard.js
+++ frontend/src/components/projectcard/projectCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FormattedMessage, FormattedRelative } from "react-intl";
-import DueDateBox from "./dueDateBox"
-import ProjectProgressBar from "./projectProgressBar"
 
+import DueDateBox from "./dueDateBox";
+import ProjectProgressBar from "./projectProgressBar";
 import messages from "./messages";
-
 import { PROJECTCARD_CONTRIBUTION_SHOWN_THRESHOLD } from '../../config/index';
+
 
 function PriorityBox({ priority }: Object) {
   let color = "blue-grey";
@@ -17,31 +17,39 @@ function PriorityBox({ priority }: Object) {
   const translated = priority ? <FormattedMessage {...messages["projectPriority"+priority]} /> : ""
   return <div className={`pa1 fr w-33 tc br1 mt3 f7 ttu ba ${borderColor} ${color}`}>
     {translated}
-    </div>;
+  </div>;
 }
 
 function ProjectTeaser({ lastUpdated, totalMappers }: Object) {
-    if (totalMappers < PROJECTCARD_CONTRIBUTION_SHOWN_THRESHOLD) {
-        return (<div className="db tl f7 blue-grey truncate mv2" >
-           <FormattedMessage  {...messages["projectLastContribution"]} /> <FormattedRelative value={lastUpdated} />
-          </div>)
-
-    } else {
-        return (<div className="f7 tl blue-light mv2" ><span className="blue-grey b">
-        {totalMappers||0}</span> <FormattedMessage {...messages["projectTotalContributors"]} />
-        </div>)
-    }
+  if (totalMappers < PROJECTCARD_CONTRIBUTION_SHOWN_THRESHOLD) {
+    return (
+      <div className="db tl f7 blue-grey truncate mv2" >
+        <FormattedMessage  {...messages["projectLastContribution"]} /> <FormattedRelative value={lastUpdated} />
+      </div>
+    );
+  } else {
+    return(
+      <div className="f7 tl blue-light mv2" >
+        <FormattedMessage {...messages["projectTotalContributors"]}
+          values={{number: <span className="blue-grey b f6">{totalMappers||0}</span>}}
+        />
+      </div>
+    );
   }
+}
 
 function ProjectOrgLogo(organisationTag) {
   return (
-      <div className="bg-black pa1" style={{'filter':'invert(1)'}}>
-          <div title={organisationTag.organisationTag} className={`contain ${getLogoClass(organisationTag)} w-auto h2`}></div>
-      </div>);
+    <div className="bg-black pa1" style={{'filter':'invert(1)'}}>
+      <div title={organisationTag.organisationTag}
+        className={`contain ${getLogoClass(organisationTag)} w-auto h2`}
+      ></div>
+    </div>
+  );
 }
 
 function getLogoClass(organisationTag: String) {
-  const orgs = [{className: "org-unicef", 
+  const orgs = [{className: "org-unicef",
     organisationTag: "UNICEF"},
       {className: "org-usaid",
       organisationTag: "#YouthMappers"
@@ -61,16 +69,16 @@ function getLogoClass(organisationTag: String) {
       {className: "org-msf",
       organisationTag: "Médecins Sans Frontières"
     }];
-  if (organisationTag.organisationTag) {
-    console.log(organisationTag.organisationTag)
-    const searchResult = orgs.find((a) => a.organisationTag === organisationTag.organisationTag)
-    return searchResult && searchResult.className
-  } else {
-    return null
-  }
-  
 
-  
+  if (organisationTag.organisationTag) {
+    const searchResult = orgs.find((a) => a.organisationTag === organisationTag.organisationTag);
+    return searchResult && searchResult.className;
+  } else {
+    return null;
+  }
+
+
+
 }
 
 export function MapperLevelLabel({mapperLevel}: Object) {
@@ -78,7 +86,7 @@ export function MapperLevelLabel({mapperLevel}: Object) {
   return (
   <span className="fl f7 mt1 ttc fw5 blue-grey">
     {translated}
-    </span>);
+  </span>);
 }
 
 export function ProjectCard({
@@ -97,29 +105,29 @@ export function ProjectCard({
 }: Object) {
   return (
     <a className="" href={`#project=${projectId}`}>
-    <article className="fl w-25-l base-font w-50-m w-100 mb3 ph2 blue-dark mw5">
-      <div className="pv3 ph3 ba br1 b--grey-light shadow-hover ">
-        <PriorityBox priority={priority} />
-        <div className="w-50 red dib"><ProjectOrgLogo organisationTag={organisationTag} /></div>
-        <div className="ma1 w-100">
-          <div className="f7 blue-grey">#{projectId}</div>
-          <h3 title={name} className="pb2 f5 fw6 h3 block-with-text lh-title overflow-y-hidden">
-            {name}
-          </h3>
-          <div className="tc f6">
-            <div className="w-100 tl pr2 f7 blue-light dib lh-title mb2 h2 overflow-y-hidden">
-              {shortDescription} {campaignTag ? " · " + campaignTag : ""}
-            </div>
-            <ProjectTeaser totalMappers={totalMappers} lastUpdated={lastUpdated} />
-            <ProjectProgressBar percentMapped={percentMapped} percentValidated={percentValidated} />
-            <div className="cf pt2 h2">
-              <MapperLevelLabel mapperLevel={mapperLevel} />
-              <DueDateBox dueDate={dueDate} />          
+      <article className="fl w-25-l base-font w-50-m w-100 mb3 ph2 blue-dark mw5">
+        <div className="pv3 ph3 ba br1 b--grey-light shadow-hover ">
+          <PriorityBox priority={priority} />
+          <div className="w-50 red dib"><ProjectOrgLogo organisationTag={organisationTag} /></div>
+          <div className="ma1 w-100">
+            <div className="f7 blue-grey">#{projectId}</div>
+            <h3 title={name} className="pb2 f5 fw6 h3 block-with-text lh-title overflow-y-hidden">
+              {name}
+            </h3>
+            <div className="tc f6">
+              <div className="w-100 tl pr2 f7 blue-light dib lh-title mb2 h2 overflow-y-hidden">
+                {shortDescription} {campaignTag ? " · " + campaignTag : ""}
+              </div>
+              <ProjectTeaser totalMappers={totalMappers} lastUpdated={lastUpdated} />
+              <ProjectProgressBar percentMapped={percentMapped} percentValidated={percentValidated} />
+              <div className="cf pt2 h2">
+                <MapperLevelLabel mapperLevel={mapperLevel} />
+                <DueDateBox dueDate={dueDate} />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </article>
+      </article>
     </a>
   );
 }
