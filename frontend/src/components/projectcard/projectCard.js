7ae541fb46bef3a981d4b09c61dcfb5eb119fd2a
Wille Marcel <wille.yyz@gmail.com>
2019-08-06 08:00:51 -0300

    add projectCard component and featured projects section (#1817)
    * show static featured ProjectCards, ready for large displays
    
    * enable pagination buttons on react homepage w useState
    
    * better i18n for project cards
    
    * project card 1 day left localization, grid fixes, test
    
    * fix card border, inject locale to humanizeDuration, overall card styling
    
    * more i18n, break big components into their own files
    
    * add distinct mobile behavior on featuredProjects
    
    * add rough API support to projectCards, will refactor
    
    * add rough API support to projectCards, will refactor
    
    * fix demo card title->name field
    
    * add ClockIcon
    
    * add due date clock icon in position
    
    * add due date clock icon in position
    
    * use a reducer for featured project API, test with staging, truncate titles
    
    * customize shadow-hover on projectcard for design
    
    * fix projectcard shadow-hover w zindex for featuredProjects
    
    * improve total contributors line + code linting
    
    * fixes for test and progress bar
    
    * remove multiline truncate to  ... from project name, fix dueDateBox with API
    
    * unset shadow-hover zindex and remove bg-white to fix % mapped out of place
    
    * match new search API totalMappers->totalContributors
    
    * remove title from h3

---
+++ frontend/src/components/projectcard/projectCard.js
@@ -0,0 +1,133 @@
+import React from "react";
+import { FormattedMessage, FormattedRelative } from "react-intl";
+
+import DueDateBox from "./dueDateBox";
+import ProjectProgressBar from "./projectProgressBar";
+import messages from "./messages";
+import { PROJECTCARD_CONTRIBUTION_SHOWN_THRESHOLD } from '../../config/index';
+
+
+function PriorityBox({ priority }: Object) {
+  let color = "blue-grey";
+  let borderColor = "b--grey";
+  if (priority === "URGENT") {
+    color = "red";
+    borderColor = "b--red";
+  }
+  const translated = priority ? <FormattedMessage {...messages["projectPriority"+priority]} /> : ""
+  return <div className={`pa1 fr w-33 tc br1 mt3 f7 ttu ba ${borderColor} ${color}`}>
+    {translated}
+  </div>;
+}
+
+function ProjectTeaser({ lastUpdated, totalContributors }: Object) {
+  if (totalContributors < PROJECTCARD_CONTRIBUTION_SHOWN_THRESHOLD) {
+    return (
+      <div className="db tl f7 blue-grey truncate mv2" >
+        <FormattedMessage  {...messages["projectLastContribution"]} /> <FormattedRelative value={lastUpdated} />
+      </div>
+    );
+  } else {
+    return(
+      <div className="f7 tl blue-light mv2" >
+        <FormattedMessage {...messages["projectTotalContributors"]}
+          values={{number: <span className="blue-grey b f6">{totalContributors||0}</span>}}
+        />
+      </div>
+    );
+  }
+}
+
+function ProjectOrgLogo(organisationTag) {
+  return (
+    <div className="bg-black pa1" style={{'filter':'invert(1)'}}>
+      <div title={organisationTag.organisationTag}
+        className={`contain ${getLogoClass(organisationTag)} w-auto h2`}
+      ></div>
+    </div>
+  );
+}
+
+function getLogoClass(organisationTag: String) {
+  const orgs = [{className: "org-unicef",
+    organisationTag: "UNICEF"},
+      {className: "org-usaid",
+      organisationTag: "#YouthMappers"
+    },
+      {className: "org-gfdrr",
+      organisationTag: "UNICEF"
+    },
+      {className: "org-aws",
+      organisationTag: "AWS"
+    },
+      {className: "org-redcross",
+      organisationTag: "Missing Maps"
+    },
+      {className: "org-redcross",
+      organisationTag: "American Red Cross"
+    },
+      {className: "org-msf",
+      organisationTag: "Médecins Sans Frontières"
+    }];
+
+  if (organisationTag.organisationTag) {
+    const searchResult = orgs.find((a) => a.organisationTag === organisationTag.organisationTag);
+    return searchResult && searchResult.className;
+  } else {
+    return null;
+  }
+
+
+
+}
+
+export function MapperLevelLabel({mapperLevel}: Object) {
+  const translated = mapperLevel ? <FormattedMessage {...messages["projectMapperLevel"+mapperLevel]} /> : "";
+  return (
+  <span className="fl f7 mt1 ttc fw5 blue-grey">
+    {translated}
+  </span>);
+}
+
+export function ProjectCard({
+  projectId,
+  name,
+  shortDescription,
+  organisationTag,
+  lastUpdated,
+  dueDate,
+  priority,
+  mapperLevel,
+  campaignTag,
+  percentMapped,
+  percentValidated,
+  totalContributors
+}: Object) {
+  return (
+    <a className="" href={`#project=${projectId}`}>
+      <article className="fl w-25-l base-font w-50-m w-100 mb3 ph2 blue-dark mw5">
+        <div className="pv3 ph3 ba br1 b--grey-light shadow-hover ">
+          <PriorityBox priority={priority} />
+          <div className="w-50 red dib"><ProjectOrgLogo organisationTag={organisationTag} /></div>
+          <div className="ma1 w-100">
+            <div className="f7 blue-grey">#{projectId}</div>
+            <h3 className="pb2 f5 fw6 h3 lh-title overflow-y-hidden">
+              {name}
+            </h3>
+            <div className="tc f6">
+              <div className="w-100 tl pr2 f7 blue-light dib lh-title mb2 h2 overflow-y-hidden">
+                {shortDescription} {campaignTag ? " · " + campaignTag : ""}
+              </div>
+              <ProjectTeaser totalContributors={totalContributors} lastUpdated={lastUpdated} />
+              <ProjectProgressBar percentMapped={percentMapped} percentValidated={percentValidated} />
+              <div className="cf pt2 h2">
+                <MapperLevelLabel mapperLevel={mapperLevel} />
+                <DueDateBox dueDate={dueDate} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </article>
+    </a>
+  );
+}
