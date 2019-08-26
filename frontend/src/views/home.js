df66547fbce2d6e6e28fbcfeae1c24528cfb82c8
Dakota Benjamin <dakota.benjamin@hotosm.org>
2019-08-26 13:34:30 -0400

    Rework matomo script for simpler loading

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -6,24 +6,8 @@ import { MappingFlow } from '../components/homepage/mappingFlow';
 import { WhoIsMapping } from '../components/homepage/whoIsMapping';
 import { Testimonials } from '../components/homepage/testimonials/index';
 import { FeaturedProjects } from '../components/homepage/featuredProjects';
-import { MATOMO_ID } from '../config';
 
 export class Home extends React.Component {
-  componentDidMount() {
-    if (MATOMO_ID != '') {
-      const s = document.createElement('script');
-      s.type = 'text/javascript';
-      s.async = true;
-      s.innerHTML = "var site_id = { MATOMO_ID };";
-      document.body.appendChild(s);
-
-      const s2 = document.createElement('script');
-      s2.src = "https://cdn.hotosm.org/tracking-v3.js";
-      s2.async = true;
-      document.body.appendChild(s2);
-    }
-    
-  }
 
   render() {
     return(
