239955d37ca72bf61914e209cc6d90ca087c3d1b
zlavergne <zack@kaartgroup.com>
2019-09-09 19:02:55 +0000

    HOTFIX: bug fixes for project files and gpx loading in RapiD

--- server/services/mapping_service.py
+++ server/services/mapping_service.py
@@ -308,42 +308,46 @@ class MappingService:
 
         dto = ProjectFiles.get_file(project_id, file_id)
 
-        with tempfile.TemporaryDirectory() as tmpdirpath:
+        filedir = os.path.join(dto.path, os.path.splitext(dto.file_name)[0])
 
-            tasks_file = os.path.join(tmpdirpath, "{project_id}_tasks.geojson".format(project_id=str(project_id)))
+        if not os.path.exists(filedir):
+            os.makedirs(filedir)
 
-            with open(tasks_file, 'w') as t:
-                t.write(str(tasks))
+        tasks_file = os.path.join(filedir, "{project_id}_tasks.geojson".format(project_id=str(project_id)))
 
-            # Convert the geojson features into separate .poly files
-            # to use with osmosis
-            poly_cmd = './server/tools/ogr2poly.py {file} -p {tmpdirpath}/ -f taskId'.format(file=tasks_file, tmpdirpath=tmpdirpath)
-            subprocess.check_output(poly_cmd, shell=True)
-            os.remove(tasks_file)
+        with open(tasks_file, 'w') as t:
+            t.write(str(tasks))
 
-            osm_files = []
-            for poly in os.listdir(tmpdirpath):
-                """ Extract from osm file into a file for each poly file """
-                task_cmd = './server/tools/osmosis/bin/osmosis  --rx file={xml} enableDateParsing=no --bp completeWays=yes clipIncompleteEntities=true file={task_poly} --wx file={task_xml}'.format(
-                        xml=os.path.join(dto.path, dto.file_name),
-                        task_poly=os.path.join(tmpdirpath, poly),
-                        task_xml=os.path.join(
-                            tmpdirpath,
-                            "task_{task_id}_{file_name}.osm".format(
-                                task_id=os.path.splitext(poly)[0],
-                                file_name=os.path.splitext(dto.file_name)[0]
-                            )
-                        )
-                    )
-                osm_files.append(
-                    os.path.join(
-                        tmpdirpath,
+        # Convert the geojson features into separate .poly files
+        # to use with osmosis
+        poly_cmd = './server/tools/ogr2poly.py {file} -p {filedir}/ -f taskId'.format(file=tasks_file, filedir=filedir)
+        subprocess.check_output(poly_cmd, shell=True)
+        os.remove(tasks_file)
+
+        osm_files = []
+        for poly in os.listdir(filedir):
+            """ Extract from osm file into a file for each poly file """
+            task_cmd = './server/tools/osmosis/bin/osmosis -q --rx file={xml} enableDateParsing=no --bp completeWays=yes file={task_poly} --wx file={task_xml}'.format(
+                    xml=os.path.join(dto.path, dto.file_name),
+                    task_poly=os.path.join(filedir, poly),
+                    task_xml=os.path.join(
+                        filedir,
                         "task_{task_id}_{file_name}.osm".format(
                             task_id=os.path.splitext(poly)[0],
-                            file_name=os.path.splitext(dto.file_name)[0])
+                            file_name=os.path.splitext(dto.file_name)[0]
                         )
                     )
-                subprocess.check_output(task_cmd, shell=True)
+                )
+            osm_files.append(
+                os.path.join(
+                    filedir,
+                    "task_{task_id}_{file_name}.osm".format(
+                        task_id=os.path.splitext(poly)[0],
+                        file_name=os.path.splitext(dto.file_name)[0])
+                    )
+                )
+            subprocess.check_output(task_cmd, shell=True)
+            os.remove(os.path.join(filedir, poly))
 
             # Merge the extracted files back together. Used if more than one task is sent in request.
             merge_cmd = ['./server/tools/osmosis/bin/osmosis']
