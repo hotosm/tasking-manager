0c90de392c80d1f23812803c9c3a02b211a4f4db
Thad Kerosky <contact@thadk.net>
2019-09-12 04:40:04 -0400

    frontend for country filter

--- frontend/src/components/projects/moreFiltersForm.js
+++ frontend/src/components/projects/moreFiltersForm.js
@@ -4,7 +4,6 @@ import { HomeIcon, RoadIcon, WavesIcon, TaskIcon } from '../svgIcons';
 import { useTagAPI } from '../../hooks/UseTagAPI';
 import { MappingTypeFilterPicker } from './mappingTypeFilterPicker';
 import { TagFilterPickerCheckboxes } from './tagFilterPicker';
-import { ShowAllTagFilterButton } from './showAllTagFilterButton';
 
 import { useQueryParams, useQueryParam, StringParam, NumberParam } from 'use-query-params';
 import { CommaArrayParam } from '../../utils/CommaArrayParam';
@@ -42,16 +41,16 @@ export const MoreFiltersForm = props => {
   };
 
   /* dereference the formQuery */
-  const { campaign: campaignInQuery, organisation: orgInQuery } = formQuery;
+  const { campaign: campaignInQuery, organisation: orgInQuery, location: countryInQuery } = formQuery;
   const [campaignAPIState] = useTagAPI([], 'campaigns');
   const [orgAPIState] = useTagAPI([], 'organisations');
+  const [countriesAPIState] = useTagAPI([], 'countries');
 
   /* another useQueryParam for the second form */
   const [mappingTypesInQuery, setMappingTypes] = useQueryParam('types', CommaArrayParam);
 
   const fieldsetStyle = 'w-100 bn';
   const titleStyle = 'tc w-100 db ttu fw5 blue-grey';
-  const inputStyle = 'inline-flex w-50 items-center mb2';
 
   return (
     <form className="pa4" onChange={handleInputChange}>
@@ -98,52 +97,18 @@ export const MoreFiltersForm = props => {
         />
       </div>
 
-      {/* Example location field, to be implemented on backend*/}
-      <fieldset id="location" className={fieldsetStyle}>
-        <legend className={titleStyle}>
-          <FormattedMessage {...messages.location} />
-        </legend>
-        <div className={inputStyle}>
-          <input className="mr2" type="radio" name="location" id="spacejam2" value="in" />
-          <label htmlFor="spacejam2" className="lh-copy">
-            India
-          </label>
-        </div>
-        <div className={inputStyle}>
-          <input className="mr2" type="radio" name="location" id="airbud2" value="mz" />
-          <label htmlFor="airbud2" className="lh-copy">
-            Mozambique
-          </label>
-        </div>
-        <div className={inputStyle}>
-          <input className="mr2" type="radio" name="location" id="hocuspocus2" value="su" />
-          <label htmlFor="hocuspocus2" className="lh-copy">
-            Sudan
-          </label>
-        </div>
-        <div className={inputStyle}>
-          <input className="mr2" type="radio" name="location" id="diehard2" value="gie" />
-          <label htmlFor="diehard2" className="lh-copy">
-            Guinea
-          </label>
-        </div>
-        <div className={inputStyle}>
-          <input className="mr2" type="radio" name="location" id="primer2" value="ug" />
-          <label htmlFor="primer2" className="lh-copy">
-            Uganda
-          </label>
-        </div>
-        <div className={inputStyle}>
-          <input className="mr2" type="radio" name="location" id="proxy2" value="tz" />
-          <label htmlFor="proxy2" className="lh-copy">
-            Tanzania
-          </label>
-        </div>
-        <ShowAllTagFilterButton
-          title=<FormattedMessage {...messages.locations} />
-          showingToggle={true}
-        ></ShowAllTagFilterButton>
-      </fieldset>
+      <TagFilterPickerCheckboxes
+          fieldsetTitle=<FormattedMessage {...messages.location} />
+          fieldsetTitlePlural=<FormattedMessage {...messages.locations} />
+          fieldsetName="location"
+          fieldsetStyle={fieldsetStyle}
+          titleStyle={titleStyle}
+          selectedTag={countryInQuery}
+          tagOptionsFromAPI={countriesAPIState}
+          setQueryForChild={setFormQuery}
+          allQueryParamsForChild={formQuery}
+        />
+
     </form>
   );
 };
