bc0173bb8a699607488a309a0b9168b345795dc4
Ramya Ragupathy <r.ragupathy85@gmail.com>
2019-09-12 17:52:43 +0530

    Field name updates

--- server/models/postgis/project.py
+++ server/models/postgis/project.py
@@ -659,8 +659,7 @@ class Project(db.Model):
         project_info = ProjectInfo.get_dto_for_locale(
             self.id, preferred_locale, self.default_locale
         )
-        summary.name = project_info.name
-        summary.short_description = project_info.short_description
+        summary.project_info = project_info
 
         return summary
 
@@ -724,6 +723,27 @@ class Project(db.Model):
         base_dto.author = User().get_by_id(self.author_id).username
         base_dto.active_mappers = Project.get_active_mappers(self.id)
         base_dto.task_creation_mode = TaskCreationMode(self.task_creation_mode).name
+        base_dto.percent_mapped = Project.calculate_tasks_percent(
+            "mapped",
+            self.total_tasks,
+            self.tasks_mapped,
+            self.tasks_validated,
+            self.tasks_bad_imagery,
+        )
+        base_dto.percent_validated = Project.calculate_tasks_percent(
+            "validated",
+            self.total_tasks,
+            self.tasks_mapped,
+            self.tasks_validated,
+            self.tasks_bad_imagery,
+        )
+        base_dto.percent_bad_imagery = Project.calculate_tasks_percent(
+            "bad_imagery",
+            self.total_tasks,
+            self.tasks_mapped,
+            self.tasks_validated,
+            self.tasks_bad_imagery,
+        )
 
         if self.custom_editor:
             base_dto.custom_editor = self.custom_editor.as_dto()
