3c51340db5ca74c5bc9e0b0424d545dc9ed8039b
Ramya Ragupathy <r.ragupathy85@gmail.com>
2019-09-17 15:36:51 +0200

    Capture latest status of tasks
    Black + flake
    
    More flake fix

--- server/api/projects/activities.py
+++ server/api/projects/activities.py
@@ -39,3 +39,37 @@ class ProjectsActivitiesAPI(Resource):
             error_msg = f"User GET - unhandled error: {str(e)}"
             current_app.logger.critical(error_msg)
             return {"Error": "Unable to fetch user activity"}, 500
+
+
+class ProjectsLastActivitiesAPI(Resource):
+    def get(self, project_id):
+        """
+        Get latest user actvity on all of project task
+        ---
+        tags:
+          - projects
+        produces:
+          - application/json
+        parameters:
+            - name: project_id
+              in: path
+              required: true
+              type: integer
+              default: 1
+        responses:
+            200:
+                description: Project activity
+            404:
+                description: No activity
+            500:
+                description: Internal Server Error
+        """
+        try:
+            activity = StatsService.get_last_activity(project_id)
+            return activity.to_primitive(), 200
+        except NotFound:
+            return {"Error": "No activity on project"}, 404
+        except Exception as e:
+            error_msg = f"User GET - unhandled error: {str(e)}"
+            current_app.logger.critical(error_msg)
+            return {"Error": "Unable to fetch user activity"}, 500
