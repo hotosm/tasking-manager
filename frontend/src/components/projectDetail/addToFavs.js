ca9cbbde3d452e8493914cb4d0982703324e4058
Thad Kerosky <contact@thadk.net>
2019-10-15 14:11:58 -0400

    Project detail page v9 w map fixes

---
+++ frontend/src/components/projectDetail/addToFavs.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { FlagIcon } from '../svgIcons';
+import { FormattedMessage } from 'react-intl';
+import {useFavProjectAPI} from '../../hooks/UseFavProjectAPI';
+import { navigate } from "@reach/router"
+import { useSelector } from 'react-redux';
+import messages from './messages';
+
+export const AddToFavs = props => {
+    const userToken = useSelector(state => state.auth.get('token'));
+    const [state, dispatchToggle] = useFavProjectAPI(false, props.projectId, userToken);
+    const isFav = state.isFav;
+    const isLoading = state.isLoading;
+  
+    return (
+      <>
+        <button
+          type="button"
+          onClick={() => userToken ? dispatchToggle() : navigate("/login")}
+          className={`${!props.projectId ? 'dn' : ''} input-reset dim base-font bg-white blue-dark button-reset f6 bn pn`}
+        >
+        <FlagIcon className={`pt3 pr2 ${isLoading? 'o-50' : ''} ${isFav? 'red' : ''}`} />
+        {state.isLoading? '...':''} {isFav? <FormattedMessage {...messages.removeFromFavorites} />: <FormattedMessage {...messages.addToFavorites} /> }
+        </button>
+        {isFav && props.children}
+      </>
+    );
+  };
+  
