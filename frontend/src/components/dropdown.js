13dabb1fbf664878c1e1957979df13e43be9f475
Wille Marcel <wille.yyz@gmail.com>
2019-10-31 10:43:03 -0300

    add redirect from old project url and fix z-index of user menu

--- frontend/src/components/dropdown.js
+++ frontend/src/components/dropdown.js
@@ -45,7 +45,7 @@ class DropdownContent extends React.PureComponent {
   render() {
     return (
       <div
-        className={`di tl mt1 ba b--grey-light br1 absolute shadow-1 z-3 flex flex-column${
+        className={`di tl mt1 ba b--grey-light br1 absolute shadow-1 z-5 flex flex-column${
           this.props.toTop ? ' bottom-3' : ''
         }`}
       >
