8b8b8943915309b1c78261b35074d38ed7de7433
Adolfo Fitoria <adolfo.fitoria@gmail.com>
2019-11-12 19:38:46 -0600

    Fixes for #1853 bad counting on master branch
    Adds migration to refresh stats:wq

--- server/services/stats_service.py
+++ server/services/stats_service.py
@@ -3,10 +3,15 @@ from cachetools import TTLCache, cached
 from sqlalchemy import func, text
 from server import db
 from server.models.dtos.stats_dto import (
-    ProjectContributionsDTO, UserContribution, Pagination, TaskHistoryDTO,
-    ProjectActivityDTO, HomePageStatsDTO, OrganizationStatsDTO,
-    CampaignStatsDTO
-    )
+    ProjectContributionsDTO,
+    UserContribution,
+    Pagination,
+    TaskHistoryDTO,
+    ProjectActivityDTO,
+    HomePageStatsDTO,
+    OrganizationStatsDTO,
+    CampaignStatsDTO,
+)
 from server.models.postgis.project import Project
 from server.models.postgis.statuses import TaskStatus
 from server.models.postgis.task import TaskHistory, User, Task
@@ -19,19 +24,29 @@ homepage_stats_cache = TTLCache(maxsize=4, ttl=30)
 
 
 class StatsService:
-
     @staticmethod
-    def update_stats_after_task_state_change(project_id: int, user_id: int, last_state: TaskStatus,
-                                             new_state: TaskStatus, action='change'):
+    def update_stats_after_task_state_change(
+        project_id: int,
+        user_id: int,
+        last_state: TaskStatus,
+        new_state: TaskStatus,
+        action="change",
+    ):
         """ Update stats when a task has had a state change """
 
-        if new_state in [TaskStatus.READY, TaskStatus.LOCKED_FOR_VALIDATION, TaskStatus.LOCKED_FOR_MAPPING]:
+        if new_state in [
+            TaskStatus.READY,
+            TaskStatus.LOCKED_FOR_VALIDATION,
+            TaskStatus.LOCKED_FOR_MAPPING,
+        ]:
             return  # No stats to record for these states
 
         project = ProjectService.get_project_by_id(project_id)
         user = UserService.get_user_by_id(user_id)
 
-        StatsService._update_tasks_stats(project, user, last_state, new_state, action)
+        project, user = StatsService._update_tasks_stats(
+            project, user, last_state, new_state, action
+        )
         UserService.upsert_mapped_projects(user_id, project_id)
         project.last_updated = timestamp()
 
@@ -39,11 +54,19 @@ class StatsService:
         return project, user
 
     @staticmethod
-    def _update_tasks_stats(project: Project, user: User, last_state: TaskStatus, new_state: TaskStatus,
-                            action='change'):
+    def _update_tasks_stats(
+        project: Project,
+        user: User,
+        last_state: TaskStatus,
+        new_state: TaskStatus,
+        action="change",
+    ):
 
         # Make sure you are aware that users table has it as incrementing counters,
         # while projects table reflect the actual state, and both increment and decrement happens
+
+        if new_state == last_state:
+            return project, user
 
         # Set counters for new state
         if new_state == TaskStatus.MAPPED:
@@ -53,7 +76,7 @@ class StatsService:
         elif new_state == TaskStatus.BADIMAGERY:
             project.tasks_bad_imagery += 1
 
-        if action == 'change':
+        if action == "change":
             if new_state == TaskStatus.MAPPED:
                 user.tasks_mapped += 1
             elif new_state == TaskStatus.VALIDATED:
@@ -69,7 +92,7 @@ class StatsService:
         elif last_state == TaskStatus.BADIMAGERY:
             project.tasks_bad_imagery -= 1
 
-        if action == 'undo':
+        if action == "undo":
             if last_state == TaskStatus.MAPPED:
                 user.tasks_mapped -= 1
             elif last_state == TaskStatus.VALIDATED:
@@ -77,19 +100,28 @@ class StatsService:
             elif last_state == TaskStatus.INVALIDATED:
                 user.tasks_invalidated -= 1
 
+        return project, user
+
     @staticmethod
     def get_latest_activity(project_id: int, page: int) -> ProjectActivityDTO:
         """ Gets all the activity on a project """
 
-        results = db.session.query(
-                TaskHistory.id, TaskHistory.task_id, TaskHistory.action, TaskHistory.action_date,
-                TaskHistory.action_text, User.username
-            ).join(User).filter(
-                TaskHistory.project_id == project_id,
-                TaskHistory.action != 'COMMENT'
-            ).order_by(
-                TaskHistory.action_date.desc()
-            ).paginate(page, 10, True)
+        results = (
+            db.session.query(
+                TaskHistory.id,
+                TaskHistory.task_id,
+                TaskHistory.action,
+                TaskHistory.action_date,
+                TaskHistory.action_text,
+                User.username,
+            )
+            .join(User)
+            .filter(
+                TaskHistory.project_id == project_id, TaskHistory.action != "COMMENT"
+            )
+            .order_by(TaskHistory.action_date.desc())
+            .paginate(page, 10, True)
+        )
 
         if results.total == 0:
             raise NotFound()
@@ -111,7 +143,7 @@ class StatsService:
     @staticmethod
     def get_user_contributions(project_id: int) -> ProjectContributionsDTO:
         """ Get all user contributions on a project"""
-        contrib_query = '''select m.mapped_by, m.username, m.mapped, v.validated_by, v.username, v.validated
+        contrib_query = """select m.mapped_by, m.username, m.mapped, v.validated_by, v.username, v.validated
                              from (select t.mapped_by, u.username, count(t.mapped_by) mapped
                                      from tasks t,
                                           users u
@@ -127,7 +159,7 @@ class StatsService:
                                       and t.validated_by is not null
                                     group by t.validated_by, u.username) v
                                        ON m.mapped_by = v.validated_by
-        '''
+        """
 
         results = db.engine.execute(text(contrib_query), project_id=project_id)
         if results.rowcount == 0:
@@ -150,58 +182,68 @@ class StatsService:
         dto = HomePageStatsDTO()
 
         dto.total_projects = Project.query.count()
-        dto.mappers_online = Task.query.filter(
-            Task.locked_by is not None
-            ).distinct(Task.locked_by).count()
+        dto.mappers_online = (
+            Task.query.filter(Task.locked_by is not None)
+            .distinct(Task.locked_by)
+            .count()
+        )
         dto.total_mappers = User.query.count()
-        dto.total_validators = Task.query.filter(
-            Task.task_status == TaskStatus.VALIDATED.value
-            ).distinct(Task.validated_by).count()
+        dto.total_validators = (
+            Task.query.filter(Task.task_status == TaskStatus.VALIDATED.value)
+            .distinct(Task.validated_by)
+            .count()
+        )
         dto.tasks_mapped = Task.query.filter(
-            Task.task_status.in_(
-                (TaskStatus.MAPPED.value, TaskStatus.VALIDATED.value)
-                )
-            ).count()
+            Task.task_status.in_((TaskStatus.MAPPED.value, TaskStatus.VALIDATED.value))
+        ).count()
         dto.tasks_validated = Task.query.filter(
             Task.task_status == TaskStatus.VALIDATED.value
-            ).count()
+        ).count()
 
-        org_proj_count = db.session.query(
-            Project.organisation_tag,
-            func.count(Project.organisation_tag)
-        ).group_by(Project.organisation_tag).all()
+        org_proj_count = (
+            db.session.query(
+                Project.organisation_tag, func.count(Project.organisation_tag)
+            )
+            .group_by(Project.organisation_tag)
+            .all()
+        )
 
         untagged_count = 0
 
         # total_area = 0
 
-
-
-       # dto.total_area = 0
+        # dto.total_area = 0
 
         # total_area_sql = """select sum(ST_Area(geometry)) from public.projects as area"""
 
         # total_area_result = db.engine.execute(total_area_sql)
         # current_app.logger.debug(total_area_result)
         # for rowproxy in total_area_result:
-            # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
-            # for tup in rowproxy.items():
-                # total_area += tup[1]
-                # current_app.logger.debug(total_area)
+        # rowproxy.items() returns an array like [(key0, value0), (key1, value1)]
+        # for tup in rowproxy.items():
+        # total_area += tup[1]
+        # current_app.logger.debug(total_area)
         # dto.total_area = total_area
 
         tasks_mapped_sql = "select coalesce(sum(ST_Area(geometry)), 0) as sum from public.tasks where task_status = :task_status"
-        tasks_mapped_result = db.engine.execute(text(tasks_mapped_sql), task_status=TaskStatus.MAPPED.value)
+        tasks_mapped_result = db.engine.execute(
+            text(tasks_mapped_sql), task_status=TaskStatus.MAPPED.value
+        )
 
-        dto.total_mapped_area = tasks_mapped_result.fetchone()['sum']
+        dto.total_mapped_area = tasks_mapped_result.fetchone()["sum"]
 
         tasks_validated_sql = "select coalesce(sum(ST_Area(geometry)), 0) as sum from public.tasks where task_status = :task_status"
-        tasks_validated_result = db.engine.execute(text(tasks_validated_sql), task_status=TaskStatus.VALIDATED.value)
+        tasks_validated_result = db.engine.execute(
+            text(tasks_validated_sql), task_status=TaskStatus.VALIDATED.value
+        )
 
-        dto.total_validated_area = tasks_validated_result.fetchone()['sum']
+        dto.total_validated_area = tasks_validated_result.fetchone()["sum"]
 
-        campaign_count = db.session.query(Project.campaign_tag, func.count(Project.campaign_tag))\
-            .group_by(Project.campaign_tag).all()
+        campaign_count = (
+            db.session.query(Project.campaign_tag, func.count(Project.campaign_tag))
+            .group_by(Project.campaign_tag)
+            .all()
+        )
         no_campaign_count = 0
         unique_campaigns = 0
 
@@ -214,12 +256,17 @@ class StatsService:
                 no_campaign_count += campaign_stats.projects_created
 
         if no_campaign_count:
-            no_campaign_proj = CampaignStatsDTO(('Untagged', no_campaign_count))
+            no_campaign_proj = CampaignStatsDTO(("Untagged", no_campaign_count))
             dto.campaigns.append(no_campaign_proj)
         dto.total_campaigns = unique_campaigns
 
-        org_proj_count = db.session.query(Project.organisation_tag, func.count(Project.organisation_tag))\
-            .group_by(Project.organisation_tag).all()
+        org_proj_count = (
+            db.session.query(
+                Project.organisation_tag, func.count(Project.organisation_tag)
+            )
+            .group_by(Project.organisation_tag)
+            .all()
+        )
         no_org_count = 0
         unique_orgs = 0
 
@@ -232,8 +279,31 @@ class StatsService:
                 no_org_count += org_stats.projects_created
 
         if no_org_count:
-            no_org_proj = OrganizationStatsDTO(('Untagged', no_org_count))
+            no_org_proj = OrganizationStatsDTO(("Untagged", no_org_count))
             dto.organizations.append(no_org_proj)
         dto.total_organizations = unique_orgs
 
         return dto
+
+    @staticmethod
+    def update_all_project_stats():
+        projects = db.session.query(Project.id)
+        for project_id in projects.all():
+            StatsService.update_project_stats(project_id)
+
+    @staticmethod
+    def update_project_stats(project_id: int):
+        project = ProjectService.get_project_by_id(project_id)
+        tasks = Task.query.filter(Task.project_id == project_id)
+
+        project.total_tasks = tasks.count()
+        project.tasks_mapped = tasks.filter(
+            Task.task_status == TaskStatus.MAPPED.value
+        ).count()
+        project.tasks_validated = tasks.filter(
+            Task.task_status == TaskStatus.VALIDATED.value
+        ).count()
+        project.tasks_bad_imagery = tasks.filter(
+            Task.task_status == TaskStatus.BADIMAGERY.value
+        ).count()
+        project.save()
