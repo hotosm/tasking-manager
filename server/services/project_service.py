9d41120995918b1b541c02b283d3d42d9214c534
Ramya <r.ragupathy85@gmail.com>
2019-12-11 20:12:57 +0530

    Fix task endpoints for logged in user (#2048)
    * Fix snafu on task details for logged in user
    
    * Formatting with black and flake
    
    * Removed unused references
    
    * Introduce task ID query parameter in TasksJson
    
    * Task filter in the main function
    
    * Formatting fixes

--- server/services/project_service.py
+++ server/services/project_service.py
@@ -130,10 +130,14 @@ class ProjectService:
 
     @staticmethod
     def get_project_tasks(
-        project_id, order_by: str = None, order_by_type: str = "ASC", status: int = None
+        project_id,
+        task_ids_str: str,
+        order_by: str = None,
+        order_by_type: str = "ASC",
+        status: int = None,
     ):
         project = ProjectService.get_project_by_id(project_id)
-        return project.all_tasks_as_geojson(order_by, order_by_type, status)
+        return project.tasks_as_geojson(task_ids_str, order_by, order_by_type, status)
 
     @staticmethod
     def get_project_aoi(project_id):
