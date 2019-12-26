aa9704d642e079b8abdaadbf78d149a40e84e5b8
Wille Marcel <wille.yyz@gmail.com>
2019-12-26 10:26:07 -0300

    user redux provider in taskSelection/footer tests

---
+++ frontend/src/components/taskSelection/tests/footer.test.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { IntlProvider, FormattedMessage } from 'react-intl';
+import { Provider } from 'react-redux';
+
+import { Imagery } from '../imagery';
+import { MappingTypes } from '../../mappingTypes';
+import { TaskSelectionFooter } from '../footer';
+import { Button } from '../../button';
+import { store } from '../../../store';
+
+describe("test if footer", () => {
+  const createComponentWithReduxAndIntl = (children, props = { locale: 'en' }) => {
+    return TestRenderer.create(
+      <Provider store={store}>
+        <IntlProvider {...props}>{children}</IntlProvider>
+      </Provider>
+    );
+  };
+
+  it('has MappingTypes with ROADS and BUILDINGS', () => {
+    const element = createComponentWithReduxAndIntl(
+      <TaskSelectionFooter
+        project={{projectId: 1, mappingTypes: ['ROADS', 'BUILDINGS'], mappingEditors: ['ID', 'JOSM'],}}
+        taskAction={'mapSelectedTask'}
+      />,
+    );
+    const testInstance = element.root;
+    expect(testInstance.findByType(MappingTypes).props.types).toStrictEqual(['ROADS', 'BUILDINGS']);
+  });
+
+  it('has imagery component returning the correct message', () => {
+    const element = createComponentWithReduxAndIntl(
+      <TaskSelectionFooter
+        project={{
+          projectId: 3,
+          mappingEditors: ['ID', 'JOSM'],
+          mappingTypes: ['ROADS', 'BUILDINGS'],
+          imagery: 'tms[1,22]:https://service.com/earthservice/tms/Layer@EPSG:3857@jpg/{zoom}/{x}/{-y}.jpg',
+        }}
+        taskAction={'mapATask'}
+      />,
+    );
+    const testInstance = element.root;
+    expect(testInstance.findByType(Imagery).props.value).toBe(
+      'tms[1,22]:https://service.com/earthservice/tms/Layer@EPSG:3857@jpg/{zoom}/{x}/{-y}.jpg',
+    );
+  });
+
+  it('returns the correct contribute button message when action is "mapATask"', () => {
+    const element = createComponentWithReduxAndIntl(
+      <TaskSelectionFooter
+        project={{projectId: 1, mappingTypes: ['LAND_USE'], mappingEditors: ['ID', 'JOSM']}}
+        taskAction={'mapATask'}
+      />,
+    );
+    const testInstance = element.root;
+    expect(testInstance.findByType(Button).findByType(FormattedMessage).props.id).toBe(
+      'project.selectTask.footer.button.mapRandomTask',
+    );
+  });
+
+  it('returns the correct contribute button message when taskAction is "validateSelectedTask"', () => {
+    const element = createComponentWithReduxAndIntl(
+      <TaskSelectionFooter
+        project={{projectId: 1, mappingTypes: ['LAND_USE'], mappingEditors: ['ID', 'JOSM'], validationEditors: ['ID', 'JOSM']}}
+        taskAction={'validateSelectedTask'}
+      />,
+    );
+    const testInstance = element.root;
+    expect(testInstance.findByType(Button).findByType(FormattedMessage).props.id).toBe(
+      'project.selectTask.footer.button.validateSelectedTask',
+    );
+  });
+});
