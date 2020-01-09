6261478b9b24b346f7dbc6e4ff1ef6ad56e9e13a
Wille Marcel <wille.yyz@gmail.com>
2020-01-09 12:42:21 -0300

    improve my-contributions and task activity modal

--- frontend/src/components/contributions/taskCard.js
+++ frontend/src/components/contributions/taskCard.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from '@reach/router';
+import Popup from 'reactjs-popup';
 import { FormattedRelative, FormattedMessage } from 'react-intl';
 // import { FormattedRelativeTime } from 'react-intl';
 // import {selectUnit} from '@formatjs/intl-utils';
@@ -7,6 +8,7 @@ import { FormattedRelative, FormattedMessage } from 'react-intl';
 import messages from './messages';
 import { ListIcon, PlayIcon, ClockIcon } from '../svgIcons';
 import { TaskStatus } from '../taskSelection/taskList';
+import { TaskActivity } from '../taskSelection/taskActivity';
 
 export function TaskCard({
   taskId,
@@ -37,12 +39,7 @@ export function TaskCard({
     new Date(+new Date(lastUpdated + '+00:00') + autoUnlockSeconds * 1000);
 
   return (
-    <Link
-      to={`/user/contributions/${projectId}/${taskId}`}
-      className={`no-underline`}
-      onMouseEnter={() => setHovered(true)}
-      onMouseLeave={() => setHovered(false)}
-    >
+    <div onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
       <article
         className={`db base-font bg-white w-75-l w-100 mb1 mh2 blue-dark mw8 ${
           isHovered ? 'shadow-4' : ''
@@ -50,9 +47,17 @@ export function TaskCard({
       >
         <div className="pv3 ph3 ba br1 b--grey-light cf">
           <div className="w-40-ns w-100 fl">
-            <h4 className="mv0 fw6 f5">
-              <FormattedMessage {...messages.projectTask} values={{task: taskId, project: projectId}}/>
-            </h4>
+            <Link
+              to={`/projects/${projectId}/tasks/?search=${taskId}`}
+              className="no-underline link blue-dark dib"
+            >
+              <h4 className="mv0 fw6 f5">
+                <FormattedMessage
+                  {...messages.projectTask}
+                  values={{ task: taskId, project: projectId }}
+                />
+              </h4>
+            </Link>
             <div className={`pt2 blue-grey f6`} title={lastUpdated}>
               {/* <FormattedRelativeTime value={value} unit={unit}/> */}
               <span>
@@ -68,29 +73,53 @@ export function TaskCard({
               <div className="db">
                 <TaskStatus status={taskStatus} lockHolder={lockHolder} />
               </div>
-              {lockHolder &&
-                <div className="dn dib-ns mv1 blue-grey bg-grey-light f7 pv1 ph2" title={timeToAutoUnlock} >
+              {lockHolder && (
+                <div
+                  className="dn dib-ns mv1 blue-grey bg-grey-light f7 pv1 ph2"
+                  title={timeToAutoUnlock}
+                >
                   <ClockIcon className="pr2 v-mid" height="19px" width="13px" />
                   <span className="v-mid">
-                    <FormattedMessage {...messages.unlock} values={{time: <FormattedRelative value={timeToAutoUnlock} />}}/>
+                    <FormattedMessage
+                      {...messages.unlock}
+                      values={{ time: <FormattedRelative value={timeToAutoUnlock} /> }}
+                    />
                   </span>
                 </div>
-              }
+              )}
             </div>
           </div>
           <div className="w-20-ns w-100 fr">
-            <ListIcon className={`fr h1 w1 mv1 pv2 v-mid pr3 blue-light hover-blue-grey`} />
-            {isHovered && ['READY', 'LOCKED_FOR_MAPPING', 'LOCKED_FOR_VALIDATION', 'INVALIDATED'].includes(taskStatus) &&
-              <Link to={`/projects/${projectId}/tasks#task=${taskId}`}>
-                <div className={`dn dib-l link pv2 ph3 mh3 mv1 bg-red white f7 fr`}>
-                  <PlayIcon className={`ph1 dib-l dn`} />
-                  <FormattedMessage {...messages.resumeTask} />
-                </div>
-              </Link>
-            }
+            <Popup
+              trigger={
+                <ListIcon className="pointer fr h1 w1 mv1 pv2 v-mid pr3 blue-light hover-blue-grey" />
+              }
+              modal
+              closeOnDocumentClick
+            >
+              {close => (
+                <TaskActivity
+                  taskId={taskId}
+                  projectName={projectId} // add project name when we have that data available on the API
+                  projectId={projectId}
+                  close={close}
+                />
+              )}
+            </Popup>
+            {isHovered &&
+              ['READY', 'LOCKED_FOR_MAPPING', 'LOCKED_FOR_VALIDATION', 'INVALIDATED'].includes(
+                taskStatus,
+              ) && (
+                <Link to={`/projects/${projectId}/tasks#search=${taskId}`}>
+                  <div className={`dn dib-l link pv2 ph3 mh3 mv1 bg-red white f7 fr`}>
+                    <PlayIcon className={`ph1 dib-l dn`} />
+                    <FormattedMessage {...messages.resumeTask} />
+                  </div>
+                </Link>
+              )}
           </div>
         </div>
       </article>
-    </Link>
+    </div>
   );
 }
