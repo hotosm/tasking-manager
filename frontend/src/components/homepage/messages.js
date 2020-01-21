646aa76005681b0d41ae9ce1892d0dae8899ed2d
Felix Delattre <felix@delattre.de>
2020-01-21 14:48:28 +0000

    Update English textstrings

--- frontend/src/components/homepage/messages.js
+++ frontend/src/components/homepage/messages.js
@@ -6,12 +6,12 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   jumbotronTitle: {
     id: 'home.mainSection.title',
-    defaultMessage: 'Map a task for people in need',
+    defaultMessage: 'Map for people in need',
   },
   jumbotronHeadLine: {
     id: 'home.mainSection.lead',
     defaultMessage:
-      "Join a global community that is helping to put the world's most vulnerable people and places on the map towards humanitarian aid and sustainable development.",
+      "Join a global community putting the world's most vulnerable people and places on the map in order to support humanitarian aid and sustainable development.",
   },
   secJumbotronTitle: {
     id: 'home.callToAction.title',
@@ -20,11 +20,11 @@ export default defineMessages({
   secJumbotronHeadLine: {
     id: 'home.callToAction.firstLeadLine',
     defaultMessage:
-      "If you have never mapped before and would like to help, don't worry it's easy. Visit our {link} page to get started mapping in less than 30 minutes.",
+      "Anyone can contribute to the map. If you have never mapped before and would like to get started, visit our {link} page.",
   },
   secJumbotronHeadLine2: {
     id: 'home.callToAction.secondLeadLine',
-    defaultMessage: 'Seasoned mapper? Pick up one of our many projects and start contributing.',
+    defaultMessage: 'Are you an experienced mapper? Click below to see what projects are available for mapping.',
   },
   featuredProjects: {
     id: 'home.featuredProjects.title',
@@ -65,12 +65,12 @@ export default defineMessages({
   mappingFlowTitle: {
     id: 'home.mappingFlow.title',
     defaultMessage:
-      "Each year, disasters around the world kill nearly {number} and affect or displace 200 million people. We're on a mission to change that. Here's how.",
+      "Each year, disasters around the world kill nearly {number} and affect or displace 200 million people. HOT is on a mission to change this.",
   },
   mappingFlowHeadline: {
     id: 'home.mappingFlow.headLine',
     defaultMessage:
-      'Through the Tasking Manager this mapping  projects are divided into smaller tasks allowing them to be completed by many different people working in the same area in a more streamlined and quick way.',
+      'Organizations use the Tasking Manager to create mapping projects for areas around the world where data is urgently needed.',
   },
   mappingCardTitle: {
     id: 'home.mappingFlow.cards.mapping.title',
@@ -86,26 +86,26 @@ export default defineMessages({
   },
   mappingCardDescription: {
     id: 'home.mappingFlow.cards.mapping.description',
-    defaultMessage: 'Volunteers trace satellite imagery into OpenStreetMap using Tasking Manager.',
+    defaultMessage: 'Volunteers use satellite imagery from OpenStreetMap to trace buildings, roadways, and other features.',
   },
   validationCardDescription: {
     id: 'home.mappingFlow.cards.validation.description',
     defaultMessage:
-      'Afterwards, seasoned volunteers double check the data entered assuring its quality.',
+      'Experience volunteers check the map data to ensure it is high quality.',
   },
   usingDataCardDescription: {
     id: 'home.mappingFlow.cards.usingData.description',
     defaultMessage:
-      'Humanitarian organizations use mapped data to plan risk reduction and disaster response activities.',
+      'Humanitarian organizations use map data to plan for disaster response activities and deliver critical services to those who need it.',
   },
   whoIsMappingTitle: {
     id: 'home.whoIsMapping.title',
-    defaultMessage: "Who's mapping",
+    defaultMessage: "Who is mapping",
   },
   whoIsMappingHeadline: {
     id: 'home.whoIsMapping.headline',
     defaultMessage:
-      'Global trusted organizations use Tasking Manager to reach those in need through maps.',
+      'Trusted global organizations use Tasking Manager to get the map data they need to reach those in need.',
   },
   organizationContactTitle: {
     id: 'home.whoIsMapping.contact.title',
@@ -125,7 +125,7 @@ export default defineMessages({
   },
   contacterTitle: {
     id: 'home.contact.contacterTitle',
-    defaultMessage: 'Send us a Note',
+    defaultMessage: 'Send us a message',
   },
   contacterName: {
     id: 'home.contact.contacterName',
@@ -142,7 +142,7 @@ export default defineMessages({
   contacterHeaderText: {
     id: 'home.contact.contacterHeadText',
     defaultMessage:
-      'Have an inquiry or some feedback for us? Fill out the form below to contact our team.',
+      'Want to discuss working with us? Or, have a question or feedback that you would like to share? Fill out the form below to contact our team.',
   },
   contacterSend: {
     id: 'home.contact.submit',
