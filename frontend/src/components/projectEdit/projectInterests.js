2ff975cf5d88dc3b8cab834353a73199246e4de6
Jorge Martinez <jorgemg0327@gmail.com>
2020-02-06 16:26:35 -0500

    Project interests frontend/backend

---
+++ frontend/src/components/projectEdit/projectInterests.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { InterestsList } from '../user/forms';
+
+export const ProjectInterests = ({ interests, projectInterests, setProjectInfo }) => {
+  const ids = projectInterests.map(i => i.id);
+  const projectSelected = interests.map(i => {
+    let selected = false;
+    if (ids.includes(i.id)) {
+      selected = true;
+    }
+    return { ...i, selected: selected };
+  });
+
+  const changeSelect = id => {
+    const index = projectSelected.findIndex(i => i.id === id);
+
+    const copy = projectSelected.map((interest, idx) => {
+      if (idx === index) {
+        interest.selected = !interest.selected;
+      }
+      return interest;
+    });
+
+    const newProjectInterests = copy.filter(i => i.selected === true);
+    newProjectInterests.map(i => delete i.selected);
+
+    setProjectInfo(p => {
+      return { ...p, interests: newProjectInterests };
+    });
+  };
+
+  return (
+    <InterestsList interests={projectSelected} field={'selected'} changeSelect={changeSelect} />
+  );
+};
