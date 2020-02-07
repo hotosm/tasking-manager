b457d3f9640dcdc799977aed86e4882eb8a87c25
Wille Marcel <wille.yyz@gmail.com>
2020-02-07 11:17:36 -0300

    avoid login popup blocking + increase popup window height

--- frontend/src/utils/login.js
+++ frontend/src/utils/login.js
@@ -4,7 +4,7 @@ import * as safeStorage from '../utils/safe_storage';
 // Code taken from https://github.com/mapbox/osmcha-frontend/blob/master/src/utils/create_popup.js
 export function createPopup(title: string = 'Authentication', location: string) {
   const width = 500;
-  const height = 600;
+  const height = 630;
   const settings = [
     ['width', width],
     ['height', height],
@@ -21,34 +21,33 @@ export function createPopup(title: string = 'Authentication', location: string) 
 }
 
 export const createLoginWindow = redirectTo => {
+  const popup = createPopup('OSM auth', '');
   let url = `system/authentication/login/?callback_url=${window.location.origin}/authorized/`;
-  fetchLocalJSONAPI(url)
-    .then(resp => {
-      createPopup('OSM auth', resp.auth_url);
-      // Perform token exchange.
-      window.authComplete = verifier => {
-        const tokens = new URLSearchParams({
-          oauth_token: resp.oauth_token,
-          oauth_token_secret: resp.oauth_token_secret,
+  fetchLocalJSONAPI(url).then(resp => {
+    popup.location = resp.auth_url;
+    // Perform token exchange.
+    window.authComplete = verifier => {
+      const tokens = new URLSearchParams({
+        oauth_token: resp.oauth_token,
+        oauth_token_secret: resp.oauth_token_secret,
+      }).toString();
+      let callback_url = `system/authentication/callback/?${tokens}&oauth_verifier=${verifier}`;
+
+      const emailAddress = safeStorage.getItem('email_address');
+      if (emailAddress !== null) {
+        callback_url += `&email_address=${emailAddress}`;
+        safeStorage.removeItem('email_address');
+      }
+
+      fetchLocalJSONAPI(callback_url).then(res => {
+        const params = new URLSearchParams({
+          username: res.username,
+          session_token: res.session_token,
+          picture: res.picture,
         }).toString();
-        let callback_url = `system/authentication/callback/?${tokens}&oauth_verifier=${verifier}`
-
-        const emailAddress = safeStorage.getItem('email_address')
-        if (emailAddress !== null) {
-          callback_url += `&email_address=${emailAddress}`
-          safeStorage.removeItem('email_address')
-        }
-
-        fetchLocalJSONAPI(callback_url)
-          .then(res => {
-            const params = new URLSearchParams({
-              username: res.username,
-              session_token: res.session_token,
-              picture: res.picture,
-            }).toString();
-            let redirectUrl = `/authorized/?${params}`;
-            window.location.href = redirectUrl;
-          });
-      };
-    });
+        let redirectUrl = `/authorized/?${params}`;
+        window.location.href = redirectUrl;
+      });
+    };
+  });
 };
