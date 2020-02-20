7a0a4cce8ecb5db2b6a7a55110f37d2d0afb83d9
Ramya Ragupathy <r.ragupathy85@gmail.com>
2020-02-20 14:41:45 +0530

    Handle invalid project ID
    Remove unused references

--- server/api/projects/activities.py
+++ server/api/projects/activities.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource, current_app, request
-from server.services.stats_service import StatsService, NotFound
+from server.services.stats_service import StatsService
+from server.services.project_service import ProjectService
+from server.models.postgis.utils import NotFound
 
 
 class ProjectsActivitiesAPI(Resource):
@@ -31,11 +33,15 @@ class ProjectsActivitiesAPI(Resource):
                 description: Internal Server Error
         """
         try:
+            ProjectService.get_project_by_id(project_id)
+        except NotFound as e:
+            current_app.logger.error(f"Error validating project: {str(e)}")
+            return {"Error": "Project not found"}, 404
+
+        try:
             page = int(request.args.get("page")) if request.args.get("page") else 1
             activity = StatsService.get_latest_activity(project_id, page)
             return activity.to_primitive(), 200
-        except NotFound:
-            return {"Error": "No activity on project"}, 404
         except Exception as e:
             error_msg = f"User GET - unhandled error: {str(e)}"
             current_app.logger.critical(error_msg)
@@ -66,10 +72,14 @@ class ProjectsLastActivitiesAPI(Resource):
                 description: Internal Server Error
         """
         try:
+            ProjectService.get_project_by_id(project_id)
+        except NotFound as e:
+            current_app.logger.error(f"Error validating project: {str(e)}")
+            return {"Error": "Project not found"}, 404
+
+        try:
             activity = StatsService.get_last_activity(project_id)
             return activity.to_primitive(), 200
-        except NotFound:
-            return {"Error": "No activity on project"}, 404
         except Exception as e:
             error_msg = f"User GET - unhandled error: {str(e)}"
             current_app.logger.critical(error_msg)
