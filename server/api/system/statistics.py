1f17b2bd7c8c5eedd3756b6e66a6f0035901eb41
Ramya Ragupathy <r.ragupathy85@gmail.com>
2020-02-25 20:45:42 +0530

    Remove favorite project activity check
    Remove projectsMapped from UserDTO
    
    Remove favorite project check from login call
    
    Remove mock messages
    
    Clean up UserDTOs
    
    Handle home page stats for faster load
    
    Remove accidental check-in of task.py
    
    Remove last updated filter on tasks
    
    Remove ununsed sqlachemy func

--- server/api/system/statistics.py
+++ server/api/system/statistics.py
@@ -1,5 +1,7 @@
 from flask_restful import Resource, current_app
 from server.services.stats_service import StatsService
+from flask_restful import request
+from distutils.util import strtobool
 
 
 class SystemStatisticsAPI(Resource):
@@ -11,6 +13,12 @@ class SystemStatisticsAPI(Resource):
           - system
         produces:
           - application/json
+        parameters:
+        - in: query
+          name: abbreviated
+          type: boolean
+          description: Set to false if complete details on projects including total area, campaigns, orgs are required
+          default: True
         responses:
             200:
                 description: Project stats
@@ -18,7 +26,13 @@ class SystemStatisticsAPI(Resource):
                 description: Internal Server Error
         """
         try:
-            stats = StatsService.get_homepage_stats()
+            abbreviated = (
+                strtobool(request.args.get("abbreviated"))
+                if request.args.get("abbreviated")
+                else True
+            )
+
+            stats = StatsService.get_homepage_stats(abbreviated)
             return stats.to_primitive(), 200
         except Exception as e:
             error_msg = f"Unhandled error: {str(e)}"
