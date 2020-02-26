7811ed8a2f01d9720624e7d4db1925cf735d0b01
Felix Delattre <felix@delattre.de>
2020-02-26 13:44:45 +0000

    Update languages for TM4

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,35 +5,22 @@ import WebFont from 'webfontloader';
 import { addLocaleData } from 'react-intl';
 
 import ar from 'react-intl/locale-data/ar';
-import lt from 'react-intl/locale-data/lt';
-import ja from 'react-intl/locale-data/ja';
-import pt from 'react-intl/locale-data/pt';
-import en from 'react-intl/locale-data/en';
-import si from 'react-intl/locale-data/si';
-import uk from 'react-intl/locale-data/uk';
-import ta from 'react-intl/locale-data/ta';
-import nl from 'react-intl/locale-data/nl';
-import id from 'react-intl/locale-data/id';
-import gl from 'react-intl/locale-data/gl';
-import mg from 'react-intl/locale-data/mg';
-import zh from 'react-intl/locale-data/zh';
-import es from 'react-intl/locale-data/es';
 import cs from 'react-intl/locale-data/cs';
-import pl from 'react-intl/locale-data/pl';
-import nb from 'react-intl/locale-data/nb';
-import fa from 'react-intl/locale-data/fa';
-import vi from 'react-intl/locale-data/vi';
-import fr from 'react-intl/locale-data/fr';
 import de from 'react-intl/locale-data/de';
+import en from 'react-intl/locale-data/en';
+import es from 'react-intl/locale-data/es';
+import fr from 'react-intl/locale-data/fr';
+import id from 'react-intl/locale-data/id';
 import it from 'react-intl/locale-data/it';
-import ru from 'react-intl/locale-data/ru';
-import sl from 'react-intl/locale-data/sl';
+import ja from 'react-intl/locale-data/ja';
+import mg from 'react-intl/locale-data/mg';
+import ml from 'react-intl/locale-data/ml';
+import nl from 'react-intl/locale-data/nl';
+import pt from 'react-intl/locale-data/pt';
+import sw from 'react-intl/locale-data/sw';
 import tl from 'react-intl/locale-data/tl';
 import tr from 'react-intl/locale-data/tr';
-import sw from 'react-intl/locale-data/sw';
-import da from 'react-intl/locale-data/da';
-import hu from 'react-intl/locale-data/hu';
-import fi from 'react-intl/locale-data/fi';
+import uk from 'react-intl/locale-data/uk';
 
 import App from './App';
 import { store } from './store';
@@ -50,35 +37,22 @@ WebFont.load({
 
 addLocaleData([
   ...ar,
-  ...lt,
-  ...ja,
-  ...pt,
-  ...en,
-  ...si,
-  ...uk,
-  ...ta,
-  ...nl,
-  ...id,
-  ...gl,
-  ...mg,
-  ...zh,
-  ...es,
   ...cs,
-  ...pl,
-  ...nb,
-  ...fa,
-  ...vi,
-  ...fr,
   ...de,
+  ...en,
+  ...es,
+  ...fr,
+  ...id,
   ...it,
-  ...ru,
-  ...sl,
+  ...ja,
+  ...mg,
+  ...ml,
+  ...nl,
+  ...pt,
   ...sw,
-  ...tr,
   ...tl,
-  ...da,
-  ...hu,
-  ...fi,
+  ...tr,
+  ...uk,
 ]);
 
 ReactDOM.render(
