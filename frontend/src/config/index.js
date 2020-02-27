f8bae8b1b0af63ee0dce66258d9e2cd77ba38bfe
Wille Marcel <wille.yyz@gmail.com>
2020-02-27 10:14:50 -0300

    read osm user stats from the edits stats host

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -26,8 +26,6 @@ export const ORG_FB = process.env.REACT_APP_ORG_FB || 'https://www.facebook.com'
 export const ORG_INSTAGRAM = process.env.REACT_APP_ORG_INSTAGRAM || 'https://www.instagram.com';
 export const ORG_YOUTUBE = process.env.REACT_APP_ORG_YOUTUBE || 'https://www.youtube.com';
 export const ORG_GITHUB = process.env.REACT_APP_ORG_GITHUB || 'https://github.com/';
-export const OSM_STATS_URL =
-  process.env.REACT_APP_OSM_STATS_URL || 'https://osm-stats-prod-api.azurewebsites.net/';
 
 export const MAX_FILESIZE = parseInt(process.env.REACT_APP_MAX_FILESIZE) || 5000000; // bytes
 
