62c23836206ed2e3e78e7e1867108b406e6c776a
Jorge Martinez <jorgemg0327@gmail.com>
2020-02-27 10:24:58 -0500

    fallback for edits api url

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -3,7 +3,8 @@ export const API_VERSION = process.env.REACT_APP_API_VERSION || 'v2';
 export const API_URL = process.env.REACT_APP_BASE_URL
   ? new URL('/api/' + API_VERSION + '/', process.env.REACT_APP_BASE_URL)
   : 'http://127.0.0.1:5000/api/' + API_VERSION + '/';
-export const EDITS_API_URL = process.env.REACT_APP_EDITS_API_URL || '';
+export const EDITS_API_URL =
+  process.env.REACT_APP_EDITS_API_URL || 'https://osm-stats-production-api.azurewebsites.net/';
 
 // APPLICATION SETTINGS
 export const DEFAULT_LOCALE = process.env.REACT_APP_DEFAULT_LOCALE || 'en';
