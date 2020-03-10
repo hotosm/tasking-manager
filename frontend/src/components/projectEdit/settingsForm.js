9e3dce8daaa890229d1dd7e5761eaa41ab8ce13d
Wille Marcel <wille.yyz@gmail.com>
2020-03-10 09:31:20 -0300

    fix error when custom editor is removed + change custom editor name display

--- frontend/src/components/projectEdit/settingsForm.js
+++ frontend/src/components/projectEdit/settingsForm.js
@@ -57,7 +57,7 @@ export const SettingsForm = ({ languages, defaultLocale }) => {
             {elm.label}
           </label>
         ))}
-        {projectInfo.hasOwnProperty('customEditor') && (
+        {projectInfo.hasOwnProperty('customEditor') && projectInfo.customEditor && (
           <label className="db pv2">
             <input
               className="mr2"
@@ -67,7 +67,8 @@ export const SettingsForm = ({ languages, defaultLocale }) => {
               type="checkbox"
               value={'CUSTOM'}
             />
-            {projectInfo.customEditor.name}
+            <FormattedMessage {...messages.customEditor} />
+            <span className="">: {projectInfo.customEditor.name}</span>
           </label>
         )}
       </div>
@@ -88,7 +89,7 @@ export const SettingsForm = ({ languages, defaultLocale }) => {
             {elm.label}
           </label>
         ))}
-        {projectInfo.hasOwnProperty('customEditor') && (
+        {projectInfo.hasOwnProperty('customEditor') && projectInfo.customEditor && (
           <label className="db pv2">
             <input
               className="mr2"
@@ -98,7 +99,8 @@ export const SettingsForm = ({ languages, defaultLocale }) => {
               type="checkbox"
               value={'CUSTOM'}
             />
-            {projectInfo.customEditor.name}
+            <FormattedMessage {...messages.customEditor} />
+            <span className="">: {projectInfo.customEditor.name}</span>
           </label>
         )}
       </div>
