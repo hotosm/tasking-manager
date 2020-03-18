ef1240c0a01bedbc51541a0053a402bb15cb8340
Dakota Benjamin <dakota.benjamin@hotosm.org>
2020-03-18 14:20:43 -0400

    Add frontend app route53 record config to cfn

--- scripts/aws/cloudformation/tasking-manager.template.js
+++ scripts/aws/cloudformation/tasking-manager.template.js
@@ -99,6 +99,10 @@ const Parameters = {
     Description: 'TM_DEFAULT_CHANGESET_COMMENT environment variable',
     Type: 'String'
   },
+  TaskingManagerURL: {
+    Description: 'URL for setting CNAME in Distribution',
+    Type: 'String'
+  }
 };
 
 const Conditions = {
@@ -501,8 +505,7 @@ const Resources = {
         DNSName: cf.getAtt('TaskingManagerLoadBalancer', 'DNSName'),
         HostedZoneId: cf.getAtt('TaskingManagerLoadBalancer', 'CanonicalHostedZoneID')
       },
-      HostedZoneId: 'Z2O929GW6VWG99', // Should we configure this?
-
+      HostedZoneId: 'Z2O929GW6VWG99',
     }
   },
   TaskingManagerTargetGroup: {
@@ -514,6 +517,7 @@ const Resources = {
       HealthCheckTimeoutSeconds: 10,
       HealthyThresholdCount: 3,
       UnhealthyThresholdCount: 3,
+      // HealthCheckPath: '/api/v2/system/heartbeat/', TODO get working path
       Port: 8000,
       Protocol: 'HTTP',
       VpcId: cf.importValue(cf.join('-', ['hotosm-network-production', 'default-vpc', cf.region])),
@@ -617,6 +621,9 @@ const Resources = {
     Properties: {
       DistributionConfig: {
         DefaultRootObject: 'index.html',
+        Aliases: [
+          cf.ref('TaskingManagerURL')
+        ],
         Enabled: true,
         Origins: [{
           Id: cf.join('-', [cf.stackName, 'react-app']),
@@ -655,6 +662,18 @@ const Resources = {
           SslSupportMethod: 'sni-only'
         }
       }
+    }
+  },
+  TaskingManagerRoute53: {
+    Type: 'AWS::Route53::RecordSet',
+    Properties: {
+      Name: cf.ref('TaskingManagerURL'),
+      Type: 'A',
+      AliasTarget: {
+        DNSName: cf.getAtt('TaskingManagerReactCloudfront', 'DomainName'),
+        HostedZoneId: 'Z2FDTNDATAQYW2'
+      },
+      HostedZoneId: 'Z2O929GW6VWG99',
     }
   }
 };
