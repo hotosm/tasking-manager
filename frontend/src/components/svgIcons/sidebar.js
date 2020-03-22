a9d23e4a89d9622f469c4595b283c741bfbde35d
Wille Marcel <wille.yyz@gmail.com>
2020-03-21 22:26:23 -0300

    fix production build error and sidebar icon props

--- frontend/src/components/svgIcons/sidebar.js
+++ frontend/src/components/svgIcons/sidebar.js
@@ -4,7 +4,7 @@ export class SidebarIcon extends React.PureComponent {
   render() {
     return (
       <svg width="20px" height="20px" viewBox="0 0 20 20" {...this.props}>
-        <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
           <rect stroke="currentColor" x="0.5" y="3.5" width="19" height="13" rx="2"></rect>
           <rect fill="currentColor" x="13" y="5" width="5" height="10"></rect>
         </g>
