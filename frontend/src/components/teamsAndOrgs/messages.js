b4bae2e054cead3e515e2e02597e0d1fc7a490d7
Wille Marcel <wille.yyz@gmail.com>
2020-03-26 07:31:59 -0300

    restore deleted translation message

--- frontend/src/components/teamsAndOrgs/messages.js
+++ frontend/src/components/teamsAndOrgs/messages.js
@@ -164,6 +164,10 @@ export default defineMessages({
     id: 'management.settings',
     defaultMessage: 'Settings',
   },
+  searchManagers: {
+    id: 'management.placeholder.search_managers',
+    defaultMessage: 'Search for Project Manager users',
+  },
   searchUsers: {
     id: 'management.placeholder.search_users',
     defaultMessage: 'Search for Tasking Manager users',
