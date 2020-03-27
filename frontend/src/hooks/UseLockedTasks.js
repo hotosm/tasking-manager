4af67b929e01318ad9baec5389ef88efda531b62
Wille Marcel <wille.yyz@gmail.com>
2020-03-27 08:36:58 -0300

    update locked tasks endpoints

--- frontend/src/hooks/UseLockedTasks.js
+++ frontend/src/hooks/UseLockedTasks.js
@@ -3,17 +3,17 @@ import { useSelector, useDispatch } from 'react-redux';
 
 import { fetchLocalJSONAPI } from '../network/genericJSONRequest';
 
-export const useGetLockedTasks = taskId => {
-  const lockedTasks = useSelector(state => state.lockedTasks);
+export const useGetLockedTasks = (taskId) => {
+  const lockedTasks = useSelector((state) => state.lockedTasks);
   return lockedTasks;
 };
 
 export const useFetchLockedTasks = () => {
-  const token = useSelector(state => state.auth.get('token'));
+  const token = useSelector((state) => state.auth.get('token'));
   const dispatch = useDispatch();
   const memoCallback = useCallback(async () => {
     if (token) {
-      const lockedTasks = await fetchLocalJSONAPI('projects/tasks/queries/own/locked/', token);
+      const lockedTasks = await fetchLocalJSONAPI('users/queries/tasks/locked/', token);
       dispatch({ type: 'SET_LOCKED_TASKS', tasks: lockedTasks.lockedTasks || [] });
       dispatch({ type: 'SET_PROJECT', project: lockedTasks.projectId || null });
       dispatch({ type: 'SET_TASKS_STATUS', status: lockedTasks.taskStatus || null });
