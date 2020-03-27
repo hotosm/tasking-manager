6518a526b2ca7e472c6040cbe6469c2130387b0a
Wille Marcel <wille.yyz@gmail.com>
2020-03-27 09:55:44 -0300

    handle projects with empty instructions

--- frontend/src/components/taskSelection/instructions.js
+++ frontend/src/components/taskSelection/instructions.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { htmlFromMarkdown } from '../../utils/htmlFromMarkdown';
 
 export function ProjectInstructions({ instructions }: Object) {
-  const htmlInstructions = { __html: '' } || (instructions && htmlFromMarkdown(instructions));
+  const htmlInstructions = instructions ? htmlFromMarkdown(instructions) : { __html: '' };
   return (
     <div
       className="markdown-content base-font blue-dark"
