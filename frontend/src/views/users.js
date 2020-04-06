87c5189e234bbf73047270909bd6ea6e24b509ef
Wille Marcel <wille.yyz@gmail.com>
2020-04-06 07:53:48 -0300

    Clear filter user button (#2647)
    * Add clear filter button to userList
    
    * only show clear filters on user management page if needed + fix style
    
    Co-authored-by: TARS <ritvikjain99@gmail.com>

--- frontend/src/views/users.js
+++ frontend/src/views/users.js
@@ -5,14 +5,15 @@ import messages from './messages';
 import { SearchNav, UsersTable } from '../components/user/list';
 
 export const UsersList = () => {
-  const [filters, setFilters] = useState({ level: 'ALL', role: 'ALL', username: '', page: 1 });
+  const initialFilters = { level: 'ALL', role: 'ALL', username: '', page: 1 };
+  const [filters, setFilters] = useState(initialFilters);
 
   return (
     <div className="ph1 blue-dark">
       <h3 className="barlow-condensed f2 ma0 pv3 mt1 v-mid dib ttu pl2 pl0-l">
         <FormattedMessage {...messages.manageUsers} />
       </h3>
-      <SearchNav filters={filters} setFilters={setFilters} />
+      <SearchNav filters={filters} setFilters={setFilters} initialFilters={initialFilters} />
       <div className="w-50-l w-70-m w-100 mb4">
         <UsersTable filters={filters} setFilters={setFilters} />
       </div>
