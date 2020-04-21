9280ca3073f274c6d5aff7ef56e33f7b09d4c00b
Jorge Martinez <jorgemg0327@gmail.com>
2020-04-21 10:15:47 -0500

    Code refactor and small adjustments

--- frontend/src/views/quickstart.js
+++ frontend/src/views/quickstart.js
@@ -20,163 +20,148 @@ import QuickstartStep9Picture from '../assets/img/quickstart/quickstart-step-9.j
 
 export function QuickstartPage() {
   useSetTitleTag('Quickstart guide');
+
+  const steps = [
+    {
+      message: 'quickstartStep1',
+      values: {
+        tmHomepage: (
+          <a className="link red fw5" href="/">
+            Tasking Manager
+          </a>
+        ),
+        signUp: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...headerMessages.signUp} />
+          </span>
+        ),
+      },
+      img: QuickstartStep1Picture,
+    },
+    {
+      message: 'quickstartStep2',
+      img: QuickstartStep2Picture,
+    },
+    {
+      message: 'quickstartStep3',
+      img: QuickstartStep3Picture,
+      values: {
+        signUp: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...headerMessages.signUp} />
+          </span>
+        ),
+      },
+    },
+    {
+      message: 'quickstartStep4',
+      img: QuickstartStep4Picture,
+      values: {
+        tmHomepage: (
+          <a className="link red fw5" href="/">
+            Tasking Manager
+          </a>
+        ),
+        logIn: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...headerMessages.logIn} />
+          </span>
+        ),
+      },
+    },
+    {
+      message: 'quickstartStep5',
+      img: QuickstartStep5Picture,
+      values: {
+        exploreProjects: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...headerMessages.exploreProjects} />
+          </span>
+        ),
+      },
+    },
+    {
+      message: 'quickstartStep6',
+      img: QuickstartStep6Picture,
+      values: {
+        contribute: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...projectMessages.contribute} />
+          </span>
+        ),
+      },
+    },
+    {
+      message: 'quickstartStep7',
+      img: QuickstartStep7Picture,
+      values: {
+        mapATask: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...taskMessages.mapATask} />
+          </span>
+        ),
+      },
+      note: 'quickstartStep7Note',
+    },
+    {
+      message: 'quickstartStep8',
+      img: QuickstartStep8Picture,
+      values: {
+        learnPage: (
+          <a className="link red fw5" href="learn">
+            <FormattedMessage {...messages.learnPages} />
+          </a>
+        ),
+        submitTask: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...taskMessages.submitTask} />
+          </span>
+        ),
+      },
+      note: 'quickstartStep8Note',
+    },
+    {
+      message: 'quickstartStep9',
+      img: QuickstartStep9Picture,
+      values: {
+        submitTask: (
+          <span className="b bg-grey-light">
+            <FormattedMessage {...taskMessages.submitTask} />
+          </span>
+        ),
+      },
+      note: 'quickstartStep9Note',
+    },
+  ];
+
   return (
     <div className="pt180 pull-center bg-white blue-dark cf lh-copy f5">
       <TopBar pageName={<FormattedMessage {...messages.quickstartTitle} />} />
       <div className="pl6-l ph4 mr4-l pt4 w-60-l">
         <p>{<FormattedMessage {...messages.quickstartIntro} />}</p>
         <p className="i">{<FormattedMessage {...messages.quickstartStep1Note} />}</p>
-        <p><span className="b">1)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep1}
-            values={{
-              tmHomepage: (
-                <a className="link red fw5" href="/">
-                  Tasking Manager
-                </a>
-              ),
-              signUp: (
-                <span className="b bg-grey-light"><FormattedMessage {...headerMessages.signUp} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep1Picture}
-            alt="Quickstart guide screenshot of step 1"
-          />
+
+        <div>
+          {steps.map((v, i) => {
+            const idx = i + 1;
+            return (
+              <div className="pv2">
+                <p>
+                  <span className="b mr1">{idx}.</span>
+                  {<FormattedMessage {...messages[v.message]} values={v.values} />}
+                  <div className="w-60-l cf flex items-center shadow-4 mv3">
+                    <img src={v.img} alt={`Quickstart guide screenshot of step ${idx}`} />
+                  </div>
+                </p>
+                {v.note ? (
+                  <p className="i">
+                    {<FormattedMessage {...messages[v.note]} values={v.values} />}
+                  </p>
+                ) : null}
+              </div>
+            );
+          })}
         </div>
-        <p><span className="b">2)</span> {<FormattedMessage {...messages.quickstartStep2} />}</p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep2Picture}
-            alt="Quickstart guide screenshot of step 2"
-          />
-        </div>
-        <p><span className="b">3)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep3}
-            values={{
-              signUp: (
-                <span className="b bg-grey-light"><FormattedMessage {...headerMessages.signUp} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep3Picture}
-            alt="Quickstart guide screenshot of step 3"
-          />
-        </div>
-        <p><span className="b">4)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep4}
-            values={{
-              tmHomepage: (
-                <a className="link red fw5" href="/">
-                  Tasking Manager
-                </a>
-              ),
-              logIn: (
-                <span className="b bg-grey-light"><FormattedMessage {...headerMessages.logIn} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep4Picture}
-            alt="Quickstart guide screenshot of step 4"
-          />
-        </div>
-        <p><span className="b">5)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep5}
-            values={{
-              exploreProjects: (
-                <span className="b bg-grey-light"><FormattedMessage {...headerMessages.exploreProjects} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep5Picture}
-            alt="Quickstart guide screenshot of step 5"
-          />
-        </div>
-        <p><span className="b">6)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep6}
-            values={{
-              contribute: (
-                <span className="b bg-grey-light"><FormattedMessage {...projectMessages.contribute} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep6Picture}
-            alt="Quickstart guide screenshot of step 6"
-          />
-        </div>
-        <p><span className="b">7)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep7}
-            values={{
-              mapATask: (
-                <span className="b bg-grey-light"><FormattedMessage {...taskMessages.mapATask} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep7Picture}
-            alt="Quickstart guide screenshot of step 7"
-          />
-        </div>
-        <p className="i">{<FormattedMessage {...messages.quickstartStep7Note} />}</p>
-        <p><span className="b">8)</span> {
-          <FormattedMessage {...messages.quickstartStep8} />}</p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep8Picture}
-            alt="Quickstart guide screenshot of step 8"
-          />
-        </div>
-        <p className="i">{
-          <FormattedMessage
-            {...messages.quickstartStep8Note}
-            values={{
-              learnPage: (
-                <a className="link red fw5" href="learn">
-                  <FormattedMessage {...messages.learnPages} />
-                </a>
-              ),
-            }}
-          />}
-        </p>
-        <p><span className="b">9)</span> {
-          <FormattedMessage
-            {...messages.quickstartStep9}
-            values={{
-              submitTask: (
-                <span className="b bg-grey-light"><FormattedMessage {...taskMessages.submitTask} /></span>
-              ),
-            }}
-          />}
-        </p>
-        <div className="w-60-l cf flex items-center shadow-4">
-          <img
-            src={QuickstartStep9Picture}
-            alt="Quickstart guide screenshot of step 9"
-          />
-        </div>
-        <p className="i">{<FormattedMessage {...messages.quickstartStep9Note} />}</p>
       </div>
     </div>
   );
