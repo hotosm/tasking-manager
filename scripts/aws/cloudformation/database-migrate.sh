6a68c62f16df47670eb406d5c844dc5d5458e164
eternaltyro <eternaltyro@gmail.com>
2020-04-22 22:10:10 +0530

    Add EnvironmentFile to Systemd service
    - Add EnvironmentFile to systemd service pointing to /opt/tasking-manager.env
      which is placed by CloudFormation
    - Import the same env file into database migration script

--- scripts/aws/cloudformation/database-migrate.sh
+++ scripts/aws/cloudformation/database-migrate.sh
@@ -2,10 +2,15 @@
 set -e
 
 BASE_DIR=/tasking-manager
+
+# Source ENV file placed by CloudFormation
+source /opt/tasking-manager.env
+
 POSTGRES_DB=${POSTGRES_DB:-tm}
 POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-blahblah123}
 POSTGRES_USER=${POSTGRES_USER:-taskingmanager}
 POSTGRES_ENDPOINT=${POSTGRES_ENDPOINT:-localhost}
+
 
 cd ${BASE_DIR}
 ${BASE_DIR:-/tasking-manager}/venv/bin/python3.6 manage.py db upgrade
