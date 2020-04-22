72c04aeed828fc26a521688e47d44c98828d2c43
eternaltyro <eternaltyro@gmail.com>
2020-04-22 20:48:15 +0530

    Fix afterInstall script - working dir

--- scripts/aws/cloudformation/database-migrate.sh
+++ scripts/aws/cloudformation/database-migrate.sh
@@ -7,4 +7,5 @@ POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-blahblah123}
 POSTGRES_USER=${POSTGRES_USER:-taskingmanager}
 POSTGRES_ENDPOINT=${POSTGRES_ENDPOINT:-localhost}
 
+cd ${BASE_DIR}
 ${BASE_DIR:-/tasking-manager}/venv/bin/python3.6 manage.py db upgrade
