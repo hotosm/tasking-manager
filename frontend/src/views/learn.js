aad50d655e27162c11c1cb92bb30cc238faf4099
Wille Marcel <wille.yyz@gmail.com>
2020-04-22 14:00:59 -0300

    improve learn page responsiveness

--- frontend/src/views/learn.js
+++ frontend/src/views/learn.js
@@ -23,7 +23,7 @@ const LearnNav = ({ sections, section, setSection }) => {
   useSetTitleTag('Learn');
   return (
     <div className="w-50 w-100-m">
-      <ul className="pa0 ma0 list bg-tan">
+      <ul className="pa0 ma0 list bg-tan dib">
         {sections.map((s) => {
           return (
             <li
@@ -43,25 +43,27 @@ const LearnNav = ({ sections, section, setSection }) => {
 
 const Steps = ({ items }) => {
   return (
-    <div className="flex justify-between mv5 db-m relative">
+    <div className="w-100 cf relative">
       {items.map((v, i) => {
         return (
-          <div className="shadow-1 w-30 pa3 z-2 bg-white">
-            <img style={{ width: '36%' }} src={v.img} alt={v.message} />
+          <div className="w-third-ns w-100 fl pa2 z-2 bg-white">
+            <div className="shadow-1 pa3">
+              <img className="w-35" src={v.img} alt={v.message} />
 
-            <p className="blue-dark b f4 pt0">
-              <span className="mr1">{i + 1}.</span>
-              {<FormattedMessage {...messages[`${v.message}Title`]} />}
-            </p>
-            <p className="blue-grey lh-title f5">
-              {<FormattedMessage {...messages[`${v.message}Description`]} values={v.values} />}
-            </p>
+              <p className="blue-dark b f4 pt0">
+                <span className="mr1">{i + 1}.</span>
+                {<FormattedMessage {...messages[`${v.message}Title`]} />}
+              </p>
+              <p className="blue-grey lh-title f5">
+                {<FormattedMessage {...messages[`${v.message}Description`]} values={v.values} />}
+              </p>
+            </div>
           </div>
         );
       })}
       <div
         style={{ height: '60%' }}
-        className="w-100 bg-tan absolute bottom--2 right--2 z-1 "
+        className="w-100 bg-tan relative bottom--2 right--2 z-1 "
       ></div>
     </div>
   );
@@ -69,14 +71,14 @@ const Steps = ({ items }) => {
 
 const Intro = ({ section, messagesObjs }) => {
   return (
-    <div className="w-100 h5">
-      <div className="flex justify-between db-m center-m">
-        <div className="w-30">
+    <div className="w-100 cf">
+      <div className="w-100 cf">
+        <div className="w-30-ns w-100 fl">
           <p className="barlow-condensed f2 ttu b fw6">
             {<FormattedMessage {...messages[section]} />}
           </p>
         </div>
-        <div className="w-60 lh-copy f4">
+        <div className="w-70-ns w-100 fr lh-copy f4">
           <p className="b">{<FormattedMessage {...messages[messagesObjs.intro]} />}</p>
           <p className="f5">{<FormattedMessage {...messages[messagesObjs.description]} />}</p>
         </div>
@@ -91,23 +93,35 @@ const Tutorials = ({ tutorials }) => {
       <h3 className="f2 ttu barlow-condensed fw6">
         <FormattedMessage {...messages.learnTutorialsTitle} />
       </h3>
-      <div className="flex justify-start">
+      <div className="w-100 cf">
         {tutorials.map((v) => {
           return (
-            <div style={{ height: '20rem' }} className="w-25 shadow-4 h5 mr4">
-              <div style={{ height: '45%' }} className="bg-tan flex items-center justify-center">
-                <img src={v.img} alt={v.message} style={{ width: '55%' }} />
-              </div>
-              <div className="pa3">
-                <p>
-                  <a className="blue-dark b" rel="noopener noreferrer" target="_blank" href={v.url}>
-                    {<FormattedMessage {...messages[`${v.message}Title`]} />}
-                  </a>
-                </p>
+            <div style={{ height: '20rem' }} className="w-25-l w-third-m w-100 fl ph2">
+              <div className="shadow-4">
+                <div
+                  className="bg-tan w-100 tc h4"
+                  style={{
+                    background: `#f0efef url(${v.img}) no-repeat center`,
+                    backgroundSize: '55%',
+                  }}
+                  bac
+                ></div>
+                <div className="pa3" style={{ height: '12rem' }}>
+                  <p>
+                    <a
+                      className="blue-dark b"
+                      rel="noopener noreferrer"
+                      target="_blank"
+                      href={v.url}
+                    >
+                      {<FormattedMessage {...messages[`${v.message}Title`]} />}
+                    </a>
+                  </p>
 
-                <p className="blue-grey lh-title f5">
-                  {<FormattedMessage {...messages[`${v.message}Description`]} />}
-                </p>
+                  <p className="blue-grey lh-title f5">
+                    {<FormattedMessage {...messages[`${v.message}Description`]} />}
+                  </p>
+                </div>
               </div>
             </div>
           );
@@ -275,7 +289,7 @@ export const LearnPage = () => {
   return (
     <div className="pt180 pull-center">
       <TopBar pageName={<FormattedMessage {...messages.learn} />} />
-      <div className="ph6-l">
+      <div className="ph6-l ph4-m ph2">
         <LearnNav sections={sections} section={section} setSection={setSection} />
         <div className="w-100 mt3">{getSection(section, sections)}</div>
       </div>
