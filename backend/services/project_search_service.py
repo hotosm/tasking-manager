fa136baf4a20fa0943033c8d114a2ef3a3e5c0e1
ramyaragupathy <r.ragupathy85@gmail.com>
2020-05-07 15:34:59 +0530

    Removeauthor filter from manage section

--- backend/services/project_search_service.py
+++ backend/services/project_search_service.py
@@ -285,8 +285,7 @@ class ProjectSearchService:
             )
             orgs_managed = [org.id for org in user_orgs_list]
             org_projects = query.filter(Project.organisation_id.in_(orgs_managed))
-            my_projects = query.filter(Project.author_id == search_dto.managed_by)
-            query = org_projects.union(team_projects).union(my_projects)
+            query = org_projects.union(team_projects)
 
         all_results = query.all()
         paginated_results = query.paginate(search_dto.page, 14, True)
