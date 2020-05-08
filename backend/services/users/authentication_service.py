b00e72ce98dcfdb5dc7483dc9282f6e90a911292
Jorge Martinez <jorgemg0327@gmail.com>
2020-05-08 09:30:02 -0500

    Return user_id instead of user object within verify token

--- backend/services/users/authentication_service.py
+++ backend/services/users/authentication_service.py
@@ -37,9 +37,7 @@ def verify_token(token):
     tm.authenticated_user_id = (
         user_id  # Set the user ID on the decorator as a convenience
     )
-    return UserService.get_user_by_id(
-        user_id
-    ).id  # All tests passed token is good for the requested resource
+    return user_id  # All tests passed token is good for the requested resource
 
 
 class AuthServiceError(Exception):
