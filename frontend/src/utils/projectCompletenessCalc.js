d744c3ecb1b64b38f6106173ebc7532e56a07f01
Wille Marcel <wille.yyz@gmail.com>
2020-05-18 09:21:46 -0300

    avoid tasks selection page to crash the app while tasks are loading

--- frontend/src/utils/projectCompletenessCalc.js
+++ frontend/src/utils/projectCompletenessCalc.js
@@ -1,14 +1,21 @@
 export function computeCompleteness(tasks) {
-  const totalTasks = tasks.features.length;
-  const mapped = tasks.features.filter((task) => task.properties.taskStatus === 'MAPPED').length;
-  const validated = tasks.features.filter((task) => task.properties.taskStatus === 'VALIDATED')
-    .length;
-  const badImagery = tasks.features.filter((task) => task.properties.taskStatus === 'BADIMAGERY')
-    .length;
-
-  return {
-    percentMapped: parseInt(((mapped + validated) / (totalTasks - badImagery)) * 100),
-    percentValidated: parseInt((validated / (totalTasks - badImagery)) * 100),
-    percentBadImagery: parseInt((badImagery / totalTasks) * 100),
-  };
+  if (tasks && tasks.features) {
+    const totalTasks = tasks.features.length;
+    const mapped = tasks.features.filter((task) => task.properties.taskStatus === 'MAPPED').length;
+    const validated = tasks.features.filter((task) => task.properties.taskStatus === 'VALIDATED')
+      .length;
+    const badImagery = tasks.features.filter((task) => task.properties.taskStatus === 'BADIMAGERY')
+      .length;
+    return {
+      percentMapped: parseInt(((mapped + validated) / (totalTasks - badImagery)) * 100),
+      percentValidated: parseInt((validated / (totalTasks - badImagery)) * 100),
+      percentBadImagery: parseInt((badImagery / totalTasks) * 100),
+    };
+  } else {
+    return {
+      percentMapped: 0,
+      percentValidated: 0,
+      percentBadImagery: 0,
+    };
+  }
 }
