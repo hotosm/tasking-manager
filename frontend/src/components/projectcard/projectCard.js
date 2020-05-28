48ecd024018bae8cc169a7c521be5a5e93c3ac73
Wille Marcel <wille.yyz@gmail.com>
2020-05-28 19:22:02 -0300

    don't show edit project buttons on team membership page

--- frontend/src/components/projectcard/projectCard.js
+++ frontend/src/components/projectcard/projectCard.js
@@ -90,57 +90,52 @@ export function ProjectCard({
       onMouseLeave={() => setHovered(false)}
       className={`relative fl ${cardWidthClass} base-font w-50-m w-100 mb3 ${bottomButtonMargin} pr3 blue-dark mw5 `}
     >
-      <Link className={`no-underline color-inherit `} to={`/projects/${projectId}`}>
-        <article className={``}>
-          <div className={`${bottomButtonSpacer} ph3 ba br1 b--grey-light bg-white shadow-hover`}>
-            <div className="cf w-100">
-              <div className="w-40 tr fr">
-                {['DRAFT', 'ARCHIVED'].includes(status) ? (
-                  <ProjectStatusBox status={status} className={'pv1 ph1 dib'} />
-                ) : (
-                  <PriorityBox
-                    priority={priority}
-                    extraClasses={'pv1 ph2 dib'}
-                    hideMediumAndLow={!showBottomButtons}
-                    showIcon={priority !== 'URGENT'} // inside the cards, don't show the icon for urgent, due to the space required
-                  />
-                )}
-              </div>
-              <div className="w-60 cf pr1 red dib">
-                <img
-                  className="h2 pa1"
-                  src={organisationLogo}
-                  alt={organisationLogo ? organisationName : ''}
+      <Link className="no-underline color-inherit" to={`/projects/${projectId}`}>
+        <div className={`${bottomButtonSpacer} ph3 ba br1 b--grey-light bg-white shadow-hover`}>
+          <div className="cf w-100">
+            <div className="w-40 tr fr">
+              {['DRAFT', 'ARCHIVED'].includes(status) ? (
+                <ProjectStatusBox status={status} className={'pv1 ph1 dib'} />
+              ) : (
+                <PriorityBox
+                  priority={priority}
+                  extraClasses={'pv1 ph2 dib'}
+                  hideMediumAndLow={!showBottomButtons}
+                  showIcon={priority !== 'URGENT'} // inside the cards, don't show the icon for urgent, due to the space required
                 />
-              </div>
+              )}
             </div>
-            <div className="ma1 w-100">
-              <div className="f7 blue-grey mt2">#{projectId}</div>
-              <h3 title={name} className="pb2 mt2 f5 fw6 h3 lh-title overflow-y-hidden">
-                {name}
-              </h3>
-              <div className="tc f6">
-                <div className="w-100 tl pr2 f7 blue-light dib lh-title mb2 h2 overflow-y-hidden">
-                  {shortDescription} {campaignTag ? ' · ' + campaignTag : ''}
-                </div>
-                <ProjectTeaser totalContributors={totalContributors} lastUpdated={lastUpdated} />
-                <ProjectProgressBar
-                  percentMapped={percentMapped}
-                  percentValidated={percentValidated}
-                />
-                <div className="cf pt2 h2">
-                  <MappingLevelMessage
-                    level={mapperLevel}
-                    className="fl f7 mt1 ttc fw5 blue-grey"
-                  />
-                  <div className="fr">
-                    <DueDateBox dueDate={dueDate} />
-                  </div>
+            <div className="w-60 cf pr1 red dib">
+              <img
+                className="h2 pa1"
+                src={organisationLogo}
+                alt={organisationLogo ? organisationName : ''}
+              />
+            </div>
+          </div>
+          <div className="ma1 w-100">
+            <div className="f7 blue-grey mt2">#{projectId}</div>
+            <h3 title={name} className="pb2 mt2 f5 fw6 h3 lh-title overflow-y-hidden">
+              {name}
+            </h3>
+            <div className="tc f6">
+              <div className="w-100 tl pr2 f7 blue-light dib lh-title mb2 h2 overflow-y-hidden">
+                {shortDescription} {campaignTag ? ' · ' + campaignTag : ''}
+              </div>
+              <ProjectTeaser totalContributors={totalContributors} lastUpdated={lastUpdated} />
+              <ProjectProgressBar
+                percentMapped={percentMapped}
+                percentValidated={percentValidated}
+              />
+              <div className="cf pt2 h2">
+                <MappingLevelMessage level={mapperLevel} className="fl f7 mt1 ttc fw5 blue-grey" />
+                <div className="fr">
+                  <DueDateBox dueDate={dueDate} />
                 </div>
               </div>
             </div>
           </div>
-        </article>
+        </div>
       </Link>
       {showBottomButtonsHovered && bottomButtons}
     </div>
