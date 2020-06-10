e5be01eaa5b34553c16d5d78b988efa37c012271
Jorge Martinez <jorgemg0327@gmail.com>
2020-06-10 10:51:42 -0500

    Contributions tab enhancements

--- frontend/src/components/taskSelection/contributions.js
+++ frontend/src/components/taskSelection/contributions.js
@@ -6,9 +6,12 @@ import { injectIntl } from 'react-intl';
 import messages from './messages.js';
 import { UserAvatar } from '../user/avatar';
 import { CheckCircle } from '../checkCircle';
+import { MappedIcon, ValidatedIcon, AsteriskIcon, StarIcon, FullStarIcon } from '../svgIcons';
 import ProjectProgressBar from '../projectcard/projectProgressBar';
 import { useComputeCompleteness } from '../../hooks/UseProjectCompletenessCalc';
 import { OSMChaButton } from '../projectDetail/osmchaButton';
+import ReactTooltip from 'react-tooltip';
+import { FormattedMessage } from 'react-intl';
 
 const Contributions = (props) => {
   const mappingLevels = [
@@ -16,6 +19,7 @@ const Contributions = (props) => {
     { value: 'ADVANCED', label: props.intl.formatMessage(messages.mappingLevelADVANCED) },
     { value: 'INTERMEDIATE', label: props.intl.formatMessage(messages.mappingLevelINTERMEDIATE) },
     { value: 'BEGINNER', label: props.intl.formatMessage(messages.mappingLevelBEGINNER) },
+    { value: 'NEWUSER', label: props.intl.formatMessage(messages.mappingLevelNEWUSER) },
   ];
 
   const [level, setLevel] = useState(mappingLevels[0]);
@@ -64,9 +68,47 @@ const Contributions = (props) => {
   };
 
   let contributionsArray = props.contribsData.userContributions || [];
-  if (level.value !== 'ALL') {
+  if (level.value !== 'ALL' && level.value !== 'NEWUSER') {
     contributionsArray = contributionsArray.filter((u) => u.mappingLevel === level.value);
   }
+
+  if (level.value === 'NEWUSER') {
+    const monthFiltered = new Date();
+    monthFiltered.setMonth(monthFiltered.getMonth() - 1);
+    contributionsArray = contributionsArray
+      .map((u) => {
+        return { ...u, dateObj: new Date(u.dateRegistered) };
+      })
+      .filter((u) => u.dateObj > monthFiltered);
+  }
+
+  const MappingLevelIcon = ({ mappingLevel }) => {
+    let numberofIcons = null;
+    switch (mappingLevel) {
+      case 'BEGINNER':
+        numberofIcons = 1;
+        break;
+      case 'INTERMEDIATE':
+        numberofIcons = 2;
+        break;
+      case 'ADVANCED':
+        numberofIcons = 3;
+        break;
+      default:
+        return null;
+    }
+
+    let icons = [];
+    for (let i = 0; i < numberofIcons; i++) {
+      icons.push(<FullStarIcon className="h1 w1" />);
+    }
+    const emptyIcons = 3 - numberofIcons;
+    for (let i = 0; i < emptyIcons; i++) {
+      icons.push(<StarIcon className="h1 w1" />);
+    }
+
+    return <div>{icons}</div>;
+  };
 
   return (
     <div className="w-100 f5 pr4-l pr2 cf blue-dark bg-white">
@@ -94,30 +136,45 @@ const Contributions = (props) => {
                   props.activeUser === user.username ? 'b--blue-dark' : 'b--tan'
                 }`}
               >
-                <div className="w-30 fl dib truncate">
-                  <UserAvatar
-                    picture={user.pictureUrl}
-                    username={user.username}
-                    colorClasses="white bg-blue-grey"
-                  />
-                  <a
-                    className="blue-dark mr2 link"
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    href={`/users/${user.username}`}
-                  >
-                    {user.username}
-                  </a>
-                  <span className="blue-grey ttl dib-ns dn">
-                    ({props.intl.formatMessage(messages[`mappingLevel${user.mappingLevel}`])})
-                  </span>
+                <FormattedMessage {...messages.registered}>
+                  {(msg) => (
+                    <div
+                      className="w-20 fl dib truncate"
+                      data-tip={`${msg} ${user.dateRegistered}`}
+                    >
+                      <UserAvatar
+                        picture={user.pictureUrl}
+                        username={user.username}
+                        colorClasses="white bg-blue-grey"
+                      />
+                      <a
+                        className="blue-dark mr2 link"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        href={`/users/${user.username}`}
+                      >
+                        {user.username}
+                      </a>
+                    </div>
+                  )}
+                </FormattedMessage>
+                <ReactTooltip />
+                <div className="w-10 fl mt2">
+                  <FormattedMessage {...messages[`mappingLevel${user.mappingLevel}`]}>
+                    {(msg) => (
+                      <span className="blue-grey ttl dib-ns dn" data-tip={msg}>
+                        <MappingLevelIcon mappingLevel={user.mappingLevel} />
+                      </span>
+                    )}
+                  </FormattedMessage>
+                  <ReactTooltip />
                 </div>
                 <div
                   className="w-25 fl tr dib pointer pt2 truncate"
                   onClick={() => displayTasks(user.taskIds, 'MAPPED', user.username)}
                 >
+                  <MappedIcon className="h1 w1 blue-grey mr2" />
                   <span className="mr1 b self-start">{user.mapped}</span>
-                  <span className="ttl mr2">{props.intl.formatMessage(messages.mapped)}</span>
                   <CheckCircle
                     className={`${checkActiveUserAndStatus('MAPPED', user.username)} white`}
                   />
@@ -126,8 +183,8 @@ const Contributions = (props) => {
                   className="w-25 fl tr dib pointer pt2 truncate"
                   onClick={() => displayTasks(user.taskIds, 'VALIDATED', user.username)}
                 >
+                  <ValidatedIcon className="h1 w1 blue-grey mr2" />
                   <span className="mr1 b">{user.validated}</span>
-                  <span className="ttl mr2">{props.intl.formatMessage(messages.validated)}</span>
                   <CheckCircle
                     className={`${checkActiveUserAndStatus('VALIDATED', user.username)} white`}
                   />
@@ -136,8 +193,8 @@ const Contributions = (props) => {
                   className="w-20 fl tr dib pointer pt2 truncate"
                   onClick={() => displayTasks(user.taskIds, 'ALL', user.username)}
                 >
+                  <AsteriskIcon className="h1 w1 blue-grey mr2" />
                   <span className="mr1 b">{user.total}</span>
-                  <span className="ttl mr2">{props.intl.formatMessage(messages.total)}</span>
                   <CheckCircle
                     className={`${checkActiveUserAndStatus('ALL', user.username)} white`}
                   />
