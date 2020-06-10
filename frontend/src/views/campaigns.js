338b14c1c83a50af4b3cce5fcda4c493b146d63e
ramyaragupathy <r.ragupathy85@gmail.com>
2020-06-10 14:19:38 +0530

    Remove check icon reference

--- frontend/src/views/campaigns.js
+++ frontend/src/views/campaigns.js
@@ -18,6 +18,7 @@ import { Projects } from '../components/teamsAndOrgs/projects';
 import { FormSubmitButton, CustomButton } from '../components/button';
 import { DeleteModal } from '../components/deleteModal';
 import { useSetTitleTag } from '../hooks/UseMetaTags';
+import { CloseIcon } from '../components/svgIcons';
 
 
 export function ListCampaigns() {
