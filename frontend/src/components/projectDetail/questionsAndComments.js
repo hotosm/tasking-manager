f246a7edd763fdf6ae3092d4d0eb83fe44ad3a2b
Jorge Martínez Gómez <jorgemg0327@gmail.com>
2020-06-22 09:31:29 -0500

    Fix username popup in textarea component (#3169)
    * Fix username popup in textarea component
    
    * fix position and z-index of autocomplete user list
    
    Co-authored-by: Wille Marcel <wille.yyz@gmail.com>

--- frontend/src/components/projectDetail/questionsAndComments.js
+++ frontend/src/components/projectDetail/questionsAndComments.js
@@ -10,6 +10,7 @@ import { Button } from '../button';
 import { CurrentUserAvatar, UserAvatar } from '../user/avatar';
 import { htmlFromMarkdown } from '../../utils/htmlFromMarkdown';
 import { pushToLocalJSONAPI, fetchLocalJSONAPI } from '../../network/genericJSONRequest';
+import '@webscopeio/react-textarea-autocomplete/style.css';
 
 const formatUserNamesToLink = (text) => {
   const regex = /@\[([^\]]+)\]/gi;
@@ -34,11 +35,17 @@ const formatUserNamesToLink = (text) => {
   return text;
 };
 
+const Item = ({ entity: { name } }) => (
+  <div className="w-100 pv2 ph3 tc bg-tan hover-bg-blue-grey blue-grey hover-white pointer">
+    {`${name}`}
+  </div>
+);
+
 export const UserFetchTextarea = ({ value, setValueFn, token }) => {
   const fetchUsers = async (user) => {
     const url = `users/queries/filter/${user}/`;
-
     let userItems;
+
     try {
       const res = await fetchLocalJSONAPI(url, token);
       userItems = res.usernames.map((u) => {
@@ -51,30 +58,23 @@ export const UserFetchTextarea = ({ value, setValueFn, token }) => {
     return userItems;
   };
 
-  const Item = ({ entity: { name } }) => (
-    <div className="w-100 f6 pv2 ph3 f5 tc bg-tan blue-grey hover-bg-blue-grey hover-white pointer">
-      {`${name}`}
-    </div>
-  );
-
   return (
-    <div className="relative">
-      <ReactTextareaAutocomplete
-        value={value}
-        listClassName="list ma0 pa0 ba b--grey-light bg-blue-grey w-40 overflow-auto absolute bottom--1"
-        onChange={setValueFn}
-        className="w-100 f5 pa2"
-        loadingComponent={() => <span>Loading</span>}
-        rows={3}
-        trigger={{
-          '@': {
-            dataProvider: fetchUsers,
-            component: Item,
-            output: (item, trigger) => '@[' + item.name + ']',
-          },
-        }}
-      />
-    </div>
+    <ReactTextareaAutocomplete
+      value={value}
+      listClassName="list ma0 pa0 ba b--grey-light bg-blue-grey overflow-y-scroll base-font f5 relative z-5"
+      listStyle={{ maxHeight: '16rem' }}
+      onChange={setValueFn}
+      className="w-100 f5 pa2"
+      loadingComponent={() => <span></span>}
+      rows={3}
+      trigger={{
+        '@': {
+          dataProvider: fetchUsers,
+          component: Item,
+          output: (item, trigger) => '@[' + item.name + ']',
+        },
+      }}
+    />
   );
 };
 
@@ -158,8 +158,12 @@ export const QuestionsAndComments = ({ projectId }) => {
             className="tr w-90 center pv3"
           />
         )}
-        {token !== null && (
+        {token ? (
           <PostProjectComment projectId={projectId} token={token} setStat={setStat} />
+        ) : (
+          <div>
+            <p>You need to log in to be able to post comments.</p>
+          </div>
         )}
       </div>
     </div>
