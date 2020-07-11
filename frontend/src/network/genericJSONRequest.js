ec96fba0f539a7d372217f4325b95cd37f8171fc
Wille Marcel <wille.yyz@gmail.com>
2020-07-10 22:44:44 -0300

    delete unused network functions

--- frontend/src/network/genericJSONRequest.js
+++ frontend/src/network/genericJSONRequest.js
@@ -14,33 +14,6 @@ export function fetchExternalJSONAPI(url): Promise<*> {
     });
 }
 
-export function wrapPromise(promise) {
-  let status = 'pending';
-  let result = '';
-  let suspender = promise.then(
-    (r) => {
-      status = 'success';
-      result = r;
-    },
-    (e) => {
-      status = 'error';
-      result = e;
-    },
-  );
-
-  return {
-    read() {
-      if (status === 'pending') {
-        throw suspender;
-      } else if (status === 'error') {
-        throw result;
-      }
-
-      return result;
-    },
-  };
-}
-
 export function fetchLocalJSONAPI(endpoint, token, method = 'GET', language = 'en'): Promise<*> {
   const url = new URL(endpoint, API_URL);
   let headers = {
