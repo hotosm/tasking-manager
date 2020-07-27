c9be6e7c954f3a3c3a0fcfaab715d0c3c19a2e35
Wille Marcel <wille.yyz@gmail.com>
2020-07-27 09:58:26 -0300

    format timeline X axes according to time interval between dates

--- frontend/src/components/projectDetail/timeline.js
+++ frontend/src/components/projectDetail/timeline.js
@@ -3,14 +3,17 @@ import { Line } from 'react-chartjs-2';
 
 import { formatTimelineData, formatTimelineTooltip } from '../../utils/formatChartJSData';
 import { CHART_COLOURS } from '../../config';
+import { useTimeDiff } from '../../hooks/UseTimeDiff';
 
 export default function ProjectTimeline({ tasksByDay }: Object) {
+  const unit = useTimeDiff(tasksByDay);
   return (
     <Line
       data={formatTimelineData(tasksByDay, CHART_COLOURS.orange, CHART_COLOURS.red)}
       options={{
         legend: { position: 'top', align: 'end', labels: { boxWidth: 12 } },
         tooltips: { callbacks: { label: (tooltip, data) => formatTimelineTooltip(tooltip, data) } },
+        scales: { xAxes: [{ type: 'time', time: { unit: unit } }] },
       }}
     />
   );
