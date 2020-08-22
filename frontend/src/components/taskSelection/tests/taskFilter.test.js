13fa5a6f36587fe88e98395833c58810eda10933
Wille Marcel <wille.yyz@gmail.com>
2020-08-21 21:23:32 -0300

    add unavailable tasks filter to validators + improve style

--- frontend/src/components/taskSelection/tests/taskFilter.test.js
+++ frontend/src/components/taskSelection/tests/taskFilter.test.js
@@ -10,17 +10,18 @@ describe('test TaskFilter with userCanValidate TRUE', () => {
   function setStatus(status) {
     statusFilter = status;
   }
-  it('render 3 buttons and the first one is active', async () => {
+  it('render 4 buttons and the first one is active', async () => {
     render(
       <IntlProvider locale="en">
         <TaskFilter userCanValidate={true} statusFilter={statusFilter} setStatusFn={setStatus} />
       </IntlProvider>,
     );
 
-    expect(screen.getAllByRole('button').length).toBe(3);
-    expect(screen.getByText('All').className).toContain('dbi bg-blue-grey white');
-    expect(screen.getByText('Available for mapping').className).toContain('dbi bg-white blue-grey');
-    expect(screen.getByText('Ready for validation').className).toContain('dbi bg-white blue-grey');
+    expect(screen.getAllByRole('button').length).toBe(4);
+    expect(screen.getByText('All').className).toContain('bg-blue-grey white');
+    expect(screen.getByText('Available for mapping').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Ready for validation').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Unavailable').className).toContain('bg-white blue-grey');
     fireEvent.click(screen.getByText('Available for mapping'));
     expect(statusFilter).toBe('readyToMap');
   });
@@ -31,22 +32,37 @@ describe('test TaskFilter with userCanValidate TRUE', () => {
         <TaskFilter userCanValidate={true} statusFilter={statusFilter} setStatusFn={setStatus} />
       </IntlProvider>,
     );
-    expect(screen.getByText('All').className).toContain('dbi bg-white blue-grey');
-    expect(screen.getByText('Available for mapping').className).toContain('dbi bg-blue-grey white');
-    expect(screen.getByText('Ready for validation').className).toContain('dbi bg-white blue-grey');
+    expect(screen.getByText('All').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Available for mapping').className).toContain('bg-blue-grey white');
+    expect(screen.getByText('Ready for validation').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Unavailable').className).toContain('bg-white blue-grey');
     fireEvent.click(screen.getByText('Ready for validation'));
     expect(statusFilter).toBe('readyToValidate');
   });
 
-  it('Ready for validation is active and then click on All', () => {
+  it('Ready for validation is active and then click on Unavailable', () => {
     render(
       <IntlProvider locale="en">
         <TaskFilter userCanValidate={true} statusFilter={statusFilter} setStatusFn={setStatus} />
       </IntlProvider>,
     );
-    expect(screen.getByText('All').className).toContain('dbi bg-white blue-grey');
-    expect(screen.getByText('Available for mapping').className).toContain('dbi bg-white blue-grey');
-    expect(screen.getByText('Ready for validation').className).toContain('dbi bg-blue-grey white');
+    expect(screen.getByText('All').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Available for mapping').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Ready for validation').className).toContain('bg-blue-grey white');
+    expect(screen.getByText('Unavailable').className).toContain('bg-white blue-grey');
+    fireEvent.click(screen.getByText('Unavailable'));
+    expect(statusFilter).toBe('unavailable');
+  });
+  it('Unavailable is active and then click on All', () => {
+    render(
+      <IntlProvider locale="en">
+        <TaskFilter userCanValidate={true} statusFilter={statusFilter} setStatusFn={setStatus} />
+      </IntlProvider>,
+    );
+    expect(screen.getByText('All').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Available for mapping').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Ready for validation').className).toContain('bg-white blue-grey');
+    expect(screen.getByText('Unavailable').className).toContain('bg-blue-grey white');
     fireEvent.click(screen.getByText('All'));
     expect(statusFilter).toBe('all');
   });
@@ -61,8 +77,9 @@ describe('test TaskFilter with userCanValidate FALSE', () => {
     );
 
     expect(screen.getAllByRole('button').length).toBe(2);
-    expect(screen.getByText('All').className).toContain('dbi bg-blue-grey white');
-    expect(screen.getByText('Available for mapping').className).toContain('dbi bg-white blue-grey');
+    expect(screen.getByText('All').className).toContain('bg-blue-grey white');
+    expect(screen.getByText('Available for mapping').className).toContain('bg-white blue-grey');
     expect(screen.queryByText('Ready for validation')).not.toBeInTheDocument();
+    expect(screen.queryByText('Unavailable')).not.toBeInTheDocument();
   });
 });
