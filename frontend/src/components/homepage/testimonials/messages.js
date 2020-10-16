2a581a9cbc33ac6e65e0c41c1c895e0acf4ba36a
Jeff Meyer <jeff@gwhat.org>
2020-10-15 22:49:19 -0700

    home page touchoups

--- frontend/src/components/homepage/testimonials/messages.js
+++ frontend/src/components/homepage/testimonials/messages.js
@@ -11,10 +11,10 @@ export default defineMessages({
   ifrcCitation: {
     id: 'home.testimonials.ifrc.citation',
     defaultMessage:
-      "In the early days of the Cyclone Idai response, IFRC was looking for detailed maps to get a sense of the scale of the flooding, which were also used for search and rescue operations. Later on, we had requests to identify where certain buildings, such as health centres or hospitals, were located so our health team could assess the damage and medical needs of patients.",
+      ".",
   },
   ifrcBio: {
     id: 'home.testimonials.ifrc.bio',
-    defaultMessage: 'Remote Coordinator for the IFRC Information Management Team for Cyclone Idai',
+    defaultMessage: 'Daniel Baránek, Praha, Česko'
   },
 });
