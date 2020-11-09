3404352dc60b5a0cb618b69ee260cdc04dc94064
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2020-11-09 05:49:45 +0300

    refactor: validation and mapping permission blocks
    - Create PermissionsBlock component
    - Use block component for validation and mapping permissions
    - Conditional render of title and description per type of permission

--- frontend/src/components/projectEdit/permissionsForm.js
+++ frontend/src/components/projectEdit/permissionsForm.js
@@ -5,6 +5,7 @@ import messages from './messages';
 import { SwitchToggle } from '../formInputs';
 import { StateContext, styleClasses } from '../../views/projectEdit';
 import { TeamSelect } from './teamSelect';
+import { PermissionsBlock } from '../permissionsBlock';
 
 export const PermissionsForm = () => {
   const { projectInfo, setProjectInfo } = useContext(StateContext);
@@ -17,56 +18,14 @@ export const PermissionsForm = () => {
 
   return (
     <div className="w-100">
-      <div className={styleClasses.divClass}>
-        <label className={styleClasses.labelClass}>
-          <FormattedMessage {...messages.mappingPermission} />
-        </label>
-        <p className={styleClasses.pClass}>
-          <FormattedMessage {...messages.mappingPermissionDescription} />
-        </p>
-        {permissions.map((permission) => (
-          <label className="db pv2" key={permission}>
-            <input
-              value={permission.value}
-              checked={projectInfo.mappingPermission === permission.value}
-              onChange={() =>
-                setProjectInfo({
-                  ...projectInfo,
-                  mappingPermission: permission.value,
-                })
-              }
-              type="radio"
-              className={`radio-input input-reset pointer v-mid dib h2 w2 mr2 br-100 ba b--blue-light`}
-            />
-            {permission.label}
-          </label>
-        ))}
-      </div>
-      <div className={styleClasses.divClass}>
-        <label className={styleClasses.labelClass}>
-          <FormattedMessage {...messages.validationPermission} />
-        </label>
-        <p className={styleClasses.pClass}>
-          <FormattedMessage {...messages.validationPermissionDescription} />
-        </p>
-        {permissions.map((permission) => (
-          <label className="db pv2" key={permission}>
-            <input
-              value={permission.value}
-              checked={projectInfo.validationPermission === permission.value}
-              onChange={() =>
-                setProjectInfo({
-                  ...projectInfo,
-                  validationPermission: permission.value,
-                })
-              }
-              type="radio"
-              className={`radio-input input-reset pointer v-mid dib h2 w2 mr2 br-100 ba b--blue-light`}
-            />
-            {permission.label}
-          </label>
-        ))}
-      </div>
+      <PermissionsBlock
+        permissions={permissions}
+        type="mappingPermission"
+      />
+      <PermissionsBlock
+        permissions={permissions}
+        type="validationPermission"
+      />
       <div className={styleClasses.divClass.replace('w-70', 'w-90')}>
         <label className={styleClasses.labelClass}>
           <FormattedMessage {...messages.teams} />
