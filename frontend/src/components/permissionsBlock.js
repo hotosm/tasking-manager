7fe13c79a48068afce7b934a30d1a4c8ada39c93
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2020-11-09 05:49:45 +0300

    refactor: validation and mapping permission blocks
    - Create PermissionsBlock component
    - Use block component for validation and mapping permissions
    - Conditional render of title and description per type of permission

---
+++ frontend/src/components/permissionsBlock.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react';
+import { FormattedMessage } from 'react-intl';
+import messages from './projectEdit/messages.js'
+import { StateContext, styleClasses } from '../views/projectEdit'
+
+export const PermissionsBlock = ({ permissions, type }: Object) => {
+  const { projectInfo, setProjectInfo } = useContext(StateContext);
+  
+  return (
+    <div className={styleClasses.divClass}>
+      <label className={styleClasses.labelClass}>
+        {type === "mappingPermission" ? (
+          <FormattedMessage {...messages.mappingPermission} />
+        ) : ( 
+          <FormattedMessage {...messages.validationPermission} />
+        )}
+      </label>
+      <p className={styleClasses.pClass}>
+        {type === "mappingPermission" ? (
+          <FormattedMessage {...messages.mappingPermissionDescription} />
+        ) : ( 
+          <FormattedMessage {...messages.validationPermissionDescription} />
+        )}
+      </p>
+      {permissions.map((permission) => (
+        <label className="db pv2" key={permission}>
+          <input
+            value={permission.value}
+            checked={projectInfo[type] === permission.value}
+            onChange={() =>
+            setProjectInfo({
+              ...projectInfo,
+              [type]: permission.value,
+              })
+            }
+            type="radio"
+            className={`radio-input input-reset pointer v-mid dib h2 w2 mr2 br-100 ba b--blue-light`}
+          />
+            {permission.label}
+        </label>
+      ))}
+    </div>
+  )
+}
