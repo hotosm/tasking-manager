47d19b243b832de122d43fd43743a8a6f3247af7
Wille Marcel <wille.yyz@gmail.com>
2020-11-17 09:04:17 -0300

    update style and logic of FeaturedProjects section

--- frontend/src/network/tests/server-handlers.js
+++ frontend/src/network/tests/server-handlers.js
@@ -1,6 +1,7 @@
 import { rest } from 'msw';
 
 import { getProjectSummary, getProjectStats } from './mockData/projects';
+import { featuredProjects } from './mockData/featuredProjects';
 import { projectContributions, projectContributionsByDay } from './mockData/contributions';
 import tasksGeojson from '../../utils/tests/snippets/tasksGeometry';
 import { API_URL } from '../../config';
@@ -22,6 +23,9 @@ const handlers = [
   }),
   rest.get(API_URL + 'projects/:id/tasks', async (req, res, ctx) => {
     return res(ctx.json(tasksGeojson));
+  }),
+  rest.get(API_URL + 'projects/queries/featured/', async (req, res, ctx) => {
+    return res(ctx.json(featuredProjects));
   }),
   rest.delete(API_URL + 'notifications/delete-multiple/', async (req, res, ctx) => {
     return res(ctx.json({ Success: 'Message deleted' }));
