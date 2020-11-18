296bd8b1e80701c1688f132e1d8b26d64a9906a3
Dakota Benjamin <dakota.benjamin@hotosm.org>
2020-11-17 19:58:52 -0500

    Change region for rdsid script

--- .circleci/rdsid.sh
+++ .circleci/rdsid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-ARNS=$(aws rds describe-db-instances --query "DBInstances[].DBInstanceArn" --output text --region us-east-1)
+ARNS=$(aws rds describe-db-instances --query "DBInstances[].DBInstanceArn" --output text --region ap-southeast-1)
 for line in $ARNS; do
-    TAGS=$(aws rds list-tags-for-resource --resource-name "$line" --query "TagList[]" --region us-east-1)
+    TAGS=$(aws rds list-tags-for-resource --resource-name "$line" --query "TagList[]" --region ap-southeast-1)
     MATCHES=$(echo $TAGS | python -c "import sys, json; tags = json.loads('$1'); remote = {t['Key']: t['Value'] for t in json.load(sys.stdin)}; print('$line'.split(':')[-1]) if len({'$line' for k, v in tags.items() if k in remote and v == remote[k]}) > 0 else ''")
     if [[ ! -z $MATCHES ]]; then
         echo $MATCHES
