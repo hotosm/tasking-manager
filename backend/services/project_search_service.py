254291d977a3ce2cbb835affabf49dfd04f769af
ramyaragupathy <r.ragupathy85@gmail.com>
2020-12-15 00:53:25 +0530

    #noqa to skip all conflicting PEP8 errors

--- backend/services/project_search_service.py
+++ backend/services/project_search_service.py
@@ -357,15 +357,12 @@ class ProjectSearchService:
     def filter_projects_by_user(query, user, permission: str, team_roles: list = []):
         if user and user.role != UserRole.ADMIN.value:
             separator = permission.find("_")
-            next_char = (
-                separator + 1
-            )  # to avoid flake E203 https://gitlab.com/pycqa/flake8/-/issues/451
             permission_class = (
                 permission[:separator].capitalize()
-                + permission[next_char:].capitalize()
+                + permission[separator + 1 :].capitalize()  # noqa
             )
             import_permission_class = getattr(
-                backend.models.postgis.statuses, permission_class # noqa
+                backend.models.postgis.statuses, permission_class  # noqa
             )
             selection = []
             # get ids of projects assigned to the user's teams
