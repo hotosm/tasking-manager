01e14d40b3112ad7823d30ed89deb59fc66222f2
Wille Marcel <wille.yyz@gmail.com>
2021-01-05 19:30:16 -0300

    Fix Relative Time expressions

--- frontend/src/utils/formattedRelativeTime.js
+++ frontend/src/utils/formattedRelativeTime.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { selectUnit } from '@formatjs/intl-utils';
 import { FormattedRelativeTime } from 'react-intl';
+
+import { selectUnit } from './selectUnit';
 
 export const RelativeTimeWithUnit = ({ date }: Object) => {
   const { value, unit } = selectUnit(new Date(date));
