34524c41c1af966fe49eacef0b070db889c1ac5f
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2021-01-08 13:28:45 +0300

    Add tasks activity stats:
    - add task stats dto
    - return tasks matching query parameters

--- backend/api/tasks/statistics.py
+++ backend/api/tasks/statistics.py
@@ -1,9 +1,9 @@
-from flask_restful import Resource, current_app
+from flask_restful import Resource, current_app, request
 from backend.services.stats_service import NotFound, StatsService
 
 
 class TasksStatisticsAPI(Resource):
-    def get(self, project_id):
+    def get(self):
         """
         Get Task Stats
         ---
@@ -40,18 +40,13 @@ class TasksStatisticsAPI(Resource):
               required: false
               default: null
             - in: query
-              name: country
-              description: Country name to filter by
-              required: false
-              default: null
-            - in: query
-              name: country
-              description: Country name to filter by
-              required: false
-              default: null
-            - in: query
               name: project_id
               description: Project IDs to filter by
+              required: false
+              default: null
+            - in: query
+              name: country
+              description: Country name to filter by
               required: false
               default: null
         responses:
@@ -63,7 +58,17 @@ class TasksStatisticsAPI(Resource):
                 description: Internal Server Error
         """
         try:
-            task_stats = StatsService.get_task_stats()
+            print(request.args)
+            start_date = request.args.get("start_date")
+            end_date = request.args.get("end_date")
+            org_id = request.args.get("org_id")
+            org_name = request.args.get("org_name")
+            campaign = request.args.get("campaigns")
+            project_ids = request.args.get("project_ids")
+            country = request.args.get("country")
+            task_stats = StatsService.get_task_stats(
+                start_date, end_date, org_id, org_name, campaign, project_ids, country
+            )
             return task_stats.to_primitive(), 200
         except NotFound:
             return {"Error": "Not found"}, 404
