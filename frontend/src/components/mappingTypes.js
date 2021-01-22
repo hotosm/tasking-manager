5df39513f70ba95e2f083d4cfab68049e9bcac3e
Wille Marcel <wille.yyz@gmail.com>
2021-01-22 11:21:03 -0300

    update react-scripts to 4.0.1

--- frontend/src/components/mappingTypes.js
+++ frontend/src/components/mappingTypes.js
@@ -4,21 +4,21 @@ import { useIntl } from 'react-intl';
 import messages from './messages';
 import { RoadIcon, HomeIcon, WavesIcon, TaskIcon, AsteriskIcon } from './svgIcons';
 
-export const titledIcons = [
-  { icon: RoadIcon, title: 'roads', value: 'ROADS' },
-  { icon: HomeIcon, title: 'buildings', value: 'BUILDINGS' },
-  { icon: WavesIcon, title: 'waterways', value: 'WATERWAYS' },
-  { icon: TaskIcon, title: 'landUse', value: 'LAND_USE' },
-  { icon: AsteriskIcon, title: 'other', value: 'OTHER' },
+export const TITLED_ICONS = [
+  { Icon: RoadIcon, title: 'roads', value: 'ROADS' },
+  { Icon: HomeIcon, title: 'buildings', value: 'BUILDINGS' },
+  { Icon: WavesIcon, title: 'waterways', value: 'WATERWAYS' },
+  { Icon: TaskIcon, title: 'landUse', value: 'LAND_USE' },
+  { Icon: AsteriskIcon, title: 'other', value: 'OTHER' },
 ];
 
 export function MappingTypes({ types = [], colorClass }: Object) {
   const intl = useIntl();
   return (
     <>
-      {titledIcons.map((Element, k) => (
+      {TITLED_ICONS.map((Element, k) => (
         <span title={intl.formatMessage(messages[Element.title])} key={k}>
-          <Element.icon
+          <Element.Icon
             className={`ml1 mr3 ${
               types && types.includes(Element.value) ? colorClass : 'grey-light'
             }`}
