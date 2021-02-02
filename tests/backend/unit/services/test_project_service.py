2442e9a834916277d25d26aacb7b86c2d33c0f43
Diana Rita Nanyanzi <31903212+d-rita@users.noreply.github.com>
2021-02-02 06:01:32 +0300

    Improve mapping permissions tests

--- tests/backend/unit/services/test_project_service.py
+++ tests/backend/unit/services/test_project_service.py
@@ -39,92 +39,75 @@ class TestProjectService(unittest.TestCase):
         # Act / Assert
         self.assertTrue(ProjectService._is_user_intermediate_or_advanced(1))
 
-    @patch.object(UserService, "is_user_blocked")
     @patch.object(ProjectAdminService, "is_user_action_permitted_on_project")
+    @patch.object(UserService, "is_user_blocked")
+    @patch.object(UserService, "has_user_accepted_license")
     @patch.object(Task, "get_locked_tasks_for_user")
     @patch.object(Project, "get")
-    def test_user_not_permitted_to_map_if_already_locked_tasks(
+    def test_user_allowed_to_map(
         self,
         mock_project,
         mock_user_tasks,
+        mock_user_license,
         mock_user_blocked,
-        mock_user_action_permitted,
+        mock_user_is_action_permitted,
     ):
-        # Arrange
-        stub_project = Project()
-        stub_project.status = ProjectStatus.PUBLISHED.value
-        mock_project.return_value = stub_project
-
-        mock_user_tasks.return_value = LockedTasksForUser()
-        mock_user_tasks.return_value.locked_tasks = [1]
-        mock_user_blocked.return_value = False
-        mock_user_action_permitted.return_value = False
-
-        # Act
-        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
-
-        # Assert
-        self.assertFalse(allowed)
-
-    @patch.object(UserService, "is_user_blocked")
-    @patch.object(ProjectAdminService, "is_user_action_permitted_on_project")
-    @patch.object(Project, "get")
-    def test_user_cant_map_if_project_not_published(
-        self, mock_project, mock_user_action_permitted, mock_user_blocked
-    ):
-        # Arrange
-        stub_project = Project()
-        stub_project.status = ProjectStatus.DRAFT.value
-        mock_project.return_value = stub_project
-
-        mock_user_blocked.return_value = False
-        mock_user_action_permitted.return_value = False
-
-        # Act
-        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
-
-        # Assert
-        self.assertFalse(allowed)
-        self.assertEqual(reason, MappingNotAllowed.PROJECT_NOT_PUBLISHED)
-
-    @patch.object(UserService, "has_user_accepted_license")
-    @patch.object(UserService, "is_user_blocked")
-    @patch.object(Task, "get_locked_tasks_for_user")
-    @patch.object(Project, "get")
-    def test_user_not_permitted_to_map_if_user_has_not_accepted_license(
-        self, mock_project, mock_user_tasks, mock_user_blocked, mock_user_service
-    ):
-        # Arrange
+        # Mock project
         stub_project = Project()
         stub_project.status = ProjectStatus.PUBLISHED.value
         stub_project.license_id = 11
-
         mock_project.return_value = stub_project
+
+        # Admin user related
+        mock_user_is_action_permitted.return_value = True
         mock_user_tasks.return_value = LockedTasksForUser()
         mock_user_tasks.return_value.locked_tasks = []
-        mock_user_service.return_value = False
+        mock_user_license.return_value = True
         mock_user_blocked.return_value = False
 
-        # Act
         allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
 
-        # Assert
+        self.assertTrue(allowed)
+        self.assertEqual(reason, "User allowed to map")
+
+        # Admin not accepted license should fail
+        mock_user_license.return_value = False
+        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
         self.assertFalse(allowed)
+        self.assertEqual(reason, MappingNotAllowed.USER_NOT_ACCEPTED_LICENSE)
 
-    @patch.object(UserService, "is_user_blocked")
-    @patch.object(Project, "get")
-    def test_user_not_permitted_to_map_if_user_is_blocked(
-        self, mock_project, mock_user_blocked
-    ):
-
-        # Arrange
-        mock_project.return_value = Project()
-        mock_user_blocked.return_value = True
-
-        # Act
+        # Admin with already locked tasks should fail
+        mock_user_license.return_value = True
+        mock_user_tasks.return_value.locked_tasks = [1]
         allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
+        self.assertFalse(allowed)
+        self.assertEqual(reason, MappingNotAllowed.USER_ALREADY_HAS_TASK_LOCKED)
 
-        # Assert
+        # Admin can access draft projects
+        stub_project.status = ProjectStatus.DRAFT.value
+        mock_user_tasks.return_value.locked_tasks = []
+        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
+        self.assertTrue(allowed)
+        self.assertEqual(reason, "User allowed to map")
+
+        # Mappers
+        mock_user_is_action_permitted.return_value = False
+        mock_user_blocked.return_value = False
+
+        # cannot access unpublished project
+        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
+        self.assertFalse(allowed)
+        self.assertEqual(reason, MappingNotAllowed.PROJECT_NOT_PUBLISHED)
+
+        # Mappers not accepted license should fail
+        mock_user_license.return_value = False
+        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
+        self.assertFalse(allowed)
+        self.assertEqual(reason, MappingNotAllowed.USER_NOT_ACCEPTED_LICENSE)
+
+        # Blocked user
+        mock_user_blocked.return_value = True
+        allowed, reason = ProjectService.is_user_permitted_to_map(1, 1)
         self.assertFalse(allowed)
         self.assertEqual(reason, MappingNotAllowed.USER_NOT_ON_ALLOWED_LIST)
 
