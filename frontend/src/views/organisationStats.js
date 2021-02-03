0bcde319fbd69ec5f3f81c7fce56a87dcb251629
Wille Marcel <wille.yyz@gmail.com>
2021-02-03 07:25:07 -0300

    restrict organisation stats to only logged users

--- frontend/src/views/organisationStats.js
+++ frontend/src/views/organisationStats.js
@@ -1,4 +1,6 @@
 import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect } from '@reach/router';
 import { startOfYear, format } from 'date-fns';
 import ReactPlaceholder from 'react-placeholder';
 import { FormattedMessage } from 'react-intl';
@@ -15,6 +17,7 @@ import { OrganisationUsageLevel } from '../components/teamsAndOrgs/orgUsageLevel
 const TasksStats = React.lazy(() => import('../components/teamsAndOrgs/tasksStats'));
 
 export const OrganisationStats = ({ id }) => {
+  const token = useSelector((state) => state.auth.get('token'));
   const [query, setQuery] = useTasksStatsQueryParams();
   const [forceUpdated, forceUpdate] = useForceUpdate();
   useEffect(() => {
@@ -36,56 +39,62 @@ export const OrganisationStats = ({ id }) => {
   const totalStats = useTotalTasksStats(useCurrentYearStats(id, query, apiState.stats));
   useSetTitleTag(`${organisation.name || 'Organization'} stats`);
 
-  return (
-    <ReactPlaceholder
-      showLoadingAnimation={true}
-      rows={26}
-      ready={!error && !loading}
-      className="pv3 ph2 ph4-ns"
-    >
-      <div className="w-100 cf pv3 ph2 ph4-ns bg-white blue-dark">
-        <img src={organisation.logo} className="w3 dib v-mid mr3" alt={organisation.name} />
-        <h3 className="f2 fw6 mv2 ttu barlow-condensed blue-dark dib v-mid">{organisation.name}</h3>
-        <div className="w-100 fl cf">
-          <h4 className="f3 fw6 ttu barlow-condensed blue-dark mt0 pt4 mb3">
-            <FormattedMessage {...messages.tasksStatistics} />
-          </h4>
-          <React.Suspense fallback={<div className={`w7 h5`}>Loading...</div>}>
-            <TasksStats
-              query={query}
-              setQuery={setQuery}
-              stats={apiState.stats}
-              error={apiState.isError}
-              loading={apiState.isLoading}
-              retryFn={forceUpdate}
-            />
-          </React.Suspense>
+  if (token) {
+    return (
+      <ReactPlaceholder
+        showLoadingAnimation={true}
+        rows={26}
+        ready={!error && !loading}
+        className="pv3 ph2 ph4-ns"
+      >
+        <div className="w-100 cf pv3 ph2 ph4-ns bg-white blue-dark">
+          <img src={organisation.logo} className="w3 dib v-mid mr3" alt={organisation.name} />
+          <h3 className="f2 fw6 mv2 ttu barlow-condensed blue-dark dib v-mid">
+            {organisation.name}
+          </h3>
+          <div className="w-100 fl cf">
+            <h4 className="f3 fw6 ttu barlow-condensed blue-dark mt0 pt4 mb3">
+              <FormattedMessage {...messages.tasksStatistics} />
+            </h4>
+            <React.Suspense fallback={<div className={`w7 h5`}>Loading...</div>}>
+              <TasksStats
+                query={query}
+                setQuery={setQuery}
+                stats={apiState.stats}
+                error={apiState.isError}
+                loading={apiState.isLoading}
+                retryFn={forceUpdate}
+              />
+            </React.Suspense>
+          </div>
+          <div className="w-100 fl cf">
+            <h4 className="f3 fw6 ttu barlow-condensed blue-dark mt0 pt4 mb2">
+              <FormattedMessage {...messages.remainingTasks} />
+            </h4>
+            <ReactPlaceholder
+              showLoadingAnimation={true}
+              rows={5}
+              delay={500}
+              ready={!errorOrgStats && !loadingOrgStats}
+            >
+              <RemainingTasksStats tasks={orgStats && orgStats.activeTasks} />
+            </ReactPlaceholder>
+          </div>
+          <div className="w-100 fl cf">
+            <h4 className="f3 fw6 ttu barlow-condensed blue-dark mt0 pt4 mb2">
+              <FormattedMessage {...messages.usageLevel} />
+            </h4>
+            <ReactPlaceholder showLoadingAnimation={true} rows={5} delay={500} ready={totalStats}>
+              <OrganisationUsageLevel
+                orgName={organisation.name}
+                completedActions={totalStats.mapped + totalStats.validated}
+              />
+            </ReactPlaceholder>
+          </div>
         </div>
-        <div className="w-100 fl cf">
-          <h4 className="f3 fw6 ttu barlow-condensed blue-dark mt0 pt4 mb2">
-            <FormattedMessage {...messages.remainingTasks} />
-          </h4>
-          <ReactPlaceholder
-            showLoadingAnimation={true}
-            rows={5}
-            delay={500}
-            ready={!errorOrgStats && !loadingOrgStats}
-          >
-            <RemainingTasksStats tasks={orgStats && orgStats.activeTasks} />
-          </ReactPlaceholder>
-        </div>
-        <div className="w-100 fl cf">
-          <h4 className="f3 fw6 ttu barlow-condensed blue-dark mt0 pt4 mb2">
-            <FormattedMessage {...messages.usageLevel} />
-          </h4>
-          <ReactPlaceholder showLoadingAnimation={true} rows={5} delay={500} ready={totalStats}>
-            <OrganisationUsageLevel
-              orgName={organisation.name}
-              completedActions={totalStats.mapped + totalStats.validated}
-            />
-          </ReactPlaceholder>
-        </div>
-      </div>
-    </ReactPlaceholder>
-  );
+      </ReactPlaceholder>
+    );
+  } else {
+    return <Redirect from={`organisations/${id}/stats/`} to={'/login'} noThrow />;
+  }
 };
