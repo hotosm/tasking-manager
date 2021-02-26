5b83d13bf9a1d61d73ba4a945d89307af4f157da
Wille Marcel <wille.yyz@gmail.com>
2021-02-26 17:10:28 -0300

    Add loading icon and use it on Preloader component

---
+++ frontend/src/components/tests/preloader.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { Preloader } from '../preloader';
+
+test('Preloader renders correct content', () => {
+  const { container } = render(<Preloader />);
+  expect(container.querySelector('div').className).toBe(
+    'fixed vh-100 w-100 flex justify-center items-center bg-white',
+  );
+  expect(container.querySelector('svg')).toBeInTheDocument();
+  expect(container.querySelector('.red.h3.w3')).toBeInTheDocument();
+});
