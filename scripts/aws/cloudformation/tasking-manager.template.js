d00e99b15f40cce8e454a6c15a3396ad9fd6d3ca
eternaltyro <eternaltyro@gmail.com>
2021-03-01 19:51:36 +0530

    Improve CloudFormation template for TM Backend containerization
    - Uplift changes to Domain name validation from develop
    - Move secrets to AWS Secrets Manager
    - Use AWS Secrets Manager - managed secrets for RDS credentials during
      creation
    - Add description fields and sane defaults for parameters
    - Add IAM policy statements for accessing secrets manager entries

--- scripts/aws/cloudformation/tasking-manager.template.js
+++ scripts/aws/cloudformation/tasking-manager.template.js
@@ -45,11 +45,13 @@ const Parameters = {
   },
   ELBSubnets: {
     Description: 'ELB subnets',
-    Type: 'String'
+    Type: 'String',
+    Default: 'ex: subnet-a1b2c3,subnet-d4e5f6,..'
   },
   SSLCertificateIdentifier: {
     Type: 'String',
-    Description: 'SSL certificate for HTTPS protocol'
+    Description: 'SSL certificate for HTTPS protocol',
+    Default: 'ex: certificate/bb59df0a-ff8d-416c-bfb2-cc3a2e97c8ec'
   },
   TaskingManagerLogDirectory: {
     Description: 'TM_LOG_DIR environment variable',
@@ -106,7 +108,8 @@ const Parameters = {
   },
   TaskingManagerSMTPPort: {
     Description: 'TM_SMTP_PORT environment variable',
-    Type: 'String'
+    Type: 'String',
+    Default: '587'
   },
   TaskingManagerDefaultChangesetComment: {
     Description: 'TM_DEFAULT_CHANGESET_COMMENT environment variable',
@@ -148,7 +151,7 @@ const Conditions = {
 
 const Resources = {
   TaskingManagerECSCluster: {
-    Type: 'AWS::ECS:Cluster',
+    Type: 'AWS::ECS::Cluster',
     Properties: {
       CapacityProviders: [
         'FARGATE',
@@ -270,13 +273,38 @@ const Resources = {
     Type: 'AWS::ECS::TaskDefinition',
     Properties: {
       ContainerDefinitions: [{
+        Name: 'TM4_Backend_Service',
+        Environment: [
+          { 'Name': 'POSTGRES_ENDPOINT', 'Value': cf.getAtt('TaskingManagerRDS','Endpoint.Address') },
+          { 'Name': 'POSTGRES_DB', 'Value': 'dummy' },
+          { 'Name': 'POSTGRES_USER', 'Value': cf.join(':', ['{{resolve:secretsmanager', cf.ref('TaskingManagerRDSSecret'), 'SecretString:username}}']) },
+          { 'Name': 'TM_APP_BASE_URL', 'Value': cf.ref('TaskingManagerAppBaseUrl') },
+          { 'Name': 'TM_CONSUMER_KEY', 'Value': cf.ref('TaskingManagerConsumerKey') },
+          { 'Name': 'TM_SMTP_HOST', 'Value': cf.ref('TaskingManagerSMTPHost') },
+          { 'Name': 'TM_SMTP_PORT', 'Value': cf.ref('TaskingManagerSMTPPort') },
+          { 'Name': 'TM_SMTP_USER', 'Value': cf.ref('TaskingManagerSMTPUser') },
+          { 'Name': 'TM_EMAIL_FROM_ADDRESS', 'Value': cf.ref('TaskingManagerEmailFromAddress') },
+          { 'Name': 'TM_EMAIL_CONTACT_ADDRESS', 'Value': cf.ref('TaskingManagerEmailContactAddress') },
+          { 'Name': 'TM_ORG_NAME', 'Value': cf.ref('TaskingManagerOrgName') },
+          { 'Name': 'TM_ORG_CODE', 'Value': cf.ref('TaskingManagerOrgCode') },
+          { 'Name': 'TM_ORG_LOGO', 'Value': cf.ref('TaskingManagerLogo') },
+          { 'Name': 'TM_IMAGE_UPLOAD_API_URL', 'Value': cf.ref('TaskingManagerImageUploadAPIURL') },
+          { 'Name': 'TM_SENTRY_BACKEND_DSN', 'Value': cf.ref('SentryBackendDSN') },
+        ],
+        Secrets: [
+          { 'Name': 'POSTGRES_PASSWORD', 'ValueFrom': cf.ref('PostgresPasswordManagedSecret') },
+          { 'Name': 'TM_SMTP_PASSWORD', 'ValueFrom': cf.ref('SMTPPassword') },
+          { 'Name': 'TM_SECRET', 'ValueFrom': cf.ref('TaskingManagerManagedSecret') },
+          { 'Name': 'TM_CONSUMER_SECRET', 'ValueFrom': cf.ref('OAuth2ConsumerSecret') },
+          { 'Name': 'NEW_RELIC_LICENSE_KEY', 'ValueFrom': cf.ref('NewRelicLicenseKey') },
+          { 'Name': 'TM_IMAGE_UPLOAD_API_KEY', 'ValueFrom': cf.ref('ImageUploadAPIKey') }
+        ],
         EnvironmentFiles: [],
         Essential: true,
         // HealthCheck: {
           // Need more input from Yogesh
         // },
-        Image: 'quay.io/hotosm/taskingmanager:latest', //configure this properly
-
+        Image: 'quay.io/hotosm/taskingmanager:develop', //configure this properly
       }],
       Cpu: '1024', 
       ExecutionRoleArn: cf.ref('TaskingManagerECSExecutionRole'),
@@ -415,7 +443,27 @@ const Resources = {
             }
           ]
         }
-      }],
+      },
+      {
+        PolicyName: "secretspolicy2",
+        PolicyDocument: {
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Effect: "Allow",
+              Action: "secretsmanager:GetSecretValue",
+              Resource: [
+                cf.join(':', ['arn:aws:secretsmanager', cf.region, cf.accountId, 'secret', cf.ref('PostgresPasswordManagedSecret')])
+              ]
+            },
+            {
+              Effect: "Allow",
+              Action: "secretsmanager:ListSecrets",
+              Resource: "*"
+            }
+         ]
+      } }
+      ],
       RoleName: cf.join('-', [cf.stackName, 'ecs', 'execution-role'])
     }
       // grants the Amazon ECS container agent permission to make AWS API calls - cloudwatch logs, secrets
@@ -528,14 +576,27 @@ const Resources = {
       Protocol: 'HTTP'
     }
   },
+  TaskingManagerRDSSecret: {
+    Type: 'AWS::SecretsManager::Secret',
+    Properties: {
+      Description: 'Experiment to create a secret via template',
+      Name: 'demo/tm-rds-secret',
+      GenerateSecretString: {
+        PasswordLength: 32,
+        ExcludePunctuation: true,
+        SecretStringTemplate: '{"username": "taskingmanager", "engine": "postgres", "port": 5432}',
+        GenerateStringKey: 'password'
+      }
+    }
+  },
   TaskingManagerRDS: {
     Type: 'AWS::RDS::DBInstance',
     Properties: {
         Engine: 'postgres',
         DBName: cf.if('UseASnapshot', cf.noValue, cf.ref('PostgresDB')),
-        EngineVersion: '11.5',
-        MasterUsername: cf.if('UseASnapshot', cf.noValue, cf.ref('PostgresUser')),
-        MasterUserPassword: cf.if('UseASnapshot', cf.noValue, cf.ref('PostgresPassword')),
+        EngineVersion: '11.10',
+        MasterUsername: cf.if('UseASnapshot', cf.noValue, cf.join(':', ['{{resolve:secretsmanager', cf.ref('TaskingManagerRDSSecret'), 'SecretString:username}}'])),
+        MasterUserPassword: cf.if('UseASnapshot', cf.noValue, cf.join(':', ['{{resolve:secretsmanager', cf.ref('TaskingManagerRDSSecret'), 'SecretString:password}}'])),
         AllocatedStorage: cf.ref('DatabaseSize'),
         BackupRetentionPeriod: 10,
         StorageType: 'gp2',
@@ -544,6 +605,14 @@ const Resources = {
         DBInstanceClass: cf.if('IsTaskingManagerProduction', 'db.t3.2xlarge', 'db.t2.small'),
         DBSnapshotIdentifier: cf.if('UseASnapshot', cf.ref('DBSnapshot'), cf.noValue),
         VPCSecurityGroups: [cf.importValue(cf.join('-', ['hotosm-network-production', cf.ref('NetworkEnvironment'), 'ec2s-security-group', cf.region]))],
+    }
+  },
+  SecretRDSInstanceAttachment: {
+    Type: 'AWS::SecretsManager::SecretTargetAttachment',
+    Properties: {
+      SecretId: cf.ref('TaskingManagerRDSSecret'),
+      TargetId: cf.ref('TaskingManagerRDS'),
+      TargetType: 'AWS::RDS::DBInstance'
     }
   },
   TaskingManagerReactBucket: {
