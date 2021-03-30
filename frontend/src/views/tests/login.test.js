71173e37e6cf493466a60f55be4af1923ca3de76
Wille Marcel <wille.yyz@gmail.com>
2021-03-30 08:37:03 -0300

    update login.test.js

--- frontend/src/views/tests/login.test.js
+++ frontend/src/views/tests/login.test.js
@@ -5,23 +5,22 @@ import '@testing-library/jest-dom';
 import { ReduxIntlProviders } from '../../utils/testWithIntl';
 import { Login } from '../login';
 
-describe('AuthButtons', () => {
-  it('without alternativeSignUpText', () => {
-    render(
-      <ReduxIntlProviders>
-        <Login redirectTo={'/welcome'} />
-      </ReduxIntlProviders>,
-    );
-    expect(screen.getByText(/Tasking Manager/)).toBeInTheDocument();
-    expect(screen.getByText('Log in').className).toContain('blue-dark bg-white');
-    expect(screen.getByText('Create an account').className).toContain(
-      'bg-blue-dark white ml1 v-mid',
-    );
-    expect(screen.queryByText('Sign up')).not.toBeInTheDocument();
-    expect(screen.queryByText('Name')).not.toBeInTheDocument();
-    fireEvent.click(screen.getByText('Create an account'));
-    expect(screen.getByText('Name')).toBeInTheDocument();
-    expect(screen.getByText('Email')).toBeInTheDocument();
-    expect(screen.getByText('Next')).toBeInTheDocument();
-  });
+
+test('Login component renders the elements correctly formatted', () => {
+  render(
+    <ReduxIntlProviders>
+      <Login redirectTo={'/welcome'} />
+    </ReduxIntlProviders>,
+  );
+  expect(screen.getByText(/Tasking Manager/)).toBeInTheDocument();
+  expect(screen.getByText('Log in').className).toContain('blue-dark bg-white');
+  expect(screen.getByText('Create an account').className).toContain(
+    'bg-blue-dark white ml1 v-mid',
+  );
+  expect(screen.queryByText('Sign up')).not.toBeInTheDocument();
+  expect(screen.queryByText('Name')).not.toBeInTheDocument();
+  fireEvent.click(screen.getByText('Create an account'));
+  expect(screen.getByText('Name')).toBeInTheDocument();
+  expect(screen.getByText('Email')).toBeInTheDocument();
+  expect(screen.getByText('Next')).toBeInTheDocument();
 });
