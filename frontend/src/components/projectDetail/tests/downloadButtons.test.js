4440ce40e52299fdd2c9bdeafab997426bd440e2
Wille Marcel <wille.yyz@gmail.com>
2021-05-03 19:31:01 -0300

    Add icon to download project aoi/grid buttons

--- frontend/src/components/projectDetail/tests/downloadButtons.test.js
+++ frontend/src/components/projectDetail/tests/downloadButtons.test.js
@@ -14,6 +14,7 @@ describe('tests DownloadAOI and DownloadTasksGrid buttons', () => {
     );
     expect(container.querySelector('a').href).toContain('projects/1/queries/aoi/?as_file=true');
     expect(container.querySelector('a').download).toBe('project-1-aoi.geojson');
+    expect(container.querySelector('svg')).toBeInTheDocument();
     expect(screen.getByText(/Download AOI/)).toBeInTheDocument();
     expect(screen.getByRole('button', { pressed: false })).toBeInTheDocument();
   });
@@ -26,6 +27,7 @@ describe('tests DownloadAOI and DownloadTasksGrid buttons', () => {
     );
     expect(container.querySelector('a').href).toContain('projects/2/tasks/?as_file=true');
     expect(container.querySelector('a').download).toBe('project-2-tasks.geojson');
+    expect(container.querySelector('svg')).toBeInTheDocument();
     expect(screen.getByText(/Download Tasks Grid/)).toBeInTheDocument();
     expect(screen.getByRole('button', { pressed: false })).toBeInTheDocument();
   });
