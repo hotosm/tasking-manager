0d78c649780ffeae2be88286f57e69b43caae867
Wille Marcel <wille.yyz@gmail.com>
2021-05-06 20:14:19 -0300

    Add Service Desk configuration option and tests to contact page

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -41,6 +41,7 @@ export const ORG_INSTAGRAM = process.env.REACT_APP_ORG_INSTAGRAM || '';
 export const ORG_YOUTUBE = process.env.REACT_APP_ORG_YOUTUBE || '';
 export const ORG_GITHUB = process.env.REACT_APP_ORG_GITHUB || '';
 export const MATOMO_ID = process.env.REACT_APP_MATOMO_ID || '';
+export const SERVICE_DESK = process.env.REACT_APP_SERVICE_DESK || '';
 export const IMAGE_UPLOAD_SERVICE = process.env.REACT_APP_IMAGE_UPLOAD_API_URL || '';
 export const HOMEPAGE_VIDEO_URL = process.env.REACT_APP_HOMEPAGE_VIDEO_URL || '';
 // Sentry.io DSN
--- frontend/src/views/fallback.js
+++ frontend/src/views/fallback.js
@@ -4,30 +4,37 @@ import { FormattedMessage } from 'react-intl';
 
 import messages from './messages';
 import { Button } from '../components/button';
+import { SERVICE_DESK } from '../config';
 
 export const FallbackComponent = (props) => {
   return (
-    <>
-      <div className="cf w-100 pv5 base-font">
-        <h3 className="f2 fw5 barlow-condensed tc">
-          <FormattedMessage {...messages.errorFallback} />
-        </h3>
-        <div className="tc pv4">
-          <p>
-            <FormattedMessage {...messages.errorFallbackMessage} />
-          </p>
-          <p className="pt2">
+    <div className="cf w-100 pv5 base-font">
+      <h3 className="f2 fw5 barlow-condensed tc">
+        <FormattedMessage {...messages.errorFallback} />
+      </h3>
+      <div className="tc pv4">
+        <p>
+          <FormattedMessage {...messages.errorFallbackMessage} />
+        </p>
+        <p className="pt2">
+          {SERVICE_DESK ? (
+            <a href={SERVICE_DESK} target="_blank" rel="noreferrer">
+              <Button className="dib tc bg-red white mh1">
+                <FormattedMessage {...messages.contactUs} />
+              </Button>
+            </a>
+          ) : (
             <Link to="/contact">
               <Button className="dib tc bg-red white mh1">
                 <FormattedMessage {...messages.contactUs} />
               </Button>
             </Link>
-            <Button className="dib tc bg-red white mh1" onClick={() => navigate(-1)}>
-              <FormattedMessage {...messages.return} />
-            </Button>
-          </p>
-        </div>
+          )}
+          <Button className="dib tc bg-red white mh1" onClick={() => navigate(-1)}>
+            <FormattedMessage {...messages.return} />
+          </Button>
+        </p>
       </div>
-    </>
+    </div>
   );
 };
