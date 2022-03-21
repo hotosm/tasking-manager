ba95a475b291692f8098c906ccc5080fcc4a7069
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-03-21 11:57:56 +0545

    Update menu items test case to include support portal link

--- frontend/src/components/header/tests/menuItens.test.js
+++ frontend/src/components/header/tests/menuItens.test.js
@@ -1,25 +1,26 @@
 import { getMenuItensForUser } from '../index';
+import { SERVICE_DESK } from '../../../config';
 
 it('test menuItems for unlogged user', () => {
   const userDetails = {};
   const menuItems = getMenuItensForUser(userDetails, []).map((i) => i.link);
-  expect(menuItems).toEqual(['explore', 'learn', 'about']);
+  expect(menuItems).toEqual(['explore', 'learn', 'about', SERVICE_DESK]);
 });
 
 it('test menuItems for logged non admin user', () => {
   const userDetails = { username: 'test', role: 'MAPPER' };
   const menuItems = getMenuItensForUser(userDetails, []).map((i) => i.link);
-  expect(menuItems).toEqual(['explore', 'contributions', 'learn', 'about']);
+  expect(menuItems).toEqual(['explore', 'contributions', 'learn', 'about', SERVICE_DESK]);
 });
 
 it('test menuItems for logged non admin user, but org manager', () => {
   const userDetails = { username: 'test', role: 'MAPPER' };
   const menuItems = getMenuItensForUser(userDetails, [1, 3, 4]).map((i) => i.link);
-  expect(menuItems).toEqual(['explore', 'contributions', 'manage', 'learn', 'about']);
+  expect(menuItems).toEqual(['explore', 'contributions', 'manage', 'learn', 'about', SERVICE_DESK]);
 });
 
 it('test menuItems for logged admin user', () => {
   const userDetails = { username: 'test', role: 'ADMIN' };
   const menuItems = getMenuItensForUser(userDetails, []).map((i) => i.link);
-  expect(menuItems).toEqual(['explore', 'contributions', 'manage', 'learn', 'about']);
+  expect(menuItems).toEqual(['explore', 'contributions', 'manage', 'learn', 'about', SERVICE_DESK]);
 });
