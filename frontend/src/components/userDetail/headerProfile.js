1acfda4d572841983e3dc01855d6932f0e751c90
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-05-18 10:34:24 +0545

    Fix active indicator for nav bar

--- frontend/src/components/userDetail/headerProfile.js
+++ frontend/src/components/userDetail/headerProfile.js
@@ -101,7 +101,7 @@ const MyContributionsNav = ({ username, authUser }) => {
   const items = [
     { url: `/contributions`, label: <FormattedMessage {...messages.myStats} /> },
     {
-      url: '/contributions/projects/?mappedByMe=1&action=any',
+      url: '/contributions/projects?mappedByMe=1&action=any',
       label: <FormattedMessage {...messages.myProjects} />,
     },
     { url: '/contributions/tasks', label: <FormattedMessage {...messages.myTasks} /> },
