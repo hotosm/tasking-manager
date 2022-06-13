a57b0ecdadcc4eae50d1ec9bfa27d46523a6702d
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-06-13 16:28:32 +0545

    Fix card heights in learn page

--- frontend/src/views/learn.js
+++ frontend/src/views/learn.js
@@ -68,12 +68,11 @@ const Intro = ({ section, messagesObjs }) => (
 );
 
 const Steps = ({ items }) => (
-  <div className="w-100 cf relative">
+  <div className="w-100 cf flex flex-wrap">
     {items.map((item, i) => (
-      <div className="w-third-ns w-100 fl pa2 z-2 bg-white" key={i}>
-        <div className="shadow-1 pa3">
+      <div className="w-50-m w-third-ns pa2 z-2 bg-white" key={i}>
+        <div className="shadow-1 pa3 h-100">
           {item.img}
-
           <p className="blue-dark b f4 pt0">
             <span className="mr1">{i + 1}.</span>
             {item.titleLink ? (
@@ -90,7 +89,6 @@ const Steps = ({ items }) => (
         </div>
       </div>
     ))}
-    <div style={{ height: '60%' }} className="w-100 bg-tan relative bottom--2 right--2 z-1 "></div>
   </div>
 );
 
@@ -99,10 +97,10 @@ const Manuals = ({ contents }) => (
     <h3 className="f2 ttu barlow-condensed fw6">
       <FormattedMessage {...messages.learnManualsTitle} />
     </h3>
-    <div className="w-100 cf">
+    <div className="w-100 cf flex flex-wrap">
       {contents.map((content, i) => (
-        <div key={i} style={{ height: '20rem' }} className="w-25-l w-third-m w-100 fl ph2">
-          <div className="shadow-4">
+        <div key={i} className="w-50-ns w-50-m w-25-l w-100 fl pa2">
+          <div className="shadow-4 h-100">
             <a
               className="no-underline"
               rel="noopener noreferrer"
@@ -116,7 +114,7 @@ const Manuals = ({ contents }) => (
                   backgroundSize: '55%',
                 }}
               ></div>
-              <div className="pa3" style={{ height: '12rem' }}>
+              <div className="pa3">
                 <p className="fw7 f4 mt0 blue-dark">
                   <FormattedMessage {...messages[`${content.message}Title`]} />
                 </p>
@@ -148,11 +146,11 @@ const Videos = ({ contents }) => {
       <h3 className="f2 ttu barlow-condensed fw6">
         <FormattedMessage {...messages.learnVideosTitle} />
       </h3>
-      <div className="w-100 cf">
+      <div className="w-100 cf flex flex-wrap">
         {contents.map((content, i) => {
           return (
-            <div className="w-25-l w-third-m w-100 fl ph2" key={i}>
-              <div className="shadow-4 pointer" onClick={() => setActiveVideo(content)}>
+            <div className="w-50-ns w-50-m w-25-l w-100 pa2" key={i}>
+              <div className="shadow-4 pointer h-100" onClick={() => setActiveVideo(content)}>
                 <div
                   className="bg-tan w-100 tc h5-l h4"
                   style={{
@@ -162,7 +160,7 @@ const Videos = ({ contents }) => {
                 >
                   <PlayIcon className="white pv5-l pv0 mv3" height="6rem" />
                 </div>
-                <div className="pa3 db" style={{ height: '8rem' }}>
+                <div className="pa3 db">
                   <p className="fw7 f4 mt0 blue-dark">
                     <FormattedMessage {...messages[`${content.message}Title`]} />
                   </p>
