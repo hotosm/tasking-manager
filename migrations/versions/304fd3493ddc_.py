e5240a4874b8fd0e8d0e6fb38839e29b754047ab
Aadesh-Baral <elkloppiko@gmail.com>
2022-06-21 12:14:20 +0545

    Add project name  in text_searchable update trigger

--- migrations/versions/304fd3493ddc_.py
+++ migrations/versions/304fd3493ddc_.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '304fd3493ddc'
-down_revision = '924a63857df4'
+revision = "304fd3493ddc"
+down_revision = "924a63857df4"
 branch_labels = None
 depends_on = None
 
@@ -21,7 +21,7 @@ def upgrade():
         """
         DROP TRIGGER IF EXISTS tsvectorupdate ON project_info;
         CREATE TRIGGER tsvectorupdate BEFORE INSERT OR UPDATE ON project_info FOR EACH ROW EXECUTE PROCEDURE
-        tsvector_update_trigger(text_searchable, "pg_catalog.english", project_id_str, short_description, description)
+        tsvector_update_trigger(text_searchable, "pg_catalog.english", project_id_str, name, short_description, description)
         """
     )
 
