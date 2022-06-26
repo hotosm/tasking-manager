f559d8f1cd920cb3da4a9637bc6aba8bde84c7a2
Hel Nershing Thapa <51614993+HelNershingThapa@users.noreply.github.com>
2022-06-26 14:10:01 +0545

    Update editor only if list doesn't contain it (#5195)

--- frontend/src/components/taskSelection/footer.js
+++ frontend/src/components/taskSelection/footer.js
@@ -139,23 +139,28 @@ const TaskSelectionFooter = ({ defaultUserEditor, project, tasks, taskAction, se
     ) {
       const validationEditorOptions = getEditors(project.validationEditors, project.customEditor);
       setEditorOptions(validationEditorOptions);
-      // activate defaultUserEditor if it's allowed. If not, use the first allowed editor for validation
-      if (project.validationEditors.includes(defaultUserEditor)) {
-        setEditor(defaultUserEditor);
-      } else {
-        updateEditor(validationEditorOptions);
+      if (!project.validationEditors.includes(editor)) {
+        // activate defaultUserEditor if it's allowed. If not, use the first allowed editor for validation
+        if (project.validationEditors.includes(defaultUserEditor)) {
+          setEditor(defaultUserEditor);
+        } else {
+          updateEditor(validationEditorOptions);
+        }
       }
     } else {
       const mappingEditorOptions = getEditors(project.mappingEditors, project.customEditor);
       setEditorOptions(mappingEditorOptions);
-      // activate defaultUserEditor if it's allowed. If not, use the first allowed editor
-      if (project.mappingEditors.includes(defaultUserEditor)) {
-        setEditor(defaultUserEditor);
-      } else {
-        updateEditor(mappingEditorOptions);
+      if (!project.mappingEditors.includes(editor)) {
+        // activate defaultUserEditor if it's allowed. If not, use the first allowed editor
+        if (project.mappingEditors.includes(defaultUserEditor)) {
+          setEditor(defaultUserEditor);
+        } else {
+          updateEditor(mappingEditorOptions);
+        }
       }
     }
   }, [
+    editor,
     taskAction,
     project.mappingEditors,
     project.validationEditors,
