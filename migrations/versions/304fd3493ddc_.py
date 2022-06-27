be2d684ecfcf10ee64ca913290f51072e858dcf7
Aadesh-Baral <elkloppiko@gmail.com>
2022-06-27 10:10:43 +0545

    Modify migration to populate project name in existing text_searchable entries

--- migrations/versions/304fd3493ddc_.py
+++ migrations/versions/304fd3493ddc_.py
@@ -7,7 +7,6 @@ Create Date: 2022-06-20 13:50:26.229142
 """
 from alembic import op
 
-
 # revision identifiers, used by Alembic.
 revision = "304fd3493ddc"
 down_revision = "924a63857df4"
@@ -16,6 +15,7 @@ depends_on = None
 
 
 def upgrade():
+    # Modify tsvector_update_trgigger to include project_name
     op.execute(
         """
         DROP TRIGGER IF EXISTS tsvectorupdate ON project_info;
@@ -23,6 +23,15 @@ def upgrade():
         tsvector_update_trigger(text_searchable, "pg_catalog.english", project_id_str, name,
         short_description, description)
         """
+    )
+    # Upgrade existing text_searchable column inserting project_name
+    op.execute(
+        """
+            UPDATE project_info
+            SET text_searchable = to_tsvector('english', coalesce(project_id_str, '') || ' ' ||
+            coalesce(name, '') || ' ' || coalesce(short_description, '') || ' ' ||
+            coalesce(description, ''))
+            """
     )
 
 
