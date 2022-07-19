23c583975d2f85ef563164c632aaf23dbffca5d3
Hel Nershing Thapa <51614993+HelNershingThapa@users.noreply.github.com>
2022-07-19 12:00:47 +0545

    Apply sticky position to some components (#5255)
    Namely map on the 'explore projects' page, map on the user's projects section on the contributions page and categories on the project edit page.

--- frontend/src/views/projectEdit.js
+++ frontend/src/views/projectEdit.js
@@ -259,7 +259,7 @@ export default function ProjectEdit({ id }) {
       <h2 className="pb2 f2 fw6 mt2 mb3 ttu barlow-condensed blue-dark">
         <FormattedMessage {...messages.editProject} />
       </h2>
-      <div className="fl w-30-l w-100 ph0-ns ph4-m ph2 pb4">
+      <div className="fl w-30-l w-100 ph0-ns ph4-m ph2 pb4 sticky-top-l">
         <ReactPlaceholder
           showLoadingAnimation={true}
           rows={8}
