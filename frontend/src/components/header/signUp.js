9116c4cdc84d0659675f96eecb7caf807ed806f2
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-07-26 10:21:29 +0545

    Disable no-useless-escape rule for email validation regex

--- frontend/src/components/header/signUp.js
+++ frontend/src/components/header/signUp.js
@@ -93,8 +93,8 @@ const SignupForm = ({ data, setData, step, setStep }) => {
       return;
     }
 
-    /* eslint-disable-next-line */
     const re =
+      // eslint-disable-next-line no-useless-escape
       /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(data.email) === false) {
       setStep({ ...step, errMessage: <FormattedMessage {...messages.invalidEmail} /> });
