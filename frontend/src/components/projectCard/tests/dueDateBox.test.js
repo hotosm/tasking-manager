4118330dacd8174ff7da86efc053109f29e172f4
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-08-14 13:49:16 +0545

    Update test cases

--- frontend/src/components/projectCard/tests/dueDateBox.test.js
+++ frontend/src/components/projectCard/tests/dueDateBox.test.js
@@ -10,13 +10,11 @@ describe('test DueDate', () => {
   it('relative date formatting in English', () => {
     // six days of milliseconds plus a few seconds for the test
     const sixDaysOut = 6 * 86400 * 1000 + 10000 + Date.now();
-    const { container } = render(
+    render(
       <ReduxIntlProviders>
         <DueDateBox dueDate={sixDaysOut} />
       </ReduxIntlProviders>,
     );
-    expect(container.querySelectorAll('span')[0].className).toContain('fr');
-    expect(container.querySelectorAll('span')[0].className).not.toContain('fl');
     expect(screen.getByText('6 days left')).toBeInTheDocument();
     expect(screen.getByRole('img')).toBeInTheDocument();
   });
@@ -26,15 +24,13 @@ describe('test DueDate', () => {
     const fiveDaysOut = 5 * 86400 * 1000 + 10000 + Date.now();
     const { container } = render(
       <ReduxIntlProviders>
-        <DueDateBox dueDate={fiveDaysOut} align="left" tooltipMsg="Tooltip works" />
+        <DueDateBox dueDate={fiveDaysOut} tooltipMsg="Tooltip works" />
       </ReduxIntlProviders>,
     );
     expect(screen.queryByText('Tooltip works')).not.toBeInTheDocument();
     userEvent.hover(screen.getByText('5 days left'));
     expect(screen.getByText('Tooltip works')).toBeInTheDocument();
-    expect(container.querySelectorAll('span')[0].className).toContain('fl');
-    expect(container.querySelectorAll('span')[0].className).toContain('bg-grey-light blue-grey');
-    expect(container.querySelectorAll('span')[0].className).not.toContain('fr');
+    expect(container.querySelectorAll('span')[0].className).toContain('bg-tan blue-grey');
     expect(container.querySelectorAll('span')[0].className).not.toContain('bg-red white fw6');
   });
 
@@ -44,13 +40,40 @@ describe('test DueDate', () => {
         <DueDateBox dueDate={1000 * 540 + Date.now()} intervalMili={true} />
       </ReduxIntlProviders>,
     );
-    expect(container.querySelectorAll('span')[0].className).toContain('fr');
-    expect(container.querySelectorAll('span')[0].className).toContain('bg-red white fw6');
-    expect(container.querySelectorAll('span')[0].className).not.toContain('fl');
-    expect(container.querySelectorAll('span')[0].className).not.toContain(
-      'bg-grey-light blue-grey',
-    );
+    expect(container.querySelectorAll('span')[0].className).toContain('bg-red white');
+    expect(container.querySelectorAll('span')[0].className).not.toContain('bg-tan blue-grey');
     expect(screen.getByText('9 minutes left')).toBeInTheDocument();
     expect(screen.getByRole('img')).toBeInTheDocument();
+  });
+
+  it('renders the appropriate clock timer icon for task status page', () => {
+    render(
+      <ReduxIntlProviders>
+        <DueDateBox dueDate={1000 * 540 + Date.now()} intervalMili={true} isTaskStatusPage />
+      </ReduxIntlProviders>,
+    );
+    // A title tag has been added to the timer svg which we can access 
+    expect(
+      screen.getByRole('img', {
+        name: 'Timer',
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it('does not render the clock timer icon for project details', () => {
+    render(
+      <ReduxIntlProviders>
+        <DueDateBox
+          dueDate={1000 * 540 + Date.now()}
+          intervalMili={true}
+          isTaskStatusPage={false}
+        />
+      </ReduxIntlProviders>,
+    );
+    expect(
+      screen.queryByRole('img', {
+        name: 'Timer',
+      }),
+    ).not.toBeInTheDocument();
   });
 });
