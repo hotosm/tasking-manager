b245ccc99da6a01fc326aa9e93907857fdf68279
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-08-16 15:03:21 +0545

    Add sample top banner

---
+++ frontend/src/components/banner/TopBanner.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TopBanner() {
+  return (
+    <div className="ph3 pv3 b--grey-light bb bg-tan">
+      <div className='fw6 flex justify-center'>
+        Support Ukraine 🇺🇦 &nbsp;<span className='red'>Help Provide Humanitarian Aid to Ukraine.</span>
+      </div>
+    </div>
+  );
+}
+
+export default TopBanner;
