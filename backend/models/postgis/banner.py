67ebad134f89ad9a935fa7c613d12f73dbf41b7b
Aadesh-Baral <elkloppiko@gmail.com>
2022-08-17 19:19:38 +0545

    Add get+patch endpoint for system banners

---
+++ backend/models/postgis/banner.py
@@ -0,0 +1,46 @@
+from backend import db
+from backend.models.dtos.banner_dto import BannerDTO
+
+
+class Banner(db.Model):
+    """ Model for Banners"""
+
+    __tablename__ = "banner"
+    
+    # Columns
+    id = db.Column(db.Integer, primary_key=True)
+    message = db.Column(db.String, nullable=False)
+    visible = db.Column(db.Boolean, default=False, nullable=False)
+
+    def create(self):
+        """ Creates and saves the current model to the DB """
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        """ Updates the current model in the DB """
+        db.session.commit()
+
+    def update_from_dto(self, dto: BannerDTO):
+        """ Updates the current model in the DB """
+        self.message = dto.message
+        self.visible = dto.visible
+        db.session.commit()
+
+    def as_dto(self):
+        """ Returns a dto for the banner """
+        banner_dto = BannerDTO()
+        banner_dto.message = self.message
+        banner_dto.visible = self.visible
+        return banner_dto
+    
+    @staticmethod
+    def get():
+        """ Returns a banner and creates one if it doesn't exist """
+        banner = Banner.query.filter_by(id=1).first()
+        if banner is None:
+            banner = Banner()
+            banner.message = "Welcome to the API"
+            banner.visible = True
+            banner.create()
+        return banner
