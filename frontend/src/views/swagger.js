5c3a9c2355ec5ce82297edfd68e96ef7b8e2ad57
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-09-15 14:00:15 +0545

    Add height for swagger iframe for small screens

--- frontend/src/views/swagger.js
+++ frontend/src/views/swagger.js
@@ -8,7 +8,7 @@ export function SwaggerView() {
   return (
     <div className="w-100 cf">
       <iframe
-        className="fixed w-100 bn vh-minus-122-ns"
+        className="fixed w-100 bn vh-minus-122 vh-minus-122-ns"
         title="api-docs"
         src={`https://hotosm.github.io/swagger/?url=${API_URL}system/docs/json/`}
       />
