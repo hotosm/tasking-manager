b152ca8fecfb1e7a1db304fc10d50b28366799df
Hel Nershing Thapa <helnershingthapa@gmail.com>
2022-12-19 16:28:13 +0545

    Update test cases components in the campaign page

--- frontend/src/utils/testWithIntl.js
+++ frontend/src/utils/testWithIntl.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { render } from '@testing-library/react';
+import { createHistory, createMemorySource, LocationProvider } from '@reach/router';
 import TestRenderer from 'react-test-renderer';
 import { IntlProvider } from 'react-intl';
 
@@ -12,6 +14,19 @@ export const createComponentWithIntl = (children, props = { locale: 'en' }) => {
 export const createComponentWithReduxAndIntl = (children, props = { locale: 'en' }) => {
   return TestRenderer.create(<ReduxIntlProviders {...props}>{children}</ReduxIntlProviders>);
 };
+
+export function renderWithRouter(
+  ui,
+  { route = '/', history = createHistory(createMemorySource(route)) } = {},
+) {
+  return {
+    ...render(<LocationProvider history={history}>{ui}</LocationProvider>),
+    // adding `history` to the returned utilities to allow us
+    // to reference it in our tests (just try to avoid using
+    // this to test implementation details).
+    history,
+  };
+}
 
 export const ReduxIntlProviders = ({
   children,
---
+++ frontend/src/views/tests/campaigns.test.js
@@ -0,0 +1,196 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor, within } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import { IntlProviders, ReduxIntlProviders, renderWithRouter } from '../../utils/testWithIntl';
+import { ListCampaigns, CampaignError, CreateCampaign, EditCampaign } from '../campaigns';
+
+describe('ListCampaigns', () => {
+  it('should fetch and list campaigns', async () => {
+    render(
+      <ReduxIntlProviders>
+        <ListCampaigns />
+      </ReduxIntlProviders>,
+    );
+    // wait for the fetch API to be complete
+    await waitFor(() => expect(screen.getByText('Campaign Name 1')).toBeInTheDocument());
+    expect(screen.getByText('Campaign Name Two')).toBeInTheDocument();
+    expect(screen.getByText('Campaign Name Tres')).toBeInTheDocument();
+  });
+});
+
+describe('CampaignError', () => {
+  it('should display error message if saving campaign fails', () => {
+    render(
+      <IntlProviders>
+        <CampaignError error />
+      </IntlProviders>,
+    );
+    expect(screen.getByText('There was an error saving this campaign.')).toBeInTheDocument();
+  });
+});
+
+describe('CreateCampaign', () => {
+  const setup = () => {
+    const { history } = renderWithRouter(
+      <ReduxIntlProviders>
+        <CreateCampaign />
+      </ReduxIntlProviders>,
+    );
+    const createButton = screen.getByRole('button', {
+      name: /create campaign/i,
+    });
+    const cancelButton = screen.getByRole('button', {
+      name: /cancel/i,
+    });
+    return {
+      createButton,
+      cancelButton,
+      history,
+    };
+  };
+
+  it('should disable create campaign button by default', async () => {
+    const { createButton } = setup();
+    expect(createButton).toBeDisabled();
+  });
+
+  it('should enable create campaign button when the value is changed', async () => {
+    const { createButton } = setup();
+    const inputText = screen.getByRole('textbox');
+    fireEvent.change(inputText, { target: { value: 'New Campaign Name' } });
+    expect(createButton).toBeEnabled();
+  });
+
+  it('should navigate to the newly created campaign detail page on creation success', async () => {
+    const { history, createButton } = setup();
+    const inputText = screen.getByRole('textbox');
+    fireEvent.change(inputText, { target: { value: 'New Campaign Name' } });
+    expect(inputText.value).toBe('New Campaign Name');
+    expect(createButton).toBeEnabled();
+    fireEvent.click(createButton);
+    await waitFor(() => expect(history.location.pathname).toBe('/manage/campaigns/123'));
+  });
+
+  it('should navigate to the newly created campaign detail page on creation success', async () => {
+    const { history, createButton } = setup();
+    const inputText = screen.getByRole('textbox');
+    fireEvent.change(inputText, { target: { value: 'New Campaign Name' } });
+    expect(inputText.value).toBe('New Campaign Name');
+    expect(createButton).toBeEnabled();
+    fireEvent.click(createButton);
+    await waitFor(() => expect(history.location.pathname).toBe('/manage/campaigns/123'));
+  });
+
+  // TODO: When cancel button is clicked, the app should navigate to a previous relative path
+});
+
+describe('EditCampaign', () => {
+  const setup = () => {
+    const { history } = renderWithRouter(
+      <ReduxIntlProviders>
+        <EditCampaign id={123} />
+      </ReduxIntlProviders>,
+    );
+    const inputText = screen.getByRole('textbox');
+
+    return {
+      inputText,
+      history,
+    };
+  };
+
+  it('should display the campaign name by default', async () => {
+    const { inputText } = setup();
+    await waitFor(() => expect(inputText.value).toBe('Campaign Name 123'));
+    expect(inputText.value).toBe('Campaign Name 123');
+  });
+
+  it('should display save button when project name is changed', async () => {
+    const { inputText } = setup();
+    await waitFor(() => expect(inputText.value).toBe('Campaign Name 123'));
+    fireEvent.change(inputText, { target: { value: 'Changed Campaign Name' } });
+    const saveButton = screen.getByRole('button', {
+      name: /save/i,
+    });
+    expect(saveButton).toBeInTheDocument();
+  });
+
+  it('should also display cancel button when project name is changed', async () => {
+    const { inputText } = setup();
+    await waitFor(() => expect(inputText.value).toBe('Campaign Name 123'));
+    fireEvent.change(inputText, { target: { value: 'Changed Campaign Name' } });
+    const cancelButton = screen.getByRole('button', {
+      name: /cancel/i,
+    });
+    expect(cancelButton).toBeInTheDocument();
+  });
+
+  it('should return input text value to default when cancel button is clicked', async () => {
+    const { inputText } = setup();
+    await waitFor(() => expect(inputText.value).toBe('Campaign Name 123'));
+    fireEvent.change(inputText, { target: { value: 'Changed Campaign Name' } });
+    const cancelButton = screen.getByRole('button', {
+      name: /cancel/i,
+    });
+    fireEvent.click(cancelButton);
+    expect(inputText.value).toBe('Campaign Name 123');
+  });
+
+  it('should display project cards under the campaign', async () => {
+    setup();
+    expect(await screen.findByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', {
+        name: 'NRCS_Khajura Mapping',
+      }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', {
+        name: 'NRCS_Duduwa Mapping',
+      }),
+    ).toBeInTheDocument();
+  });
+
+  it('should ask for confirmation when user tries to delete a campaign', async () => {
+    setup();
+    expect(await screen.findByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+    const deleteButton = screen.getByRole('button', {
+      name: /delete/i,
+    });
+    fireEvent.click(deleteButton);
+    expect(screen.getByText('Are you sure you want to delete this campaign?')).toBeInTheDocument();
+  });
+
+  it('should close the confirmation popup when cancel is clicked', async () => {
+    setup();
+    expect(await screen.findByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+    const deleteButton = screen.getByRole('button', {
+      name: /delete/i,
+    });
+    fireEvent.click(deleteButton);
+    const cancelButton = screen.getByRole('button', {
+      name: /cancel/i,
+    });
+    fireEvent.click(cancelButton);
+    expect(
+      screen.queryByText('Are you sure you want to delete this campaign?'),
+    ).not.toBeInTheDocument();
+  });
+
+  it('should direct to campaigns list page on successful deletion of a campaign', async () => {
+    const { history } = setup();
+    expect(await screen.findByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+    const deleteButton = screen.getByRole('button', {
+      name: /delete/i,
+    });
+    fireEvent.click(deleteButton);
+    const dialog = screen.getByRole('dialog');
+    const deleteConfirmationButton = within(dialog).getByRole('button', {
+      name: /delete/i,
+    });
+    fireEvent.click(deleteConfirmationButton);
+    expect(await screen.findByText('Campaign deleted successfully.')).toBeInTheDocument();
+    await waitFor(() => expect(history.location.pathname).toBe('/manage/campaigns'));
+  });
+});
