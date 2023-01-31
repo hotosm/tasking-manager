547cda8864c74f484996dd226ec59421076d7627
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-01-31 12:11:55 +0545

    Add tests for update app dialog

---
+++ frontend/src/components/header/tests/updateDialog.test.js
@@ -0,0 +1,94 @@
+import '@testing-library/jest-dom';
+import { screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import messages from '../messages';
+import { UpdateDialog } from '../updateDialog';
+import { IntlProviders, renderWithRouter } from '../../../utils/testWithIntl';
+
+describe('Update Dialog', () => {
+  const originalWindowContext = window.location;
+
+  beforeAll(() => {
+    Object.defineProperty(window, 'location', {
+      configurable: true,
+      value: { reload: jest.fn() },
+    });
+  });
+
+  afterAll(() => {
+    Object.defineProperty(window, 'location', { configurable: true, value: originalWindowContext });
+  });
+
+  it('should not render prompt for mapping page', () => {
+    const { container } = renderWithRouter(
+      <IntlProviders>
+        <UpdateDialog />
+      </IntlProviders>,
+      {
+        route: '/projects/map/',
+      },
+    );
+    fireEvent(
+      document,
+      new CustomEvent('onNewServiceWorker', { detail: { registration: 'hello' } }),
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('should not render prompt for validation page', () => {
+    const { container } = renderWithRouter(
+      <IntlProviders>
+        <UpdateDialog />
+      </IntlProviders>,
+      {
+        route: '/projects/validate/',
+      },
+    );
+    fireEvent(
+      document,
+      new CustomEvent('onNewServiceWorker', { detail: { registration: 'hello' } }),
+    );
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('should render prompt for other pages', () => {
+    renderWithRouter(
+      <IntlProviders>
+        <UpdateDialog />
+      </IntlProviders>,
+    );
+    fireEvent(
+      document,
+      new CustomEvent('onNewServiceWorker', { detail: { registration: { waiting: true } } }),
+    );
+    expect(screen.getByText(messages.newVersionAvailable.defaultMessage)).toBeInTheDocument();
+    expect(
+      screen.getByText(messages.newVersionAvailableLineTwo.defaultMessage),
+    ).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: /refresh/i })).toBeInTheDocument();
+  });
+
+  it('should update the service worker', async () => {
+    renderWithRouter(
+      <IntlProviders>
+        <UpdateDialog />
+      </IntlProviders>,
+    );
+    const postMessageMock = jest.fn();
+    fireEvent(
+      document,
+      new CustomEvent('onNewServiceWorker', {
+        detail: { registration: { waiting: { postMessage: postMessageMock } } },
+      }),
+    );
+
+    Object.defineProperty(global.navigator, 'serviceWorker', {
+      value: {
+        addEventListener: jest.fn(),
+      },
+    });
+    await userEvent.click(screen.getByRole('button', { name: /refresh/i }));
+    expect(postMessageMock).toHaveBeenCalled();
+  });
+});
