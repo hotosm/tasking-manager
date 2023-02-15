c236a362e4d64829148f73d72ec5b12ae2b8e7e8
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-02-15 21:47:13 +0545

    Add CreateProject and ProjectsPageIndex tests, add Project Detail Page tests

--- frontend/src/views/tests/project.test.js
+++ frontend/src/views/tests/project.test.js
@@ -1,13 +1,53 @@
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
+import { globalHistory } from '@reach/router';
 import { ReachAdapter } from 'use-query-params/adapters/reach';
 import { QueryParamProvider } from 'use-query-params';
 import { act, render, screen, waitFor } from '@testing-library/react';
+import { setupFaultyHandlers } from '../../network/tests/server';
 
 import { store } from '../../store';
-import { ManageProjectsPage, UserProjectsPage } from '../project';
+import {
+  CreateProject,
+  ManageProjectsPage,
+  MoreFilters,
+  ProjectDetailPage,
+  ProjectsPage,
+  ProjectsPageIndex,
+  UserProjectsPage,
+} from '../project';
 import { ReduxIntlProviders, renderWithRouter } from '../../utils/testWithIntl';
 import { projects } from '../../network/tests/mockData/projects';
+
+test('More Filters should close the more filters container when clicked outside the container', async () => {
+  const navigateMock = jest.fn();
+  renderWithRouter(
+    <QueryParamProvider adapter={ReachAdapter}>
+      <ReduxIntlProviders>
+        <MoreFilters navigate={navigateMock} />
+      </ReduxIntlProviders>
+    </QueryParamProvider>,
+  );
+  expect(
+    screen.getByRole('link', {
+      name: /apply/i,
+    }),
+  ).toBeInTheDocument();
+
+  await userEvent.click(screen.getAllByRole('button')[2]);
+  expect(navigateMock).toHaveBeenCalled();
+});
+
+test('CreateProject renders ProjectCreate', async () => {
+  renderWithRouter(
+    <QueryParamProvider adapter={ReachAdapter}>
+      <ReduxIntlProviders>
+        <CreateProject />
+      </ReduxIntlProviders>
+    </QueryParamProvider>,
+  );
+  expect(screen.getByText('Loading...')).toBeInTheDocument();
+});
 
 describe('UserProjectsPage Component', () => {
   it('should redirect to login page if no user token is present', async () => {
@@ -35,7 +75,7 @@ describe('UserProjectsPage Component', () => {
     renderWithRouter(
       <QueryParamProvider adapter={ReachAdapter}>
         <ReduxIntlProviders>
-          <UserProjectsPage management={false} />
+          <UserProjectsPage management={false} location={{ pathname: '/manage' }} />
         </ReduxIntlProviders>
       </QueryParamProvider>,
     );
@@ -72,6 +112,25 @@ describe('UserProjectsPage Component', () => {
     expect(screen.getByRole('heading', { name: 'WebGL Context Not Found' })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'WebGL is enabled' })).toBeInTheDocument();
   });
+});
+
+test('More Filters should close the more filters container when clicked outside the container', async () => {
+  const navigateMock = jest.fn();
+  renderWithRouter(
+    <QueryParamProvider adapter={ReachAdapter}>
+      <ReduxIntlProviders>
+        <MoreFilters navigate={navigateMock} />
+      </ReduxIntlProviders>
+    </QueryParamProvider>,
+  );
+  expect(
+    screen.getByRole('link', {
+      name: /apply/i,
+    }),
+  ).toBeInTheDocument();
+
+  await userEvent.click(screen.getAllByRole('button')[2]);
+  expect(navigateMock).toHaveBeenCalledWith('/explore?managedByMe=1');
 });
 
 describe('ManageProjectsPage', () => {
@@ -114,5 +173,101 @@ describe('ManageProjectsPage', () => {
     // Since WebGL is not supported by Node, we'll assume that the map context will be loaded
     expect(screen.getByRole('heading', { name: 'WebGL Context Not Found' })).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'WebGL is enabled' })).toBeInTheDocument();
+  });
+});
+
+test('ProjectsPageIndex is a null DOM element', () => {
+  const { container } = render(<ProjectsPageIndex />);
+  expect(container).toBeEmptyDOMElement();
+});
+
+describe('Projects Page', () => {
+  const setup = () => {
+    renderWithRouter(
+      <QueryParamProvider adapter={ReachAdapter}>
+        <ReduxIntlProviders>
+          <ProjectsPage location={globalHistory.location} />
+        </ReduxIntlProviders>
+      </QueryParamProvider>,
+    );
+  };
+
+  it('should render component details', async () => {
+    setup();
+    expect(await screen.findByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+  });
+
+  it('should display map by user preferences', async () => {
+    setup();
+    await waitFor(() => {
+      expect(screen.getByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+    });
+    expect(screen.getByRole('heading', { name: 'WebGL Context Not Found' })).toBeInTheDocument();
+  });
+
+  it('should not display map by user preferences', async () => {
+    act(() => {
+      store.dispatch({ type: 'TOGGLE_MAP' });
+    });
+    setup();
+    await waitFor(() => {
+      expect(screen.getByText('NRCS_Duduwa Mapping')).toBeInTheDocument();
+    });
+    expect(
+      screen.queryByRole('heading', { name: 'WebGL Context Not Found' }),
+    ).not.toBeInTheDocument();
+  });
+});
+
+describe('Project Detail Page', () => {
+  jest.mock('react-chartjs-2', () => ({
+    Doughnut: () => null,
+    Bar: () => null,
+    Line: () => null,
+  }));
+
+  it('should render component details', async () => {
+    act(() => {
+      store.dispatch({ type: 'SET_LOCALE', locale: 'es-AR' });
+    });
+    render(
+      <ReduxIntlProviders>
+        <ProjectDetailPage id={123} navigate={() => jest.fn()} />
+      </ReduxIntlProviders>,
+    );
+    await waitFor(() => {
+      expect(screen.getByText(/sample project/i)).toBeInTheDocument();
+      expect(screen.getByText(/hello world/i)).toBeInTheDocument();
+    });
+  });
+
+  it('should display private project error message', async () => {
+    setupFaultyHandlers();
+    render(
+      <ReduxIntlProviders>
+        <ProjectDetailPage id={123} navigate={() => jest.fn()} />
+      </ReduxIntlProviders>,
+    );
+    await waitFor(() =>
+      expect(
+        screen.getByText("You don't have permission to access this project"),
+      ).toBeInTheDocument(),
+    );
+  });
+
+  it('should display generic error message', async () => {
+    setupFaultyHandlers();
+    render(
+      <ReduxIntlProviders>
+        <ProjectDetailPage id={123} navigate={() => jest.fn()} />
+      </ReduxIntlProviders>,
+    );
+    await waitFor(() =>
+      expect(
+        screen.getByRole('heading', {
+          name: 'Project 123 not found',
+        }),
+      ).toBeInTheDocument(),
+    );
   });
 });
