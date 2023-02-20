7a7c054932dc17103198c5c413dd973b7330acca
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-02-20 11:06:24 +0545

    Update tests for MappingTypeFilterPicker

--- frontend/src/components/projects/tests/mappingTypeFilterPicker.test.js
+++ frontend/src/components/projects/tests/mappingTypeFilterPicker.test.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import '@testing-library/jest-dom';
+import { render, screen } from '@testing-library/react';
+import { ReachAdapter } from 'use-query-params/adapters/reach';
+import { QueryParamProvider } from 'use-query-params';
+import userEvent from '@testing-library/user-event';
 
-import { createComponentWithIntl } from '../../../utils/testWithIntl';
+import { createComponentWithIntl, ReduxIntlProviders } from '../../../utils/testWithIntl';
 import { MappingTypeFilterPicker } from '../mappingTypeFilterPicker';
 import { RoadIcon } from '../../svgIcons';
 
@@ -10,5 +14,86 @@ it('mapping type options show the road icon', () => {
   // RoadIcon is present because mapping types is rendered
   expect(() => testInstance.findByType(RoadIcon)).not.toThrow(
     new Error('No instances found with node type: "RoadIcon"'),
+  );
+});
+
+it('should set query for the clicked icon', async () => {
+  const setMappingTypesQueryMock = jest.fn();
+  render(
+    <ReduxIntlProviders>
+      <QueryParamProvider adapter={ReachAdapter}>
+        <MappingTypeFilterPicker setMappingTypesQuery={setMappingTypesQueryMock} />
+      </QueryParamProvider>
+    </ReduxIntlProviders>,
+  );
+  await userEvent.click(
+    screen.getByRole('checkbox', {
+      name: /roads/i,
+    }),
+  );
+  expect(setMappingTypesQueryMock).toHaveBeenCalledWith(['ROADS'], 'pushIn');
+});
+
+it('should highlight active selected map icon', async () => {
+  const setMappingTypesQueryMock = jest.fn();
+  render(
+    <ReduxIntlProviders>
+      <QueryParamProvider adapter={ReachAdapter}>
+        <MappingTypeFilterPicker
+          mappingTypes={['ROADS']}
+          setMappingTypesQuery={setMappingTypesQueryMock}
+        />
+      </QueryParamProvider>
+    </ReduxIntlProviders>,
+  );
+  expect(screen.getByTitle('roads')).toHaveClass('blue-dark');
+  expect(screen.getByTitle('buildings')).not.toHaveClass('blue-dark');
+});
+
+it('should concatinate values with the present query', async () => {
+  const setMappingTypesQueryMock = jest.fn();
+  render(
+    <ReduxIntlProviders>
+      <QueryParamProvider adapter={ReachAdapter}>
+        <MappingTypeFilterPicker
+          mappingTypes={['ROADS', 'BUILDINGS']}
+          setMappingTypesQuery={setMappingTypesQueryMock}
+        />
+      </QueryParamProvider>
+    </ReduxIntlProviders>,
+  );
+
+  await userEvent.click(
+    screen.getByRole('checkbox', {
+      name: /waterways/i,
+    }),
+  );
+  expect(setMappingTypesQueryMock).toHaveBeenCalledWith(
+    ['ROADS', 'BUILDINGS', 'WATERWAYS'],
+    'pushIn',
+  );
+});
+
+it('should deselect from the query value', async () => {
+  const setMappingTypesQueryMock = jest.fn();
+  render(
+    <ReduxIntlProviders>
+      <QueryParamProvider adapter={ReachAdapter}>
+        <MappingTypeFilterPicker
+          mappingTypes={['ROADS', 'BUILDINGS']}
+          setMappingTypesQuery={setMappingTypesQueryMock}
+        />
+      </QueryParamProvider>
+    </ReduxIntlProviders>,
+  );
+
+  await userEvent.click(
+    screen.getByRole('checkbox', {
+      name: /roads/i,
+    }),
+  );
+  expect(setMappingTypesQueryMock).toHaveBeenCalledWith(
+    ['BUILDINGS'],
+    'pushIn',
   );
 });
