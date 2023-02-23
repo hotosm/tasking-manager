414d7f575bb5fab283b943cbe4a5388c72d5d212
Aadesh-Baral <elkloppiko@gmail.com>
2023-02-23 14:14:55 +0545

    Add tests for endpoints to search user by username and fetch all users

--- tests/backend/integration/api/users/test_resources.py
+++ tests/backend/integration/api/users/test_resources.py
@@ -1,5 +1,5 @@
 from backend.models.postgis.task import Task, TaskStatus
-from backend.models.postgis.statuses import UserGender
+from backend.models.postgis.statuses import UserGender, UserRole, MappingLevel
 
 from tests.backend.base import BaseTestCase
 from tests.backend.helpers.test_helpers import (
@@ -235,3 +235,196 @@ class UsersQueriesInterestsAPI(BaseTestCase):
         self.assertEqual(response.json["interests"][0]["name"], interest_1.name)
         self.assertEqual(response.json["interests"][1]["id"], interest_2.id)
         self.assertEqual(response.json["interests"][1]["name"], interest_2.name)
+
+
+class TestUsersQueriesUsernameFilterAPI(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.user = return_canned_user(TEST_USERNAME, TEST_USER_ID)
+        self.user.create()
+        self.user_2 = return_canned_user("user_2", 2222222)
+        self.user_2.create()
+        self.user_3 = return_canned_user("user_3", 3333333)
+        self.user_3.create()
+        self.user_session_token = generate_encoded_token(TEST_USER_ID)
+        self.url = "/api/v2/users/queries/filter/tes/"
+
+    def test_returns_401_without_session_token(self):
+        """ Test that the API returns 401 if no session token is provided """
+        # Act
+        response = self.client.get(self.url)
+        # Assert
+        self.assertEqual(response.status_code, 401)
+
+    def test_returns_404_if_no_users_found(self):
+        """ Test that the API returns 404 if no users found """
+        # Act
+        response = self.client.get(
+            "/api/v2/users/queries/filter/invalid_username/",
+            headers={"Authorization": self.user_session_token},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.json["SubCode"], "NotFound")
+
+    def test_returns_users_if_users_found(self):
+        """ Test that the API returns users if users found """
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertListEqual(
+            list(response.json.keys()), ["pagination", "usernames", "users"]
+        )
+        self.assertEqual(len(response.json["usernames"]), 1)
+        self.assertEqual(response.json["usernames"][0], self.user.username)
+        self.assertEqual(response.json["pagination"]["page"], 1)
+        self.assertEqual(response.json["pagination"]["perPage"], 20)
+        self.assertEqual(response.json["pagination"]["total"], 1)
+
+    def test_returnns_matching_project_contributors(self):
+        """ Test that the API returns matching project contributors """
+        # Arrange
+        test_project, _ = create_canned_project()
+        task = Task.get(1, test_project.id)
+        task.lock_task_for_mapping(self.user_2.id)
+        task.unlock_task(self.user_2.id, TaskStatus.MAPPED)
+        # Act
+        response = self.client.get(
+            f"/api/v2/users/queries/filter/user_/?projectId={test_project.id}",
+            headers={"Authorization": self.user_session_token},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["usernames"]), 2)
+        self.assertEqual(response.json["usernames"][0], self.user_2.username)
+        self.assertEqual(response.json["usernames"][1], self.user_3.username)
+        self.assertEqual(response.json["users"][0]["username"], self.user_2.username)
+        self.assertEqual(response.json["users"][0]["projectId"], test_project.id)
+
+
+class TestUsersAllAPI(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.user = return_canned_user(TEST_USERNAME, TEST_USER_ID)
+        self.user.create()
+        self.user_session_token = generate_encoded_token(TEST_USER_ID)
+        for i in range(30):
+            user = return_canned_user(f"user_{i}", i)
+            user.create()
+        self.url = "/api/v2/users/"
+
+    def test_returns_401_without_session_token(self):
+        """ Test that the API returns 401 if no session token is provided """
+        # Act
+        response = self.client.get(self.url)
+        # Assert
+        self.assertEqual(response.status_code, 401)
+
+    def test_returns_400_if_invalid_data(self):
+        """ Test that the API returns 400 if invalid data is provided """
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+            query_string={"role": "GOD"},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(response.json["SubCode"], "InvalidData")
+
+    def test_returns_per_page_20_users_by_default(self):
+        """ Test that the API paginates and returns 20 users by default """
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["users"]), 20)
+        self.assertEqual(response.json["pagination"]["page"], 1)
+        self.assertEqual(response.json["pagination"]["perPage"], 20)
+        self.assertEqual(response.json["pagination"]["total"], 31)
+
+    def test_pagination_can_be_disabled(self):
+        """ Test that the API can return all users if pagination is disabled """
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+            query_string={"pagination": "false"},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["users"]), 31)
+
+    def test_returns_specified_per_page_users(self):
+        """ Test that the API returns specified per page users """
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+            query_string={"perPage": 10},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["users"]), 10)
+        self.assertEqual(response.json["pagination"]["page"], 1)
+        self.assertEqual(response.json["pagination"]["perPage"], 10)
+        self.assertEqual(response.json["pagination"]["total"], 31)
+        self.assertEqual(response.json["pagination"]["hasNext"], True)
+        self.assertEqual(response.json["pagination"]["pages"], 4)
+
+    def test_returns_specified_page_users(self):
+        """ Test that the API returns specified page users """
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+            query_string={"page": 2},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["users"]), 11)
+        self.assertEqual(response.json["pagination"]["page"], 2)
+        self.assertEqual(response.json["pagination"]["hasNext"], False)
+        self.assertEqual(response.json["pagination"]["hasPrev"], True)
+        self.assertEqual(response.json["pagination"]["pages"], 2)
+
+    def test_returns_users_with_specified_role_(self):
+        """ Test that the API returns users with specified role """
+        # Arrange
+        self.user.role = UserRole.ADMIN.value
+        self.user.save()
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+            query_string={"role": "ADMIN"},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["users"]), 1)
+        self.assertEqual(response.json["pagination"]["page"], 1)
+        self.assertEqual(response.json["pagination"]["total"], 1)
+
+    def test_returns_users_with_specified_level(self):
+        """ Test that the API returns users with specified level """
+        # Arrange
+        self.user.mapping_level = MappingLevel.ADVANCED.value
+        self.user.save()
+        # Act
+        response = self.client.get(
+            self.url,
+            headers={"Authorization": self.user_session_token},
+            query_string={"level": "ADVANCED"},
+        )
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json["users"]), 1)
+        self.assertEqual(response.json["pagination"]["page"], 1)
+        self.assertEqual(response.json["pagination"]["total"], 1)
