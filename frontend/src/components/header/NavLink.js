be5bf4221c9c5e5c95c5f03c5ed8725afa32e153
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-02-23 23:37:32 +0545

    Implement getProps on Links with hooks

--- frontend/src/components/header/NavLink.js
+++ frontend/src/components/header/NavLink.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { Link } from '@reach/router';
+import { Link, useMatch } from 'react-router-dom';
 
 export const TopNavLink = (props) => {
   const { isActive, ...otherProps } = props;
+  const linkCombo = 'link mh3 barlow-condensed blue-dark f4 ttu lh-solid nowrap pv2';
+  let match = useMatch(props.to + '/*');
+
   return (
-    <Link getProps={isActive} {...otherProps}>
+    <Link className={match ? `${linkCombo} bb b--blue-dark bw1` : linkCombo} {...otherProps}>
       {props.children}
     </Link>
   );
