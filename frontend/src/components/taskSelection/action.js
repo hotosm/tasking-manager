b9de7bd75aa6e83959733b21c7f2ad38d11d43de
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-03-28 10:45:36 +0545

    Remove onChange event for MultipleTaskHistoriesAccordion

--- frontend/src/components/taskSelection/action.js
+++ frontend/src/components/taskSelection/action.js
@@ -78,7 +78,6 @@ export function TaskMapAction({
   const [validationComments, setValidationComments] = useState({});
   const [validationStatus, setValidationStatus] = useState({});
   const [historyTabChecked, setHistoryTabChecked] = useState(false);
-  const [multipleTasksInfo, setMultipleTasksInfo] = useState({});
   const [showMapChangesModal, setShowMapChangesModal] = useState(false);
   const [showSessionExpiringDialog, setShowSessionExpiringDialog] = useState(false);
   const [showSessionExpiredDialog, setSessionTimeExpiredDialog] = useState(false);
@@ -106,18 +105,6 @@ export function TaskMapAction({
   const historyTabSwitch = () => {
     setHistoryTabChecked(true);
     setActiveSection('history');
-  };
-
-  const handleTaskHistories = (taskIds) => {
-    if (taskIds.length < 1) return;
-
-    taskIds.forEach((id) => {
-      if (!Object.keys(multipleTasksInfo).includes(id.toString())) {
-        fetchLocalJSONAPI(`projects/${project.projectId}/tasks/${id}/`, token).then((data) =>
-          setMultipleTasksInfo({ ...multipleTasksInfo, [id]: data }),
-        );
-      }
-    });
   };
 
   useEffect(() => {
@@ -438,7 +425,6 @@ export function TaskMapAction({
                       )}
                       {action === 'VALIDATION' && activeTasks.length > 1 && (
                         <MultipleTaskHistoriesAccordion
-                          handleChange={handleTaskHistories}
                           tasks={activeTasks}
                           projectId={project.projectId}
                         />
