f851cb5043d798b54a4be67cdfca46168b4afa1e
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-04-19 11:29:32 +0545

    Add tests for deleting and marking multiple notifications as read - should return nothing if no notification is selected - should decrement unread count in redux store if all notifications are not selected upon marking notifications as read - should fetch unread count if all notifications are selected upon marking notifications as read - should decrement unread count in redux store if all notifications are not selected upon deleting notifications - should fetch unread count if all notifications are selected upon deleting notifications - should catch error when marking multiple selected notifications as read - should catch error when marking all notifications in a category as read - should catch error when deleting multiple selected notifications - should catch error when deleting all notifications in a category

---
+++ frontend/src/components/notifications/tests/actionButtons.test.js
@@ -0,0 +1,219 @@
+import '@testing-library/jest-dom';
+import { render, screen, act, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { setupFaultyHandlers } from '../../../network/tests/server';
+import { store } from '../../../store';
+import { ActionButtons } from '../actionButtons';
+import { ReduxIntlProviders } from '../../../utils/testWithIntl';
+
+describe('Action Buttons', () => {
+  const retryFnMock = jest.fn();
+  const setSelectedMock = jest.fn();
+  const updateUnreadCountMock = jest.fn();
+  it('should return nothing if no notification is selected', () => {
+    act(() => {
+      store.dispatch({ type: 'SET_TOKEN', token: 'validToken' });
+    });
+    const { container } = render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: undefined }}
+          selected={[]}
+          retryFn={retryFnMock}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+        />
+      </ReduxIntlProviders>,
+    );
+
+    expect(container).toBeEmptyDOMElement();
+  });
+
+  it('should decrement unread count in redux store if all notifications are not selected upon marking notifications as read', async () => {
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: undefined }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={false}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+          unreadCountInSelected={1}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /mark as read/i,
+      }),
+    );
+    await waitFor(() => expect(setSelectedMock).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(retryFnMock).toHaveBeenCalledTimes(1));
+    expect(updateUnreadCountMock).not.toHaveBeenCalled();
+  });
+
+  it('should fetch unread count if all notifications are selected upon marking notifications as read', async () => {
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: [6, 9] }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={true}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /mark as read/i,
+      }),
+    );
+    await waitFor(() => expect(setSelectedMock).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(retryFnMock).toHaveBeenCalledTimes(1));
+    expect(updateUnreadCountMock).toHaveBeenCalled();
+  });
+
+  it('should decrement unread count in redux store if all notifications are not selected upon deleting notifications', async () => {
+    act(() => {});
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: undefined }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={false}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+          unreadCountInSelected={1}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /delete/i,
+      }),
+    );
+    await waitFor(() => expect(setSelectedMock).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(retryFnMock).toHaveBeenCalledTimes(1));
+    expect(updateUnreadCountMock).not.toHaveBeenCalled();
+  });
+
+  it('should fetch unread count if all notifications are selected upon deleting notifications', async () => {
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: [6, 9] }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={true}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /delete/i,
+      }),
+    );
+    await waitFor(() => expect(setSelectedMock).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(retryFnMock).toHaveBeenCalledTimes(1));
+    expect(updateUnreadCountMock).toHaveBeenCalled();
+  });
+
+  // Error are consoled in all cases of POST error
+  it('should catch error when marking multiple selected notifications as read', async () => {
+    setupFaultyHandlers();
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: undefined }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={false}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+          unreadCountInSelected={1}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /mark as read/i,
+      }),
+    );
+    // Error is then consoled
+  });
+
+  it('should catch error when marking all notifications in a category as read', async () => {
+    setupFaultyHandlers();
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: [6, 9] }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={true}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /mark as read/i,
+      }),
+    );
+    // Error is then consoled
+  });
+
+  it('should catch error when deleting multiple selected notifications', async () => {
+    act(() => {});
+    setupFaultyHandlers();
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: undefined }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={false}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+          unreadCountInSelected={1}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /delete/i,
+      }),
+    );
+    // Error is then consoled
+  });
+
+  it('should catch error when deleting all notifications in a category', async () => {
+    setupFaultyHandlers();
+    render(
+      <ReduxIntlProviders>
+        <ActionButtons
+          inboxQuery={{ types: [6, 9] }}
+          selected={[1, 2, 3]}
+          retryFn={retryFnMock}
+          isAllSelected={true}
+          setSelected={setSelectedMock}
+          updateUnreadCount={updateUnreadCountMock}
+        />
+      </ReduxIntlProviders>,
+    );
+    await userEvent.click(
+      screen.getByRole('button', {
+        name: /delete/i,
+      }),
+    );
+    // Error is then consoled
+  });
+});
