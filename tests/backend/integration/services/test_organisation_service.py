32e31adbfc0c24cbe7eb3364ba7ac23b59ce9a52
Aadesh-Baral <elkloppiko@gmail.com>
2023-04-21 13:42:55 +0545

    Add test case for function to return organisation as dto

--- tests/backend/integration/services/test_organisation_service.py
+++ tests/backend/integration/services/test_organisation_service.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from schematics.exceptions import UndefinedValueError
 
 from tests.backend.base import BaseTestCase
-from backend.models.postgis.statuses import ProjectStatus
+from backend.models.postgis.statuses import ProjectStatus, TeamVisibility
 from tests.backend.helpers.test_helpers import (
     add_manager_to_organisation,
     create_canned_organisation,
@@ -10,6 +10,7 @@ from tests.backend.helpers.test_helpers import (
     TEST_USER_ID,
     create_canned_project,
     create_canned_user,
+    return_canned_team,
 )
 from backend.services.organisation_service import OrganisationService, NotFound
 
@@ -137,3 +138,67 @@ class TestOrgansitaionService(BaseTestCase):
         self.assertEqual(org_stats.active_tasks.badimagery, 0)
         self.assertEqual(org_stats.active_tasks.validated, 0)
         self.assertEqual(org_stats.active_tasks.invalidated, 0)
+
+    def assert_org_dto(self, org_dto):
+        """Asserts that the organisation DTO is correct"""
+        self.assertEqual(org_dto.organisation_id, self.test_org.id)
+        self.assertEqual(org_dto.name, self.test_org.name)
+        self.assertEqual(org_dto.description, None)
+        self.assertEqual(org_dto.url, None)
+
+    def test_get_organisation_dto(self):
+        """Test get_organisation_dto returns correct DTO"""
+        # Arrange
+        test_user = create_canned_user()
+        test_team_1 = return_canned_team("test_team_1", self.test_org.name)
+        test_team_1.visibility = TeamVisibility.PUBLIC.value
+        test_team_1.create()
+        test_team_2 = return_canned_team("test_team_2", self.test_org.name)
+        test_team_2.visibility = TeamVisibility.PRIVATE.value
+        test_team_2.create()
+
+        # Test with valid org and user id as non manager
+        org_dto = OrganisationService.get_organisation_dto(
+            self.test_org, test_user.id, abbreviated=False
+        )
+        self.assert_org_dto(org_dto)
+        self.assertFalse(org_dto.is_manager)
+        self.assertEqual(len(org_dto.teams), 1)
+        self.assertEqual(org_dto.teams[0].team_id, test_team_1.id)
+
+        # Test with valid org with abbreviated=True
+        org_dto = OrganisationService.get_organisation_dto(
+            self.test_org, test_user.id, abbreviated=True
+        )
+        self.assertEqual(org_dto.organisation_id, self.test_org.id)
+        self.assertEqual(org_dto.name, self.test_org.name)
+        self.assertEqual(org_dto.description, None)
+        self.assertEqual(org_dto.url, None)
+        self.assertFalse(org_dto.is_manager)
+        self.assertEqual(org_dto.teams, None)
+
+        # Test with valid org and user id as manager
+        add_manager_to_organisation(self.test_org, test_user)
+        org_dto = OrganisationService.get_organisation_dto(
+            self.test_org, test_user.id, abbreviated=False
+        )
+        self.assert_org_dto(org_dto)
+        self.assertTrue(org_dto.is_manager)
+        self.assertEqual(len(org_dto.teams), 2)
+        self.assertEqual(org_dto.teams[0].team_id, test_team_1.id)
+        self.assertEqual(org_dto.teams[1].team_id, test_team_2.id)
+
+        # Test with valid org and user id as 0
+        org_dto = OrganisationService.get_organisation_dto(
+            self.test_org, 0, abbreviated=False
+        )
+        self.assert_org_dto(org_dto)
+        self.assertFalse(org_dto.is_manager)
+        self.assertEqual(len(org_dto.teams), 1)
+        self.assertEqual(org_dto.teams[0].team_id, test_team_1.id)
+
+        # Test with invalid org
+        with self.assertRaises(NotFound):
+            OrganisationService.get_organisation_dto(
+                None, test_user.id, abbreviated=False
+            )
