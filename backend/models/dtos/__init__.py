9492dd97a8f64f14084230804b4f3cc146a0c861
Aadesh-Baral <elkloppiko@gmail.com>
2023-06-21 11:18:41 +0545

    Refactor request body validation decorator and improve request handling in views. --------------------------------------------------------- The previous implementation of the request body validation decorator has been refactored to provide better flexibility and handling of requests. The new implementation introduces the following changes:
    - Renamed the decorator from validate_request_body to validate_request to reflect its broader functionality of validating the entire request, including request body, query parameters, and path parameters.
    
    - Added a note in the decorator's docstring regarding the recommended order of applying decorators, emphasizing that it should come after the token_auth decorator if used. This ensures that the authenticated user ID can be set on the DTO if it has a user_id field.
    
    - Modified the decorator to dynamically set attribute values on the DTO based on the request's body, query parameters, and path parameters. This enables more flexible assignment of values to DTO attributes, accommodating different request sources.
    
    - Introduced the use of request.is_json to check if the request body is in JSON format before accessing its attributes. This helps avoid errors when handling non-JSON request bodies.
    
    - Added support for setting the authenticated user ID on the DTO if it contains a user_id field. The user ID is obtained from the token_auth service's current_user() method.
    
    This decorator should only be applied after the token_auth decorator (if token_auth is used) so that this decorator can access token_auth.current_user.

--- backend/models/dtos/__init__.py
+++ backend/models/dtos/__init__.py
@@ -13,16 +13,43 @@ def get_validation_errors(e):
     ]
 
 
-def validate_request_body(dto_class):
-    """Decorator to validate request body against a DTO class"""
+def validate_request(dto_class):
+    """
+    Decorator to validate request against a DTO class.
+    --------------------------------
+    NOTE: This decorator should be applied after the token_auth decorator (if used) so that
+    the authenticated user id can be set on the DTO if the DTO has a user_id field.
+    --------------------------------
+    Parameters:
+        dto_class: DTO
+            The DTO class to validate against
+    """
 
     def decorator(f):
         @wraps(f)
         def wrapper(*args, **kwargs):
+            from backend.services.users.authentication_service import token_auth
+
             try:
-                dto = dto_class(request.get_json())
+                dto = dto_class()
+
+                # Set attribute values from request body, query parameters, and path parameters
+                for attr in dto.__class__._fields:
+                    if request.is_json and attr in request.json:
+                        setattr(dto, attr, request.json[attr])
+                    elif attr in request.args:
+                        setattr(dto, attr, request.args.get(attr))
+                    elif attr in kwargs:
+                        setattr(dto, attr, kwargs[attr])
+
+                #  Set authenticated user id if user_id is a field in the DTO
+                if "user_id" in dto.__class__._fields:
+                    dto.user_id = token_auth.current_user()
+
                 dto.validate()
-                request.validated_dto = dto
+                request.validated_dto = (
+                    dto  # Set validated DTO on request object for use in view function
+                )
             except DataError as e:
                 field_errors = get_validation_errors(e)
                 raise BadRequest(field_errors=field_errors)
