cbf6cbdf5c95289249a7e992ef94d1b2508be802
Hel Nershing Thapa <helnershingthapa@gmail.com>
2023-06-28 14:20:12 +0545

    Wrap landing page stats with error boundary

--- frontend/src/views/home.js
+++ frontend/src/views/home.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { FormattedMessage } from 'react-intl';
 
 import { Jumbotron, SecondaryJumbotron } from '../components/homepage/jumbotron';
 import { StatsSection } from '../components/homepage/stats';
 import { MappingFlow } from '../components/homepage/mappingFlow';
 import { WhoIsMapping } from '../components/homepage/whoIsMapping';
 import { Testimonials } from '../components/homepage/testimonials';
+import { Alert } from '../components/alert';
+import homeMessages from '../components/homepage/messages';
 
 export function Home() {
   return (
     <div className="pull-center">
       <Jumbotron />
-      <StatsSection />
+      <ErrorBoundary
+        fallback={
+          <div className="pt5 pb2 ph6-l ph4">
+            <Alert type="error">
+              <FormattedMessage {...homeMessages.statsLoadingError} />
+            </Alert>
+          </div>
+        }
+      >
+        <StatsSection />
+      </ErrorBoundary>
       <MappingFlow />
       <WhoIsMapping />
       <Testimonials />
