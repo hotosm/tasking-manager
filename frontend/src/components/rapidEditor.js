35b3e2d7147e5e216750db890b708bb51db751f1
Taylor Smock <tsmock@meta.com>
2023-08-30 15:57:24 -0600

    Rapid: Use system interface for setting up Rapid
    Also, import Rapid from CDN instead of bundling it with the TM.
    We still have to bundle the Rapid CSS file -- initial try on
    importing the CSS file from the CDN failed. If that is fixed,
    we could use a semver query for the CDN version, and get minor
    Rapid updates for "free".
    
    Signed-off-by: Taylor Smock <tsmock@meta.com>

--- frontend/src/components/rapidEditor.js
+++ frontend/src/components/rapidEditor.js
@@ -1,13 +1,231 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
-import { gpx } from '@tmcw/togeojson';
-import * as Rapid from 'Rapid/dist/rapid.legacy';
 
-import 'Rapid/dist/rapid.css';
+// We import from a CDN
+import { version as rapidVersion, name as rapidName } from '@rapideditor/rapid/package.json'
+
+import '@rapideditor/rapid/dist/rapid.css';
 
 import { OSM_CLIENT_ID, OSM_CLIENT_SECRET, OSM_REDIRECT_URI, OSM_SERVER_URL } from '../config';
-import messages from './messages';
+
+/**
+ * The HOT TM system for Rapid. This should (eventually) extend AbstractSystem from Rapid
+ * @typedef {import("@rapideditor/rapid/modules").AbstractSystem} AbstractSystem
+ */
+class HotTaskingManagerSystem {
+  constructor(context) {
+    this.context = context
+    this.id = 'HotTaskingManagerSystem';
+    this.dependencies = new Set();
+    this.dependencies.add('storage');
+    this.dependencies.add('imagery');
+    this.dependencies.add('presets');
+    this.autoStart = true;
+    this._started = false;
+  }
+
+  initAsync() {
+    for (const id of this.dependencies) {
+      if (!this.context.systems[id]) {
+        return Promise.reject(`Cannot init: ${this.id} requires ${id}`);
+      }
+    }
+    return Promise.resolve();
+  }
+
+  startAsync() {
+    this._started = true;
+    return this.resetAsync();
+  }
+
+  resetAsync() {
+    this.bulkUpdate = false;
+    this._presetsInit(this._presets);
+    this._taskBoundaryInit(this._taskBoundary);
+    this._powerUserInit(this._powerUser);
+    this._imageryInit(this._imagery);
+    this.bulkUpdate = true;
+    return Promise.resolve();
+  }
+
+  /**
+   * Set the comment for the task
+   * @param {string} value The comment
+   */
+  set comment(value) {
+    this.context.defaultChangesetComment = value
+  }
+
+  set presets(presets) {
+    this._presets = presets;
+    this._presetsInit(this._presets);
+  }
+
+  _presetsInit(value) {
+    const presetSystem = this.context.systems.presets;
+    // if presets is not a populated list we need to set it as null
+    if (value && value.length) {
+      presetSystem.addablePresetIDs = new Set(value);
+    } else {
+      presetSystem.addablePresetIDs = null;
+    }
+  }
+
+  /**
+   * Set the task boundary
+   * @param {string} value The URL for the boundary information.
+   * Rapid treats URLs with 'project', 'task', and 'gpx' in them as tasking manager boundaries.
+   */
+  set taskBoundary(value) {
+    this._taskBoundary = value;
+    this._taskBoundaryInit(this._taskBoundary);
+  }
+
+  /**
+   * Set the task boundary
+   * @param {string} value The URL for the boundary information.
+   * Rapid treats URLs with 'project', 'task', and 'gpx' in them as tasking manager boundaries.
+   */
+  _taskBoundaryInit(value) {
+    this._updateHash(hashParams => {
+      hashParams.set('data', value);
+    });
+  }
+
+  set imagery(value) {
+    this._imagery = value;
+    this._imageryInit(this._imagery);
+  }
+
+  /**
+   *
+   * @param {ImagerySource} value
+   * @typedef {import("@rapideditor/rapid/modules").ImagerySource} ImagerySource
+   * @private
+   */
+  _imageryInit(value) {
+    this._updateHash(hashParams => {
+      if (value) {
+        // RapidContext.urlhash().setParam('background', imagery)
+        // window.location.hash = window.location.hash + '&background=' + imagery;
+
+        // SearchParams will actually need the leading hash if the param is first in the hash list.
+        hashParams.set('background', value);
+      }
+    });
+  }
+
+  /**
+   * Set whether the user is a power user
+   * @param {boolean} value true if the user should be shown power user options
+   */
+  set powerUser(value) {
+    this._powerUser = value;
+    this._powerUserInit(this._powerUser);
+  }
+
+  /**
+   * Set whether the user is a power user
+   * @param {boolean} value true if the user should be shown power user options
+   */
+  _powerUserInit(value) {
+    this._updateHash(hashParams => {
+      /* Set the poweruser */
+      if (value && !hashParams.has('poweruser')) {
+        hashParams.set('poweruser', value);
+      } else if (!value) {
+        hashParams.delete('poweruser');
+      }
+      this.context.systems.rapid.showPowerUser = value;
+    });
+  }
+
+  /**
+   * Check if this system has been started
+   * @returns {boolean} true if started
+   */
+  get started() {
+    return this._started;
+  }
+
+  /**
+   *
+   * @param {function(URLSearchParams)} runnable
+   * @private
+   */
+  _updateHash(runnable) {
+    const hash = window.location.hash.substring(1);
+    const hashParams = this._newHash ? this._newHash : new URLSearchParams(hash);
+    runnable.call(this, hashParams);
+    if (!this._equalsUrlParameters(hashParams, new URLSearchParams(hash))) {
+      if (!this.bulkUpdate) {
+        window.history.pushState(null, '', this._getNewUrl(hashParams));
+      } else {
+        this._newHash = hashParams;
+      }
+    }
+  }
+
+  /**
+   * Use to avoid firing window history updates
+   * @param {boolean} value true if we shouldn't push a new state yet
+   */
+  set bulkUpdate(value) {
+    this._bulkUpdate = value;
+    if (!value && this._newHash) {
+
+      window.history.pushState(null, '', this._getNewUrl(this._newHash));
+      this._newHash = undefined;
+    }
+  }
+
+  /**
+   * Generate a new URL that should be parsable by Rapid
+   * @param {URLSearchParams} hashParams The parameters to add to the URL
+   * @returns {string} The new URL
+   * @private
+   */
+  _getNewUrl(hashParams) {
+    let newUrl =
+      window.location.pathname + window.location.search + '#';
+    let first = true;
+    for (const [key, value] of hashParams) {
+      if (!first) {
+        newUrl += '&';
+      }
+      first = false;
+      newUrl += key + '=' + value;
+    }
+    return newUrl;
+  }
+
+  /**
+   * Check to see if we are in a bulk update
+   * @returns {boolean} true if we are currently updating the state
+   */
+  get bulkUpdate() {
+    return this._bulkUpdate;
+  }
+
+  /**
+   * Check if two URL search parameters are semantically equal
+   * @param {URLSearchParams} first
+   * @param {URLSearchParams} second
+   * @return {boolean} true if they are semantically equal
+   */
+  _equalsUrlParameters(first, second) {
+    if (first.size === second.size) {
+      for (const [key, value] of first) {
+        if (!second.has(key) || second.get(key) !== value) {
+          return false;
+        }
+      }
+      return true;
+    }
+    return false;
+  }
+}
 
 /**
  * Get the background for Rapid
@@ -82,121 +300,93 @@ export default function RapidEditor({
   const dispatch = useDispatch();
   const session = useSelector((state) => state.auth.session);
   const intl = useIntl();
-  const RapidContext = useSelector((state) => state.editor.rapidContext);
+  const [rapidLoaded, setRapidLoaded] = useState(window.Rapid !== undefined);
+  const rapidContext = useSelector((state) => state.editor.rapidContext);
   const locale = useSelector((state) => state.preferences.locale);
   const [customImageryIsSet, setCustomImageryIsSet] = useState(false);
   const windowInit = typeof window !== 'undefined';
 
+  // This significantly reduces build time _and_ means different TM instances can share the same download of Rapid.
+  // Unfortunately, Rapid doesn't use a public CDN itself, so we cannot reuse that.
   useEffect(() => {
-    if (windowInit) {
-      if (RapidContext === null) {
-        // we need to keep Rapid context on redux store because Rapid works better if
-        // the context is not restarted while running in the same browser session
-        const context = new window.Rapid.Context();
-        dispatch({ type: 'SET_RAPIDEDITOR', context: context });
-      }
-    }
-  }, [windowInit, RapidContext, dispatch]);
+    // This could be ^${rapidVersion} if we are able to import the css here as well.
+    const baseCdnUrl = `https://cdn.jsdelivr.net/npm/${rapidName}@${rapidVersion}/dist/`;
+    const script = document.createElement('script');
+    script.src = baseCdnUrl + 'rapid.js';
+    script.async = true;
+    script.onload = () => setRapidLoaded(true);
+    document.body.appendChild(script);
+  }, [setRapidLoaded]);
 
   useEffect(() => {
-    if (RapidContext) {
+    return () => {
+      dispatch({ type: 'SET_VISIBILITY', isVisible: true });
+    };
+    // eslint-disable-next-line
+  }, []);
+
+  useEffect(() => {
+    if (windowInit && rapidContext === null && rapidLoaded) {
+      // we need to keep Rapid context on redux store because Rapid works better if
+      // the context is not restarted while running in the same browser session
+      const context = new window.Rapid.Context();
       // setup the context
-      RapidContext.embed(true);
-      RapidContext.locale = locale;
-      RapidContext.containerNode = document.getElementById('rapid-container');
-      RapidContext.assetPath = '/static/rapid/';
+      context.embed(true);
+      context.locale = locale;
+      context.containerNode = document.getElementById('rapid-container');
+      context.assetPath = '/static/rapid/';
+      dispatch({ type: 'SET_RAPIDEDITOR', context: context });
+    }
+  }, [windowInit, rapidLoaded, rapidContext, dispatch, locale]);
+
+  useEffect(() => {
+    if (rapidContext) {
       // init the ui or restart if it was loaded previously
       let promise;
-      if (RapidContext?.systems?.ui !== undefined) {
-        promise = RapidContext.resetAsync();
+      if (rapidContext?.systems?.ui !== undefined) {
         // Currently commented out in Rapid source code (2023-07-20)
         // RapidContext.systems.ui.restart();
+        // resetAsync was a possible replacement, but it ran into issues when switching tasks.
+        promise = rapidContext.resetAsync();
       } else {
-        promise = RapidContext.initAsync();
+        promise = rapidContext.initAsync();
+        rapidContext.systems.hottaskingmanager = new HotTaskingManagerSystem(rapidContext);
+        promise.then(() => rapidContext.systems.hottaskingmanager.initAsync());
       }
 
       /* Perform tasks after Rapid has started up */
       promise.then(() => {
-        // Set the default comment
-        if (comment) {
-          RapidContext.defaultChangesetComment = comment;
-        }
-
-        /* Set the presets */
-        const presetSystem = RapidContext.systems.presets;
-        // if presets is not a populated list we need to set it as null
-        if (presets && presets.length) {
-          presetSystem.addablePresetIDs = new Set(presets);
-        } else {
-          presetSystem.addablePresetIDs = null;
-        }
-
-        /* Set the task boundaries */
-        if (gpxUrl) {
-          fetch(gpxUrl)
-            .then((response) => response.text())
-            .then((data) => {
-              let gpxData = new DOMParser().parseFromString(data, 'text/xml');
-              let nameNode = gpxData.getElementsByTagName('trk')[0].childNodes[0];
-              let projectId = nameNode.textContent.match(/\d+/g);
-              nameNode.textContent = intl.formatMessage(messages.gpxNameAttribute, {
-                projectId: projectId[0],
-              });
-              RapidContext.systems.map.scene.layers.get('custom-data').geojson(gpx(gpxData));
-            })
-            .catch((err) => {
-              console.error(err);
-              console.error('Error loading GPX data');
-            });
-        }
+        rapidContext.systems.hottaskingmanager.bulkUpdate = true;
+        rapidContext.systems.hottaskingmanager.comment = comment;
+        rapidContext.systems.hottaskingmanager.presets = presets;
+        rapidContext.systems.hottaskingmanager.taskBoundary = gpxUrl
+        rapidContext.systems.hottaskingmanager.powerUser = powerUser
 
         /* Set the background */
-        const hashParams = new URLSearchParams(window.location.hash.substring(1));
-        let changedHash = false;
-        const imagerySystem = RapidContext?.systems?.imagery;
-        if (imagerySystem) {
-          const customSource = RapidContext.systems.imagery.findSource('custom');
+        const imagerySystem = rapidContext?.systems?.imagery;
+        if (imagerySystem && imagerySystem?.findSource) {
+          const customSource = imagerySystem.findSource('custom');
           setupImagery(
-            RapidContext,
+            rapidContext,
             customImageryIsSet,
             setCustomImageryIsSet,
             imagery,
             customSource,
           );
-          const imageryInfo = getBackground(
+          if(getBackground(
             customImageryIsSet,
             imagery,
             imagerySystem,
             customSource,
-          );
-
-          if (imageryInfo) {
-            // RapidContext.urlhash().setParam('background', imagery)
-            // window.location.hash = window.location.hash + '&background=' + imagery;
-
-            // SearchParams will actually need the leading hash if the param is first in the hash list.
-            hashParams.set('background', imagery);
-            changedHash = true;
+          )) {
+            rapidContext.systems.hottaskingmanager.imagery = imagery;
           }
         }
 
-        /* Set the poweruser */
-        if (powerUser && !hashParams.has('poweruser')) {
-          hashParams.set('poweruser', powerUser);
-          changedHash = true;
-        } else if (!powerUser) {
-          hashParams.delete('poweruser');
-        }
-        RapidContext.systems.rapid.showPowerUser = powerUser;
-
-        if (changedHash) {
-          let newBackgroundImageURL =
-            window.location.pathname + window.location.search + '#' + hashParams.toString();
-          window.history.pushState(null, '', newBackgroundImageURL);
-        }
+        rapidContext.systems.hottaskingmanager.bulkUpdate = false;
 
         /* Keep track of edits */
-        const editSystem = RapidContext.systems.edits;
+        const editSystem = rapidContext.systems.edits;
         const thereAreChanges = (changes) =>
           changes.modified.length || changes.created.length || changes.deleted.length;
 
@@ -210,21 +400,26 @@ export default function RapidEditor({
       });
     }
   }, [
-    RapidContext,
+    rapidContext,
     comment,
     customImageryIsSet,
     gpxUrl,
     imagery,
     intl,
-    locale,
     powerUser,
     presets,
     setDisable,
   ]);
 
   useEffect(() => {
-    if (RapidContext && session) {
-      RapidContext.apiConnections = [
+    if (rapidContext) {
+      return () => rapidContext.save();
+    }
+  }, [rapidContext]);
+
+  useEffect(() => {
+    if (rapidContext && session) {
+      rapidContext.apiConnections = [
         {
           url: OSM_SERVER_URL,
           client_id: OSM_CLIENT_ID,
@@ -234,7 +429,7 @@ export default function RapidEditor({
         },
       ];
     }
-  }, [RapidContext, session, session?.osm_oauth_token]);
+  }, [rapidContext, session, session?.osm_oauth_token]);
 
   return <div className="w-100 vh-minus-69-ns" id="rapid-container"></div>;
 }
