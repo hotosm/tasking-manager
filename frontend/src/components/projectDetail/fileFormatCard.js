71f11847aa9b425bf97aca84f0c7be923e405db5
Deepak Pradhan <deepak.pradhan048@gmail.com>
2023-12-15 11:05:34 +0545

    feat: added spinner for loading on download

--- frontend/src/components/projectDetail/fileFormatCard.js
+++ frontend/src/components/projectDetail/fileFormatCard.js
@@ -1,24 +1,35 @@
 import React from 'react';
+import { AnimatedLoadingIcon } from '../button';
 
 /**
  * Renders a list of file formats as clickable links.
  *
  * @param {Object[]} fileFormats - An array of file format objects.
- * @param {string} fileFormats[].url - The URL of the file.
  * @param {string} fileFormats[].format - The format of the file.
  * @return {JSX.Element} The rendered list of file formats.
  */
 
-function FileFormatCard({ fileFormats }) {
+function FileFormatCard({ title, fileFormats, downloadS3Data, isDownloadingState }) {
   return (
     <>
       {fileFormats.map((fileFormat, index) => (
         <React.Fragment key={index}>
-          <a href={fileFormat.url} download className="link hover-red color-inherit">
+          <div
+            tabIndex={0}
+            style={{ cursor: 'pointer' }}
+            role="button"
+            onClick={() => downloadS3Data(title, fileFormat.format)}
+            className="link hover-red color-inherit"
+          >
             <p className="underline fw5" style={{ textUnderlineOffset: '5px' }}>
               {fileFormat.format}
+              {isDownloadingState?.isDownloading &&
+              isDownloadingState?.title === title &&
+              isDownloadingState?.fileFormat === fileFormat?.format ? (
+                <AnimatedLoadingIcon />
+              ) : null}
             </p>
-          </a>
+          </div>
           {index !== fileFormats.length - 1 && <hr className="file-list-separator" />}
         </React.Fragment>
       ))}
