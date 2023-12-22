b910488cc01c2b901e7dd6fccddd965392336010
Deepak Pradhan <deepak.pradhan048@gmail.com>
2023-12-22 16:24:21 +0545

    feat: loadingstate refactored

--- frontend/src/components/projectDetail/fileFormatCard.js
+++ frontend/src/components/projectDetail/fileFormatCard.js
@@ -12,27 +12,34 @@ import { AnimatedLoadingIcon } from '../button';
 function FileFormatCard({ title, fileFormats, downloadS3Data, isDownloadingState }) {
   return (
     <>
-      {fileFormats.map((fileFormat, index) => (
-        <React.Fragment key={index}>
-          <div
-            tabIndex={0}
-            style={{ cursor: 'pointer' }}
-            role="button"
-            onClick={() => downloadS3Data(title, fileFormat.format)}
-            className="link hover-red color-inherit"
-          >
-            <p className="underline fw5" style={{ textUnderlineOffset: '5px' }}>
-              {fileFormat.format}
-              {isDownloadingState?.isDownloading &&
-              isDownloadingState?.title === title &&
-              isDownloadingState?.fileFormat === fileFormat?.format ? (
-                <AnimatedLoadingIcon />
-              ) : null}
-            </p>
-          </div>
-          {index !== fileFormats.length - 1 && <hr className="file-list-separator" />}
-        </React.Fragment>
-      ))}
+      {fileFormats.map((fileFormat, index) => {
+        const loadingState =
+          isDownloadingState?.isDownloading &&
+          isDownloadingState?.title === title &&
+          isDownloadingState?.fileFormat === fileFormat?.format;
+
+        return (
+          <React.Fragment key={index}>
+            <div
+              tabIndex={0}
+              style={
+                loadingState
+                  ? { cursor: 'not-allowed', pointerEvents: 'none' }
+                  : { cursor: 'pointer' }
+              }
+              role="button"
+              onClick={() => downloadS3Data(title, fileFormat.format)}
+              className="link hover-red color-inherit"
+            >
+              <p className="underline fw5" style={{ textUnderlineOffset: '5px' }}>
+                {fileFormat.format}
+                {loadingState ? <AnimatedLoadingIcon /> : null}
+              </p>
+            </div>
+            {index !== fileFormats.length - 1 && <hr className="file-list-separator" />}
+          </React.Fragment>
+        );
+      })}
     </>
   );
 }
