3610c55d093d6701066097260c8493a6a19e4eb2
Deepak Pradhan <deepak.pradhan048@gmail.com>
2024-01-23 21:58:11 +0545

    feat: fetched meta.json for meta data and append onto config array

---
+++ frontend/src/utils/formatBytes.js
@@ -0,0 +1,13 @@
+function formatBytes(bytes, decimals = 2) {
+  if (!+bytes) return '0 Bytes';
+
+  const k = 1024;
+  const dm = decimals < 0 ? 0 : decimals;
+  const sizes = ['Bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+}
+
+export default formatBytes;
