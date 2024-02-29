a1e6035e5bebd28993e498534eaf1bd858724aaf
DK Benjamin <dakota.benjamin@gmail.com>
2024-02-29 15:29:02 -0500

    Add ListObjects Permission for OIDC frontend deploy

--- scripts/aws/cloudformation/tasking-manager.template.js
+++ scripts/aws/cloudformation/tasking-manager.template.js
@@ -820,7 +820,8 @@ const Resources = {
                 "s3:PutObject",
                 "s3:PutObjectAcl",
                 "s3:GetObject",
-                "s3:DeleteObject"
+                "s3:DeleteObject",
+                "s3:ListObjects"
             ],
             "Resource": [
               cf.join("/", [
