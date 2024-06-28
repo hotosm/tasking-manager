d1d1a91ec82f4a21e789f9194f46286c698540b3
royallsilwallz <royalsilwal@gmail.com>
2024-06-28 09:02:31 +0545

    Do not fetch data if current projects is null in `Partners Page`

--- frontend/src/components/partners/currentProjects.js
+++ frontend/src/components/partners/currentProjects.js
@@ -65,6 +65,7 @@ export function CurrentProjects({ currentProjects }) {
   };
 
   useEffect(() => {
+    if (!currentProjects) return;
     fetchData();
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
