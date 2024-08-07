ec31860b3c56fd6dcef9074816cab6b4154e0b7f
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2024-08-07 18:28:13 +0530

    feat: define columns of table

--- frontend/src/components/projects/exploreProjectsTable.js
+++ frontend/src/components/projects/exploreProjectsTable.js
@@ -7,9 +7,163 @@ import { PriorityBox } from '../projectCard/priorityBox';
 import messages from './messages';
 import './exploreProjectsTable.css';
 
+const COLUMNS = [
+  {
+    accessorKey: 'name',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.nameColumn} />
+      </span>
+    ),
+  },
+  {
+    accessorKey: 'author',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.authorColumn} />
+      </span>
+    ),
+  },
+  {
+    accessorKey: 'organisationName',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.organisationColumn} />
+      </span>
+    ),
+  },
+  {
+    accessorKey: 'progress',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.progressColumn} />
+      </span>
+    ),
+    minSize: 130,
+    cell: ({ row }) => {
+      return (
+        <ProgressBar
+          firstBarValue={row.original.percentMapped}
+          secondBarValue={row.original.percentValidated}
+          height="half"
+          small={false}
+        >
+          <p className="f6 lh-copy ma0 white f7 fw4">
+            <FormattedMessage
+              {...messages['percentMapped']}
+              values={{ n: <span className="fw8">{row.original.percentMapped}</span> }}
+            />
+          </p>
+          <p className="f6 lh-copy ma0 white f7 fw4">
+            <FormattedMessage
+              {...messages['percentValidated']}
+              values={{ n: <span className="fw8">{row.original.percentValidated}</span> }}
+            />
+          </p>
+        </ProgressBar>
+      );
+    },
+  },
+  {
+    accessorKey: 'totalContributors',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.mappersColumn} />
+      </span>
+    ),
+  },
+  {
+    accessorKey: 'priority',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.priorityColumn} />
+      </span>
+    ),
+    cell: ({ row }) => {
+      return (
+        <PriorityBox
+          priority={row.original.priority}
+          extraClasses="f6 pv1 ph2 dib"
+          showIcon={!['URGENT', 'MEDIUM', 'LOW'].includes(row.original.priority)}
+        />
+      );
+    },
+  },
+  {
+    accessorKey: 'difficulty',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.difficultyColumn} />
+      </span>
+    ),
+    cell: ({ row }) => {
+      const difficulty = row.original.difficulty;
+      if (difficulty === 'EASY') return <div className="i green">Easy</div>;
+      if (difficulty === 'MODERATE') return <div className="i orange">Medium</div>;
+      if (difficulty === 'CHALLENGING') return <div className="i red">Challenging</div>;
+    },
+  },
+  {
+    accessorKey: 'status',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.statusColumn} />
+      </span>
+    ),
+    cell: ({ row }) => {
+      const status = row.original.status;
+      if (status === 'DRAFT') return <div className="orange">Draft</div>;
+      if (status === 'PUBLISHED') return <div className="">Published</div>;
+      if (status === 'ARCHIVED') return <div className="gray">Archived</div>;
+    },
+  },
+  {
+    accessorKey: 'location',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.locationColumn} />
+      </span>
+    ),
+    cell: ({ row }) => {
+      if (!row.original?.country[0]) return null;
+      return row.original.country[0];
+    },
+  },
+  {
+    accessorKey: 'lastUpdated',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.lastUpdatedColumn} />
+      </span>
+    ),
+    cell: ({ row }) => {
+      if (!row.original.lastUpdated) return null;
+      return `${formatDistance(new Date(row.original.lastUpdated), new Date())} ago`;
+    },
+  },
+  {
+    accessorKey: 'dueDate',
+    header: () => (
+      <span>
+        <FormattedMessage {...messages.dueDateColumn} />
+      </span>
+    ),
+    cell: ({ row }) => {
+      if (!row.original.dueDate) return null;
+
+      const dueDateObject = new Date(row.original.dueDate);
+      return dueDateObject < new Date() ? (
+        <span className="gray">Finished</span>
+      ) : (
+        <span>`in ${formatDistance(dueDateObject, new Date())}`</span>
+      );
+    },
+  },
+];
+
 export const ExploreProjectsTable = ({ projects }) => {
   const table = useReactTable({
-    // columns: COLUMNS,
+    columns: COLUMNS,
     data: projects,
     getCoreRowModel: getCoreRowModel(),
     columnResizeMode: 'onChange',
