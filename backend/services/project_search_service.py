06b0d66616a4995990ce5da3020e47fbe851242f
bshankar <ebhavanishankar@gmail.com>
2024-08-22 11:42:05 +0530

    Feat: Add organisation name to CSV

--- backend/services/project_search_service.py
+++ backend/services/project_search_service.py
@@ -219,7 +219,6 @@ class ProjectSearchService:
             columns=[
                 "locale",
                 "shortDescription",
-                "organisationName",
                 "organisationLogo",
                 "campaigns",
             ],
