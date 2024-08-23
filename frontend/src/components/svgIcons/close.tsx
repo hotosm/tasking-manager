913e5d3d28e01b3f64c3a946c2d18ac40efe45b7
JoltCode <joe@joecc.dev>
2024-08-23 21:33:12 +0100

    chore(frontend): migrate more svg icons to TS

---
+++ frontend/src/components/svgIcons/close.tsx
@@ -0,0 +1,10 @@
+import { HTMLProps } from "react";
+
+export const CloseIcon = (props: HTMLProps<SVGSVGElement>) => (
+  <svg width="8" height="8" viewBox="0 0 8 8" {...props}>
+    <path
+      fill="currentColor"
+      d="M1.41 0l-1.41 1.41.72.72 1.78 1.81-1.78 1.78-.72.69 1.41 1.44.72-.72 1.81-1.81 1.78 1.81.69.72 1.44-1.44-.72-.69-1.81-1.78 1.81-1.81.72-.72-1.44-1.41-.69.72-1.78 1.78-1.81-1.78-.72-.72z"
+    />
+  </svg>
+);
---
+++ frontend/src/components/svgIcons/right.tsx
@@ -0,0 +1,14 @@
+import { HTMLProps } from "react";
+
+export const RightIcon = (props: HTMLProps<SVGSVGElement>) => (
+  <svg width="26" height="16" viewBox="0 0 26 16" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path d="M-7-12h40v40H-7z" />
+      <path
+        fill="currentColor"
+        fillRule="nonzero"
+        d="M18 0l-1.41 1.41L22.17 7H0v2h22.17l-5.58 5.59L18 16l8-8z"
+      />
+    </g>
+  </svg>
+);
