cdbb0468da3a53b6d6ba4b2e01c877ab24faf484
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2024-09-03 12:57:55 +0530

    Create a dir for partner mapswipe stats view, add Overview component with mock data

---
+++ frontend/src/components/partnerMapswipeStats/messages.js
@@ -0,0 +1,19 @@
+import { defineMessages } from 'react-intl';
+
+/**
+ * Internationalized messages for use on teams and orgs.
+ */
+export default defineMessages({
+  totalSwipes: {
+    id: 'management.partners.stats.mapswipe.totalSwipes',
+    defaultMessage: 'Total Swipes',
+  },
+  totalTimeSpent: {
+    id: 'management.partners.stats.mapswipe.totalTimeSpent',
+    defaultMessage: 'Total Time Spent',
+  },
+  totalContributors: {
+    id: 'management.partners.stats.mapswipe.totalContributors',
+    defaultMessage: 'Total Contributors',
+  },
+});
