d97ca4ceae8a139b0f64a0c3fa31e2b116db785d
royallsilwallz <royalsilwal@gmail.com>
2024-09-11 16:48:37 +0545

    Add tab switch feature in partners stats page
    - Add tabname param to partner stats route
    - Conditionally render tab content based selected tab
    - Separate Leaderboard section to a seperate component

---
+++ frontend/src/components/partners/leaderboard.js
@@ -0,0 +1,33 @@
+import { FormattedMessage } from 'react-intl';
+import messages from '../../views/messages';
+
+import { StatsSection } from './partnersStats';
+import { Activity } from './partnersActivity';
+import { CurrentProjects } from './currentProjects';
+
+export const Leaderboard = ({ partner, partnerStats }) => {
+  return (
+    <div className="pa4 bg-tan flex flex-column" style={{ gap: '1.25rem' }}>
+      <div className="flex justify-between items-center">
+        <h3 className="f2 blue-dark fw7 ma0 barlow-condensed v-mid dib">
+          {partner.primary_hashtag
+            ?.split(',')
+            ?.map((str) => `#${str}`)
+            ?.join(', ')}
+        </h3>
+      </div>
+
+      <StatsSection partner={partnerStats} />
+
+      <CurrentProjects currentProjects={partner.current_projects} />
+
+      {/* activity section */}
+      <div className="w-100 fl cf">
+        <h3 className="f2 fw6 ttu barlow-condensed blue-dark mt0 pt2 mb3">
+          <FormattedMessage {...messages.activity} />
+        </h3>
+        <Activity partner={partner} />
+      </div>
+    </div>
+  );
+};
--- frontend/src/views/partnersStats.js
+++ frontend/src/views/partnersStats.js
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import ReactPlaceholder from 'react-placeholder';
 import { FormattedMessage } from 'react-intl';
 
 import messages from './messages';
 import { NotFound } from './notFound';
 import { useFetch } from '../hooks/UseFetch';
-import { StatsSection } from '../components/partners/partnersStats';
-import { Activity } from '../components/partners/partnersActivity';
-import { CurrentProjects } from '../components/partners/currentProjects';
+import { Leaderboard } from '../components/partners/leaderboard';
 import { Resources } from '../components/partners/partnersResources';
 import { OHSOME_STATS_BASE_URL } from '../config';
 import { Button } from '../components/button';
 import { TwitterIcon, FacebookIcon, InstagramIcon } from '../components/svgIcons';
 
+const tabData = [{ id: 'leaderboard', title: 'Leaderboard' }];
+
 export const PartnersStats = () => {
-  const { id } = useParams();
+  const { id, tabname } = useParams();
+  const navigate = useNavigate();
   const [partnerStats, setPartnerStats] = useState(null);
   const [error, loading, partner] = useFetch(`partners/${id}/`);
+
+  // navigate to /leaderboard path when no tab param present
+  useEffect(() => {
+    if (!tabname) {
+      navigate('leaderboard');
+    }
+  }, [navigate, tabname]);
 
   const fetchData = async (name) => {
     try {
@@ -45,6 +53,15 @@ export const PartnersStats = () => {
     }
   }, [partner]);
 
+  function getTabContent() {
+    switch (tabname) {
+      case 'leaderboard':
+        return <Leaderboard partner={partner} partnerStats={partnerStats} />;
+      default:
+        return <></>;
+    }
+  }
+
   return (
     <ReactPlaceholder
       showLoadingAnimation={true}
@@ -56,79 +73,87 @@ export const PartnersStats = () => {
         <NotFound />
       ) : (
         <div className="">
-          <div className="flex items-center justify-between bg-blue-dark pa4">
+          <div className="flex flex-column bg-blue-dark ph4">
             {/* logo */}
             {partner.logo_url ? (
               <img src={partner.logo_url} alt="logo" height={70} />
             ) : (
-              <h3 className="f2 fw6 ttu barlow-condensed white">{partner.name}</h3>
-            )}
-            <div className="flex">
-              {/* new to mapping button */}
-              <Link to={`/learn/map/`}>
-                <Button className="bg-grey-dark white mr3 br1 f5 bn">
-                  <FormattedMessage {...messages.newToMapping} />
-                </Button>
-              </Link>
-              {/* resources button */}
-              <Resources partner={partner} />
-            </div>
-          </div>
-          {/* social logos */}
-          <div className="pa4 bg-tan flex flex-column" style={{ gap: '1.25rem' }}>
-            <div className="flex justify-between items-center">
-              <h3 className="f2 blue-dark fw7 ma0 barlow-condensed v-mid dib">
-                {partner.primary_hashtag
-                  ?.split(',')
-                  ?.map((str) => `#${str}`)
-                  ?.join(', ')}
+              <h3 className="f2 fw6 ttu barlow-condensed white" style={{ marginBottom: '1.75rem' }}>
+                {partner.name}
               </h3>
-              <div className="flex" style={{ gap: '0.5rem' }}>
-                {!!partner.link_x && (
-                  <a
-                    href={partner.link_x}
-                    className="link barlow-condensed white f4 ttu di-l dib"
-                    target="_blank"
-                    rel="noreferrer"
+            )}
+            <div className="flex justify-between">
+              <div className="flex" style={{ gap: '0.75rem' }}>
+                {tabData.map(({ id: tabId, title }) => (
+                  <div
+                    key={tabId}
+                    className={`flex items-center pointer ${
+                      tabname === tabId ? 'bg-tan blue-dark' : 'bg-grey-dark white'
+                    }`}
+                    style={{
+                      borderRadius: '3px 3px 0px 0px',
+                      padding: '0.625rem 1.375rem',
+                      fontWeight: '500',
+                    }}
+                    onClick={() => navigate(`/partners/${id}/stats/${tabId}`)}
                   >
-                    <TwitterIcon className="blue-dark partners-social-icon" />
-                  </a>
-                )}
-                {!!partner.link_meta && (
-                  <a
-                    href={partner.link_meta}
-                    className="link barlow-condensed white f4 ttu di-l dib"
-                    target="_blank"
-                    rel="noreferrer"
+                    <p className="ma0">{title}</p>
+                  </div>
+                ))}
+              </div>
+              <div className="flex" style={{ gap: '1.5rem' }}>
+                {/* new to mapping button */}
+                <Link to={`/learn/map/`}>
+                  <Button
+                    className="bg-transparent white br1 f5 bn"
+                    style={{ padding: '0.75rem 0' }}
                   >
-                    <FacebookIcon className="blue-dark partners-social-icon" />
-                  </a>
-                )}
-                {!!partner.link_instagram && (
-                  <a
-                    href={partner.link_instagram}
-                    className="link barlow-condensed white f4 ttu di-l dib"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <InstagramIcon className="blue-dark partners-social-icon" />
-                  </a>
-                )}
+                    <FormattedMessage {...messages.newToMapping} />
+                  </Button>
+                </Link>
+
+                {/* resources button */}
+                <Resources partner={partner} />
+
+                {/* social logos */}
+                <div className="flex items-center" style={{ gap: '0.625rem' }}>
+                  {!!partner.link_x && (
+                    <a
+                      href={partner.link_x}
+                      className="link barlow-condensed white f4 ttu di-l dib"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <TwitterIcon noBg className="partners-social-icon" />
+                    </a>
+                  )}
+                  {!!partner.link_meta && (
+                    <a
+                      href={partner.link_meta}
+                      className="link barlow-condensed white f4 ttu di-l dib"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FacebookIcon className="partners-social-icon" />
+                    </a>
+                  )}
+                  {!!partner.link_instagram && (
+                    <a
+                      href={partner.link_instagram}
+                      className="link barlow-condensed white f4 ttu di-l dib"
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <InstagramIcon className="partners-social-icon" />
+                    </a>
+                  )}
+                </div>
               </div>
             </div>
-
-            <StatsSection partner={partnerStats} />
-
-            <CurrentProjects currentProjects={partner.current_projects} />
-
-            {/* activity section */}
-            <div className="w-100 fl cf">
-              <h3 className="f2 fw6 ttu barlow-condensed blue-dark mt0 pt2 mb3">
-                <FormattedMessage {...messages.activity} />
-              </h3>
-              <Activity partner={partner} />
-            </div>
           </div>
+
+          {/* tab content */}
+          {getTabContent()}
         </div>
       )}
     </ReactPlaceholder>
