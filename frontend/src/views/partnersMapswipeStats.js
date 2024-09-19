ac1db7c6ce2f85e5e268d10b59b1ff678014dee9
A Vinayak Rugvedi <vinayakrugvedi@gmail.com>
2024-09-20 02:15:46 +0530

    feat: add API call to filtered statistics

--- frontend/src/views/partnersMapswipeStats.js
+++ frontend/src/views/partnersMapswipeStats.js
@@ -1,5 +1,7 @@
 import { FormattedMessage } from 'react-intl';
+import { useParams } from 'react-router-dom';
 import ReactPlaceholder from 'react-placeholder';
+import { useQuery } from '@tanstack/react-query';
 
 import { InfoIcon } from '../components/svgIcons';
 import { Overview } from '../components/partnerMapswipeStats/overview';
@@ -14,20 +16,21 @@ import { SwipesByOrganisation } from '../components/partnerMapswipeStats/swipesB
 import { StatsCardWithFooter } from '../components/statsCard';
 import messages from './messages';
 import { BanIcon } from '../components/svgIcons';
+import { fetchLocalJSONAPI } from '../network/genericJSONRequest';
 import './partnersMapswipeStats.css';
 
 const PagePlaceholder = () => (
-  <div className="pa4 bg-tan flex flex-column" style={{ gap: '1.25rem' }}>
-    <ReactPlaceholder type="rect" style={{ width: 300, height: 40 }} showLoadingAnimation />
-    <div
+  <div className="bg-tan flex flex-column" style={{ gap: '1.25rem' }}>
+    {/* <ReactPlaceholder type="rect" style={{ width: 300, height: 40 }} showLoadingAnimation /> */}
+    {/* <div
       className="flex justify-between items-center flex-wrap flex-nowrap-ns mt3"
       style={{ gap: '1.6rem' }}
     >
       <ReactPlaceholder type="rect" style={{ width: '100%', height: 170 }} showLoadingAnimation />
       <ReactPlaceholder type="rect" style={{ width: '100%', height: 170 }} showLoadingAnimation />
       <ReactPlaceholder type="rect" style={{ width: '100%', height: 170 }} showLoadingAnimation />
-    </div>
-    <div className="mt3">
+    </div> */}
+    <div className="mt4">
       <ReactPlaceholder type="rect" style={{ width: 300, height: 40 }} showLoadingAnimation />
       <ReactPlaceholder
         className="mt4"
@@ -60,69 +63,91 @@ const InfoBanner = () => {
 };
 
 export const PartnersMapswipeStats = () => {
-  const isPageReady = true;
-  const isError = false;
+  const { id: partnerPermalink } = useParams();
+  const { isLoading, isError, data, isRefetching } = useQuery({
+    queryKey: ['partners-mapswipe-filtered-statistics', partnerPermalink],
+    queryFn: async () => {
+      const today = new Date();
+      const currentYear = today.getFullYear();
+
+      const formatDate = (date) => {
+        const offset = date.getTimezoneOffset();
+        const adjustedDate = new Date(date.getTime() - offset * 60 * 1000);
+        return adjustedDate.toISOString().split('T')[0];
+      };
+
+      const fromDate = formatDate(new Date(currentYear, 0, 1));
+      const endDate = formatDate(today);
+
+      const response = await fetchLocalJSONAPI(
+        `partners/${partnerPermalink}/filtered-statistics/?fromDate=${fromDate}&toDate=${endDate}`,
+      );
+      return response;
+    },
+  });
 
   return (
-    <ReactPlaceholder customPlaceholder={<PagePlaceholder />} ready={isPageReady}>
-      {isPageReady && isError ? (
-        <div className="pa4 bg-tan">
-          <div className="flex items-center justify-start pa5 gap-1 pl1">
-            <BanIcon className="red" width="20" height="20" />
-            <p className="ma0">
-              <FormattedMessage {...messages.partnersMapswipeStatsError} />
-            </p>
-          </div>
-        </div>
-      ) : (
-        <div className="pa4 bg-tan flex flex-column" style={{ gap: '1.25rem' }}>
-          <InfoBanner />
-          <Overview />
+    <div className="pa4 bg-tan flex flex-column" style={{ gap: '1.25rem' }}>
+      <InfoBanner />
+      <Overview />
 
-          <div className="mt3">
-            <ContributionsGrid />
+      <ReactPlaceholder customPlaceholder={<PagePlaceholder />} ready={!isLoading && !isRefetching}>
+        {!isLoading && isError ? (
+          <div className="pa3 pl0 bg-tan">
+            <div className="flex items-center justify-start pa5 gap-1 pl1">
+              <BanIcon className="red" width="20" height="20" />
+              <p className="ma0">
+                <FormattedMessage {...messages.partnersMapswipeStatsError} />
+              </p>
+            </div>
           </div>
+        ) : (
+          <>
+            <div className="mt3">
+              <ContributionsGrid />
+            </div>
 
-          <div className="mt3">
-            <ContributionsHeatmap />
-          </div>
+            <div className="mt3">
+              <ContributionsHeatmap />
+            </div>
 
-          <div className="mt3">
-            <TimeSpentContributing />
-          </div>
+            <div className="mt3">
+              <TimeSpentContributing />
+            </div>
 
-          <div className="mt3">
-            <TimeSpentContributingByDay />
-          </div>
+            <div className="mt3">
+              <TimeSpentContributingByDay />
+            </div>
 
-          <div className="mt4">
-            <ProjectTypeAreaStats />
-          </div>
+            <div className="mt4">
+              <ProjectTypeAreaStats />
+            </div>
 
-          <div className="mt4 flex items-center justify-between">
-            <StatsCardWithFooter
-              description={<FormattedMessage {...messages.swipes} />}
-              value="338K"
-              style={{ width: '48.5%' }}
-            />
-            <StatsCardWithFooter
-              description={<FormattedMessage {...messages.timeSpentContributing} />}
-              value="11 days 5 hrs"
-              className="w-100"
-              style={{ width: '48.5%' }}
-            />
-          </div>
+            <div className="mt4 flex items-center justify-between">
+              <StatsCardWithFooter
+                description={<FormattedMessage {...messages.swipes} />}
+                value="338K"
+                style={{ width: '48.5%' }}
+              />
+              <StatsCardWithFooter
+                description={<FormattedMessage {...messages.timeSpentContributing} />}
+                value="11 days 5 hrs"
+                className="w-100"
+                style={{ width: '48.5%' }}
+              />
+            </div>
 
-          <div className="mt3 flex items-center justify-between">
-            <SwipesByProjectType />
-            <SwipesByOrganisation />
-          </div>
+            <div className="mt3 flex items-center justify-between">
+              <SwipesByProjectType />
+              <SwipesByOrganisation />
+            </div>
 
-          <div className="mt3">
-            <GroupMembers />
-          </div>
-        </div>
-      )}
-    </ReactPlaceholder>
+            <div className="mt3">
+              <GroupMembers />
+            </div>
+          </>
+        )}
+      </ReactPlaceholder>
+    </div>
   );
 };
