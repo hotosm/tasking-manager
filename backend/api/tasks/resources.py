0906fb0a810236f92a6a485f1d8b62daed0c31eb
prabinoid <38830224+prabinoid@users.noreply.github.com>
2024-12-11 15:20:29 +0545

    Aoi, tasks and project non geometries exports and cleanups

--- backend/api/tasks/resources.py
+++ backend/api/tasks/resources.py
@@ -121,18 +121,19 @@ async def get(request: Request, project_id: int, db: Database = Depends(get_db))
 
         tasks_json = await ProjectService.get_project_tasks(db, int(project_id), tasks)
         if as_file:
-            tasks_json = json.dumps(tasks_json, indent=4)  # Pretty-printed JSON
-            file_bytes = io.BytesIO(tasks_json.encode("utf-8"))
+            tasks_str = json.dumps(tasks_json, indent=4)  # Pretty-printed GeoJSON
+            file_bytes = io.BytesIO(tasks_str.encode("utf-8"))
             file_bytes.seek(0)  # Reset stream position
 
-            # Return the file response for download
+            # Return the GeoJSON file response for download
             return StreamingResponse(
                 file_bytes,
-                media_type="application/json",
+                media_type="application/geo+json",
                 headers={
-                    "Content-Disposition": f'attachment; filename="{project_id}-tasks.json"'
+                    "Content-Disposition": f'attachment; filename="{project_id}-tasks.geojson"'
                 },
             )
+
         return tasks_json
     except ProjectServiceError as e:
         return JSONResponse(content={"Error": str(e)}, status_code=403)
