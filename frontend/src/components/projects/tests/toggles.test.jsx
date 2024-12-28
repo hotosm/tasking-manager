5d29587a9cb3b261ef9d7c82a0511dca44fffab7
JoltCode <joe@joecc.dev>
2024-12-28 18:59:30 +0000

    fix,chore(frontend/tests): implement final missing test

--- frontend/src/components/projects/tests/toggles.test.jsx
+++ frontend/src/components/projects/tests/toggles.test.jsx
@@ -1,7 +1,8 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { store } from '../../../store';
 import { ReduxIntlProviders } from '../../../utils/testWithIntl';
 import { ShowMapToggle, ProjectListViewToggle } from '../projectNav';
+import userEvent from '@testing-library/user-event';
 
 describe('test if ShowMapToggle component', () => {
   const setup = () => render(
@@ -25,28 +26,29 @@ describe('test if ProjectListViewToggle', () => {
       <ProjectListViewToggle />
     </ReduxIntlProviders>,
   );
+  const user = userEvent.setup();
   it('has the correct CSS classes', () => {
     const { container } = setup();
     expect(container.getElementsByTagName("div").length).toBe(1);
     expect(container.querySelector('.dib.pointer.v-mid.ph1.blue-light')).toBeInTheDocument();
     expect(container.querySelector('.dib.pointer.v-mid.ph1.blue-grey')).toBeInTheDocument();
   });
-  test.todo('updates the redux state and css classes when clicked', () => {
-    // expect(store.getState().preferences['projectListView']).toBeFalsy();
-    // act(() => {
-    //   instance.findByType(ListIcon).props.onClick();
-    //   return undefined;
-    // });
-    // expect(store.getState().preferences['projectListView']).toBeTruthy();
-    // expect(instance.findByType(GripIcon).props.className).toBe('dib pointer v-mid ph1 blue-light');
-    // expect(instance.findByType(ListIcon).props.className).toBe('dib pointer v-mid ph1 blue-grey');
-    // // click on GripIcon
-    // act(() => {
-    //   instance.findByType(GripIcon).props.onClick();
-    //   return undefined;
-    // });
-    // expect(store.getState().preferences['projectListView']).toBeFalsy();
-    // expect(instance.findByType(GripIcon).props.className).toBe('dib pointer v-mid ph1 blue-grey');
-    // expect(instance.findByType(ListIcon).props.className).toBe('dib pointer v-mid ph1 blue-light');
+  it('updates the redux state and css classes when clicked', async () => {
+    const { container } = setup();
+    expect(store.getState().preferences['projectListView']).toBeFalsy();
+    await user.click(container.querySelector("svg.dib.pointer.v-mid.ph1.blue-light"))
+    expect(store.getState().preferences['projectListView']).toBeTruthy();
+
+    // Check both icons have the correct classes
+    expect(container.querySelector('.dib.pointer.v-mid.ph1.blue-grey')).toBeInTheDocument();
+    expect(container.querySelector('.dib.pointer.v-mid.ph1.blue-light')).toBeInTheDocument();
+
+    // click on GripIcon
+    await user.click(container.querySelector("svg.dib.pointer.v-mid.ph1.blue-light"))
+    expect(store.getState().preferences['projectListView']).toBeFalsy();
+
+    // Check both icons have the correct classes
+    expect(container.querySelector('.dib.pointer.v-mid.ph1.blue-light')).toBeInTheDocument();
+    expect(container.querySelector('.dib.pointer.v-mid.ph1.blue-grey')).toBeInTheDocument();
   });
 });
