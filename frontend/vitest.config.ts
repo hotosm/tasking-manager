9c67ff8cfabc8e094027abf53a6f94d95f3c9e1f
royallsilwallz <royalsilwal@gmail.com>
2025-01-26 01:50:31 +0545

    fix: resolve test cases failure due to import path aliases

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -11,5 +11,19 @@ export default defineConfig({
     environment: "happy-dom",
     testTimeout: 20000
   },
-  envPrefix: 'REACT_APP_',
+  resolve: {
+    alias: {
+      '@': new URL('./src/', import.meta.url).pathname,
+      '@Api': new URL('./src/api/', import.meta.url).pathname,
+      '@Assets': new URL('./src/assets/', import.meta.url).pathname,
+      '@Components': new URL('./src/components/', import.meta.url).pathname,
+      '@Config': new URL('./src/config/', import.meta.url).pathname,
+      '@Hooks': new URL('./src/hooks/', import.meta.url).pathname,
+      '@Network': new URL('./src/network/', import.meta.url).pathname,
+      '@Store': new URL('./src/store/', import.meta.url).pathname,
+      '@Utils': new URL('./src/utils/', import.meta.url).pathname,
+      '@Views': new URL('./src/views/', import.meta.url).pathname
+    },
+  },
+  envPrefix: 'REACT_APP_'
 })
