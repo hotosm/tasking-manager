aa4988ecab59f2e4127a6d321115e0634567a694
royallsilwallz <royalsilwal@gmail.com>
2025-01-26 00:37:10 +0545

    feat: configure import path aliases for folders

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,5 +7,20 @@ export default defineConfig({
   envPrefix: 'REACT_APP_',
   server: {
     port: 3000
-  }
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    alias: {
+      '@': new URL('./src/', import.meta.url).pathname,
+      '@Api': new URL('./src/api/', import.meta.url).pathname,
+      '@Assets': new URL('./src/assets/', import.meta.url).pathname,
+      '@Components': new URL('./src/components/', import.meta.url).pathname,
+      '@Config': new URL('./src/config/', import.meta.url).pathname,
+      '@Hooks': new URL('./src/hooks/', import.meta.url).pathname,
+      '@Network': new URL('./src/network/', import.meta.url).pathname,
+      '@Store': new URL('./src/store/', import.meta.url).pathname,
+      '@Utils': new URL('./src/utils/', import.meta.url).pathname,
+      '@Views': new URL('./src/views/', import.meta.url).pathname
+    },
+  },
 });
