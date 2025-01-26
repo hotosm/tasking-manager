fd68e19e0f4727097fc66a8c0da9e30712fc0522
royallsilwallz <royalsilwal@gmail.com>
2025-01-26 18:59:38 +0545

    chore(frontend): upgrade and setup eslint & prettier configs
    - Upgrade eslint to v9
    - Upgrade prettier to v3
    - Setup config files with latest standards

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   envPrefix: 'REACT_APP_',
   server: {
-    port: 3000
+    port: 3000,
   },
 });
