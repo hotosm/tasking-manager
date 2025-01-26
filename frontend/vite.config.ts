b452cdfcd534235b2fd5b598e6754da100c02af1
royallsilwallz <royalsilwal@gmail.com>
2025-01-26 10:50:48 +0545

    chore: use `vite-tsconfig-paths` for reducing code duplication

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   envPrefix: 'REACT_APP_',
   server: {
     port: 3000
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    alias: {
-      '@': new URL('./src/', import.meta.url).pathname,
-      '@Api': new URL('./src/api/', import.meta.url).pathname,
-      '@Assets': new URL('./src/assets/', import.meta.url).pathname,
-      '@Components': new URL('./src/components/', import.meta.url).pathname,
-      '@Config': new URL('./src/config/', import.meta.url).pathname,
-      '@Hooks': new URL('./src/hooks/', import.meta.url).pathname,
-      '@Network': new URL('./src/network/', import.meta.url).pathname,
-      '@Store': new URL('./src/store/', import.meta.url).pathname,
-      '@Utils': new URL('./src/utils/', import.meta.url).pathname,
-      '@Views': new URL('./src/views/', import.meta.url).pathname
-    },
   },
 });
--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,29 +1,16 @@
 // vitest.config.ts
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vitest/config'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
     clearMocks: true,
     setupFiles: ['./src/setup.ts', './src/network/tests/server.ts'],
     environment: "happy-dom",
     testTimeout: 20000
-  },
-  resolve: {
-    alias: {
-      '@': new URL('./src/', import.meta.url).pathname,
-      '@Api': new URL('./src/api/', import.meta.url).pathname,
-      '@Assets': new URL('./src/assets/', import.meta.url).pathname,
-      '@Components': new URL('./src/components/', import.meta.url).pathname,
-      '@Config': new URL('./src/config/', import.meta.url).pathname,
-      '@Hooks': new URL('./src/hooks/', import.meta.url).pathname,
-      '@Network': new URL('./src/network/', import.meta.url).pathname,
-      '@Store': new URL('./src/store/', import.meta.url).pathname,
-      '@Utils': new URL('./src/utils/', import.meta.url).pathname,
-      '@Views': new URL('./src/views/', import.meta.url).pathname
-    },
   },
   envPrefix: 'REACT_APP_'
 })
