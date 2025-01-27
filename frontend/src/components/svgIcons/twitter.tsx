2d53a5c5400fe413be220697a095d0d6b08b5b70
royallsilwallz <royalsilwal@gmail.com>
2025-01-27 12:22:16 +0545

    fix(frontend): fix props destructure issue

--- frontend/src/components/svgIcons/twitter.tsx
+++ frontend/src/components/svgIcons/twitter.tsx
@@ -5,7 +5,7 @@ export const TwitterIcon = (
     noBg?: boolean;
   },
 ) => {
-  const { ...rest } = props;
+  const { noBg, ...rest } = props;
 
   return (
     <svg
@@ -17,7 +17,7 @@ export const TwitterIcon = (
       {...rest}
       aria-label="Twitter"
     >
-      {!props.noBg && <rect width="15" height="15" rx="1" fill="black" />}
+      {!noBg && <rect width="15" height="15" rx="1" fill="black" />}
       <path
         d="M8.54822 6.66248L12.644 2H11.6735L8.1156 6.04753L5.27595 2H2L6.29505 8.12111L2 13.01H2.97043L6.72541 8.7347L9.72488 13.01H13.0008L8.54822 6.66248ZM7.21864 8.17485L6.7828 7.56494L3.32038 2.71647H4.81116L7.60626 6.63082L8.04027 7.24073L11.6731 12.3285H10.1823L7.21864 8.17485Z"
         fill="white"
