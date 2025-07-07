9415f82fc8801e6bea0410dc15ac5d70d6ab2924
Emilio Mariscal <emi.mariscal.dev@gmail.com>
2025-07-07 20:43:55 -0300

    Replace localStorage by safeStorage wrapper utility

--- frontend/src/utils/login.js
+++ frontend/src/utils/login.js
@@ -1,12 +1,13 @@
 import { fetchLocalJSONAPI } from '../network/genericJSONRequest';
+import { setItem } from './safe_storage';
 import { OSM_REDIRECT_URI } from '../config';
 
 export const osmLoginRedirect = (redirectTo) => {
   let url = `system/authentication/login/?redirect_uri=${OSM_REDIRECT_URI}`;
   fetchLocalJSONAPI(url).then((resp) => {
     let location = resp.auth_url;
-    localStorage.setItem('osm_oauth_state', resp.state);
-    localStorage.setItem('osm_oauth_redirect_to', redirectTo);
+    setItem('osm_oauth_state', resp.state);
+    setItem('osm_oauth_redirect_to', redirectTo);
     window.location.href = location;
   });
 };
