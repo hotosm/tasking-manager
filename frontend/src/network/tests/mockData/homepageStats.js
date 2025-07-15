c8c357ce0244d71a1cc678b541881a2e066bf12a
Sujit Karki <suzeett10@gmail.com>
2025-07-15 16:40:33 +0545

    Update mock API to match new endpoint structure

--- frontend/src/network/tests/mockData/homepageStats.js
+++ frontend/src/network/tests/mockData/homepageStats.js
@@ -8,3 +8,22 @@ export const homepageStats = {
     latest: '2023-10-03T15:28:07Z',
   },
 };
+
+export const updatedOhsomeStats = {
+  result: {
+    topics: {
+      edit: {
+        value: 134184186,
+        topic: 'edit',
+      },
+      building: {
+        topic: 'building',
+        value: 101367027,
+      },
+      road: {
+        topic: 'road',
+        value: 2380562.9900000077,
+      },
+    },
+  },
+};
