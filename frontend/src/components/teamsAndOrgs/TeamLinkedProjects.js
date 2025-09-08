46616ea8f6db77ca8bb16f8432ea7e271367bc59
Sujit Karki <suzeett10@gmail.com>
2025-09-08 14:49:00 +0545

    Display persistent success/error message insted of toast on project unlink response
    Show inline error message on sinle project unlink failure
    
    Show error message on top on bulk unlink failure
    
    Show persistent success message on unlink successfull

--- frontend/src/components/teamsAndOrgs/TeamLinkedProjects.js
+++ frontend/src/components/teamsAndOrgs/TeamLinkedProjects.js
@@ -10,8 +10,8 @@ import { CustomMenu } from '../CustomMenu';
 import TeamLinkedProjectCard, { nTeamProjectsCardPlaceholders } from './TeamLinkedProjectCard';
 import { useSelector } from 'react-redux';
 import { fetchLocalJSONAPI, pushToLocalJSONAPI } from '../../network/genericJSONRequest';
-import toast from 'react-hot-toast';
 import { Button, CustomButton } from '../button';
+import { Alert } from '../alert';
 
 const items = [
   { id: 'PUBLISHED', label: <FormattedMessage {...statusMessages.status_PUBLISHED} /> },
@@ -24,6 +24,11 @@ export function TeamLinkedProjects({ viewAllEndpoint, border = true, canUserEdit
   const token = useSelector((state) => state.auth.token);
   const [selectedProjectStatus, setSelectedProjectStatus] = useState('PUBLISHED');
   const [selectedProjects, setSelectedProjects] = useState([]);
+  const [response, setResponse] = useState({
+    type: 'success',
+    projectId: 0,
+    message: '',
+  });
 
   // eslint-disable-next-line no-unused-vars
   const [, isloading, projects, refetch] = useFetchWithAbort(
@@ -35,14 +40,16 @@ export function TeamLinkedProjects({ viewAllEndpoint, border = true, canUserEdit
   const unlinkAllProjectsFromTeam = () => {
     fetchLocalJSONAPI(`teams/projects/teams/${id}/unlink`, token, 'DELETE')
       .then(() => {
-        toast.success('Unlinked Successfully');
+        setResponse({ type: 'success', message: 'Projects Unlinked Successfully' });
         refetch();
       })
       .catch((e) => {
         if (e.message === 'ProjectPermissionError') {
-          toast.error(
-            'Project on list has mapping/validation permissions restricted to teams, but no other team is assigned. Please contact the project author before unlinking.',
-          );
+          setResponse({
+            type: 'error',
+            message:
+              'Project has mapping/validation permissions restricted to teams, but no other team is assigned. Please contact the project author before unlinking.',
+          });
         }
         console.log(e.message);
       })
@@ -51,20 +58,23 @@ export function TeamLinkedProjects({ viewAllEndpoint, border = true, canUserEdit
       });
   };
 
-  const unlinkProjectsByIds = (projectIds) => {
+  const unlinkProjectsByIds = (projectIds = []) => {
     const payload = {
       items: projectIds.map((projectId) => ({ project_id: projectId, team_id: Number(id) })),
     };
     pushToLocalJSONAPI(`teams/projects/unlink`, JSON.stringify(payload), token, 'DELETE')
       .then(() => {
-        toast.success('Unlinked Successfully');
+        setResponse({ type: 'success', message: 'Project Unlinked Successfully' });
         refetch();
       })
       .catch((e) => {
         if (e.message === 'ProjectPermissionError') {
-          toast.error(
-            'Project has mapping/validation permissions restricted to teams, but no other team is assigned. Please contact the project author before unlinking.',
-          );
+          setResponse({
+            type: 'error',
+            message:
+              'Project has mapping/validation permissions restricted to teams, but no other team is assigned. Please contact the project author before unlinking.',
+            projectId: projectIds.length === 1 ? projectIds[0] : 0,
+          });
         }
         console.log(e.message);
       })
@@ -80,69 +90,80 @@ export function TeamLinkedProjects({ viewAllEndpoint, border = true, canUserEdit
   };
 
   return (
-    <div className={`bg-white mb3 ${border ? 'b--grey-light ba pa4' : ''}`}>
-      <div className="flex justify-between">
-        <h3 className="f3 barlow-condensed ttu blue-dark mv0 fw6 dib v-mid">
-          <FormattedMessage {...messages.projects} />
-        </h3>
-        <div className="flex items-center">
-          <ViewAllLink link={viewAllEndpoint} />
-          <CustomButton
-            className="dib bn fr red link hover-dark-red bg-transparent mt2 ml4"
-            onClick={() => {
-              unlinkAllProjectsFromTeam();
-            }}
-          >
-            <FormattedMessage {...messages.unlinkAll} />
-          </CustomButton>
+    <>
+      {!response.projectId && response.message && (
+        <Alert type={response.type}>{response.message}</Alert>
+      )}
+      <div className={`bg-white mb3 ${border ? 'b--grey-light ba pa4' : ''}`}>
+        <div className="flex justify-between">
+          <h3 className="f3 barlow-condensed ttu blue-dark mv0 fw6 dib v-mid">
+            <FormattedMessage {...messages.projects} />
+          </h3>
+          <div className="flex items-center">
+            <ViewAllLink link={viewAllEndpoint} />
+            <CustomButton
+              className="dib bn fr red link hover-dark-red bg-transparent mt2 ml4"
+              onClick={() => {
+                unlinkAllProjectsFromTeam();
+              }}
+            >
+              <FormattedMessage {...messages.unlinkAll} />
+            </CustomButton>
+          </div>
         </div>
-      </div>
-      <div className="pv2">
-        {canUserEditTeam && (
-          <div style={{ width: '280px' }}>
-            <CustomMenu
-              items={items}
-              activeMenuItem={selectedProjectStatus}
-              onItemClick={(itemId) => setSelectedProjectStatus(itemId)}
-            />
+        <div className="pv2">
+          {canUserEditTeam && (
+            <div style={{ width: '280px' }}>
+              <CustomMenu
+                items={items}
+                activeMenuItem={selectedProjectStatus}
+                onItemClick={(itemId) => setSelectedProjectStatus(itemId)}
+              />
+            </div>
+          )}
+          <div className="pt3 flex flex-column">
+            <ReactPlaceholder
+              customPlaceholder={nTeamProjectsCardPlaceholders(4)}
+              showLoadingAnimation={true}
+              delay={10}
+              ready={projects?.results && !isloading}
+            >
+              {projects?.results?.map((card) => (
+                <TeamLinkedProjectCard
+                  key={card.projectId}
+                  projectId={card.projectId}
+                  projectName={card.name}
+                  unLinkFunc={canUserEditTeam ? unlinkProjectsByIds : () => {}}
+                  canUserEditTeam={canUserEditTeam}
+                  isProjectSelected={selectedProjects.includes(card.projectId)}
+                  handleProjectSelection={handleProjectSelection}
+                  errorMessage={
+                    response?.message && response.projectId === card.projectId
+                      ? response.message
+                      : ''
+                  }
+                />
+              ))}
+            </ReactPlaceholder>
+          </div>
+        </div>
+        {selectedProjects?.length > 0 && (
+          <div className="flex justify-between items-center">
+            <div className="gray text">
+              {selectedProjects?.length} {`project${selectedProjects.length > 1 ? 's' : ''}`}{' '}
+              selected
+            </div>
+            <Button
+              className="bg-red white hover-bg-dark-red"
+              onClick={() => {
+                unlinkProjectsByIds(selectedProjects);
+              }}
+            >
+              <FormattedMessage {...messages.unlinkSelected} />
+            </Button>
           </div>
         )}
-        <div className="pt3 flex flex-column">
-          <ReactPlaceholder
-            customPlaceholder={nTeamProjectsCardPlaceholders(4)}
-            showLoadingAnimation={true}
-            delay={10}
-            ready={projects?.results && !isloading}
-          >
-            {projects?.results?.map((card) => (
-              <TeamLinkedProjectCard
-                key={card.projectId}
-                projectId={card.projectId}
-                projectName={card.name}
-                unLinkFunc={canUserEditTeam ? unlinkProjectsByIds : () => {}}
-                canUserEditTeam={canUserEditTeam}
-                isProjectSelected={selectedProjects.includes(card.projectId)}
-                handleProjectSelection={handleProjectSelection}
-              />
-            ))}
-          </ReactPlaceholder>
-        </div>
       </div>
-      {selectedProjects?.length > 0 && (
-        <div className="flex justify-between items-center">
-          <div className="gray text">
-            {selectedProjects?.length} {`project${selectedProjects.length > 1 ? 's' : ''}`} selected
-          </div>
-          <Button
-            className="bg-red white hover-bg-dark-red"
-            onClick={() => {
-              unlinkProjectsByIds(selectedProjects);
-            }}
-          >
-            <FormattedMessage {...messages.unlinkSelected} />
-          </Button>
-        </div>
-      )}
-    </div>
+    </>
   );
 }
