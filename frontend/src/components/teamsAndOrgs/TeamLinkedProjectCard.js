d9e4a94917db53995502242e3be69a7b2d9e52b3
Sujit Karki <suzeett10@gmail.com>
2025-09-08 14:38:22 +0545

    Show inline error message for individual project unlink failure

--- frontend/src/components/teamsAndOrgs/TeamLinkedProjectCard.js
+++ frontend/src/components/teamsAndOrgs/TeamLinkedProjectCard.js
@@ -4,35 +4,55 @@ import { TextBlock } from 'react-placeholder/lib/placeholders';
 import { CustomButton } from '../button';
 import messages from './messages';
 import { CheckBoxInput } from '../formInputs';
+import { Alert } from '../alert';
 
-const TeamLinkedProjectCard = ({ projectId, projectName, canUserEditTeam, unLinkFunc, isProjectSelected, handleProjectSelection }: object) => {
+const TeamLinkedProjectCard = ({
+  projectId,
+  projectName,
+  canUserEditTeam,
+  unLinkFunc,
+  isProjectSelected,
+  handleProjectSelection,
+  errorMessage,
+}: object) => {
   return (
-    <div className="flex items-center bb b--black-10">
-      {canUserEditTeam && <CheckBoxInput isActive={isProjectSelected} changeState={() => {handleProjectSelection(projectId)}} className="mr3" />}
-      <div className="w-70 flex items-center">
-        <h3 className="truncate">{projectName}</h3>
-      </div>
-      <div className="w-30 flex items-center justify-end">
-        <Link
-          to={`/projects/${projectId}`}
-          className="link ttc ph3 pv2 bg-mid-gray br2 white mr3 hover-bg-gray"
-        >
-          <FormattedMessage {...messages.view} />
-        </Link>
-
-        {/* User with a team edit access can unlink the project */}
+    <>
+      <div className="flex items-center bb b--black-10">
         {canUserEditTeam && (
-          <CustomButton
-            className="bg-transparent br2 pr2 ph3 pv2 pointer ba hover-bg-black-10"
-            onClick={() => {
-              unLinkFunc([projectId]);
+          <CheckBoxInput
+            isActive={isProjectSelected}
+            changeState={() => {
+              handleProjectSelection(projectId);
             }}
-          >
-            <FormattedMessage {...messages.unlink} />
-          </CustomButton>
+            className="mr3"
+          />
         )}
+        <div className="w-70 flex items-center">
+          <h3 className="truncate">{projectName}</h3>
+        </div>
+        <div className="w-30 flex items-center justify-end">
+          <Link
+            to={`/projects/${projectId}`}
+            className="link ttc ph3 pv2 bg-mid-gray br2 white mr3 hover-bg-gray"
+          >
+            <FormattedMessage {...messages.view} />
+          </Link>
+
+          {/* User with a team edit access can unlink the project */}
+          {canUserEditTeam && (
+            <CustomButton
+              className="bg-transparent br2 pr2 ph3 pv2 pointer ba hover-bg-black-10"
+              onClick={() => {
+                unLinkFunc([projectId]);
+              }}
+            >
+              <FormattedMessage {...messages.unlink} />
+            </CustomButton>
+          )}
+        </div>
       </div>
-    </div>
+      {errorMessage && <Alert type="error">{errorMessage}</Alert>}
+    </>
   );
 };
 
