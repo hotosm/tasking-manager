d4bc6df0767084de3d0c903ed8574718548e99f0
Sujit Karki <suzeett10@gmail.com>
2026-03-09 20:08:42 +0545

    Update the test case as per changes on star indicator

--- frontend/src/components/taskSelection/tests/mappingLevelIcon.test.js
+++ frontend/src/components/taskSelection/tests/mappingLevelIcon.test.js
@@ -1,34 +1,38 @@
-import { FormattedMessage } from 'react-intl';
-
 import { createComponentWithIntl } from '../../../utils/testWithIntl';
 import { MappingLevelIcon } from '../contributions';
 import { FullStarIcon, HalfStarIcon } from '../../svgIcons';
 
+const mappingLevelList = [
+  { name: 'BEGINNER', ordering: 1 },
+  { name: 'INTERMEDIATE', ordering: 2 },
+  { name: 'ADVANCED', ordering: 3 },
+];
+
 describe('if mappingLevel is "Intermediate mapper"', () => {
-  const element = createComponentWithIntl(<MappingLevelIcon mappingLevel="INTERMEDIATE" />);
+  const element = createComponentWithIntl(
+    <MappingLevelIcon mappingLevel="INTERMEDIATE" mappingLevelList={mappingLevelList} />,
+  );
   const instance = element.root;
   it('HalfStarIcon with correct classNames', () => {
     expect(instance.findByType(HalfStarIcon).props.className).toBe('h1 w1 v-mid pb1');
   });
-  it('FormattedMessage with the correct id', () => {
-    expect(instance.findByType(FormattedMessage).props.id).toBe('project.level.intermediate');
-  });
 });
 
 describe('if mappingLevel is "Advanced mapper"', () => {
-  const element = createComponentWithIntl(<MappingLevelIcon mappingLevel="ADVANCED" />);
+  const element = createComponentWithIntl(
+    <MappingLevelIcon mappingLevel="ADVANCED" mappingLevelList={mappingLevelList} />,
+  );
   const instance = element.root;
   it('should render a FullStarIcon', () => {
     expect(instance.findByType(FullStarIcon).props.className).toBe('h1 w1 v-mid pb1');
-  });
-  it('should render a FormattedMessage with the correct id', () => {
-    expect(instance.findByType(FormattedMessage).props.id).toBe('project.level.advanced');
   });
 });
 
 describe('if mappingLevel is anything else', () => {
   it('should not render anything', () => {
-    const element = createComponentWithIntl(<MappingLevelIcon mappingLevel="BEGINNER" />);
+    const element = createComponentWithIntl(
+      <MappingLevelIcon mappingLevel="MAPPER" mappingLevelList={mappingLevelList} />,
+    );
     expect(element.toJSON()).toBeNull();
   });
 });
