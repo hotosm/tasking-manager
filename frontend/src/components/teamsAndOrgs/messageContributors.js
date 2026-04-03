64d8f8c6ad3439dad98c4062b721ca633fe4d248
Sumit Dahal <sumitdahal123@gmail.com>
2026-04-03 17:36:11 +0545

    feat: add MessageContributors component to campaign view for messaging contributors

---
+++ frontend/src/components/teamsAndOrgs/messageContributors.js
@@ -0,0 +1,106 @@
+import { lazy, Suspense, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { FormattedMessage } from 'react-intl';
+import toast from 'react-hot-toast';
+
+import messages from './messages';
+import { Button } from '../button';
+import { MessageStatus } from '../comments/status';
+import { pushToLocalJSONAPI } from '../../network/genericJSONRequest';
+import ReactPlaceholder from 'react-placeholder';
+
+const CommentInputField = lazy(() =>
+  import('../comments/commentInput' /* webpackChunkName: "commentInput" */),
+);
+
+export function MessageContributors({ campaignId }) {
+  const token = useSelector((state) => state.auth.token);
+  const [message, setMessage] = useState('');
+  const [subject, setSubject] = useState('');
+  const [status, setStatus] = useState(null);
+
+  const sendMessage = () => {
+    if (message && subject && token) {
+      setStatus('sending');
+      pushToLocalJSONAPI(
+        `campaigns/${campaignId}/actions/message-contributors/`,
+        JSON.stringify({ message: message, subject: subject }),
+        token,
+        'POST',
+      )
+        .then(() => {
+          toast.success(<FormattedMessage {...messages.sendMessageSuccess} />);
+          setStatus('messageSent');
+          setMessage('');
+          setSubject('');
+        })
+        .catch(() => {
+          toast.error(<FormattedMessage {...messages.sendMessageFailure} />);
+          setStatus('error');
+        });
+    }
+  };
+
+  return (
+    <>
+      <div className={`bg-white b--grey-light pa4 ${message ? 'bt bl br' : 'ba'}`}>
+        <div className="cf db">
+          <h3 className="f3 blue-dark mt0 fw6 fl">
+            <FormattedMessage {...messages.messageContributors} />
+          </h3>
+        </div>
+        {(message || subject) && (
+          <div className="cf mb1">
+            <FormattedMessage {...messages.subjectPlaceholder}>
+              {(msg) => {
+                return (
+                  <input
+                    value={subject}
+                    onChange={(e) => setSubject(e.target.value)}
+                    name="subject"
+                    className="db center pa2 w-100 fl mb3"
+                    type="text"
+                    placeholder={msg}
+                  />
+                );
+              }}
+            </FormattedMessage>
+          </div>
+        )}
+        <div className="cf mb1">
+          <Suspense
+            fallback={<ReactPlaceholder showLoadingAnimation={true} rows={10} delay={300} />}
+          >
+            <CommentInputField comment={message} setComment={setMessage} isShowTabNavs />
+          </Suspense>
+        </div>
+        {!message && <MessageStatus status={status} />}
+      </div>
+      {(message || subject) && (
+        <div className="cf pt0">
+          <div className="w-70-l w-50 fl tr dib bg-grey-light">
+            <Button
+              className="blue-dark bg-grey-light h3"
+              onClick={() => {
+                setMessage('');
+                setSubject('');
+                setStatus(null);
+              }}
+            >
+              <FormattedMessage {...messages.cancel} />
+            </Button>
+          </div>
+          <div className="w-30-l w-50 fr dib">
+            <Button
+              className="white bg-red h3 w-100"
+              onClick={() => sendMessage()}
+              disabled={!message || !subject}
+            >
+              <FormattedMessage {...messages.send} />
+            </Button>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
