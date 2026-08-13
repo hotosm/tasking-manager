243cb3f46cdd72a6f6f4d25c1b02d49f06908640
prabinoid <38830224+prabinoid@users.noreply.github.com>
2026-08-13 14:49:21 +0545

    fix: re-parent index migration onto develop's head to resolve dual alembic heads

--- migrations/versions/c3f8a1b2e9d4_.py
+++ migrations/versions/c3f8a1b2e9d4_.py
@@ -1,7 +1,7 @@
 """Add composite index on projects(status, private) and index on projects(priority)
 
 Revision ID: c3f8a1b2e9d4
-Revises: a1b2c3d4e5f6
+Revises: 4ee8b1efdebd
 Create Date: 2026-06-26 00:00:00.000000
 
 """
@@ -10,7 +10,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "c3f8a1b2e9d4"
-down_revision = "a1b2c3d4e5f6"
+down_revision = "4ee8b1efdebd"
 branch_labels = None
 depends_on = None
 
